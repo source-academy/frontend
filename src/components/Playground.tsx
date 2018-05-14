c9de874e998b12f5a0a00aab38bfa826a8b3d170
ning <ningyuan.sg@gmail.com>
2018-05-14 16:55:53 +0800

    Add react-ace for IDE

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 
+import AceEditor from 'react-ace'
+
 const Playground: React.SFC<{}> = () => (
   <div className="Playground">
     <h2>Playground</h2>
+    <AceEditor />
   </div>
 )
 
