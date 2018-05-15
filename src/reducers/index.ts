b17cb2511b4813fddbf8722a78c10b33bef4a9f1
remo5000 <vigneshshankar@gmail.com>
2018-05-15 17:43:44 +0800

    Add IPlaygroundState to IState in index reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,12 @@
 import { IApplicationState, reducer as application } from './application'
+import { IPlaygroundState, reducer as playground } from './playground'
 
 export interface IState {
   readonly application: IApplicationState
+  readonly playground: IPlaygroundState
 }
 
 export default {
-  application
+  application,
+  playground
 }
