8145a2a7f113b577671b31046c6e4efb5fc98496
remo5000 <vigneshshankar@gmail.com>
2018-05-15 12:51:41 +0800

    Remove unused onChange()

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
 import AceEditor from 'react-ace'
-
+import 'brace/mode/java';
+import 'brace/theme/github';
 
 export interface IPlaygroundProps {
   initialCode: string;
 };
-
-function onChange(newValue : string) : void {
-  // console.log('change', newValue);
-}
 
 export default class Playground extends React.Component<IPlaygroundProps, {}> {
   public render() {
@@ -16,8 +13,10 @@ export default class Playground extends React.Component<IPlaygroundProps, {}> {
       <div className="Playground">
        <h2>Playground</h2>
         <AceEditor 
+          mode="java"
+          theme="github"
           value={this.props.initialCode}
-          onChange={onChange} />
+        />
       </div>
     );
   }
