52210e53007ebe960472bdf055fa24714248c37c
remo5000 <vigneshshankar@gmail.com>
2018-05-17 15:17:05 +0800

    Add button to call on handleEvalEditor
    This change allows the newly added button to call upon the
    handleEvalEditor callback, whose reducer is not defined (yet)

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,29 +1,59 @@
 import * as React from 'react'
 import AceEditor from 'react-ace'
-
 import 'brace/mode/javascript'
 import 'brace/theme/github'
 
+import {
+  Button, IconName, Intent
+} from '@blueprintjs/core'
+
 /**
  * @property editorValue - The string content of the react-ace editor
- * @property updateCode  - A callback function for the react-ace editor's `onChange`
+ * @property handleEditorChange  - A callback function 
+ *           for the react-ace editor's `onChange`
+ * @property handleEvalEditor  - A callback function for evaluation 
+ *           of the editor's content, using `slang`
  */
 export interface IEditorProps {
   editorValue: string
   handleEditorChange: (newCode: string) => void
+  handleEvalEditor: () => void
 }
+
 
 class Editor extends React.Component<IEditorProps, {}> {
   public render() {
+
+    const genericButton = (
+      label: string,
+      icon: IconName,
+      handleClick = () => { },
+      intent = Intent.NONE,
+      notMinimal = false
+    ) =>
+      (<Button
+        onClick={handleClick}
+        className={notMinimal ? "" : "pt-minimal"}
+        intent={intent}
+        icon={icon}
+      >
+        {label}
+      </Button>
+      )
+    const runButton = genericButton('Run', 'play', this.props.handleEvalEditor)
+
     return (
-      <AceEditor
-        height="90%"
-        width="90%"
-        mode="javascript"
-        theme="github"
-        value={this.props.editorValue}
-        onChange={this.props.handleEditorChange}
-      />
+      <div>
+        <AceEditor
+          height="90%"
+          width="90%"
+          mode="javascript"
+          theme="github"
+          value={this.props.editorValue}
+          onChange={this.props.handleEditorChange}
+        />
+        {runButton}
+      </div>
     )
   }
 }
