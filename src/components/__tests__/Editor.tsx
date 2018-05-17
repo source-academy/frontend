e4f7743fe38a0791155786e2329685747de97e46
ning <ningyuan.sg@gmail.com>
2018-05-17 15:06:38 +0800

    Fix Editor test (tslint errors)
    1. Missing comma in object props
    2. Syntax error and missing object comprehension in creating Editor

--- src/components/__tests__/Editor.tsx
+++ src/components/__tests__/Editor.tsx
@@ -6,10 +6,10 @@ import Editor, { IEditorProps } from '../Editor'
 
 test('Editor renders correctly', () => {
   const props: IEditorProps = {
-    editorValue: ''
-    handleEditorChange: (newCode) => {}
+    editorValue: '',
+    handleEditorChange: newCode => {}
   }
-  const app = <Editor { props }/>
+  const app = <Editor {...props} />
   const tree = shallow(app)
   expect(tree.debug()).toMatchSnapshot()
 })
