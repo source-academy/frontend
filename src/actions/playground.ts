e1b6fe4cb11a87b64cb8d2d98222625d6d662041
ning <ningyuan.sg@gmail.com>
2018-05-18 17:50:35 +0800

    Add action UPDATE_REPL_VALUE

--- src/actions/playground.ts
+++ src/actions/playground.ts
@@ -1,23 +1,14 @@
-import { Action, ActionCreator } from 'redux'
+import { ActionCreator } from 'redux'
 import * as actionTypes from './actionTypes'
 
-/**
- * Represents an `Action` which updates the `editorValue` of a
- * `IPlaygroundState`
- * @property type           - Unique string identifier for this `Action`
- * @property newEditorValue - The new string value for `editorValue`
- */
-export interface IUpdateEditorValue extends Action {
-  payload: string
-}
-
-/**
- * An `ActionCreator` returning an `IUpdateEditorValue` `Action`
- * @param newEditorValue - The new string value for `editorValue`
- */
-export const updateEditorValue: ActionCreator<IUpdateEditorValue> = (newEditorValue: string) => ({
+export const updateEditorValue: ActionCreator<actionTypes.IAction> = (newEditorValue: string) => ({
   type: actionTypes.UPDATE_EDITOR_VALUE,
   payload: newEditorValue
+})
+
+export const updateReplValue: ActionCreator<actionTypes.IAction> = (newReplValue: string) => ({
+  type: actionTypes.UPDATE_REPL_VALUE,
+  payload: newReplValue
 })
 
 export const evalEditor = () => ({
--- src/reducers/playground.ts
+++ src/reducers/playground.ts
@@ -1,20 +1,23 @@
 import { Reducer } from 'redux'
 import {
   EVAL_INTERPRETER_SUCCESS,
-  ILoadedAction,
-  UPDATE_EDITOR_VALUE
+  IAction,
+  UPDATE_EDITOR_VALUE,
+  UPDATE_REPL_VALUE
 } from '../actions/actionTypes'
 import { defaultPlayground, InterpreterOutput, IPlaygroundState } from './states'
 
-export const reducer: Reducer<IPlaygroundState> = (
-  state = defaultPlayground,
-  action: ILoadedAction
-) => {
+export const reducer: Reducer<IPlaygroundState> = (state = defaultPlayground, action: IAction) => {
   switch (action.type) {
     case UPDATE_EDITOR_VALUE:
       return {
         ...state,
         editorValue: action.payload
+      }
+    case UPDATE_REPL_VALUE:
+      return {
+        ...state,
+        replValue: action.payload
       }
     case EVAL_INTERPRETER_SUCCESS:
       const outputClone = state.output.slice(0)
