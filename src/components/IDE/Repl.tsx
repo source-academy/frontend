b3b9b72c404cdae301fcd35008025697d5c839c5
ning <ningyuan.sg@gmail.com>
2018-05-18 12:46:42 +0800

    Implement multiple Outputs in Repl (history)

--- src/components/IDE/Repl.tsx
+++ src/components/IDE/Repl.tsx
@@ -1,19 +1,27 @@
 import * as React from 'react'
 
-import { TextArea } from '@blueprintjs/core'
-
+import { Card } from '@blueprintjs/core'
 import { InterpreterOutput, ResultOutput } from '../../reducers/states'
 
 export interface IReplProps {
   output: InterpreterOutput[]
 }
 
-const Repl: React.SFC<IReplProps> = props => (
-  <TextArea
-    value={JSON.stringify((props.output.slice(-1)[0] as ResultOutput).value)}
-    disabled={true}
-    className="col-xs-6"
-  />
+export interface IOutputProps {
+  type: string
+  value: string
+}
+
+const Repl: React.SFC<IReplProps> = props => {
+  let keyOutput = 0
+  const cards = props.output.map(slice => <Output {...slice as ResultOutput} key={keyOutput++} />)
+  return <div>{cards}</div>
+}
+
+const Output: React.SFC<IOutputProps> = props => (
+  <Card className="col-xs-6">
+    <code>{props.value}</code>
+  </Card>
 )
 
 export default Repl
