c048a86a203e8ef1c28db5c4f325314ad7f062f6
ning <ningyuan.sg@gmail.com>
2018-05-18 17:56:35 +0800

    Implement persistent repl content across routes

--- src/containers/IDE/ReplInputContainer.ts
+++ src/containers/IDE/ReplInputContainer.ts
@@ -1,11 +1,13 @@
 import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 
+import { updateReplValue } from '../../actions/playground'
 import ReplInput, { IReplInputProps } from '../../components/IDE/ReplInput'
 import { IState } from '../../reducers/states'
 
 type StateProps = Pick<IReplInputProps, 'replValue'>
-type DispatchProps = Pick<IReplInputProps, 'handleEvalReplInput'>
+type DispatchProps = Pick<IReplInputProps, 'handleEvalReplInput'> &
+  Pick<IReplInputProps, 'handleReplChange'>
 
 const mapStateToProps: MapStateToProps<StateProps, {}, IState> = state => {
   return {
@@ -16,6 +18,7 @@ const mapStateToProps: MapStateToProps<StateProps, {}, IState> = state => {
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dispatch<any>) =>
   bindActionCreators(
     {
+      handleReplChange: updateReplValue,
       handleEvalReplInput: (newCode: string) => {} // TODO: fill in
     },
     dispatch
