f8c10cddf252439d984bfc400bcf29802801b674
remo5000 <vigneshshankar@gmail.com>
2018-05-24 13:11:48 +0800

    Make editor parent Resizable

--- src/components/IDE/index.tsx
+++ src/components/IDE/index.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react'
 
+import Resizable from 're-resizable'
+
 import EditorContainer from '../../containers/IDE/EditorContainer'
 import ReplContainer from '../../containers/IDE/ReplContainer'
 
 const IDE: React.SFC<{}> = () => (
   <div className="IDE">
     <div className="row ide-content-parent">
-      <div className="col-xs-6 editor-parent">
-        <EditorContainer />
-      </div>
-      <div className="col-xs-6 repl-parent">
+      <Resizable
+        defaultSize={{
+          width: '50%',
+          height: '100%'
+        }}
+      >
+        <div className="editor-parent">
+          <EditorContainer />
+        </div>
+      </Resizable>
+      <div className="repl-parent">
         <ReplContainer />
       </div>
     </div>
