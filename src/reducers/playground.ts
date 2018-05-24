eea6ebccb2c0cb3bb43e9647c069d281b3f3bcf2
ning <ningyuan.sg@gmail.com>
2018-05-24 15:03:08 +0800

    Make context chapter 2 by default for dev

--- src/reducers/playground.ts
+++ src/reducers/playground.ts
@@ -41,7 +41,7 @@ export const reducer: Reducer<IPlaygroundState> = (state = defaultPlayground, ac
     case CLEAR_CONTEXT:
       return {
         ...state,
-        context: createContext()
+        context: createContext(2)
       }
     case HANDLE_CONSOLE_LOG:
       /* Possible cases:
