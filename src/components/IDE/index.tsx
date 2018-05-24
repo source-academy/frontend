3f0219435a729ddfccb9e46b9058bce622a0707e
remo5000 <vigneshshankar@gmail.com>
2018-05-24 14:35:44 +0800

    Disable resize for directions other than right

--- src/components/IDE/index.tsx
+++ src/components/IDE/index.tsx
@@ -14,6 +14,16 @@ const IDE: React.SFC<{}> = () => (
           width: '50%',
           height: '100%'
         }}
+        enable={{
+          top: false,
+          right: true,
+          bottom: false,
+          left: false,
+          topRight: false,
+          bottomRight: false,
+          bottomLeft: false,
+          topLeft: false
+        }}
       >
         <EditorContainer />
       </Resizable>
