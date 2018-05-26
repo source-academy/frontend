a5aae4711c30d8635dc387613b92d989a06ca3cb
Vignesh <vigneshshankar@gmail.com>
2018-05-26 16:53:53 +0800

    Disallow unspecified operators (#30)
    * Add unspecified operators rule
    
    * Remove strict equality
    
    * Update error message slightly

--- src/slang/rules/index.ts
+++ src/slang/rules/index.ts
@@ -11,21 +11,21 @@ import noIfWithoutElse from './noIfWithoutElse'
 import noImplicitDeclareUndefined from './noImplicitDeclareUndefined'
 import noImplicitReturnUndefined from './noImplicitReturnUndefined'
 import noNonEmptyList from './noNonEmptyList'
+import noUnspecifiedOperator from './noUnspecifiedOperator'
 import singleVariableDeclaration from './singleVariableDeclaration'
-import strictEquality from './strictEquality'
 
 const rules: Array<Rule<es.Node>> = [
   bracesAroundIfElse,
   bracesAroundWhile,
   singleVariableDeclaration,
-  strictEquality,
   noIfWithoutElse,
   noImplicitDeclareUndefined,
   noImplicitReturnUndefined,
   noNonEmptyList,
   noBlockArrowFunction,
   noDeclareReserved,
-  noDeclareMutable
+  noDeclareMutable,
+  noUnspecifiedOperator
 ]
 
 export default rules
