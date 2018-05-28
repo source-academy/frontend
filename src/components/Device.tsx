710d6a328a238c5da96436451c8e2ad61f36b1a5
ning <ningyuan.sg@gmail.com>
2018-05-28 11:46:08 +0800

    Add a background image for device

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
 
+import deviceBackground from '../assets/device_background.png'
+
+const deviceStyle = {
+  backgroundImage: `url(${deviceBackground})`
+}
+
 const Device: React.SFC<{}> = () => (
-  <div className="Device">
+  <div className="Device" style={deviceStyle}>
     <h2>Device</h2>
   </div>
 )
