6dc529d24f9a735f46d14bd0b07ad4ad2486ed94
remo5000 <vigneshshankar@gmail.com>
2018-05-28 16:47:39 +0800

    Produce a blank canvas in the Game component
    The latest version of pixi was used, because I could only get the
    typedefs for 4.7.X. A blank canvas is produced but the story is not
    loaded onto it (no attempt was made).
    
    There was a webpack error when I used the older version of pixi, probably due to the discepency between the
    typedef and the actual exports.
    
    No verify.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react'
 
-import LoginContainer from '../containers/LoginContainer'
+import * as Pixi from 'pixi.js'
 
-const Game: React.SFC<{}> = () => (
-  <div className="Game">
-    <LoginContainer />
-  </div>
-)
+export class Game extends React.Component<{}, {}> {
+  private app: Pixi.Application
+  private gameCanvas: HTMLDivElement
+
+  /**
+   * After mounting, add the Pixi Renderer to the div and start the Application.
+   */
+  public componentDidMount() {
+    this.app = new Pixi.Application(window.innerWidth, window.innerHeight) // TODO change this
+    this.gameCanvas.appendChild(this.app.view)
+    this.app.start()
+  }
+
+  /**
+   * Stop the Application when unmounting.
+   */
+  // public componentWillUnmount() {
+  //   this.app.stop();
+  // }
+
+  /**
+   * Simply render the div that will contain the Pixi Renderer.
+   */
+  public render() {
+    return (
+      <div
+        ref={thisDiv => {
+          this.gameCanvas = thisDiv as HTMLDivElement
+        }}
+      />
+    )
+  }
+
+  // public render() {
+  //   return (
+  //     <div className="Game">
+  //       <div id="game-display"
+  //         className="sa-game"
+  //         data-story="<%= @story_override || @story %>"
+  //         data-attempted-all="<%= @attempted_all %>"
+  //         data-username="<%= @username %>" />
+  //     </div>
+  //   )
+  // }
+}
 
 export default Game
