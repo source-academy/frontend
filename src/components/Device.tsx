3b60d4394b40d34cb768c089ffe8f117f10a66da
remo5000 <vigneshshankar@gmail.com>
2018-05-28 13:24:51 +0800

    Use SCSS to load background image
    Added a variable to define images path as well.

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -2,14 +2,8 @@ import * as React from 'react'
 
 import LoginContainer from '../containers/LoginContainer'
 
-import deviceBackground from '../assets/device_background.png'
-
-const deviceStyle = {
-  backgroundImage: `url(${deviceBackground})`
-}
-
 const Device: React.SFC<{}> = () => (
-  <div className="Device" style={deviceStyle}>
+  <div className="Device">
     <LoginContainer />
   </div>
 )
