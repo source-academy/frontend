83f46bea637a369c2c3e81f1df861f2424bf21ad
ning <ningyuan.sg@gmail.com>
2018-05-28 12:43:27 +0800

    Add action to change the session token

--- src/reducers/session.ts
+++ src/reducers/session.ts
@@ -1,7 +1,16 @@
 import { Reducer } from 'redux'
 
+import { CHANGE_TOKEN, IAction } from '../actions/actionTypes'
 import { defaultSession, ISessionState } from './states'
 
-export const reducer: Reducer<ISessionState> = (state = defaultSession, {}) => {
-  return state
+export const reducer: Reducer<ISessionState> = (state = defaultSession, action: IAction) => {
+  switch (action.type) {
+    case CHANGE_TOKEN:
+      return {
+        ...state,
+        token: action.payload
+      }
+    default:
+      return state
+  }
 }
