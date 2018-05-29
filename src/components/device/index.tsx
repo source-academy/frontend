42e681e746e6e27f5ed2a3273421df12c2f6dab5
remo5000 <vigneshshankar@gmail.com>
2018-05-29 17:02:59 +0800

    Add card for device child content

--- src/components/device/index.tsx
+++ src/components/device/index.tsx
@@ -1,4 +1,4 @@
-import { Icon, Navbar, NavbarGroup } from '@blueprintjs/core'
+import { Card, Icon, Navbar, NavbarGroup } from '@blueprintjs/core'
 import * as React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 import { NavLink } from 'react-router-dom'
@@ -9,7 +9,7 @@ const redirectToJournal = () => <Redirect to="/device/journal" />
 
 const Device: React.SFC<{}> = () => (
   <div className="Device">
-    <Navbar className="NavigationBar pt-dark">
+    <Navbar className="NavigationBar pt-dark row">
       <NavbarGroup className="pt-align-left">
         <NavLink
           to="/device/journal"
@@ -21,12 +21,16 @@ const Device: React.SFC<{}> = () => (
         </NavLink>
       </NavbarGroup>
     </Navbar>
-    <div className="device-content-parent">
-      <Switch>
-        <Route path="/device/journal" component={JournalContainer} />
-        <Route exact={true} path="/device" component={redirectToJournal} />
-        <Route component={NotFound} />
-      </Switch>
+    <div className="row center-xs">
+      <div className="col-xs-10 device-content-parent">
+        <Card className="device-content" elevation={1}>
+          <Switch>
+            <Route path="/device/journal" component={JournalContainer} />
+            <Route exact={true} path="/device" component={redirectToJournal} />
+            <Route component={NotFound} />
+          </Switch>
+        </Card>
+      </div>
     </div>
   </div>
 )
