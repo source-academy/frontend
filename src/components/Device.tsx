a95e988271f96c45dc2f397b3b1cf6310b0d297b
remo5000 <vigneshshankar@gmail.com>
2018-05-29 15:31:37 +0800

    Add Navbar and Journal page
    Not sure if routing is the best option here.

--- src/components/Device.tsx
+++ src/components/Device.tsx
@@ -1,10 +1,33 @@
+import { Icon, Navbar, NavbarGroup } from '@blueprintjs/core'
 import * as React from 'react'
+import { Redirect, Route, Switch } from 'react-router'
+import { NavLink } from 'react-router-dom'
+import JournalContainer from '../containers/JournalContainer'
+import NotFound from './NotFound'
 
-import LoginContainer from '../containers/LoginContainer'
+const redirectToJournal = () => <Redirect to="/device/journal" />
 
 const Device: React.SFC<{}> = () => (
-  <div className="Device">
-    <LoginContainer />
+  <div className="device">
+    <Navbar className="NavigationBar pt-dark">
+      <NavbarGroup className="pt-align-left">
+        <NavLink
+          to="/device/journal"
+          activeClassName="pt-active"
+          className="NavigationBar__link pt-button pt-minimal"
+        >
+          <Icon icon="book" />
+          Journal
+        </NavLink>
+      </NavbarGroup>
+    </Navbar>
+    <div className="device-content-parent">
+      <Switch>
+        <Route path="/device/journal" component={JournalContainer} />
+        <Route exact={true} path="/device" component={redirectToJournal} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </div>
 )
 
---
+++ src/containers/JournalContainer.ts
@@ -0,0 +1,5 @@
+import { connect } from 'react-redux'
+
+import Journal from '../components/Journal'
+
+export default connect()(Journal)
