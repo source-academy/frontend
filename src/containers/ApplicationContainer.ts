c65fbe221d364c14c55329a34b5748de082d4645
remo5000 <vigneshshankar@gmail.com>
2018-05-30 12:26:23 +0800

    Use application state in ApplicationContainer

--- src/containers/ApplicationContainer.ts
+++ src/containers/ApplicationContainer.ts
@@ -1,7 +1,7 @@
 import { connect, MapStateToProps } from 'react-redux'
 import { withRouter } from 'react-router'
 import Application from '../components/Application'
-import { IState } from '../reducers/states'
+import { IApplicationState } from '../reducers/states'
 
 /**
  * Provides the title of the application for display.
@@ -10,8 +10,8 @@ import { IState } from '../reducers/states'
  * as the routing properties of @type {RouteComponentProps} are
  * provided using the withRouter() method below.
  */
-const mapStateToProps: MapStateToProps<{ title: string }, {}, IState> = state => ({
-  title: state.application.title
+const mapStateToProps: MapStateToProps<{ title: string }, {}, IApplicationState> = state => ({
+  title: state.title
 })
 
 export default withRouter(connect(mapStateToProps)(Application))
