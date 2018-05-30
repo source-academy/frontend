4150325d55cc7ec5360a9ad3259d5454f8e572f5
remo5000 <vigneshshankar@gmail.com>
2018-05-30 12:27:53 +0800

    Use session state in LoginContainer

--- src/containers/LoginContainer.ts
+++ src/containers/LoginContainer.ts
@@ -3,13 +3,13 @@ import { bindActionCreators, Dispatch } from 'redux'
 
 import { changeToken } from '../actions/session'
 import Login, { ILoginProps } from '../components/Login'
-import { IState } from '../reducers/states'
+import { ISessionState } from '../reducers/states'
 
 type stateProps = Pick<ILoginProps, 'token'>
 type dispatchProps = Pick<ILoginProps, 'handleChangeToken'>
 
-const mapStateToProps: MapStateToProps<stateProps, {}, IState> = state => ({
-  token: state.session.token
+const mapStateToProps: MapStateToProps<stateProps, {}, ISessionState> = state => ({
+  token: state.token
 })
 
 const mapDispatchToProps: MapDispatchToProps<dispatchProps, {}> = (dispatch: Dispatch<any>) =>
