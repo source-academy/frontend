fbc3899d2bded17e9c7d5a69eb588fd3acf7fcc8
ning <ningyuan.sg@gmail.com>
2018-05-31 13:21:31 +0800

    Wire up state, dispatches in ControlBar components

--- src/components/workspace/ControlBar.tsx
+++ src/components/workspace/ControlBar.tsx
@@ -1,5 +1,8 @@
+import { Button, MenuItem } from '@blueprintjs/core'
+import { ItemRenderer, Select } from '@blueprintjs/select'
 import * as React from 'react'
 
+import { sourceChapters } from '../../reducers/states'
 import { controlButton } from '../commons'
 
 interface IControlBarProps {
@@ -31,16 +34,42 @@ class ControlBar extends React.Component<IControlBarProps, {}> {
     return (
       <div className="ControlBar">
         <div className="ControlBar_editor pt-button-group">
-          {controlButton('Run', 'code', () => {})}
-          {controlButton('Run', 'code', () => {})}
+          {chapterSelect(this.props.sourceChapter, this.props.handleChapterSelect)}
+          {controlButton('Run', 'play', this.props.handleEditorEval)}
         </div>
         <div className="ControlBar_repl pt-button-group">
-          {controlButton('Eval', 'code', () => {})}
-          {controlButton('Clear', 'code', () => {})}
+          {controlButton('Eval', 'code', this.props.handleReplEval)}
+          {controlButton('Clear', 'remove', this.props.handleReplOutputClear)}
         </div>
       </div>
     )
   }
+}
+
+const chapters = sourceChapters.map(chap => ({ displayName: styliseChapter(chap), chapter: chap }))
+
+const chapterSelect = (
+  currentChap: number,
+  handleSelect = (i: IChapter, e: React.ChangeEvent<HTMLSelectElement>) => {}
+) => (
+  <ChapterSelectComponent
+    items={chapters}
+    onItemSelect={handleSelect}
+    itemRenderer={chapterRenderer}
+    filterable={false}
+  >
+    <Button text={styliseChapter(currentChap)} rightIcon="double-caret-vertical" />
+  </ChapterSelectComponent>
+)
+
+const ChapterSelectComponent = Select.ofType<IChapter>()
+
+const chapterRenderer: ItemRenderer<IChapter> = (chap, { handleClick, modifiers, query }) => (
+  <MenuItem active={false} key={chap.chapter} onClick={handleClick} text={chap.displayName} />
+)
+
+function styliseChapter(chap: number) {
+  return `Source \xa7${chap}`
 }
 
 export default ControlBar
