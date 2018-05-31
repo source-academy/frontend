b49b41879cda1d6abc6ba948ab7e3b11218e9a9e
remo5000 <vigneshshankar@gmail.com>
2018-05-31 16:48:32 +0800

    Add output for empty announcements array

--- src/components/device/Announcements.tsx
+++ src/components/device/Announcements.tsx
@@ -19,19 +19,21 @@ class Announcements extends React.Component<IAnnouncementsProps, {}> {
   }
 
   public render() {
-    const announcements =
-      this.props.announcements === undefined ? (
-        <Spinner />
-      ) : (
-        this.props.announcements.map((ann, index) => (
-          <div key={index}>
-            <AnnouncementCard {...ann} />
-          </div>
-        ))
-      )
+    let output
+    if (this.props.announcements === undefined) {
+      output = <Spinner />
+    } else if (this.props.announcements.length === 0) {
+      output = <h4>There are no announcements.</h4>
+    } else {
+      output = this.props.announcements.map((ann, index) => (
+        <div key={index}>
+          <AnnouncementCard {...ann} />
+        </div>
+      ))
+    }
     return (
       <div className="Announcements row center-xs">
-        <div className="col-xs-10">{announcements}</div>
+        <div className="col-xs-10">{output}</div>
       </div>
     )
   }
