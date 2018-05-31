c93d8e9d7b6250187ec6c77e86b50608f38fa3e2
remo5000 <vigneshshankar@gmail.com>
2018-05-31 12:26:16 +0800

    Make Announcements a stateful component

--- src/components/device/Announcements.tsx
+++ src/components/device/Announcements.tsx
@@ -12,15 +12,17 @@ export interface IAnnouncementsProps {
   announcements?: Announcement[]
 }
 
-const Announcements: React.SFC<IAnnouncementsProps> = props => {
-  const announcements = props.announcements === undefined ? [] : props.announcements
-  // make an API call here if no announcements
-  const cards = announcements.map((ann, index) => (
-    <Card className="col-xs-8" key={index}>
-      <h4>{ann.title}</h4>
-      <Text>{ann.content}</Text>
-    </Card>
-  ))
-  return <div className="Announcements row center-xs">{cards}</div>
+class Announcements extends React.Component<IAnnouncementsProps, {}> {
+  public render() {
+    const announcements = this.props.announcements === undefined ? [] : this.props.announcements
+    // make an API call here if no announcements
+    const cards = announcements.map((ann, index) => (
+      <Card className="col-xs-8" key={index}>
+        <h4>{ann.title}</h4>
+        <Text>{ann.content}</Text>
+      </Card>
+    ))
+    return <div className="Announcements row center-xs">{cards}</div>
+  }
 }
 export default Announcements
