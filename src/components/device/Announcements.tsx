9c2649a2ad860582d521ba048ca41aab9d22f36e
remo5000 <vigneshshankar@gmail.com>
2018-05-31 11:22:25 +0800

    Add announcement props
    No verify

--- src/components/device/Announcements.tsx
+++ src/components/device/Announcements.tsx
@@ -1,7 +1,37 @@
+import { Card, Text } from '@blueprintjs/core';
 import * as React from 'react'
 
-const Announcements: React.SFC<{}> = () => (
-  <div className="Announcements">Announcements Content</div>
-)
+type Announcement = {
+  author: string
+  title : string
+  content : string
+  pinned : boolean
+  date: Date
+}
 
+interface IAnnouncementsProps  {
+  announcements: Announcement[]
+}
+
+const Announcements: React.SFC<IAnnouncementsProps> = props => {
+  const cards = props.announcements.map((ann, index) => 
+    <Card
+    className="col-xs-8"
+    key={index}> 
+      <h4>
+         {ann.title}
+      </h4>
+      <Text>
+         {ann.content}
+      </Text>
+      <Text>
+         {ann.date.toString()}
+      </Text>
+    </Card>)
+  return (
+    <div className="Announcements row center-xs">
+      {cards}
+    </div>
+  )
+}
 export default Announcements
