b924acbb8565e37b3309945519adebb4ecfb6aa1
remo5000 <vigneshshankar@gmail.com>
2018-06-04 12:10:26 +0800

    Abstract Announcements by using ContentDisplay
    To do this, the ContentDisplay is rendered inside Announcements, passing
    it props.
    - Handling content being undefined (and showing a spinner) is handled by
    the display SFC (in this case, AnnouncementCard). As such,
    - The `contentLoaded` property of IContentDisplayProps was removed
    - Respective test was updated.

--- src/components/device/ContentDisplay.tsx
+++ src/components/device/ContentDisplay.tsx
@@ -1,9 +1,7 @@
-import { NonIdealState, Spinner } from '@blueprintjs/core'
 import * as React from 'react'
 
 export interface IContentDisplayProps {
   displayStream: () => JSX.Element
-  contentLoaded?: boolean
   loadContentDispatch: () => void
 }
 
@@ -13,16 +11,9 @@ class ContentDisplay extends React.Component<IContentDisplayProps, {}> {
   }
 
   public render() {
-    let output
-    if (!this.props.contentLoaded) {
-      output = <NonIdealState description="Give it a second..." visual={<Spinner />} />
-    } else {
-      output = this.props.displayStream()
-    }
-
     return (
       <div className="Announcements row center-xs">
-        <div className="col-xs-10">{output}</div>
+        <div className="col-xs-10">{this.props.displayStream()}</div>
       </div>
     )
   }
