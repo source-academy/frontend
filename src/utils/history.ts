dfae073bb215a5b861c2a5168c40663610d50098
ning <ningyuan.sg@gmail.com>
2018-06-05 12:47:39 +0800

    Fix typo in comment

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -1,5 +1,5 @@
 /** history is created and exported here instead of in src/index.tsx as jest
- * would otherwise throw an arrow. See: https://stackoverflow.com/a/42611909/6910451
+ * would otherwise throw an error. See: https://stackoverflow.com/a/42611909/6910451
  */
 import { createBrowserHistory } from 'history'
 
