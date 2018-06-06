aadf9299bb60a84a931d82c32df32c4acbe635f3
remo5000 <vigneshshankar@gmail.com>
2018-06-06 15:31:08 +0800

    Add snapping for Editor's parent Resizable
    - Renamed dividerDiv to sideDividerDiv for verbosity

--- src/components/workspace/index.tsx
+++ src/components/workspace/index.tsx
@@ -15,11 +15,12 @@ export interface IWorkspaceProps {
 }
 
 class Workspace extends React.Component<IWorkspaceProps, {}> {
-  private dividerDiv: HTMLDivElement
+  private sideDividerDiv: HTMLDivElement
   private maxDividerHeight: number
+  private leftParentResizable: Resizable
 
   public componentDidMount() {
-    this.maxDividerHeight = this.dividerDiv.clientHeight
+    this.maxDividerHeight = this.sideDividerDiv.clientHeight
   }
 
   /**
@@ -34,14 +35,16 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
         <ControlBarContainer />
         <div className="row workspace-parent">
           <Resizable
-            className="left-parent"
+            className="resize-editor left-parent"
             size={{ width: this.props.editorWidth, height: '100%' }}
-            maxWidth={window.innerWidth - 10}
+            minWidth={0}
+            onResize={this.toggleEditorDividerDisplay}
             // tslint:disable-next-line jsx-no-lambda
             onResizeStop={(e, dir, ref, diff) => {
               this.props.handleEditorWidthChange(diff.width * 100 / window.innerWidth)
             }}
             enable={rightResizeOnly}
+            ref={e => (this.leftParentResizable = e as Resizable)}
           >
             <EditorContainer />
           </Resizable>
@@ -65,7 +68,7 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
               <SideContent />
               <div
                 className="side-content-divider"
-                ref={e => (this.dividerDiv = e as HTMLDivElement)}
+                ref={e => (this.sideDividerDiv = e as HTMLDivElement)}
               />
             </Resizable>
             <ReplContainer />
@@ -76,20 +79,33 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
   }
 
   /**
+   * Snaps the left-parent resizable to 100% or 0% when percentage width goes
+   * above 95% or below 5% respectively.
+   */
+  private toggleEditorDividerDisplay: ResizeCallback = (e, dir, ref) => {
+    const editorWidthPercentage = (ref as HTMLDivElement).clientWidth / window.innerWidth * 100
+    if (editorWidthPercentage > 95) {
+      this.leftParentResizable.updateSize({ width: '100%', height: '100%' })
+    } else if (editorWidthPercentage < 5) {
+      this.leftParentResizable.updateSize({ width: '0%', height: '100%' })
+    }
+  }
+
+  /**
    * Hides the side-content-divider div when side-content is resized downwards
    * so that it's bottom border snaps flush with editor's bottom border
    */
   private toggleDividerDisplay: ResizeCallback = (e, dir, ref) => {
     this.maxDividerHeight =
-      this.dividerDiv.clientHeight > this.maxDividerHeight
-        ? this.dividerDiv.clientHeight
+      this.sideDividerDiv.clientHeight > this.maxDividerHeight
+        ? this.sideDividerDiv.clientHeight
         : this.maxDividerHeight
     const resizableHeight = (ref as HTMLDivElement).clientHeight
     const rightParentHeight = (ref.parentNode as HTMLDivElement).clientHeight
     if (resizableHeight + this.maxDividerHeight + 2 > rightParentHeight) {
-      this.dividerDiv.style.display = 'none'
+      this.sideDividerDiv.style.display = 'none'
     } else {
-      this.dividerDiv.style.display = 'initial'
+      this.sideDividerDiv.style.display = 'initial'
     }
   }
 }
