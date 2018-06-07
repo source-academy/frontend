7f09d0b94cb2fea77b113b5a403cf3d0f593cb91
ningOTI <ningyuan.sg@gmail.com>
2018-06-07 15:11:22 +0800

    Abstract resizable options out of render method (#96)

--- src/components/workspace/index.tsx
+++ src/components/workspace/index.tsx
@@ -1,4 +1,4 @@
-import Resizable, { ResizeCallback } from 're-resizable'
+import Resizable, { ResizableProps, ResizeCallback } from 're-resizable'
 import * as React from 'react'
 import { HotKeys } from 'react-hotkeys'
 
@@ -25,10 +25,10 @@ export type StateProps = Pick<IWorkspaceProps, 'editorWidth'> &
   Pick<IWorkspaceProps, 'sideContentHeight'>
 
 class Workspace extends React.Component<IWorkspaceProps, {}> {
-  private sideDividerDiv: HTMLDivElement
-  private maxDividerHeight: number
-  private leftParentResizable: Resizable
   private editorDividerDiv: HTMLDivElement
+  private leftParentResizable: Resizable
+  private maxDividerHeight: number
+  private sideDividerDiv: HTMLDivElement
 
   public componentDidMount() {
     this.maxDividerHeight = this.sideDividerDiv.clientHeight
@@ -49,37 +49,11 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
             className="editor-divider"
             ref={e => (this.editorDividerDiv = e as HTMLDivElement)}
           />
-          <Resizable
-            className="resize-editor left-parent"
-            size={{ width: this.props.editorWidth, height: '100%' }}
-            minWidth={0}
-            onResize={this.toggleEditorDividerDisplay}
-            // tslint:disable-next-line jsx-no-lambda
-            onResizeStop={(e, dir, ref, diff) => {
-              this.props.handleEditorWidthChange(diff.width * 100 / window.innerWidth)
-            }}
-            enable={rightResizeOnly}
-            ref={e => (this.leftParentResizable = e as Resizable)}
-          >
+          <Resizable {...this.editorResizableProps()}>
             <EditorContainer />
           </Resizable>
           <div className="right-parent">
-            <Resizable
-              bounds="parent"
-              className="resize-side-content"
-              minHeight={0}
-              size={
-                this.props.sideContentHeight === undefined
-                  ? undefined
-                  : { height: this.props.sideContentHeight, width: '100%' }
-              }
-              onResize={this.toggleDividerDisplay}
-              // tslint:disable-next-line jsx-no-lambda
-              onResizeStop={(e, dir, ref, diff) => {
-                this.props.handleSideContentHeightChange(ref.clientHeight)
-              }}
-              enable={bottomResizeOnly}
-            >
+            <Resizable {...this.sideContentResizableProps()}>
               <SideContent {...{ tabs: this.props.sideContentTabs }} />
               <div
                 className="side-content-divider"
@@ -93,12 +67,45 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
     )
   }
 
+  private editorResizableProps() {
+    const onResizeStop: ResizeCallback = ({}, {}, {}, diff) =>
+      this.props.handleEditorWidthChange(diff.width * 100 / window.innerWidth)
+    const ref = (e: Resizable) => (this.leftParentResizable = e as Resizable)
+    return {
+      className: 'resize-editor left-parent',
+      enable: rightResizeOnly,
+      minWidth: 0,
+      onResize: this.toggleEditorDividerDisplay,
+      onResizeStop,
+      ref,
+      size: { width: this.props.editorWidth, height: '100%' }
+    } as ResizableProps
+  }
+
+  private sideContentResizableProps() {
+    const size =
+      this.props.sideContentHeight === undefined
+        ? undefined
+        : { height: this.props.sideContentHeight, width: '100%' }
+    const onResizeStop: ResizeCallback = ({}, {}, ref, {}) =>
+      this.props.handleSideContentHeightChange(ref.clientHeight)
+    return {
+      bounds: 'parent',
+      className: 'resize-side-content',
+      enable: bottomResizeOnly,
+      minHeight: 0,
+      onResize: this.toggleDividerDisplay,
+      onResizeStop,
+      size
+    } as ResizableProps
+  }
+
   /**
    * Snaps the left-parent resizable to 100% or 0% when percentage width goes
    * above 95% or below 5% respectively. Also changes the editor divider width
    * in the case of < 5%.
    */
-  private toggleEditorDividerDisplay: ResizeCallback = (e, dir, ref) => {
+  private toggleEditorDividerDisplay: ResizeCallback = ({}, {}, ref) => {
     const leftThreshold = 2
     const rightThreshold = 95
     const editorWidthPercentage = (ref as HTMLDivElement).clientWidth / window.innerWidth * 100
@@ -120,7 +127,7 @@ class Workspace extends React.Component<IWorkspaceProps, {}> {
    * Hides the side-content-divider div when side-content is resized downwards
    * so that it's bottom border snaps flush with editor's bottom border
    */
-  private toggleDividerDisplay: ResizeCallback = (e, dir, ref) => {
+  private toggleDividerDisplay: ResizeCallback = ({}, {}, ref) => {
     this.maxDividerHeight =
       this.sideDividerDiv.clientHeight > this.maxDividerHeight
         ? this.sideDividerDiv.clientHeight
