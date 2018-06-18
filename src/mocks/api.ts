d945ea1ac8101342df8c7bf4b480e3f6de4af1c4
remo5000 <vigneshshankar@gmail.com>
2018-06-18 14:02:23 +0800

    Add closed and all missions
    Also added a counter for all the ids

--- src/mocks/api.ts
+++ src/mocks/api.ts
@@ -1,4 +1,7 @@
-export const mockMissionsResult = [
+let i = 0
+const mockId = () => i++
+
+export const mockOpenAssessmentsResult = [
   {
     title: 'An Odessey to Runes',
     summary_short:
@@ -9,7 +12,7 @@ export const mockMissionsResult = [
     open_at: '2018-06-18T05:24:26.026Z',
     mission_pdf: 'www.google.com',
     max_xp: 3000,
-    id: 0,
+    id: mockId(),
     cover_picture: 'www.imgur.com',
     close_at: '2048-06-18T05:24:26.026Z',
     category: 'mission'
@@ -24,7 +27,7 @@ export const mockMissionsResult = [
     open_at: '2018-07-18T05:24:26.026Z',
     mission_pdf: 'www.google.com',
     max_xp: 3000,
-    id: 1,
+    id: mockId(),
     cover_picture: 'www.imgur.com',
     close_at: '2048-06-18T05:24:26.026Z',
     category: 'mission'
@@ -39,9 +42,44 @@ export const mockMissionsResult = [
     open_at: '2018-07-18T05:24:26.026Z',
     mission_pdf: 'www.google.com',
     max_xp: 3000,
-    id: 2,
+    id: mockId(),
     cover_picture: 'www.imgur.com',
     close_at: '2048-06-18T05:24:26.026Z',
     category: 'sidequest'
   }
 ]
+
+const mockClosedAssessments = [
+  {
+    title: 'A closed Mission',
+    summary_short:
+      'Once upon a time, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec vulputate sapien. Fusce vel lacus fermentum, efficitur ipsum.',
+    summary_long:
+      'This is the sidequest briefing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra, sem scelerisque ultricies ullamcorper, sem nibh sollicitudin enim, at ultricies sem orci eget odio. Pellentesque varius et mauris quis vestibulum. Etiam in egestas dolor. Nunc consectetur, sapien sodales accumsan convallis, lectus mi tempus ipsum, vel ornare metus turpis sed justo. Vivamus at tellus sed ex convallis commodo at in lectus. Pellentesque pharetra pulvinar sapien pellentesque facilisis. Curabitur efficitur malesuada urna sed aliquam. Quisque massa metus, aliquam in sagittis non, cursus in sem. Morbi vel nunc at nunc pharetra lobortis. Aliquam feugiat ultricies ipsum vel sollicitudin. Vivamus nulla massa, hendrerit sit amet nibh quis, porttitor convallis nisi. ',
+    order: 0,
+    open_at: '2007-07-18T05:24:26.026Z',
+    mission_pdf: 'www.google.com',
+    max_xp: 3000,
+    id: mockId(),
+    cover_picture: 'www.imgur.com',
+    close_at: '2008-06-18T05:24:26.026Z',
+    category: 'mission'
+  },
+  {
+    title: 'A closed sidequest',
+    summary_short:
+      'Once upon a time, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec vulputate sapien. Fusce vel lacus fermentum, efficitur ipsum.',
+    summary_long:
+      'This is the sidequest briefing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra, sem scelerisque ultricies ullamcorper, sem nibh sollicitudin enim, at ultricies sem orci eget odio. Pellentesque varius et mauris quis vestibulum. Etiam in egestas dolor. Nunc consectetur, sapien sodales accumsan convallis, lectus mi tempus ipsum, vel ornare metus turpis sed justo. Vivamus at tellus sed ex convallis commodo at in lectus. Pellentesque pharetra pulvinar sapien pellentesque facilisis. Curabitur efficitur malesuada urna sed aliquam. Quisque massa metus, aliquam in sagittis non, cursus in sem. Morbi vel nunc at nunc pharetra lobortis. Aliquam feugiat ultricies ipsum vel sollicitudin. Vivamus nulla massa, hendrerit sit amet nibh quis, porttitor convallis nisi. ',
+    order: 0,
+    open_at: '2007-07-18T05:24:26.026Z',
+    mission_pdf: 'www.google.com',
+    max_xp: 3000,
+    id: mockId(),
+    cover_picture: 'www.imgur.com',
+    close_at: '2008-06-18T05:24:26.026Z',
+    category: 'sidequest'
+  }
+]
+
+export const mockAssessmentsResult = [...mockOpenAssessmentsResult, ...mockClosedAssessments]
