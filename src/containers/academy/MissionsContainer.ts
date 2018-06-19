a10ac0f1812e584c15ad6c4b1e3e6e4f4402d21f
remo5000 <vigneshshankar@gmail.com>
2018-06-19 12:35:10 +0800

    Use assessment-related names in MissionsContainer

--- src/containers/academy/MissionsContainer.ts
+++ src/containers/academy/MissionsContainer.ts
@@ -2,20 +2,26 @@ import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux'
 import { withRouter } from 'react-router'
 import { bindActionCreators, Dispatch } from 'redux'
 
-import { fetchMissionsInfo } from '../../actions/session'
+import { fetchAssessmentOverview } from '../../actions/session'
 import Missions, { DispatchProps, StateProps } from '../../components/academy/Missions'
+import {
+  IAssessmentOverview,
+  AssessmentCategories
+} from '../../components/assessment/assessmentShape'
 import { IState } from '../../reducers/states'
+
+const isMission = (x: IAssessmentOverview) => x.category === AssessmentCategories.MISSION
 
 const mapStateToProps: MapStateToProps<StateProps, {}, IState> = state => {
   return {
-    missionsInfo: state.session.missionsInfo
+    assessmentOverviews: state.session.assessmentOverviews.filter(isMission)
   }
 }
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dispatch<any>) =>
   bindActionCreators(
     {
-      handleMissionsInfoFetch: fetchMissionsInfo
+      handleAssessmentOverviewFetch: fetchAssessmentOverview
     },
     dispatch
   )
