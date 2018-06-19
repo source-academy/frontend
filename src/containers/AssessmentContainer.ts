67c58964041175546f56e49bfec58de544c1f1fe
remo5000 <vigneshshankar@gmail.com>
2018-06-19 10:58:13 +0800

    Use proper variable names in AssessmentContainer

--- src/containers/AssessmentContainer.ts
+++ src/containers/AssessmentContainer.ts
@@ -7,17 +7,15 @@ import { IState } from '../reducers/states'
 
 const mapStateToProps: MapStateToProps<StateProps, OwnProps, IState> = (state, props) => {
   return {
-    assessmentInfo:
-      state.session.assessmentInfos === undefined
-        ? undefined
-        : state.session.assessmentInfos[props.missionId]
+    assessment:
+      state.session.assessments[props.missionId]
   }
 }
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dispatch<any>) =>
   bindActionCreators<DispatchProps>(
     {
-      handleAssessmentInfoFetch: fetchAssessment
+      handleAssessmentFetch: fetchAssessment
     },
     dispatch
   )
