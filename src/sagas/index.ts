637d534a11b3d08b595028c8dffea0e4be527cbb
remo5000 <vigneshshankar@gmail.com>
2018-06-19 23:46:17 +0800

    Format and update test files

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -5,7 +5,7 @@ import { call, put, race, select, take, takeEvery } from 'redux-saga/effects'
 
 import * as actions from '../actions'
 import * as actionTypes from '../actions/actionTypes'
-import { mockAssessmentOverviews, mockAssessments} from '../mocks/api'
+import { mockAssessmentOverviews, mockAssessments } from '../mocks/api'
 import { IState } from '../reducers/states'
 import { Context, interrupt, runInContext } from '../slang'
 import { showSuccessMessage, showWarningMessage } from '../utils/notification'
