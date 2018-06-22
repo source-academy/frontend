88308c7f284b775820416dca8214ab4f44a53f93
remo5000 <vigneshshankar@gmail.com>
2018-06-22 11:53:07 +0800

    Format with prettier

--- src/utils/paramParseHelpers.ts
+++ src/utils/paramParseHelpers.ts
@@ -3,16 +3,16 @@ import { AssessmentCategory } from '../components/assessment/assessmentShape'
 /**
  * Converts an AssessmentCategory into a string for use in URLs.
  *
- * @param {AssessmentCategory} cat - Any AssessmentCategory, usually 
+ * @param {AssessmentCategory} cat - Any AssessmentCategory, usually
  *   retrieved from the AssessmentCategories enum
  */
 export const assessmentCategoryLink = (cat: AssessmentCategory): string =>
   cat.toLowerCase().concat('s')
 
-/** Converts an optinal string 
+/** Converts an optinal string
  *  parameter into an integer or null value.
  *
- *  @param {string} str - An optional string to be 
+ *  @param {string} str - An optional string to be
  *    converted to an integer.
  */
 export const stringParamToInt = (str?: string): number | null => {
