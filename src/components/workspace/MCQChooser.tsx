a123805fc0a96c2e7d9d2208403f18ac403f89ef
remo5000 <vigneshshankar@gmail.com>
2018-06-22 17:17:11 +0800

    Arrange content of MCQChooser

--- src/components/workspace/MCQChooser.tsx
+++ src/components/workspace/MCQChooser.tsx
@@ -1,3 +1,4 @@
+import { Card, Tooltip } from '@blueprintjs/core'
 import * as React from 'react'
 
 import { MCQChoice } from '../assessment/assessmentShape'
@@ -8,9 +9,27 @@ export interface IMCQChooserProps {
 
 class MCQChooser extends React.Component<IMCQChooserProps, {}> {
   public render() {
+    const options = this.props.choices.map((choice, i) => (
+      <Card className="mcq-option col-xs-6">
+        <Tooltip key={i} content={choice.hint}>
+          { choice.content }
+        </Tooltip>
+      </ Card>
+    ))
     return (
-        <div className="row editor-react-ace">
-          <h2> MCQQQ </h2>
+        <div className="MCQChooser">
+          <Card className="row center-xs">
+            <div className="col-xs-8">
+              <div className="mcq-content-parent row center-xs ">
+                <Card className="mcq-content col-xs-12">
+                    IS NICE
+                </ Card>
+              </div>
+              <div className="row mcq-options-parent center-xs">
+                { options } 
+              </div>
+            </div>
+          </Card>
         </div>
     )
   }
