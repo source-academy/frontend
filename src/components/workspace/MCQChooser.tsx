81d070c785ab6bb8af6aa5c3964fd42e441a4257
remo5000 <vigneshshankar@gmail.com>
2018-06-22 17:36:20 +0800

    Style components to look consistent

--- src/components/workspace/MCQChooser.tsx
+++ src/components/workspace/MCQChooser.tsx
@@ -1,4 +1,4 @@
-import { Card, Tooltip } from '@blueprintjs/core'
+import { Button, Card, Tooltip } from '@blueprintjs/core'
 import * as React from 'react'
 
 import { MCQChoice } from '../assessment/assessmentShape'
@@ -10,19 +10,19 @@ export interface IMCQChooserProps {
 class MCQChooser extends React.Component<IMCQChooserProps, {}> {
   public render() {
     const options = this.props.choices.map((choice, i) => (
-      <Card className="mcq-option col-xs-6">
+      <Button className="mcq-option col-xs-6">
         <Tooltip key={i} content={choice.hint}>
           { choice.content }
         </Tooltip>
-      </ Card>
+      </ Button>
     ))
     return (
         <div className="MCQChooser">
-          <Card className="row center-xs">
-            <div className="col-xs-8">
-              <div className="mcq-content-parent row center-xs ">
-                <Card className="mcq-content col-xs-12">
-                    IS NICE
+          <Card className="mcq-content-parent row center-xs">
+            <div className="col-xs-10">
+              <div className="mcq-task-parent row center-xs ">
+                <Card className="mcq-task col-xs-12" elevation={2}>
+                    MCQ QUESTION
                 </ Card>
               </div>
               <div className="row mcq-options-parent center-xs">
