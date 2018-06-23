fb842dbfe1be5537ace29b4d31028de913574dad
ningOTI <ningyuan.sg@gmail.com>
2018-06-23 14:41:23 +0800

    Fix parser not checking rules/* when minified (#126)
    * Fix behaviour on server, but tests fail
    
    * Fix off-by-one allowed syntax types conditional
    
    I was tired, okay
    
    * Improve readability
    
    * Remove commented-out line

--- src/slang/parser.ts
+++ src/slang/parser.ts
@@ -6,7 +6,7 @@ import * as es from 'estree'
 
 import rules from './rules'
 import syntaxTypes from './syntaxTypes'
-import { Context, ErrorSeverity, ErrorType, SourceError } from './types'
+import { Context, ErrorSeverity, ErrorType, Rule, SourceError } from './types'
 
 // tslint:disable-next-line:interface-name
 export interface ParserOptions {
@@ -19,7 +19,7 @@ export class DisallowedConstructError implements SourceError {
   public nodeType: string
 
   constructor(public node: es.Node) {
-    this.nodeType = this.splitNodeType()
+    this.nodeType = this.formatNodeType(this.node.type)
   }
 
   get location() {
@@ -27,7 +27,7 @@ export class DisallowedConstructError implements SourceError {
   }
 
   public explain() {
-    return `${this.nodeType} is not allowed`
+    return `${this.nodeType} are not allowed`
   }
 
   public elaborate() {
@@ -36,20 +36,22 @@ export class DisallowedConstructError implements SourceError {
     `
   }
 
-  private splitNodeType() {
-    const nodeType = this.node.type
-    const tokens: string[] = []
-    let soFar = ''
-    for (let i = 0; i < nodeType.length; i++) {
-      const isUppercase = nodeType[i] === nodeType[i].toUpperCase()
-      if (isUppercase && i > 0) {
-        tokens.push(soFar)
-        soFar = ''
-      } else {
-        soFar += nodeType[i]
-      }
+  /**
+   * Converts estree node.type into english
+   * e.g. ThisExpression -> 'this' expressions
+   *      Property -> Properties
+   *      EmptyStatement -> Empty Statements
+   */
+  private formatNodeType(nodeType: string) {
+    switch (nodeType) {
+      case 'ThisExpression':
+        return "'this' expressions"
+      case 'Property':
+        return 'Properties'
+      default:
+        const words = nodeType.split(/(?=[A-Z])/)
+        return words.map((word, i) => (i === 0 ? word : word.toLowerCase())).join(' ') + 's'
     }
-    return tokens.join(' ')
   }
 }
 
@@ -95,47 +97,30 @@ export class TrailingCommaError implements SourceError {
   }
 }
 
-export const freshId = (() => {
-  let id = 0
-  return () => {
-    id++
-    return 'node_' + id
-  }
-})()
-
-function compose<T extends es.Node, S>(
-  w1: (node: T, state: S) => void,
-  w2: (node: T, state: S) => void
-) {
-  return (node: T, state: S) => {
-    w1(node, state)
-    w2(node, state)
-  }
-}
-
-const walkers: {
-  [name: string]: (node: es.Node, context: Context) => void
-} = {}
-
-for (const type of Object.keys(syntaxTypes)) {
-  walkers[type] = (node: es.Node, context: Context) => {
-    const id = freshId()
-    Object.defineProperty(node, '__id', {
-      enumerable: true,
-      configurable: false,
-      writable: false,
-      value: id
-    })
-    context.cfg.nodes[id] = {
-      id,
-      node,
-      scope: undefined,
-      usages: []
+export function parse(source: string, context: Context) {
+  let program: es.Program | undefined
+  try {
+    program = acornParse(source, createAcornParserOptions(context))
+    simple(program, walkers, undefined, context)
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      // tslint:disable-next-line:no-any
+      const loc = (error as any).loc
+      const location = {
+        start: { line: loc.line, column: loc.column },
+        end: { line: loc.line, column: loc.column + 1 }
+      }
+      context.errors.push(new FatalSyntaxError(location, error.toString()))
+    } else {
+      throw error
     }
-    context.cfg.edges[id] = []
-    if (syntaxTypes[node.type] > context.chapter) {
-      context.errors.push(new DisallowedConstructError(node))
-    }
+  }
+  const hasErrors = context.errors.find(m => m.severity === ErrorSeverity.ERROR)
+  if (program && !hasErrors) {
+    // context.cfg.scopes[0].node = program
+    return program
+  } else {
+    return undefined
   }
 }
 
@@ -163,43 +148,77 @@ const createAcornParserOptions = (context: Context): AcornOptions => ({
   }
 })
 
-rules.forEach(rule => {
-  const keys = Object.keys(rule.checkers)
-  keys.forEach(key => {
-    walkers[key] = compose(walkers[key], (node, context) => {
-      if (typeof rule.disableOn !== 'undefined' && context.chapter >= rule.disableOn) {
-        return
+function createWalkers(
+  allowedSyntaxes: { [nodeName: string]: number },
+  parserRules: Array<Rule<es.Node>>
+) {
+  const newWalkers = new Map<string, (n: es.Node, c: Context) => void>()
+
+  // Provide callbacks checking for disallowed syntaxes, such as case, switch...
+  const syntaxPairs = Object.entries(allowedSyntaxes)
+  syntaxPairs.map(pair => {
+    const syntax = pair[0]
+    const allowedChap = pair[1]
+    newWalkers.set(syntax, (node: es.Node, context: Context) => {
+      const id = freshId()
+      Object.defineProperty(node, '__id', {
+        enumerable: true,
+        configurable: false,
+        writable: false,
+        value: id
+      })
+      context.cfg.nodes[id] = {
+        id,
+        node,
+        scope: undefined,
+        usages: []
       }
-      const checker = rule.checkers[key]
-      const errors = checker(node)
-      errors.forEach(e => context.errors.push(e))
+      context.cfg.edges[id] = []
+      if (context.chapter < allowedChap) {
+        context.errors.push(new DisallowedConstructError(node))
+      }
     })
   })
-})
 
-export const parse = (source: string, context: Context) => {
-  let program: es.Program | undefined
-  try {
-    program = acornParse(source, createAcornParserOptions(context))
-    simple(program, walkers, undefined, context)
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      // tslint:disable-next-line:no-any
-      const loc = (error as any).loc
-      const location = {
-        start: { line: loc.line, column: loc.column },
-        end: { line: loc.line, column: loc.column + 1 }
+  // Provide callbacks checking for rule violations, e.g. no block arrow funcs, non-empty lists...
+  parserRules.forEach(rule => {
+    const checkers = rule.checkers
+    const syntaxCheckerPair = Object.entries(checkers)
+    syntaxCheckerPair.forEach(pair => {
+      const syntax = pair[0]
+      const checker = pair[1]
+      const oldCheck = newWalkers.get(syntax)
+      const newCheck = (node: es.Node, context: Context) => {
+        if (typeof rule.disableOn !== 'undefined' && context.chapter >= rule.disableOn) {
+          return
+        }
+        const errors = checker(node)
+        errors.forEach(e => context.errors.push(e))
       }
-      context.errors.push(new FatalSyntaxError(location, error.toString()))
-    } else {
-      throw error
-    }
-  }
-  const hasErrors = context.errors.find(m => m.severity === ErrorSeverity.ERROR)
-  if (program && !hasErrors) {
-    // context.cfg.scopes[0].node = program
-    return program
-  } else {
-    return undefined
-  }
+      newWalkers.set(syntax, (node, context) => {
+        if (oldCheck) {
+          oldCheck(node, context)
+        }
+        newCheck(node, context)
+      })
+    })
+  })
+
+  return mapToObj(newWalkers)
 }
+
+export const freshId = (() => {
+  let id = 0
+  return () => {
+    id++
+    return 'node_' + id
+  }
+})()
+
+const mapToObj = (map: Map<string, any>) =>
+  Array.from(map).reduce((obj, [k, v]) => Object.assign(obj, { [k]: v }), {})
+
+const walkers: { [name: string]: (node: es.Node, ctxt: Context) => void } = createWalkers(
+  syntaxTypes,
+  rules
+)
