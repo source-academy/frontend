7fd34d3de65b870ca4fac333faa558a92f78861c
remo5000 <vigneshshankar@gmail.com>
2018-06-28 14:41:15 +0800

    Remove all usage of redux-persist

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,24 +1,23 @@
 import { History } from 'history'
 import { routerMiddleware, routerReducer } from 'react-router-redux'
-import { applyMiddleware, compose, createStore as _createStore, Store, StoreEnhancer } from 'redux'
-import { persistCombineReducers, PersistConfig, persistStore } from 'redux-persist'
-import { createFilter } from 'redux-persist-transform-filter'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  createStore as _createStore,
+  Store,
+  StoreEnhancer
+} from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import reducers from './reducers'
-import { IApplicationState, ISessionState, IState } from './reducers/states'
+import { IState } from './reducers/states'
 import mainSaga from './sagas'
 import { history as appHistory } from './utils/history'
 
 declare var __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: () => StoreEnhancer<IState>
 
-type IPersistState = Pick<IApplicationState, 'environment'> &
-  Pick<ISessionState, 'historyHelper'> &
-  Pick<ISessionState, 'token'> &
-  Pick<ISessionState, 'username'>
-
-function createStore(history: History) {
+function createStore(history: History): Store<IState> {
   let composeEnhancers: any = compose
   const sagaMiddleware = createSagaMiddleware()
   const middleware = [sagaMiddleware, routerMiddleware(history)]
@@ -27,31 +26,15 @@ function createStore(history: History) {
     composeEnhancers = __REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   }
 
-  const transforms = [
-    createFilter<IState, IPersistState>('application', ['environment']),
-    createFilter<IState, IPersistState>('session', ['token', 'username', 'historyHelper'])
-  ]
-
-  const persistConfig: PersistConfig = {
-    key: 'root',
-    storage,
-    transforms: [...transforms]
-  }
-
-  const persistedReducer = persistCombineReducers<IState>(persistConfig, {
+  const rootReducer = combineReducers({
     ...reducers,
     router: routerReducer
   })
-
   const enchancers = composeEnhancers(applyMiddleware(...middleware))
-  const createdStore = _createStore(persistedReducer, enchancers) as Store<IState>
+  const createdStore = _createStore(rootReducer, enchancers)
+
   sagaMiddleware.run(mainSaga)
   return createdStore
 }
 
-function createPersistor(createdStore: Store<IState>) {
-  return persistStore(createdStore)
-}
-
-export const store = createStore(appHistory)
-export const persistor = createPersistor(store)
+export const store = createStore(appHistory) as Store<IState>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
+
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 
 import ApplicationContainer from './containers/ApplicationContainer'
-import { persistor, store } from './createStore'
+import { store } from './createStore'
 import { VERSION } from './utils/constants'
 import { history } from './utils/history'
 import registerServiceWorker from './utils/registerServiceWorker'
@@ -23,11 +23,9 @@ console.log(
 
 render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ConnectedRouter history={history}>
-        <ApplicationContainer />
-      </ConnectedRouter>
-    </PersistGate>
+    <ConnectedRouter history={history}>
+      <ApplicationContainer />
+    </ConnectedRouter>
   </Provider>,
   rootContainer
 )
