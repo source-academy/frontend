b380d30763a96f178943f5a5edd02253273aa380
remo5000 <vigneshshankar@gmail.com>
2018-07-03 12:56:04 +0800

    Rename reference in assessment/index

--- src/components/assessment/index.tsx
+++ src/components/assessment/index.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { NavLink } from 'react-router-dom'
 
-import AssessmentContainer from '../../containers/assessment'
+import AssessmentWorkspaceContainer from '../../containers/assessment/AssessmentWorkspaceContainer'
 import { assessmentCategoryLink, stringParamToInt } from '../../utils/paramParseHelpers'
 import { OwnProps as AssessmentProps } from '../assessment'
 import { AssessmentCategory } from '../assessment/assessmentShape'
@@ -81,7 +81,7 @@ class AssessmentListing extends React.Component<IAssessmentListingProps, {}> {
         assessmentId,
         questionId
       }
-      return <AssessmentContainer {...props} />
+      return <AssessmentWorkspaceContainer {...props} />
     }
   }
 }
