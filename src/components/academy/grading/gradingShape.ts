b02cd87400ba88728f618a53c1b1f1760eb86a7b
remo5000 <vigneshshankar@gmail.com>
2018-07-04 12:48:35 +0800

    Add types for Grading and GradingQuestion

--- src/components/academy/grading/gradingShape.ts
+++ src/components/academy/grading/gradingShape.ts
@@ -1,3 +1,5 @@
+import { IMCQQuestion, IProgrammingQuestion, AssessmentCategory } from '../../assessment/assessmentShape'
+
 /**
  * Information on a Grading, for a particular student submission
  * for a particular assessment. Used for display in the UI.
@@ -13,3 +15,28 @@ export type GradingOverview = {
   studentName: string
   submissionId: number
 }
+
+/**
+ * The information fetched before 
+ * grading a submission.
+ */
+export type Grading = GradingQuestion[]
+
+/**
+ * Encapsulates information regarding grading a 
+ * particular question in a submission.
+ */
+export type GradingQuestion = {
+  question: IAnsweredQuestion, 
+  maximumXP": 0,
+  grade: {
+    xp: 0,
+    comment: "string"
+  }
+}
+
+interface IAnsweredQuestion extends IMCQQuestion, IProgrammingQuestion {
+  solution: number,
+  answer: string | number
+}
+
