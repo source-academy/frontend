5d307c5045603ce1d68bd74b6af9f0914cbef9aa
remo5000 <vigneshshankar@gmail.com>
2018-07-17 16:02:42 +0800

    Add CanvasOutput class

---
+++ src/components/workspace/CanvasOutput.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+
+/**
+ * Takes the output of the rendered graphics (in a canvas tag under <body>) 
+ * and makes it an output for viewing
+ */
+class CanvasOutput extends React.Component<{}, {}> {
+  private $canvas: HTMLCanvasElement | null
+  private $parent: HTMLElement | null
+
+  public componentDidMount() {
+    const source = document.querySelector('.rune-canvas') as HTMLCanvasElement
+    const context = (window as any).RUNE_CONTEXT || '2d'
+    if (context === '2d') {
+      const ctx = this.$canvas!.getContext(context)
+      ctx!.drawImage(source, 0, 0)
+    } else {
+      this.$canvas!.hidden = true
+      this.$parent!.appendChild(source)
+      source.hidden = false
+    }
+  }
+
+  public render() {
+    return (
+      <div ref={r => (this.$parent = r)}
+           className="canvas-container">
+        <canvas width={512} height={512} ref={r => (this.$canvas = r)} />
+      </div>
+    )
+  }
+}
+
+export default CanvasOutput
