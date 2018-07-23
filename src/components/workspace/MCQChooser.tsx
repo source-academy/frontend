a3d5e99b45eb5dc50c167f289f765542f08449a3
remo5000 <vigneshshankar@gmail.com>
2018-07-23 17:59:43 +0800

    Add localState to remove visual lag of choosing
    Before: Option chosen (active momentarily) -> dispatch (button inactive
    due to current state) ->
    component updated with answer (active)
    
    Now: Constructor takes in current option from state -> Option chosen (active momentarily) -> local state updated (active) and dispatch is sent -> any update from the dispatch will cause constructor to set the state (during render) to the new option

--- src/components/workspace/MCQChooser.tsx
+++ src/components/workspace/MCQChooser.tsx
@@ -4,14 +4,25 @@ import * as React from 'react'
 import { IMCQQuestion } from '../assessment/assessmentShape'
 
 export interface IMCQChooserProps {
+  chosenOption: number | null
   mcq: IMCQQuestion
   handleMCQSubmit: (choiceId: number) => void
 }
 
-class MCQChooser extends React.PureComponent<IMCQChooserProps, {}> {
+type State = {
+  mcqOption: number | null
+}
+
+class MCQChooser extends React.PureComponent<IMCQChooserProps, State> {
+  constructor(props: IMCQChooserProps) {
+    super(props)
+    this.state = {
+      mcqOption: props.chosenOption
+    }
+  }
   public render() {
     const options = this.props.mcq.choices.map((choice, i) => (
-      <Button key={i} className="mcq-option col-xs-6" onClick={this.onButtonClickFactory(i)}>
+      <Button key={i} className="mcq-option col-xs-6" active={i === this.state.mcqOption} onClick={this.onButtonClickFactory(i)} >
         <Tooltip content={choice.hint}>
           <Text className="Text"> {choice.content} </Text>
         </Tooltip>
@@ -33,7 +44,21 @@ class MCQChooser extends React.PureComponent<IMCQChooserProps, {}> {
     )
   }
 
-  private onButtonClickFactory = (i: number) => (e: any) => this.props.handleMCQSubmit(i)
+  /**
+   * A function to generate an onClick function that causes
+   * and mcq submission with a given answer id.
+   *
+   * Post-condition: the local state will be updated to store the 
+   * mcq option selected.
+   *
+   * @param i the id of the answer
+   */
+  private onButtonClickFactory = (i: number) => (e: any) => {
+    this.props.handleMCQSubmit(i)
+    this.setState({
+      mcqOption: i
+    })
+  }
 }
 
 export default MCQChooser
