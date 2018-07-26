76c7d7bc99d6d8e8c66f9aa39b40d90d1c8214e9
remo5000 <vigneshshankar@gmail.com>
2018-07-26 16:56:12 +0800

    Add conversion for grading
    And a helper file castBackend.ts to help cast thigs like the Library

---
+++ src/utils/castBackend.ts
@@ -0,0 +1,15 @@
+import { Library } from '../components/assessment/assessmentShape'
+
+export const castLibrary = (lib: any): Library => ({
+  chapter: lib.chapter,
+  external: {
+    name: lib.external.name.toUpperCase() as ExternalLibraryName,
+    symbols: lib.external.symbols
+  },
+  globals: Object.entries(lib.globals as object).map(entry => {
+      try {
+        entry[0] = (window as any).eval(entry[1])
+      } catch (e) {}
+      return entry
+    })
+})
