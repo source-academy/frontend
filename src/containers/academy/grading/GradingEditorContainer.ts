77abac36f0f9181394d9211d25f9278c3f5dbf20
remo5000 <vigneshshankar@gmail.com>
2018-08-02 16:16:19 +0800

    Remove redundant mapStateToProps

--- src/containers/academy/grading/GradingEditorContainer.ts
+++ src/containers/academy/grading/GradingEditorContainer.ts
@@ -1,11 +1,8 @@
-import { connect, MapDispatchToProps, MapStateToProps } from 'react-redux'
+import { connect, MapDispatchToProps } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 
 import { submitGrading } from '../../../actions'
 import GradingEditor, { DispatchProps } from '../../../components/academy/grading/GradingEditor'
-import { IState } from '../../../reducers/states'
-
-const mapStateToProps: MapStateToProps<{}, {}, IState> = state => ({})
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dispatch<any>) =>
   bindActionCreators<DispatchProps>(
@@ -15,4 +12,4 @@ const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dis
     dispatch
   )
 
-export default connect(mapStateToProps, mapDispatchToProps)(GradingEditor)
+export default connect(null, mapDispatchToProps)(GradingEditor)
