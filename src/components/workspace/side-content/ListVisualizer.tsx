9ceeceddb623a148349de21730663dd77771207f
remo5000 <vigneshshankar@gmail.com>
2018-08-06 23:58:06 +0800

    Remove buttons from list visualiser
    This means that one draw funciton creates one output

--- src/components/workspace/side-content/ListVisualizer.tsx
+++ src/components/workspace/side-content/ListVisualizer.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@blueprintjs/core'
 import * as React from 'react'
 
 class ListVisualizer extends React.Component<{}, {}> {
@@ -12,28 +11,8 @@ class ListVisualizer extends React.Component<{}, {}> {
 
   public render() {
     return (
-      <div ref={r => this.$parent = r} className="sa-list-visualizer pt-dark">
-        <div className="row">
-          <div className="pt-button-group col-xs-12">
-            <Button id="next" onClick={this.handleNext}>Next</Button>
-            <Button id="previous" onClick={this.handlePrevious}>Previous</Button>
-            <Button id="clear" onClick={this.handleClear}>Clear</Button>
-          </div>
-        </div>
-      </div>
+      <div ref={r => this.$parent = r} className="sa-list-visualizer pt-dark" />
     )
-  }
-
-  private handleClear() {
-    (window as any).ListVisualizer.clear()
-  }
-
-  private handleNext() {
-    (window as any).ListVisualizer.next()
-  }
-
-  private handlePrevious() {
-    (window as any).ListVisualizer.previous()
   }
 }
 
