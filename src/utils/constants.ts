d0ec6a2a9c2bb66e213e77a639f95dfedf7e79d7
ning <ningyuan.sg@gmail.com>
2018-08-13 13:12:49 +0800

    Make LINKS an enum

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,12 +7,12 @@ export const VERSION = process.env.REACT_APP_VERSION
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 export const USE_BACKEND = process.env.REACT_APP_USE_BACKEND
 
-export const LINKS = {
-  GITHUB_ISSUES: 'https://github.com/source-academy/cadet-frontend/issues',
-  GITHUB_ORG: 'https://github.com/source-academy',
-  LUMINUS: 'https://luminus.nus.edu.sg/modules/8722e9a5-abc5-4160-820d-bf69d8a63c6f',
-  PIAZZA: 'https://piazza.com/nus.edu.sg/fall2018/cs1101s',
-  SOURCE_DOCS: 'https://www.comp.nus.edu.sg/~cs1101s/source/',
-  TECH_SVC: 'mailto:techsvc@comp.nus.edu.sg',
-  TEXTBOOK: 'https://www.comp.nus.edu.sg/~cs1101s/sicp/'
+export enum LINKS {
+  GITHUB_ISSUES = 'https://github.com/source-academy/cadet-frontend/issues',
+  GITHUB_ORG = 'https://github.com/source-academy',
+  LUMINUS = 'https://luminus.nus.edu.sg/modules/8722e9a5-abc5-4160-820d-bf69d8a63c6f',
+  PIAZZA = 'https://piazza.com/nus.edu.sg/fall2018/cs1101s',
+  SOURCE_DOCS = 'https://www.comp.nus.edu.sg/~cs1101s/source/',
+  TECH_SVC = 'mailto:techsvc@comp.nus.edu.sg',
+  TEXTBOOK = 'https://www.comp.nus.edu.sg/~cs1101s/sicp/'
 }
