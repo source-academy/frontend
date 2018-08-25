378677418a3ad5102f6e5c3b3dfefaf1fffb2d5d
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-08-25 08:47:42 +0800

    Improved Table UI and made functionality more intuitive

---
+++ src/components/academy/grading/GradingHistory.tsx
@@ -0,0 +1,43 @@
+import { Icon, Popover, PopoverInteractionKind, Position } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
+
+import * as React from 'react'
+
+import { GradingOverview } from './gradingShape'
+
+type GradingNavLinkProps = {
+  data: GradingOverview
+}
+
+/**
+ * Used to render the marks in the table that displays GradingOverviews.
+ * This is a fully fledged component (not SFC) by specification in
+ * ag-grid.
+ *
+ * See {@link https://www.ag-grid.com/example-react-dynamic}
+ */
+class GradingHistory extends React.Component<GradingNavLinkProps, {}> {
+  constructor(props: GradingNavLinkProps) {
+    super(props)
+  }
+
+  public render() {
+
+    const popoverInfo = () => (
+        <div className="col-xs-12" style={{padding: 10}}>
+          <p>Initial Grade: {this.props.data.initialGrade}</p>
+          <p>Grade Adjustments: {this.props.data.gradeAdjustment}</p>
+          <p>Initial XP: {this.props.data.initialXp}</p>
+          <p>XP Adjustments: {this.props.data.xpAdjustment}</p>
+        </div>
+    )
+
+    return (
+      <Popover content={popoverInfo()} position={Position.LEFT} interactionKind={PopoverInteractionKind.HOVER}>
+        <Icon className="grade-edit-icon" iconSize={16} icon={IconNames.HISTORY} />
+      </Popover>
+    )
+  }
+}
+
+export default GradingHistory
