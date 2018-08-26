19256a0dd7e65cf69cafb07df7da3ed37581a2b1
Rahul Rajesh <rahul.rajesh@hotmail.com>
2018-08-26 10:12:34 +0800

    Modified constant to original value

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ dotenv.config()
 export const IVLE_KEY = process.env.REACT_APP_IVLE_KEY
 export const VERSION = process.env.REACT_APP_VERSION
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
-export const USE_BACKEND = false
+export const USE_BACKEND = process.env.REACT_APP_USE_BACKEND
 
 export enum LINKS {
   GITHUB_ISSUES = 'https://github.com/source-academy/cadet-frontend/issues',
