9d17692c8206f89b815070de5e1cb6493550edd9
remo5000 <vigneshshankar@gmail.com>
2018-09-12 16:53:35 +0800

    Revert ControlBarProps order
    This was causing problems with the order of private properties (some
    tslint thing)
    
    This reverts commit f278e6ec900ef0aadf31b95d25d877e8bd40a791.

--- src/components/workspace/ControlBar.tsx
+++ src/components/workspace/ControlBar.tsx
@@ -14,26 +14,26 @@ import { controlButton } from '../commons'
  *   the current question number is 1-based.
  */
 export type ControlBarProps = {
-  externalLibraryName?: string
-  handleChapterSelect?: (i: IChapter, e: React.ChangeEvent<HTMLSelectElement>) => void
-  handleEditorEval: () => void
-  handleExternalSelect?: (i: IExternal, e: React.ChangeEvent<HTMLSelectElement>) => void
-  handleGenerateLz?: () => void
-  handleInterruptEval: () => void
-  handleReplEval: () => void
-  handleReplOutputClear: () => void
+  questionProgress: [number, number] | null
   hasChapterSelect: boolean
   hasSaveButton: boolean
   hasShareButton: boolean
   hasUnsavedChanges?: boolean
   isRunning: boolean
+  queryString?: string
+  sourceChapter: number
+  externalLibraryName?: string
+  handleChapterSelect?: (i: IChapter, e: React.ChangeEvent<HTMLSelectElement>) => void
+  handleExternalSelect?: (i: IExternal, e: React.ChangeEvent<HTMLSelectElement>) => void
+  handleEditorEval: () => void
+  handleGenerateLz?: () => void
+  handleInterruptEval: () => void
+  handleReplEval: () => void
+  handleReplOutputClear: () => void
   onClickNext?(): any
   onClickPrevious?(): any
   onClickReturn?(): any
   onClickSave?(): any
-  queryString?: string
-  questionProgress: [number, number] | null
-  sourceChapter: number
 }
 
 interface IChapter {
