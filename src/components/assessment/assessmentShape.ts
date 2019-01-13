4b255197c235965d50b5a92b5701e9fc2f275b8a
jiachen <jiachen247@users.noreply.github.com>
2019-01-13 20:50:05 +0800

    Show grading status in assessment overview (#422)
    * initial commit
    
    * made changes from review
    
    * update icons

--- src/components/assessment/assessmentShape.ts
+++ src/components/assessment/assessmentShape.ts
@@ -20,6 +20,7 @@ export interface IAssessmentOverview {
   status: AssessmentStatus
   story: string | null
   xp: number
+  gradingStatus: GradingStatus
 }
 
 export enum AssessmentStatuses {
@@ -30,6 +31,14 @@ export enum AssessmentStatuses {
 }
 
 export type AssessmentStatus = keyof typeof AssessmentStatuses
+
+export enum GradingStatuses {
+  none = 'none',
+  grading = 'grading',
+  graded = 'graded'
+}
+
+export type GradingStatus = keyof typeof GradingStatuses
 
 /*
  * Used when an assessment is being actively attempted/graded.
