2578241a9df28f6e1e7b7592c9bed8295e23e0c2
Ger Hean <gerhean@live.com>
2019-02-10 17:57:00 +0800

    AddedAssignmentSupport

--- src/components/commons/ImportFromFileComponent.tsx
+++ src/components/commons/ImportFromFileComponent.tsx
@@ -5,11 +5,17 @@ import { parseString } from 'xml2js'
 import { updateAssessment, updateAssessmentOverview} from '../../actions/session'
 import { 
 	AssessmentCategories,
-	AssessmentStatuses, 
+	AssessmentStatuses,
+	ExternalLibraryNames, 
 	GradingStatuses,
 	IAssessment, 
-	IAssessmentOverview 
+	IAssessmentOverview,
+	IMCQQuestion,
+  IProgrammingQuestion,
+	Library,
+	MCQChoice 
 } from '../../components/assessment/assessmentShape'
+import { externalLibraries } from '../../reducers/externalLibraries'
 // import { IDispatchProps } from '../../components/assessment'
 
 export interface IDispatchProps {
@@ -52,6 +58,92 @@ const makeAssessmentOverview = (task: any) => {
 	}
 }
 
+const makeAssessment =  (task: any) => {
+	const rawOverview = task.$;
+	return {
+		category: capitalizeFirstLetter(rawOverview.kind) as AssessmentCategories,
+  	id: 7,
+  	longSummary: task.TEXT[0],
+  	missionPDF: "google.com",
+  	questions: makeQuestions(task),
+  	title: rawOverview.title,
+	}
+}
+
+const mockGlobals: Array<[string, any]> = [
+  ['testNumber', 3.141592653589793],
+  ['testString', 'who dat boi'],
+  ['testBooleanTrue', true],
+  ['testBooleanFalse', false],
+  ['testBooleanUndefined', undefined],
+  ['testBooleanNull', null],
+  ['testObject', { a: 1, b: 2 }],
+  ['testArray', [1, 2, 'a', 'b']]
+]
+
+const mockSoundLibrary: Library = {
+  chapter: 1,
+  external: {
+    name: ExternalLibraryNames.SOUND,
+    symbols: externalLibraries.get(ExternalLibraryNames.SOUND)!
+  },
+  globals: mockGlobals
+}
+
+const makeQuestions = (task: any) => {
+	const questions: Array<IProgrammingQuestion | IMCQQuestion> = [];
+	task.PROBLEMS[0].PROBLEM.forEach(
+		(problem: any, curId: number) => {
+			const question = {
+			  comment: null,
+			  content: problem.TEXT[0],
+			  id: curId,
+			  library: mockSoundLibrary,
+			  type: problem.$.type,
+			  grader: {
+			    name: "fake person",
+			    id: 1,
+			  },
+			  gradedAt: '2038-06-18T05:24:26.026Z',
+			  xp: 0,
+			  grade: 0,
+			  maxGrade: problem.$.maxgrade,
+			  maxXp: problem.$.maxxp,
+			}
+			let qn: IProgrammingQuestion | IMCQQuestion;
+			if (question.type === "programming"){
+				qn = {
+					...question,
+					answer: problem.SNIPPET[0].TEMPLATE[0],
+					solutionTemplate: problem.SNIPPET[0].SOLUTION[0],
+				}
+				questions.push(qn);
+			}  
+			if (question.type === "mcq"){
+				const choicess: MCQChoice[] = [];
+				let ans = 0;
+				problem.CHOICE.forEach(
+					(choice: any, i: number) => {
+						choicess.push({
+							content: choice.TEXT[0],
+							hint: null
+						});
+						ans = choice.$.correct === "true" ? i : ans;
+					}
+				)
+				qn = {
+					...question,
+					answer: problem.SNIPPET[0].SOLUTION[0],
+					choices: choicess,
+					solution: ans	
+				}
+				questions.push(qn);
+			}
+		}
+	)
+	return questions;
+}
+
 export class ImportFromFileComponent extends React.Component<any, any>{
 	private fileReader : FileReader;
 	public constructor(props: any) {
@@ -82,6 +174,8 @@ export class ImportFromFileComponent extends React.Component<any, any>{
 	    	console.dir(task);
 	    	const overview: IAssessmentOverview  =  makeAssessmentOverview(task);
 	    	this.props.newAssessmentOverview(overview);
+	    	const assessment: IAssessment = makeAssessment(task);
+	    	this.props.newAssessment(assessment);
 			});
 		}
 	  // You can set content in state and show it in render.
