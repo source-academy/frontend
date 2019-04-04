b0db3cef158a65829eb2f190bd58dadecc16bf7f
zephyr <shejiayu1998@qq.com>
2019-04-04 20:00:46 +0800

    Update snapshot and pass test

--- src/components/workspace/__tests__/Editor.tsx
+++ src/components/workspace/__tests__/Editor.tsx
@@ -4,6 +4,10 @@ import { shallow } from 'enzyme';
 
 import Editor, { IEditorProps } from '../Editor';
 
+const componentDidMountSpy = jest.fn();
+
+jest.spyOn(Editor.prototype, 'componentDidMount').mockImplementation(componentDidMountSpy);
+
 test('Editor renders correctly', () => {
   const props: IEditorProps = {
     editorValue: '',
@@ -13,4 +17,5 @@ test('Editor renders correctly', () => {
   const app = <Editor {...props} />;
   const tree = shallow(app);
   expect(tree.debug()).toMatchSnapshot();
+  expect(componentDidMountSpy).toHaveBeenCalled();
 });
