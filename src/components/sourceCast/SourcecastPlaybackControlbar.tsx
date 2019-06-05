5c339a7d9880b84d8ea5915eea4be4f0a6aa4170
zephyr <shejiayu1998@qq.com>
2019-06-06 03:42:25 +0800

    Add layout for recorder

--- src/components/sourceCast/SourcecastPlaybackControlbar.tsx
+++ src/components/sourceCast/SourcecastPlaybackControlbar.tsx
@@ -5,94 +5,139 @@ import * as React from 'react';
 
 import { controlButton } from '../commons';
 
-class SourcecastPlaybackControlbar extends
-    React.PureComponent<ISourcecastPlaybackControlbarProps, ISourcecastPlaybackControlbarState> {
+class SourcecastPlaybackControlbar extends React.PureComponent<
+  ISourcecastPlaybackControlbarProps,
+  ISourcecastPlaybackControlbarState
+> {
   private audio: React.RefObject<HTMLAudioElement>;
 
   constructor(props: ISourcecastPlaybackControlbarProps) {
     super(props);
     this.audio = React.createRef();
     this.state = {
-      currentTime: 0,
-      currentProgress: 0,
+      currentPlayerTime: 0,
+      currentPlayerProgress: 0,
+      isPlayerMode: true
     };
   }
 
   public render() {
-    const PlayButton = controlButton('Play', IconNames.PLAY, this.handlePlaying);
-    const PauseButton = controlButton('Pause', IconNames.PAUSE, this.handlePausing);
-    const StopButton = controlButton('Stop', IconNames.STOP, this.handleStopping);
+    const PlayerButton = controlButton('Player Mode', IconNames.MUSIC, this.handleSetPlayerMode);
+    const RecorderButton = controlButton(
+      'Recorder Mode',
+      IconNames.MOBILE_VIDEO,
+      this.handleSetRecorderMode
+    );
+    const PlayerPlayButton = controlButton('Play', IconNames.PLAY, this.handlePlayerPlaying);
+    const PlayerPauseButton = controlButton('Pause', IconNames.PAUSE, this.handlePlayerPausing);
+    const PlayerStopButton = controlButton('Stop', IconNames.STOP, this.handlePlayerStopping);
+    const RecorderPlayButton = controlButton('Play', IconNames.PLAY, () => {});
+    const RecorderPauseButton = controlButton('Pause', IconNames.PAUSE, () => {});
+    const RecorderStopButton = controlButton('Stop', IconNames.STOP, () => {});
     return (
       <div>
         <audio
           src="http://www.amclassical.com/mp3/amclassical_beethoven_fur_elise.mp3"
           ref={this.audio}
-          onTimeUpdate={this.updateTime}
+          onTimeUpdate={this.updatePlayerTime}
           // controls={true}
         />
-        <div className="Slider">
-          <Slider
-            min={0}
-            max={1}
-            stepSize={0.001}
-            onChange={this.handleProgressBarChange}
-            value={this.state.currentProgress}
-            labelRenderer={this.renderLabel}
-          />
-        </div>
         <div className="PlayerControl">
-          {PlayButton}
-          {PauseButton}
-          {StopButton}
+          {PlayerButton}
+          {RecorderButton}
         </div>
+        <br />
+        {this.state.isPlayerMode ? (
+          <div>
+            <div className="Slider">
+              <Slider
+                min={0}
+                max={1}
+                stepSize={0.001}
+                onChange={this.handlePlayerProgressBarChange}
+                value={this.state.currentPlayerProgress}
+                labelRenderer={this.renderLabel}
+              />
+            </div>
+            <div className="PlayerControl">
+              {PlayerPlayButton}
+              {PlayerPauseButton}
+              {PlayerStopButton}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div className="Slider">
+              <Slider min={0} max={1} stepSize={0.001} value={1} labelRenderer={this.renderLabel} />
+            </div>
+            <div className="PlayerControl">
+              {RecorderPlayButton}
+              {RecorderPauseButton}
+              {RecorderStopButton}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 
-  private handlePlaying = () => {
+  private handleSetPlayerMode = () => {
+    this.setState({
+      isPlayerMode: true
+    });
+  };
+
+  private handleSetRecorderMode = () => {
+    this.handlePlayerStopping();
+    this.setState({
+      isPlayerMode: false
+    });
+  };
+
+  private handlePlayerPlaying = () => {
     const { handleSetSourcecastPlaybackIsPlaying } = this.props;
     const audio = this.audio.current;
     audio!.play();
     handleSetSourcecastPlaybackIsPlaying(true);
   };
 
-  private handlePausing = () => {
+  private handlePlayerPausing = () => {
     const { handleSetSourcecastPlaybackIsPlaying } = this.props;
     const audio = this.audio.current;
     audio!.pause();
     handleSetSourcecastPlaybackIsPlaying(false);
   };
 
-  private handleStopping = () => {
+  private handlePlayerStopping = () => {
     const { handleSetSourcecastPlaybackIsPlaying } = this.props;
     const audio = this.audio.current;
     audio!.pause();
     audio!.currentTime = 0;
     handleSetSourcecastPlaybackIsPlaying(false);
     this.setState({
-      currentTime: 0,
-      currentProgress: 0,
+      currentPlayerTime: 0,
+      currentPlayerProgress: 0
     });
   };
 
-  private updateTime: React.ReactEventHandler<HTMLAudioElement> = (e) => {
-    const { currentTime } : { currentTime: number } = e.target as HTMLMediaElement;
+  private updatePlayerTime: React.ReactEventHandler<HTMLAudioElement> = e => {
+    const { currentTime }: { currentTime: number } = e.target as HTMLMediaElement;
     this.setState({
-      currentTime,
-      currentProgress: currentTime / 211,
+      currentPlayerTime: currentTime,
+      currentPlayerProgress: currentTime / 211
     });
-  }
+  };
 
-  private handleProgressBarChange = (value: number) => {
+  private handlePlayerProgressBarChange = (value: number) => {
     if (this.audio.current) {
       const currentTime = 211 * value;
       this.audio.current.currentTime = currentTime;
       this.setState({
-        currentTime,
-        currentProgress: value,
+        currentPlayerTime: currentTime,
+        currentPlayerProgress: value
       });
     }
-  }
+  };
 
   private renderLabel = (value: number) => {
     const totalTime = 211 * value;
@@ -101,7 +146,7 @@ class SourcecastPlaybackControlbar extends
     const minString = min < 10 ? '0' + min : min;
     const secString = sec < 10 ? '0' + sec : sec;
     return minString + ':' + secString;
-  }
+  };
 }
 
 export interface ISourcecastPlaybackControlbarProps {
@@ -110,8 +155,9 @@ export interface ISourcecastPlaybackControlbarProps {
 }
 
 export interface ISourcecastPlaybackControlbarState {
-  currentTime: number;
-  currentProgress: number;
+  currentPlayerTime: number;
+  currentPlayerProgress: number;
+  isPlayerMode: boolean;
 }
 
 export default SourcecastPlaybackControlbar;
