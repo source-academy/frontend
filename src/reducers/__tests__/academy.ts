8a316240a2e376db4d90762d0e9497963c3e25bc
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-06-07 17:42:13 +0800

    sessions and academy tests

---
+++ src/reducers/__tests__/academy.ts
@@ -0,0 +1,27 @@
+import { IAction, LOG_OUT, SAVE_CANVAS } from '../../actions/actionTypes';
+import { reducer } from '../academy';
+import { defaultAcademy, IAcademyState } from '../states';
+
+test('LOG_OUT works correctly on default academy', () => {
+    const action: IAction = {
+      type: LOG_OUT,
+      payload: {}
+    };
+    const result: IAcademyState = reducer(defaultAcademy, action);
+  
+    expect(result).toEqual(defaultAcademy);
+});
+
+test('SAVE_CANVAS works correctly on default academy', () => {
+    const action : IAction = {
+        type: SAVE_CANVAS,
+        payload: null
+    };
+    const result : IAcademyState = reducer(defaultAcademy, action);
+
+    expect(result).toEqual({
+        ...defaultAcademy,
+        gameCanvas: null
+    });
+});
+
