9a7e9c6160ecffe4a7a3d8ffbc43cb82a152e13e
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-06-24 13:14:33 +0800

    updated canvas payload

--- src/reducers/__tests__/academy.ts
+++ src/reducers/__tests__/academy.ts
@@ -2,6 +2,13 @@ import { IAction, LOG_OUT, SAVE_CANVAS } from '../../actions/actionTypes';
 import { reducer } from '../academy';
 import { defaultAcademy, IAcademyState } from '../states';
 
+function createContext(width: number, height: number): HTMLCanvasElement {
+  let canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  return canvas;
+}
+
 test('LOG_OUT works correctly on default academy', () => {
   const action: IAction = {
     type: LOG_OUT,
@@ -13,14 +20,16 @@ test('LOG_OUT works correctly on default academy', () => {
 });
 
 test('SAVE_CANVAS works correctly on default academy', () => {
+  const payloadCanvas: HTMLCanvasElement = createContext(42, 42);
+
   const action: IAction = {
     type: SAVE_CANVAS,
-    payload: null
+    payload: payloadCanvas
   };
   const result: IAcademyState = reducer(defaultAcademy, action);
 
   expect(result).toEqual({
     ...defaultAcademy,
-    gameCanvas: null
+    gameCanvas: payloadCanvas
   });
 });
