7ca296bcbef963e8f14f0fa3dce891fbccb4fb01
Shuming <geshuming2@gmail.com>
2019-06-24 09:16:00 +0800

    Fix off be 1 line error when prepend is empty

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -29,17 +29,13 @@ function* workspaceSaga(): SagaIterator {
 
   yield takeEvery(actionTypes.EVAL_EDITOR, function*(action) {
     const location = (action as actionTypes.IAction).payload.workspaceLocation;
-    const code: string = yield select(
-      (state: IState) => {
-        const prepend = (state.workspaces[location] as IWorkspaceState).editorPrepend!;
-        const value =  (state.workspaces[location] as IWorkspaceState).editorValue!;
-        const postpend = (state.workspaces[location] as IWorkspaceState).editorPostpend!;
+    const code: string = yield select((state: IState) => {
+      const prepend = (state.workspaces[location] as IWorkspaceState).editorPrepend!;
+      const value = (state.workspaces[location] as IWorkspaceState).editorValue!;
+      const postpend = (state.workspaces[location] as IWorkspaceState).editorPostpend!;
 
-        return prepend + (prepend.length > 0 ? '\n' : '')
-          + value + '\n'
-          + postpend;
-      }
-    );
+      return prepend + (prepend.length > 0 ? '\n' : '') + value + '\n' + postpend;
+    });
     const chapter: number = yield select(
       (state: IState) => (state.workspaces[location] as IWorkspaceState).context.chapter
     );
@@ -135,19 +131,23 @@ function* workspaceSaga(): SagaIterator {
   yield takeEvery(actionTypes.EVAL_TESTCASE, function*(action) {
     const location = (action as actionTypes.IAction).payload.workspaceLocation;
     const index = (action as actionTypes.IAction).payload.testcaseId;
-    const code: string = yield select(
-      (state: IState) => {
-          const prepend = (state.workspaces[location] as IWorkspaceState).editorPrepend!;
-          const value =  (state.workspaces[location] as IWorkspaceState).editorValue!;
-          const postpend = (state.workspaces[location] as IWorkspaceState).editorPostpend!;
-          const testcase = (state.workspaces[location] as IWorkspaceState).editorTestcases[index].program!;
-  
-          return prepend + (prepend.length > 0 ? '\n' : '')
-            + value + '\n'
-            + postpend + (postpend.length > 0 ? '\n' : '')
-            + testcase;
-      }
-    );
+    const code: string = yield select((state: IState) => {
+      const prepend = (state.workspaces[location] as IWorkspaceState).editorPrepend!;
+      const value = (state.workspaces[location] as IWorkspaceState).editorValue!;
+      const postpend = (state.workspaces[location] as IWorkspaceState).editorPostpend!;
+      const testcase = (state.workspaces[location] as IWorkspaceState).editorTestcases[index]
+        .program!;
+
+      return (
+        prepend +
+        (prepend.length > 0 ? '\n' : '') +
+        value +
+        '\n' +
+        postpend +
+        (postpend.length > 0 ? '\n' : '') +
+        testcase
+      );
+    });
     const chapter: number = yield select(
       (state: IState) => (state.workspaces[location] as IWorkspaceState).context.chapter
     );
