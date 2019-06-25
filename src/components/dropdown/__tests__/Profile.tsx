093e4ae49b25fdd4d3fb5a5be0f04392111e2ab9
Cheong Siu Hong <cheongsiuhong@gmail.com>
2019-06-25 19:50:07 +0800

    new tests for profile dropdown

--- src/components/dropdown/__tests__/Profile.tsx
+++ src/components/dropdown/__tests__/Profile.tsx
@@ -6,7 +6,7 @@ import Profile from '../Profile';
 
 import { Role } from '../../../reducers/states';
 
-test('Profile renders "no XP, no Grade" correctly', () => {
+test('Profile renders "no XP, no Grade and Staff role" correctly', () => {
   const props = {
     grade: 0,
     maxGrade: 99,
@@ -14,6 +14,51 @@ test('Profile renders "no XP, no Grade" correctly', () => {
     name: 'yeet',
     role: Role.Staff,
     xp: 0,
+    isOpen: true,
+    onClose: () => {}
+  };
+  const tree = shallow(<Profile {...props} />);
+  expect(tree.debug()).toMatchSnapshot();
+});
+
+test('Profile renders "no XP, no Grade and Student role" correctly', () => {
+  const props = {
+    grade: 0,
+    maxGrade: 99,
+    maxXp: 99,
+    name: 'yeeet',
+    role: Role.Student,
+    xp: 0,
+    isOpen: true,
+    onClose: () => {}
+  };
+  const tree = shallow(<Profile {...props} />);
+  expect(tree.debug()).toMatchSnapshot();
+});
+
+test('Profile renders with XP, Grade and Student role" correctly', () => {
+  const props = {
+    grade: 33,
+    maxGrade: 99,
+    maxXp: 99,
+    name: 'yeeeet',
+    role: Role.Student,
+    xp: 66,
+    isOpen: true,
+    onClose: () => {}
+  };
+  const tree = shallow(<Profile {...props} />);
+  expect(tree.debug()).toMatchSnapshot();
+});
+
+test('Profile renders with XP, Grade and Staff role" correctly', () => {
+  const props = {
+    grade: 33,
+    maxGrade: 99,
+    maxXp: 99,
+    name: 'yeeeeet',
+    role: Role.Staff,
+    xp: 66,
     isOpen: true,
     onClose: () => {}
   };
