7f36c5fd6825dc5d2f21da96ee1ace0f0a64a482
Shuming <geshuming2@gmail.com>
2019-06-29 12:45:47 +0800

    Change from interface to type

--- src/components/notification/notificationShape.ts
+++ src/components/notification/notificationShape.ts
@@ -1,18 +1,18 @@
-export interface AcademyNotification {
-    assessment_id?: number;
-    id: number;
-    question_id?: number;
-    read: boolean;
-    submission_id?: number;
-    type: AcademyNotificationType;
-}
+export type AcademyNotification = {
+  assessment_id?: number;
+  id: number;
+  question_id?: number;
+  read: boolean;
+  submission_id?: number;
+  type: AcademyNotificationType;
+};
 
 enum AcademyNotificationTypes {
-    new = 'new',
-    deadline = 'deadline',
-    autograded = 'autograded',
-    graded = 'graded',
-    submitted = 'submitted'
-  }
+  new = 'new',
+  deadline = 'deadline',
+  autograded = 'autograded',
+  graded = 'graded',
+  submitted = 'submitted'
+}
 
 type AcademyNotificationType = keyof typeof AcademyNotificationTypes;
