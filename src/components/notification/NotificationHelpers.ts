e4ca4c4a24be88382bc61f264222504c67e5c954
Shuming <geshuming2@gmail.com>
2019-07-02 22:09:17 +0800

    Added helper function for notification filtering

---
+++ src/components/notification/NotificationHelpers.ts
@@ -0,0 +1,17 @@
+import { AcademyNotification } from './notificationShape';
+
+type filterOptions = {
+  assessment_id?: number;
+  submission_id?: number;
+};
+
+export function filterNotificationsBy(
+  notifications: AcademyNotification[],
+  opts: filterOptions = {}
+) {
+  return notifications.filter(
+    n =>
+      (opts.assessment_id !== undefined ? n.assessment_id === opts.assessment_id : true) &&
+      (opts.submission_id !== undefined ? n.submission_id === opts.submission_id : true)
+  );
+}
