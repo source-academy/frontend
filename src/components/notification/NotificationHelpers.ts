8c66c66570e2df96a6e4c99c214b46c69060188c
Shuming <geshuming2@gmail.com>
2019-07-20 22:53:18 +0800

    Updated helper functions

--- src/components/notification/NotificationHelpers.ts
+++ src/components/notification/NotificationHelpers.ts
@@ -3,21 +3,35 @@ import { Notification } from './notificationShape';
 
 type filterByTypeOptions = AssessmentCategories | 'Grading';
 
+/**
+ * @param assessmentId the assessment id to filter the notifications with.
+ *
+ * @return A function that takes in an array of notification and filters it.
+ */
 export function filterNotificationsByAssessment(assessmentId: number) {
   return (notifications: Notification[]) =>
-    notifications.filter(n => n.assessment_id === assessmentId);
+    notifications.filter(n => !n.submission_id && n.assessment_id === assessmentId);
 }
 
+/**
+ * @param submissionId the submission id to filter the notifications with.
+ *
+ * @return A function that takes in an array of notification and filters it.
+ */
 export function filterNotificationsBySubmission(submissionId: number) {
   return (notifications: Notification[]) =>
     notifications.filter(n => n.submission_id === submissionId);
 }
 
-/*
-  Notifications will be filtered to either one of the Assessment Category, or the Grading Category.
-
-  Notifications with a submission id belong to Grading.
-*/
+/**
+ * Notifications will be filtered to either one of the Assessment Category, or the Grading Category.
+ *
+ * Notifications with a submission id belongs to the Grading category.
+ *
+ * @param submissionId the submission id to filter the notifications with.
+ *
+ * @return A function that takes in an array of notification and filters it.
+ */
 export function filterNotificationsByType(assessmentType: filterByTypeOptions) {
   return (notifications: Notification[]) =>
     notifications.filter(n => {
