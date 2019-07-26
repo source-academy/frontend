c895beac9d3bba6875c70ef3f93691b594e31825
zephyr <shejiayu1998@qq.com>
2019-07-26 09:42:34 +0800

    Allow downloading of files

--- src/components/academy/materials/SelectCell.tsx
+++ src/components/academy/materials/SelectCell.tsx
@@ -1,6 +1,7 @@
 import { IconNames } from '@blueprintjs/icons';
 import * as React from 'react';
 
+import { BACKEND_URL } from '../../../utils/constants';
 import { controlButton } from '../../commons';
 
 interface ISelectCellProps {
@@ -13,10 +14,20 @@ class SelectCell extends React.Component<ISelectCellProps, {}> {
   }
 
   public render() {
-    return <div>{controlButton('', IconNames.TICK, this.handleSelect)}</div>;
+    return <div>{controlButton('', IconNames.DOWNLOAD, this.handleSelect)}</div>;
   }
 
-  private handleSelect = () => {};
+  private handleSelect = () => {
+    const url = BACKEND_URL + this.props.data.url;
+    const click = document.createEvent('Event');
+    click.initEvent('click', true, true);
+    const link = document.createElement('A') as HTMLAnchorElement;
+    link.href = url;
+    link.download = 'output.wav';
+    link.dispatchEvent(click);
+    link.click();
+    return link;
+  };
 }
 
 export default SelectCell;
