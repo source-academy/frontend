b01512f1471f7edb5c3d6bd804b4601ed40c28dc
Aulud <44989315+Aulud@users.noreply.github.com>
2019-07-30 16:47:12 +0800

    Reduce vertical footprint of Testcase and Autograder Result cards

--- src/components/workspace/side-content/AutograderCard.tsx
+++ src/components/workspace/side-content/AutograderCard.tsx
@@ -1,9 +1,7 @@
 import { Card, Elevation, Pre } from '@blueprintjs/core';
-import { IconNames } from '@blueprintjs/icons';
 import { stringify } from 'js-slang/dist/interop';
 import * as React from 'react';
 import { ITestcase } from '../../assessment/assessmentShape';
-import { controlButton } from '../../commons';
 
 type AutograderCardProps = {
   testcase: ITestcase;
@@ -25,34 +23,21 @@ class AutograderCard extends React.Component<AutograderCardProps, {}> {
 
     return (
       <div className={'AutograderCard' + gradingStatus}>
-        <Card elevation={Elevation.ONE}>
-          <div className="row autograder-controls">
-            {'Testcase ' + (this.props.index + 1)}
-            {controlButton('Test', IconNames.PLAY, () =>
-              this.props.handleTestcaseEval(this.props.index)
-            )}
-          </div>
-          <div className="row autograder-program">
-            <Pre>{this.props.testcase.program}</Pre>
-          </div>
-          <div className="row">
-            <div className="col autograder-expected">
-              Expected Answer:
-              <Pre>{this.props.testcase.answer}</Pre>
-            </div>
-            <div className="col autograder-actual">
-              Actual Answer:
-              {this.props.testcase.result ? (
-                <Pre>{stringify(this.props.testcase.result)}</Pre>
-              ) : (
-                <Pre>No Answer</Pre>
-              )}
-            </div>
-          </div>
+        <Card className="bp3-interactive" elevation={Elevation.ONE} onClick={this.evalSelf}>
+          <Pre className="testcase-program">{this.props.testcase.program}</Pre>
+          <Pre className="testcase-expected">{this.props.testcase.answer}</Pre>
+          <Pre className="testcase-actual">
+            {this.props.testcase.result ? stringify(this.props.testcase.result) : 'No Answer'}
+          </Pre>
         </Card>
       </div>
     );
   }
+
+  // Enable clicks on the card to run the testcase
+  private evalSelf = () => {
+    this.props.handleTestcaseEval(this.props.index);
+  };
 }
 
 export default AutograderCard;
--- src/components/workspace/side-content/ResultCard.tsx
+++ src/components/workspace/side-content/ResultCard.tsx
@@ -9,61 +9,14 @@ type ResultCardProps = {
 
 class ResultCard extends React.Component<ResultCardProps, {}> {
   public render() {
-    const showResult = (result: string) => {
-      switch (result) {
-        case 'pass':
-          return <></>;
-        case 'fail':
-          return (
-            <div className="row autograder-program">
-              <div className="col autograder-expected">
-                Expected Answer:
-                <Pre>{this.props.result.expected!}</Pre>
-              </div>
-              <div className="col autograder-actual">
-                Actual Answer:
-                <Pre>{this.props.result.actual!}</Pre>
-              </div>
-            </div>
-          );
-        case 'error':
-          return (
-            <div className="row autograder-errors">{this.props.result.errors!.map(showError)}</div>
-          );
-        default:
-          return null;
-      }
-    };
-
-    const showStatus = (result: string) => {
-      return <div className="status">{result}</div>;
-    };
-
-    const showError = (error: AutogradingError, index: number) => {
-      switch (error.errorType) {
-        case 'timeout':
-          return (
-            <div key={index} className="autograder-error">
-              <div className="row error-explanation">
-                {'Error: '}
-                <Pre>{'Timeout... Your code ran for too long'}</Pre>
-              </div>
-            </div>
-          );
-        default:
-          return (
-            <div key={index} className="autograder-error">
-              <div className="row">
-                {' '}
-                {'Line: '} <Pre>{error.errorLine}</Pre>
-              </div>
-              <div className="row error-explanation">
-                {'Error: '}
-                <Pre>{'Line ' + error.line + ': ' + error.errorExplanation}</Pre>
-              </div>
-            </div>
-          );
-      }
+    const buildErrorString = (errors: AutogradingError[]) => {
+      return errors
+        .map(error =>
+          error.errorType === 'timeout'
+            ? 'Timeout: Submission exceeded time limit for this test case.'
+            : `Line ${error.line}: Error: ${error.errorExplanation}`
+        )
+        .join('\n');
     };
 
     const isCorrect = this.props.result.resultType === 'pass' ? ' correct' : ' wrong';
@@ -71,11 +24,16 @@ class ResultCard extends React.Component<ResultCardProps, {}> {
     return (
       <div className={'ResultCard' + isCorrect}>
         <Card elevation={Elevation.ONE}>
-          <div className="row">
-            Testcase {this.props.index + 1}
-            {showStatus(this.props.result.resultType)}
+          <div className="result-data">
+            <div className="result-idx">{this.props.index + 1}</div>
+            <div className="result-status">{this.props.result.resultType.toUpperCase()}</div>
           </div>
-          {showResult(this.props.result.resultType)}
+          <Pre className="result-expected">{this.props.result.expected!}</Pre>
+          <Pre className="result-actual">
+            {this.props.result.resultType === 'error'
+              ? buildErrorString(this.props.result.errors!)
+              : this.props.result.actual!}
+          </Pre>
         </Card>
       </div>
     );
