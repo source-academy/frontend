aa69536237c2d04ca97960dc57f1b37d8d22ac4c
Aulud <44989315+Aulud@users.noreply.github.com>
2019-07-30 16:48:55 +0800

    Add column headers to Testcases and Autograder Results when non-empty

--- src/components/workspace/side-content/Autograder.tsx
+++ src/components/workspace/side-content/Autograder.tsx
@@ -1,4 +1,4 @@
-import { Collapse } from '@blueprintjs/core';
+import { Collapse, Icon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import * as React from 'react';
 import { AutogradingResult, ITestcase } from '../../assessment/assessmentShape';
@@ -21,34 +21,79 @@ class Autograder extends React.Component<AutograderProps, State> {
   public constructor(props: AutograderProps) {
     super(props);
 
+    const hasTestcases: boolean = this.props.testcases.length > 0;
     const hasResults: boolean = this.props.autogradingResults.length > 0;
 
     this.state = {
-      showTestcases: !hasResults,
+      showTestcases: hasTestcases,
       showResults: hasResults
     };
   }
 
   public render() {
+    const testcasesHeader = (
+      <div className="testcases-header" key={-1}>
+        <div className="header-fn">
+          <Icon icon={IconNames.CARET_DOWN} />
+          Testcase (click to run)
+        </div>
+        <div className="header-expected">
+          <Icon icon={IconNames.CARET_DOWN} />
+          Expected result
+        </div>
+        <div className="header-actual">
+          <Icon icon={IconNames.CARET_DOWN} />
+          Actual result
+        </div>
+      </div>
+    );
+
+    const resultsHeader = (
+      <div className="results-header" key={-1}>
+        <div className="header-data">
+          <div className="header-sn">
+            <Icon icon={IconNames.CARET_DOWN} />
+            S/N
+          </div>
+          <div className="header-status">
+            <Icon icon={IconNames.CARET_DOWN} />
+            Testcase status
+          </div>
+        </div>
+        <div className="header-expected">
+          <Icon icon={IconNames.CARET_DOWN} />
+          Expected result
+        </div>
+        <div className="header-actual">
+          <Icon icon={IconNames.CARET_DOWN} />
+          Actual result
+        </div>
+      </div>
+    );
+
     const testcases =
       this.props.testcases.length > 0 ? (
-        this.props.testcases.map((testcase, index) => (
-          <AutograderCard
-            key={index}
-            index={index}
-            testcase={testcase}
-            handleTestcaseEval={this.props.handleTestcaseEval}
-          />
-        ))
+        [testcasesHeader].concat(
+          this.props.testcases.map((testcase, index) => (
+            <AutograderCard
+              key={index}
+              index={index}
+              testcase={testcase}
+              handleTestcaseEval={this.props.handleTestcaseEval}
+            />
+          ))
+        )
       ) : (
         <div className="noResults">There are no testcases provided for this mission.</div>
       );
 
     const results =
       this.props.autogradingResults.length > 0 ? (
-        this.props.autogradingResults.map((result, index) => (
-          <ResultCard key={index} index={index} result={result} />
-        ))
+        [resultsHeader].concat(
+          this.props.autogradingResults.map((result, index) => (
+            <ResultCard key={index} index={index} result={result} />
+          ))
+        )
       ) : (
         <div className="noResults">There are no results to show.</div>
       );
