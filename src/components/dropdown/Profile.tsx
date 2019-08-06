eedf05d1426e9b077c366c8f4232ca8aafff50ae
Aulud <44989315+Aulud@users.noreply.github.com>
2019-08-06 22:55:03 +0800

    Edit Profile to render a placeholder when there are 0 closed assessments

--- src/components/dropdown/Profile.tsx
+++ src/components/dropdown/Profile.tsx
@@ -44,151 +44,171 @@ class Profile extends React.Component<ProfileProps> {
     if (!isLoaded) {
       content = <NonIdealState description="Loading..." icon={<Spinner />} />;
     } else {
-      // Compute the user's current total grade and XP from submitted assessments
-      const [currentGrade, currentXp, maxGrade, maxXp] = this.props.assessmentOverviews!.reduce(
-        (acc, item) =>
-          item.status === AssessmentStatuses.submitted
-            ? [acc[0] + item.grade, acc[1] + item.xp, acc[2] + item.maxGrade, acc[3] + item.maxXp]
-            : acc,
-        [0, 0, 0, 0]
-      );
+      // Check if there are any closed assessments, else render a placeholder <div>
+      const numClosed = this.props.assessmentOverviews!.filter(
+        item => item.status === AssessmentStatuses.submitted
+      ).length;
 
-      // Performs boundary checks if denominator is 0 or if it exceeds 1 (100%)
-      const getFrac = (num: number, den: number): number => {
-        return den <= 0 || num / den > 1 ? 1 : num / den;
-      };
-
-      // Given a fraction between 0 and 1 inclusive, returns the corresponding Intent (colour)
-      const parseFrac = (frac: number): Intent => {
-        return frac < 0
-          ? Intent.NONE
-          : frac >= 0.9
-          ? Intent.PRIMARY
-          : frac >= 0.8
-          ? Intent.SUCCESS
-          : frac >= 0.6
-          ? Intent.WARNING
-          : Intent.DANGER;
-      };
-
-      // Given an assessment category, return its icon
-      const renderIcon = (category: AssessmentCategory) => {
-        switch (category) {
-          case AssessmentCategories.Mission:
-            return IconNames.FLAME;
-          case AssessmentCategories.Sidequest:
-            return IconNames.LIGHTBULB;
-          case AssessmentCategories.Path:
-            return IconNames.PREDICTIVE_ANALYSIS;
-          case AssessmentCategories.Contest:
-            return IconNames.COMPARISON;
-          default:
-            // For rendering hidden assessments not visible to the student
-            // e.g. studio participation marks
-            return IconNames.PULSE;
-        }
-      };
-
-      // Build condensed assessment cards from an array of assessments
-      const summaryCallouts = this.props
-        .assessmentOverviews!.filter(item => item.status === AssessmentStatuses.submitted)
-        .map(item => {
-          return (
-            // Make each card navigate the user to the respective assessment
-            <NavLink
-              className="profile-summary-navlink"
-              key={`${item.title}-${item.id}`}
-              target="_blank"
-              to={`/academy/${assessmentCategoryLink(item.category)}/${item.id}/0`}
-              activeClassName="profile-summary-navlink"
-            >
-              <Callout
-                className="profile-summary-callout"
-                key={`${item.title}-${item.id}`}
-                icon={renderIcon(item.category)}
-                title={item.title}
-              >
-                {item.maxGrade <= 0 && item.grade === 0 ? (
-                  ''
-                ) : (
-                  <div className="grade-details">
-                    <div className="title">Grade</div>
-                    <div className="value">
-                      {item.grade} / {item.maxGrade}
-                    </div>
-                    <ProgressBar
-                      animate={false}
-                      className="value-bar"
-                      intent={parseFrac(getFrac(item.grade, item.maxGrade))}
-                      stripes={false}
-                      value={getFrac(item.grade, item.maxGrade)}
-                    />
-                  </div>
-                )}
-                {item.maxXp <= 0 && item.xp === 0 ? (
-                  ''
-                ) : (
-                  <div className="xp-details">
-                    <div className="title">XP</div>
-                    <div className="value">
-                      {item.xp} / {item.maxXp}
-                    </div>
-                    <ProgressBar
-                      animate={false}
-                      className="value-bar"
-                      intent={parseFrac(getFrac(item.xp, item.maxXp))}
-                      stripes={false}
-                      value={getFrac(item.xp, item.maxXp)}
-                    />
-                  </div>
-                )}
-              </Callout>
-            </NavLink>
-          );
-        });
-
-      // Compute the user's maximum total grade and XP from submitted assessments
-      content = (
-        <div className="profile-content">
-          <div className="profile-header">
-            <div className="profile-username">
-              <div className="name">{this.props.name}</div>
-              <div className="role">{this.props.role}</div>
-            </div>
+      const userDetails = (
+        <div className="profile-header">
+          <div className="profile-username">
+            <div className="name">{this.props.name}</div>
+            <div className="role">{this.props.role}</div>
           </div>
-          <div className="profile-progress">
-            <div className="profile-grade">
-              <Spinner
-                className="grade-spinner"
-                intent={parseFrac(getFrac(currentGrade, maxGrade))}
-                size={144}
-                value={getFrac(currentGrade, maxGrade)}
-              />
-              <div className="type">Grade</div>
-              <div className="total-value">
-                {currentGrade} / {maxGrade}
-              </div>
-              <div className="percentage">
-                {(getFrac(currentGrade, maxGrade) * 100).toFixed(2)}%
-              </div>
-            </div>
-            <div className="profile-xp">
-              <Spinner
-                className="xp-spinner"
-                intent={parseFrac(getFrac(currentXp, maxXp))}
-                size={144}
-                value={getFrac(currentXp, maxXp)}
-              />
-              <div className="type">XP</div>
-              <div className="total-value">
-                {currentXp} / {maxXp}
-              </div>
-              <div className="percentage">{(getFrac(currentXp, maxXp) * 100).toFixed(2)}%</div>
-            </div>
-          </div>
-          <div className="profile-callouts">{summaryCallouts}</div>
         </div>
       );
+
+      if (numClosed === 0) {
+        content = (
+          <div className="profile-content">
+            {userDetails}
+            <div className="profile-placeholder">
+              There are no closed assessments to render grade and XP of.
+            </div>
+          </div>
+        );
+      } else {
+        // Compute the user's current total grade and XP from submitted assessments
+        const [currentGrade, currentXp, maxGrade, maxXp] = this.props.assessmentOverviews!.reduce(
+          (acc, item) =>
+            item.status === AssessmentStatuses.submitted
+              ? [acc[0] + item.grade, acc[1] + item.xp, acc[2] + item.maxGrade, acc[3] + item.maxXp]
+              : acc,
+          [0, 0, 0, 0]
+        );
+
+        // Performs boundary checks if denominator is 0 or if it exceeds 1 (100%)
+        const getFrac = (num: number, den: number): number => {
+          return den <= 0 || num / den > 1 ? 1 : num / den;
+        };
+
+        // Given a fraction between 0 and 1 inclusive, returns the corresponding Intent (colour)
+        const parseFrac = (frac: number): Intent => {
+          return frac < 0
+            ? Intent.NONE
+            : frac >= 0.9
+            ? Intent.PRIMARY
+            : frac >= 0.8
+            ? Intent.SUCCESS
+            : frac >= 0.6
+            ? Intent.WARNING
+            : Intent.DANGER;
+        };
+
+        // Given an assessment category, return its icon
+        const renderIcon = (category: AssessmentCategory) => {
+          switch (category) {
+            case AssessmentCategories.Mission:
+              return IconNames.FLAME;
+            case AssessmentCategories.Sidequest:
+              return IconNames.LIGHTBULB;
+            case AssessmentCategories.Path:
+              return IconNames.PREDICTIVE_ANALYSIS;
+            case AssessmentCategories.Contest:
+              return IconNames.COMPARISON;
+            default:
+              // For rendering hidden assessments not visible to the student
+              // e.g. studio participation marks
+              return IconNames.PULSE;
+          }
+        };
+
+        // Build condensed assessment cards from an array of assessments
+        const summaryCallouts = this.props
+          .assessmentOverviews!.filter(item => item.status === AssessmentStatuses.submitted)
+          .map(item => {
+            return (
+              // Make each card navigate the user to the respective assessment
+              <NavLink
+                className="profile-summary-navlink"
+                key={`${item.title}-${item.id}`}
+                target="_blank"
+                to={`/academy/${assessmentCategoryLink(item.category)}/${item.id}/0`}
+                activeClassName="profile-summary-navlink"
+              >
+                <Callout
+                  className="profile-summary-callout"
+                  key={`${item.title}-${item.id}`}
+                  icon={renderIcon(item.category)}
+                  title={item.title}
+                >
+                  {item.maxGrade <= 0 && item.grade === 0 ? (
+                    ''
+                  ) : (
+                    <div className="grade-details">
+                      <div className="title">Grade</div>
+                      <div className="value">
+                        {item.grade} / {item.maxGrade}
+                      </div>
+                      <ProgressBar
+                        animate={false}
+                        className="value-bar"
+                        intent={parseFrac(getFrac(item.grade, item.maxGrade))}
+                        stripes={false}
+                        value={getFrac(item.grade, item.maxGrade)}
+                      />
+                    </div>
+                  )}
+                  {item.maxXp <= 0 && item.xp === 0 ? (
+                    ''
+                  ) : (
+                    <div className="xp-details">
+                      <div className="title">XP</div>
+                      <div className="value">
+                        {item.xp} / {item.maxXp}
+                      </div>
+                      <ProgressBar
+                        animate={false}
+                        className="value-bar"
+                        intent={parseFrac(getFrac(item.xp, item.maxXp))}
+                        stripes={false}
+                        value={getFrac(item.xp, item.maxXp)}
+                      />
+                    </div>
+                  )}
+                </Callout>
+              </NavLink>
+            );
+          });
+
+        // Compute the user's maximum total grade and XP from submitted assessments
+        content = (
+          <div className="profile-content">
+            {userDetails}
+            <div className="profile-progress">
+              <div className="profile-grade">
+                <Spinner
+                  className="grade-spinner"
+                  intent={parseFrac(getFrac(currentGrade, maxGrade))}
+                  size={144}
+                  value={getFrac(currentGrade, maxGrade)}
+                />
+                <div className="type">Grade</div>
+                <div className="total-value">
+                  {currentGrade} / {maxGrade}
+                </div>
+                <div className="percentage">
+                  {(getFrac(currentGrade, maxGrade) * 100).toFixed(2)}%
+                </div>
+              </div>
+              <div className="profile-xp">
+                <Spinner
+                  className="xp-spinner"
+                  intent={parseFrac(getFrac(currentXp, maxXp))}
+                  size={144}
+                  value={getFrac(currentXp, maxXp)}
+                />
+                <div className="type">XP</div>
+                <div className="total-value">
+                  {currentXp} / {maxXp}
+                </div>
+                <div className="percentage">{(getFrac(currentXp, maxXp) * 100).toFixed(2)}%</div>
+              </div>
+            </div>
+            <div className="profile-callouts">{summaryCallouts}</div>
+          </div>
+        );
+      }
     }
 
     return (
