6972679347f9f8bfeb0cb9a36a20cf905a3d7b80
Shuming <43642522+geshuming@users.noreply.github.com>
2019-08-09 11:47:47 +0800

    Refactor: Type Safety for redux actions using typesafe-actions (#814)
    * Update dependencies
    
    * Revamped redux

--- src/reducers/__tests__/playground.ts
+++ src/reducers/__tests__/playground.ts
@@ -1,9 +1,9 @@
-import { CHANGE_QUERY_STRING, IAction } from '../../actions/actionTypes';
+import { CHANGE_QUERY_STRING } from '../../actions/actionTypes';
 import { reducer } from '../playground';
 import { defaultPlayground } from '../states';
 
 test('CHANGE_QUERY_STRING sets queryString correctly ', () => {
-  const action: IAction = {
+  const action = {
     type: CHANGE_QUERY_STRING,
     payload: 'hello world'
   };
