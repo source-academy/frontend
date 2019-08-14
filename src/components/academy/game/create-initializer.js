4d2f80241149ae8ae9c2c5c786e7c5f7d1c22424
jetkan-yk <jetkan.yk@gmail.com>
2019-08-14 19:28:29 +0800

    Game: Implement automated story loader for week 1

--- src/components/academy/game/create-initializer.js
+++ src/components/academy/game/create-initializer.js
@@ -1,78 +1,92 @@
-import { LINKS } from '../../../utils/constants'
-import { history } from '../../../utils/history'
+import {LINKS} from '../../../utils/constants'
+import {history} from '../../../utils/history'
 
 export default function (StoryXMLPlayer, story, username, attemptedAll) {
-  function saveToServer() { }
-
-  function loadFromServer() { }
-
-  var hookHandlers = {
-    startMission: function (number) {
-      const assessmentType = story.split('-')[0] + 's';
-      // return history.push('/academy/' + assessmentType)
-      return history.push('/academy/' + assessmentType + '/' + number);
-    },
-    openTemplate: function (name) {
-      switch (name) {
-          case 'textbook':
-              return window.open(LINKS.TEXTBOOK, '_blank');
-          case 'announcements':
-              return window.open(LINKS.LUMINUS);
-          case 'lesson_plan':
-              return history.push('/academy/missions');
-          case 'students':
-              return history.push(LINKS.PIAZZA);
-          case 'materials':
-              return history.push('/material');
-          case 'IDE':
-              return history.push('/playground');
-          case 'path':
-              return history.push('/academy/paths');
-          case 'sourcecast':
-              return history.push('/sourcecast');
-          default:
-              return window.open(LINKS.LUMINUS);
-      }
-    },
-    pickUpCollectible: function (collectible) {
-      if (typeof Storage !== 'undefined') {
-        localStorage.setItem(collectible, 'collected');
-      }
-    },
-    playSound: function (name) {
-      var sound = new Audio(ASSETS_HOST + 'sounds/' + name + '.mp3');
-      if (sound) {
-        sound.play();
-      }
+    function saveToServer() {
     }
-  };
 
-  function openWristDevice() {
-    window.open(LINKS.LUMINUS);
-  }
+    function loadFromServer() {
+    }
 
-  function startGame(div, canvas, saveData) {
-    saveData = saveData || loadFromServer();
-    StoryXMLPlayer.init(div, canvas, {
-      saveData: saveData,
-      hookHandlers: hookHandlers,
-      saveFunc: saveToServer,
-      wristDeviceFunc: openWristDevice,
-      playerName: username,
-      playerImageCanvas: $('<canvas />'),
-      changeLocationHook: function (newLocation) {
-        if (typeof Storage !== 'undefined') {
-          // Code for localStorage/sessionStorage.
-          localStorage.cs1101s_source_academy_location = newLocation;
+    var hookHandlers = {
+        startMission: function (number) {
+            const assessmentType = story.split('-')[0] + 's';
+            // return history.push('/academy/' + assessmentType)
+            return history.push('/academy/' + assessmentType + '/' + number);
+        },
+        openTemplate: function (name) {
+            switch (name) {
+                case 'textbook':
+                    return window.open(LINKS.TEXTBOOK, '_blank');
+                case 'announcements':
+                    return window.open(LINKS.LUMINUS);
+                case 'lesson_plan':
+                    return history.push('/academy/missions');
+                case 'students':
+                    return history.push(LINKS.PIAZZA);
+                case 'materials':
+                    return history.push('/material');
+                case 'IDE':
+                    return history.push('/playground');
+                case 'path':
+                    return history.push('/academy/paths');
+                case 'sourcecast':
+                    return history.push('/sourcecast');
+                default:
+                    return window.open(LINKS.LUMINUS);
+            }
+        },
+        pickUpCollectible: function (collectible) {
+            if (typeof Storage !== 'undefined') {
+                localStorage.setItem(collectible, 'collected');
+            }
+        },
+        playSound: function (name) {
+            var sound = new Audio(ASSETS_HOST + 'sounds/' + name + '.mp3');
+            if (sound) {
+                sound.play();
+            }
         }
-      }
-    });
-  }
+    };
 
-  function initialize(div, canvas) {
-    startGame(div, canvas);
-    StoryXMLPlayer.loadStory('default', function () { });
-  }
+    function openWristDevice() {
+        window.open(LINKS.LUMINUS);
+    }
 
-  return initialize;
+    function startGame(div, canvas, saveData) {
+        saveData = saveData || loadFromServer();
+        StoryXMLPlayer.init(div, canvas, {
+            saveData: saveData,
+            hookHandlers: hookHandlers,
+            saveFunc: saveToServer,
+            wristDeviceFunc: openWristDevice,
+            playerName: username,
+            playerImageCanvas: $('<canvas />'),
+            changeLocationHook: function (newLocation) {
+                if (typeof Storage !== 'undefined') {
+                    // Code for localStorage/sessionStorage.
+                    localStorage.cs1101s_source_academy_location = newLocation;
+                }
+            }
+        });
+    }
+
+    function initialize(div, canvas) {
+        startGame(div, canvas);
+        const now = new Date();
+        const mock_act_A1_deployDate = new Date("August 14, 2019 20:30:00");
+        const mock_mission_M1_deployDate = new Date("August 14, 2019 21:00:00");
+        if (mock_act_A1_deployDate <= now && now < mock_mission_M1_deployDate) {
+            StoryXMLPlayer.loadStory('act-A1', function () {
+            });
+        } else if (mock_mission_M1_deployDate <= now) {
+            StoryXMLPlayer.loadStory('mission-M1', function () {
+            });
+        } else {
+            StoryXMLPlayer.loadStory('default', function () {
+            });
+        }
+    }
+
+    return initialize;
 };
