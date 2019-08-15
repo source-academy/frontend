91e734f33497c3e56fdf522d2d844a831d0f7236
Jet Kan <jetkan.yk@gmail.com>
2019-08-15 13:49:40 +0700

    Frontend for week 1 (#856)
    * Game: add redirect links of clickable objects to different component of the SA
    
    * Update .gitignore
    
    * Game: Implement automated story loader for week 1
    
    * Game: Allow story parser to skip selected <QUEST> nodes
    
    * Game: Use localStorage to store completed <QUEST>
    
    * Game: Update act 1 and mission 1 file name
    
    * Game: add redirect links of clickable objects to different component of the SA
    
    * Game: temporary set the game to download story xml from localhost
    
    * Revert "Game: temporary set the game to download story xml from localhost"
    
    This reverts commit b86098a8
    
    * Game: Update act 1 and mission 1 auto-deploy time
    
    * Game: Add dev tool to set mock mission-1 deploy date
    
    * Game: Update dev tool to set mock mission-1 deploy date
    
    * Frontend for week 1

--- src/components/academy/game/create-initializer.js
+++ src/components/academy/game/create-initializer.js
@@ -1,82 +1,93 @@
-import { LINKS } from '../../../utils/constants'
-import { history } from '../../../utils/history'
+import {LINKS} from '../../../utils/constants'
+import {history} from '../../../utils/history'
 
 export default function (StoryXMLPlayer, story, username, attemptedAll) {
-  function saveToServer() { }
-
-  function loadFromServer() { }
-
-  var hookHandlers = {
-    startMission: function (number) {
-      console.log('startMission: ' + number)
-      const assessmentType = story.split('-')[0] + 's'
-      return history.push('/academy/' + assessmentType)
-      // TODO: Reimplement redirection to actual assessment rather than the
-      //       listing, after story.xml files have been changed. Currently, the
-      //       story xml number points to the mission number, but the
-      //       assessment id we obtain and therefore organise our assessments
-      //       by refers to the database table ID
-      // return history.push('/academy/' + assessmentType + '/' + number)
-    },
-    openTemplate: function (name) {
-      switch (name) {
-        case 'textbook':
-          return window.open(LINKS.TEXTBOOK, '_blank');
-        case 'announcements':
-          return window.open(LINKS.LUMINUS);
-        case 'lesson_plan':
-          return history.push('/academy/missions');
-        case 'students':
-          return history.push(LINKS.PIAZZA);
-        case 'materials':
-          return window.open(LINKS.LUMINUS);
-        case 'IDE':
-          return history.push('/playground');
-        case 'path':
-          return history.push('/academy/paths');
-        default:
-          return window.open(LINKS.LUMINUS);
-      }
-    },
-    pickUpCollectible: function (collectible) {
-      if (typeof Storage !== 'undefined') {
-        localStorage.setItem(collectible, 'collected');
-      }
-    },
-    playSound: function (name) {
-      var sound = new Audio(ASSETS_HOST + 'sounds/' + name + '.mp3');
-      if (sound) {
-        sound.play();
-      }
+    function saveToServer() {
     }
-  };
 
-  function openWristDevice() {
-    window.open(LINKS.LUMINUS);
-  }
+    function loadFromServer() {
+    }
 
-  function startGame(div, canvas, saveData) {
-    saveData = saveData || loadFromServer();
-    StoryXMLPlayer.init(div, canvas, {
-      saveData: saveData,
-      hookHandlers: hookHandlers,
-      saveFunc: saveToServer,
-      wristDeviceFunc: openWristDevice,
-      playerName: username,
-      playerImageCanvas: $('<canvas />'),
-      changeLocationHook: function (newLocation) {
-        if (typeof Storage !== 'undefined') {
-          // Code for localStorage/sessionStorage.
-          localStorage.cs1101s_source_academy_location = newLocation;
+    var hookHandlers = {
+        startMission: function (number) {
+            const assessmentType = story.split('-')[0] + 's';
+            // return history.push('/academy/' + assessmentType)
+            return history.push('/academy/' + assessmentType + '/' + number);
+        },
+        openTemplate: function (name) {
+            switch (name) {
+                case 'textbook':
+                    return window.open(LINKS.TEXTBOOK, '_blank');
+                case 'announcements':
+                    return window.open(LINKS.LUMINUS);
+                case 'assessments':
+                    return history.push('/academy/missions');
+                case 'forum':
+                    return history.push(LINKS.PIAZZA);
+                case 'materials':
+                    return history.push('/material');
+                case 'IDE':
+                    return history.push('/playground');
+                case 'path':
+                    return history.push('/academy/paths');
+                case 'sourcecast':
+                    return history.push('/sourcecast');
+                case 'about':
+                    return history.push('/contributors');
+                default:
+                    return window.open(LINKS.LUMINUS);
+            }
+        },
+        pickUpCollectible: function (collectible) {
+            if (typeof Storage !== 'undefined') {
+                localStorage.setItem(collectible, 'collected');
+            }
+        },
+        playSound: function (name) {
+            var sound = new Audio(ASSETS_HOST + 'sounds/' + name + '.mp3');
+            if (sound) {
+                sound.play();
+            }
+        },
+        saveCompletedQuest: function (questId) {
+            if (typeof Storage !== 'undefined') {
+                localStorage.setItem(questId, 'completed');
+            }
         }
-      }
-    });
-  }
+    };
 
-  function initialize(div, canvas) {
-    startGame(div, canvas);
-    StoryXMLPlayer.loadStory('default', function () { });
-  }
+    function openWristDevice() {
+        window.open(LINKS.LUMINUS);
+    }
 
-  return initialize;
+    function startGame(div, canvas, saveData) {
+        saveData = saveData || loadFromServer();
+        StoryXMLPlayer.init(div, canvas, {
+            saveData: saveData,
+            hookHandlers: hookHandlers,
+            saveFunc: saveToServer,
+            wristDeviceFunc: openWristDevice,
+            playerName: username,
+            playerImageCanvas: $('<canvas />'),
+            changeLocationHook: function (newLocation) {
+                if (typeof Storage !== 'undefined') {
+                    // Code for localStorage/sessionStorage.
+                    localStorage.cs1101s_source_academy_location = newLocation;
+                }
+            }
+        });
+    }
+
+    function initialize(div, canvas) {
+        startGame(div, canvas);
+        const now = new Date();
+        const mission_1_deployDate = new Date("August 16, 2019 12:00:00");
+        if (mission_1_deployDate <= now) {
+            StoryXMLPlayer.loadStory('mission-1', function () {});
+        } else {
+            StoryXMLPlayer.loadStory('act-1', function () {});
+        }
+    }
+
+    return initialize;
 };
