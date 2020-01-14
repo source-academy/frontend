5d84205f1113d13680e596b70f91ed6b7bd405ba
jiachen247 <jiachen247@gmail.com>
2020-01-14 13:41:12 +0800

    update links for sem 2

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,15 +13,15 @@ export const USE_CHATKIT =
   process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR !== undefined &&
   process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR !== '';
 export const INSTANCE_LOCATOR = process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR;
-export const DEFAULT_SOURCE_CHAPTER = 4;
+export const DEFAULT_SOURCE_CHAPTER = 1;
 
 export enum LINKS {
   GITHUB_ISSUES = 'https://github.com/source-academy/cadet-frontend/issues',
   GITHUB_ORG = 'https://github.com/source-academy',
 
   MODULE_DETAILS = 'https://www.comp.nus.edu.sg/~cs1101s',
-  LUMINUS = 'https://luminus.nus.edu.sg/modules/57290e55-335a-4c09-b904-a795572d6cda',
-  PIAZZA = 'https://piazza.com/class/juazn2axpf35wp',
+  LUMINUS = 'https://luminus.nus.edu.sg/modules/b1340bf4-fc99-4898-be2a-2c20e38c065f/announcements/active',
+  PIAZZA = 'https://luminus.nus.edu.sg/modules/b1340bf4-fc99-4898-be2a-2c20e38c065f/forum',
   SHAREDB_SERVER = 'api2.sourceacademy.nus.edu.sg/',
   SOURCE_DOCS = 'https://sicp.comp.nus.edu.sg/source/',
   SOURCE_DOCS_CHAPTER_2_2 = 'https://sicp.comp.nus.edu.sg/chapters/29',
