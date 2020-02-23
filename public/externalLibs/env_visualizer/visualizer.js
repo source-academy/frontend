09ed457d9cf2780693054d89e1a65579b3efe7ad
simonteozw <simonteozw@gmail.com>
2020-02-23 20:34:21 +0800

    minor changes

--- public/externalLibs/env_visualizer/visualizer.js
+++ public/externalLibs/env_visualizer/visualizer.js
@@ -798,7 +798,7 @@
                    findElementPosition(fnObject, parent) * FRAME_HEIGHT_LINE
                    + OBJECT_FRAME_TOP_SPACING;
     });
-
+    
     // prevents data structures in the same row from overlapping
     let prevObjectsHeights = 0;
     for (d in dataObjects) {
