1324a23db6ae1c057d7b919b7131362a8992f385
Zijian Zhou <zephyroszhou@gmail.com>
2020-03-06 15:33:35 +0800

    update source mode for editor and repl and bump js-slang version (#1014)
    * update source mode for editor and repl and bump js-slang version
    
    * update format

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -312,6 +312,7 @@ class Playground extends React.Component<IPlaygroundProps, PlaygroundState> {
       handleEditorWidthChange: this.props.handleEditorWidthChange,
       handleSideContentHeightChange: this.props.handleSideContentHeightChange,
       replProps: {
+        sourceChapter: this.props.sourceChapter,
         output: this.props.output,
         replValue: this.props.replValue,
         handleBrowseHistoryDown: this.props.handleBrowseHistoryDown,
