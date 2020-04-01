edb6be7278bf6ece84a58e3cf34a0822fa4ecda5
travisryte <thuyaoo200@gmail.com>
2020-04-02 01:42:39 +0800

    To use test bucket

--- src/components/academy/game/backend/hosting.js
+++ src/components/academy/game/backend/hosting.js
@@ -2,10 +2,11 @@ import { isStudent } from './user';
 
 const LIVE_ASSETS_HOST = 'https://s3-ap-southeast-1.amazonaws.com/source-academy-assets/';
 
+const LIVE_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/';
 // placeholder URL
 // const TEST_ASSETS_HOST = 'https://localhost:8080/source-academy-assets/';
-const TEST_ASSETS_HOST = 'https://sa2021assets.blob.core.windows.net/sa2021-assets/';
+const TEST_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/testing-stories/';
 
 // placeholder predicate
 export const ASSETS_HOST = LIVE_ASSETS_HOST;
-export const STORY_HOST = isStudent() ? LIVE_ASSETS_HOST : TEST_ASSETS_HOST;
+export const STORY_HOST = isStudent() ? LIVE_STORIES_HOST : TEST_STORIES_HOST;
