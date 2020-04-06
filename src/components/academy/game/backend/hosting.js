841f602d3193b1fb7b9bb727470914fc05604d79
travisryte <thuyaoo200@gmail.com>
2020-04-06 17:57:26 +0800

    Fixed location of stories

--- src/components/academy/game/backend/hosting.js
+++ src/components/academy/game/backend/hosting.js
@@ -2,7 +2,7 @@ import { isStudent } from './user';
 
 const LIVE_ASSETS_HOST = 'https://s3-ap-southeast-1.amazonaws.com/source-academy-assets/';
 
-export const LIVE_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/';
-export const TEST_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/testing-stories/';
+export const TEST_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/';
+export const LIVE_STORIES_HOST = 'https://s3-ap-southeast-1.amazonaws.com/test-gamedev-bucket/live-stories/';
 
 export const ASSETS_HOST = LIVE_ASSETS_HOST;
