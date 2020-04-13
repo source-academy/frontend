ad05a0176755ff346cee7dc0facff875406e1781
Anubhav <35621759+anubh-v@users.noreply.github.com>
2020-04-13 21:49:14 +0800

    Encode chapter and variant separately in Share URL (#1065)
    * Encode chapter and variant separately in URL
    
    * Use variant in url test

--- src/reducers/states.ts
+++ src/reducers/states.ts
@@ -237,21 +237,6 @@ const variantDisplay: Map<Variant, string> = new Map([
   ['non-det', 'Non-Det'],
   ['concurrent', 'Concurrent']
 ]);
-export const languageURLNames: Map<string, ISourceLanguage> = new Map([
-  ['3_Non_Det', { chapter: 3, variant: 'non-det' }],
-  ['3_Concurrent', { chapter: 3, variant: 'concurrent' }]
-]);
-
-export const urlName = (chapter: number, variant: Variant = 'default'): string => {
-  for (const name of languageURLNames.keys()) {
-    const language: ISourceLanguage = languageURLNames.get(name)!;
-    if (language.chapter === chapter && language.variant === variant) {
-      return name;
-    }
-  }
-
-  return chapter.toString();
-};
 
 export const styliseChapter = (chap: number, variant: Variant = 'default') => {
   let res = `Source \xa7${chap}`;
