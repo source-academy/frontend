bb7ebbc5253908fc0401c41c23d8614d6da83e18
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-04-26 17:29:04 +0800

    Update node scripts to support windows

---
+++ scripts/wasm_postinstall.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const execSync = require('child_process').execSync;
+const os = require('os');
+
+const command = (os.platform() === "win32") ? 'move' : 'mv';
+const directories = '../node_modules/sourceror-driver/node_modules/@btzy/sourceror ../public/externalLibs/sourceror';
+
+try {
+    const output = execSync(command.concat(directories), { encoding: 'utf-8' });  // the default is 'buffer'
+} catch(err) {
+    err.stdout;
+    err.stderr;
+}
