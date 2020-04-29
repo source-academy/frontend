1279ece9fe7c878366d5f11f352ed7e88fee8eb0
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-04-30 01:48:08 +0800

    Add libsourceror dependency

--- scripts/wasm_postinstall.js
+++ scripts/wasm_postinstall.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 const execSync = require('child_process').execSync;
 const os = require('os');
-
 const command = (os.platform() === "win32") ? 'move' : 'mv';
-const directories = ' ../node_modules/sourceror-driver/node_modules/@btzy/libsourceror ../public/externalLibs/sourceror';
+const directories = ' ./node_modules/@btzy/libsourceror ./public/externalLibs/sourceror';
 
 try {
     const output = execSync(command.concat(directories), { encoding: 'utf-8' });  // the default is 'buffer'
