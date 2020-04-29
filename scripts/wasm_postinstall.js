a523dad8d2e0f213575c28a1e4903614cc048bf4
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-04-29 15:55:31 +0800

    Update wasm_postinstall script

--- scripts/wasm_postinstall.js
+++ scripts/wasm_postinstall.js
@@ -3,7 +3,7 @@ const execSync = require('child_process').execSync;
 const os = require('os');
 
 const command = (os.platform() === "win32") ? 'move' : 'mv';
-const directories = '../node_modules/sourceror-driver/node_modules/@btzy/sourceror ../public/externalLibs/sourceror';
+const directories = ' ../node_modules/sourceror-driver/node_modules/@btzy/libsourceror ../public/externalLibs/sourceror';
 
 try {
     const output = execSync(command.concat(directories), { encoding: 'utf-8' });  // the default is 'buffer'
