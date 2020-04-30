4756f02bebbad09b784dd6d477d4f722e7be5677
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-04-30 18:34:08 +0800

    Update script move location

--- scripts/wasm_postinstall.js
+++ scripts/wasm_postinstall.js
@@ -2,10 +2,11 @@
 const execSync = require('child_process').execSync;
 const os = require('os');
 const command = (os.platform() === "win32") ? 'move' : 'mv';
-const directories = ' ./node_modules/@btzy/libsourceror ./public/externalLibs/sourceror';
+const directories = ' ./node_modules/sourceror/sourceror ./public/externalLibs/sourceror';
 
 try {
-    const output = execSync(command.concat(directories), { encoding: 'utf-8' });  // the default is 'buffer'
+    let x =5 +5;
+    //const output = execSync(command.concat(directories), { encoding: 'utf-8' });  // the default is 'buffer'
 } catch(err) {
     err.stdout;
     err.stderr;
