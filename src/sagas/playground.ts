41ee267d3541bcc4c8b04fefc5b9ce1fc1a92d1e
rrtheonlyone <rahul.rajesh.bhat@gmail.com>
2020-05-20 07:48:22 +0800

    Add new saga function that calls microservice API and shortens URL

--- src/sagas/playground.ts
+++ src/sagas/playground.ts
@@ -1,16 +1,32 @@
 import { compressToEncodedURIComponent } from 'lz-string';
 import * as qs from 'query-string';
 import { SagaIterator } from 'redux-saga';
-import { put, select, takeEvery } from 'redux-saga/effects';
+import { call, put, select, takeEvery } from 'redux-saga/effects';
 import * as actions from '../actions';
 import * as actionTypes from '../actions/actionTypes';
 import { ExternalLibraryName } from '../components/assessment/assessmentShape';
 import { defaultEditorValue, IState } from '../reducers/states';
 
 import { Variant } from 'js-slang/dist/types';
+import { URL_SHORTENER } from '../utils/constants';
 
 export default function* playgroundSaga(): SagaIterator {
   yield takeEvery(actionTypes.GENERATE_LZ_STRING, updateQueryString);
+
+  yield takeEvery(actionTypes.SHORTEN_URL, function*(
+    action: ReturnType<typeof actions.shortenURL>
+  ) {
+    const queryString = yield select((state: IState) => state.playground.queryString);
+    const keyword = action.payload;
+
+    const resp = yield call(shortenURLRequest, queryString, keyword);
+    if (!resp) {
+      yield put(actions.updateShortURL('ERROR'));
+      return;
+    }
+
+    yield put(actions.updateShortURL(resp.shorturl));
+  });
 }
 
 function* updateQueryString() {
@@ -40,4 +56,36 @@ function* updateQueryString() {
     exec: execTime
   });
   yield put(actions.changeQueryString(newQueryString));
+}
+
+/**
+ * Gets short url from microservice
+ * @returns {(Response|null)} Response if successful, otherwise null.
+ */
+async function shortenURLRequest(queryString: string, keyword: string): Promise<Response | null> {
+  let url = `${window.location.protocol}//${window.location.hostname}/playground#${queryString}`;
+  if (window.location.port !== '') {
+    url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/playground#${queryString}`;
+  }
+
+  const method = 'GET';
+  const fetchOpts: any = {
+    method
+  };
+  const params = {
+    username: 'username',
+    password: 'password',
+    action: 'shorturl',
+    format: 'json',
+    keyword,
+    url
+  };
+
+  const query = Object.keys(params)
+    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&');
+
+  const resp = await fetch(URL_SHORTENER + '?' + query, fetchOpts);
+  const res = await resp.json();
+  return res;
 }
