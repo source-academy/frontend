300793b8a4e6c8ec0a82bf03a6b4354d83addb29
rrtheonlyone <rahul.rajesh.bhat@gmail.com>
2020-05-20 07:51:24 +0800

    update share button component to have url shortening

--- src/components/workspace/controlBar/shareButton.tsx
+++ src/components/workspace/controlBar/shareButton.tsx
@@ -5,24 +5,31 @@ import * as CopyToClipboard from 'react-copy-to-clipboard';
 
 import { controlButton } from '../../commons';
 
+type ShareButtonState = {
+  keyword: string;
+};
+
 export type ShareButtonProps = {
   handleGenerateLz?: () => void;
+  handleShortenURL: (s: string) => void;
+  handleUpdateShortURL: (s: string) => void;
   queryString?: string;
+  shortURL?: string;
   key: string;
 };
 
-export class ShareButton extends React.PureComponent<ShareButtonProps, {}> {
+export class ShareButton extends React.PureComponent<ShareButtonProps, ShareButtonState> {
   private shareInputElem: React.RefObject<HTMLInputElement>;
 
   constructor(props: ShareButtonProps) {
     super(props);
     this.selectShareInputText = this.selectShareInputText.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.shareInputElem = React.createRef();
+    this.state = { keyword: '' };
   }
 
   public render() {
-    const shareUrl = `${window.location.protocol}//${window.location.hostname}/playground#${this.props.queryString}`;
-
     return (
       <Popover popoverClassName="Popover-share" inheritDarkTheme={false}>
         <Tooltip content="Get shareable link">
@@ -35,16 +42,38 @@ export class ShareButton extends React.PureComponent<ShareButtonProps, {}> {
           </Text>
         ) : (
           <>
-            <input defaultValue={shareUrl} readOnly={true} ref={this.shareInputElem} />
-            <Tooltip content="Copy link to clipboard">
-              <CopyToClipboard text={shareUrl}>
-                {controlButton('', IconNames.DUPLICATE, this.selectShareInputText)}
-              </CopyToClipboard>
-            </Tooltip>
+            {!this.props.shortURL ? (
+              <div>
+                <input placeholder={'Custom URL (optional)'} onChange={this.handleChange} />
+                {controlButton('Get Link', IconNames.SHARE, () =>
+                  this.props.handleShortenURL(this.state.keyword)
+                )}
+              </div>
+            ) : (
+              <div key={this.props.shortURL}>
+                <input
+                  defaultValue={this.props.shortURL}
+                  readOnly={true}
+                  ref={this.shareInputElem}
+                />
+                <Tooltip content="Copy link to clipboard">
+                  <CopyToClipboard text={this.props.shortURL}>
+                    {controlButton('', IconNames.DUPLICATE, this.selectShareInputText)}
+                  </CopyToClipboard>
+                </Tooltip>
+                {controlButton('Regen Link', IconNames.SHARE, () =>
+                  this.props.handleUpdateShortURL('')
+                )}
+              </div>
+            )}
           </>
         )}
       </Popover>
     );
+  }
+
+  private handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ keyword: event.currentTarget.value });
   }
 
   private selectShareInputText() {
