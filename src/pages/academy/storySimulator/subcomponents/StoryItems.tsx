52a484df1082172cb15b0bef1099585b0662e42a
Tiffany Chong <sciffany@gmail.com>
2020-06-04 19:14:50 +0800

    Enable row selection

--- src/pages/academy/storySimulator/subcomponents/StoryItems.tsx
+++ src/pages/academy/storySimulator/subcomponents/StoryItems.tsx
@@ -9,19 +9,36 @@ type OwnProps = {
 };
 
 function StoryItems({ storyList, includeStory }: OwnProps) {
+  const [gridApi, setGridApi] = React.useState<any>();
+
   if (!storyList.length) {
     return <div className="VerticalStack StoryItems">No Stories Loaded</div>;
   }
 
   const columnDefs = [
-    { headerName: 'Filename', field: 'filename' },
+    { headerName: 'Filename', field: 'filename', checkboxSelection: true },
     { headerName: 'Open Date', field: 'openAt' },
     { headerName: 'Close Date', field: 'closeAt' }
   ];
 
+  const onButtonClick = (_: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedData = selectedNodes!.map((node: any) => node.data);
+    console.log(selectedData);
+  };
+
   return (
     <>
-      <AgGridReact columnDefs={columnDefs} rowData={storyList}></AgGridReact>
+      <div className="ag-theme-alpine" style={{ height: '200px', width: '600px' }}>
+        <button onClick={onButtonClick}>Get selected rows</button>
+        <AgGridReact
+          onGridReady={(params: any) => setGridApi(params.api)}
+          rowSelection={'multiple'}
+          columnDefs={columnDefs}
+          rowData={storyList}
+          onSelectionChanged={onButtonClick}
+        ></AgGridReact>
+      </div>
     </>
   );
 }
