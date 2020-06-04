8a987101bfa6e3ff78f60b9cc7d8fdead6b40a53
Anthony Halim <anthony.halim@live.com>
2020-06-04 16:19:33 +0800

    Provide API to recover last location

--- src/commons/sideContent/SideContentHelper.ts
+++ src/commons/sideContent/SideContentHelper.ts
@@ -1,8 +1,10 @@
-import { DebuggerContext } from '../workspace/WorkspaceTypes';
+import { DebuggerContext, WorkspaceLocation, WorkspaceLocations } from '../workspace/WorkspaceTypes';
 import { sampleTab } from './SideContentSampleTab';
 import { SideContentTab } from './SideContentTypes';
 
 const potentialTabs: SideContentTab[] = [sampleTab];
+
+let lastWorkspaceLocation : WorkspaceLocation = WorkspaceLocations.assessment;
 
 export const getDynamicTabs = (debuggerContext: DebuggerContext): SideContentTab[] => {
   const isEmptyDebuggerContext = Object.keys(debuggerContext).length === 0;
@@ -10,5 +12,13 @@ export const getDynamicTabs = (debuggerContext: DebuggerContext): SideContentTab
     return [] as SideContentTab[];
   }
 
+  if (debuggerContext.workspaceLocation) {
+    lastWorkspaceLocation = debuggerContext.workspaceLocation;
+  }
+
   return potentialTabs.filter(tab => tab.toSpawn(debuggerContext));
+};
+
+export const getLastWorkspaceLocation = () => {
+  return lastWorkspaceLocation;
 };
--- src/commons/sideContent/SideContentSampleTab.tsx
+++ src/commons/sideContent/SideContentSampleTab.tsx
@@ -1,16 +1,30 @@
 import { Classes } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 
+import { OverallState } from '../application/ApplicationTypes';
 import { DebuggerContext } from '../workspace/WorkspaceTypes';
+import { getLastWorkspaceLocation } from './SideContentHelper';
 import { SideContentTab } from './SideContentTypes';
 
 const SideContentSampleTab = (props: any) => {
+
+  const workspaceLocation = getLastWorkspaceLocation();
+  const debuggerContext = useSelector((state: OverallState) =>
+    state.workspaces[workspaceLocation].debuggerContext);
+
+  let replOutput = 'Your output is ';
+  if (Object.keys(debuggerContext).length === 0) {
+    replOutput = '';
+  } else {
+    replOutput = replOutput.concat(debuggerContext.result.value);
+  }
+
   return (
     <div>
       <p id="env-visualizer-default-text" className={Classes.RUNNING_TEXT}>
-        The environmental visualizer generates the environmental model diagram based on breakpoints
-        set in the editor.
+        {replOutput}
       </p>
     </div>
   );
