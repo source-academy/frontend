fdbc82ba8c266d050e383d97ca6cdcf4145243ca
angelsl <angelsl@in04.sg>
2020-06-08 15:49:38 +0800

    Attempt to fix Travis builds

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   webpack: {
     configure: webpackConfig => {
+      // disable source maps to speed up CI build
+      // note: if we ever start deploying from CI, this must be undone in
+      // deployment builds
+      if (process.env.CI) {
+        webpackConfig.devtool = false;
+        webpackConfig.optimization.minimizer.find(
+          p => p.constructor.name === 'TerserPlugin'
+        ).options.sourceMap = false;
+      }
+
+      // workaround .mjs files by Acorn
       webpackConfig.module.rules.push({
         test: /\.mjs$/,
         include: /node_modules/,
