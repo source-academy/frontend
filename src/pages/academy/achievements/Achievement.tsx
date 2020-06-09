4df2fd50709d0fbe43fef66c05a276cdc9dc50ce
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-06-09 14:12:47 +0800

    Create Rough Mockup

--- src/pages/academy/achievements/Achievement.tsx
+++ src/pages/academy/achievements/Achievement.tsx
@@ -1,13 +1,65 @@
 import React from 'react';
 
+import { Icon } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
+
 export type DispatchProps = {};
 
 export type StateProps = {};
 
 class Achievement extends React.Component {
   public render() {
-    return <div></div>;
+    return  (
+    <div className='Achievements'>
+        <div className='achievement-main'>
+           <div className='icons'> 
+            <div> </div> 
+                <AchievementCategory 
+                    icon={IconNames.GLOBE}
+                    category={'ALL'}
+                    missions={22}
+                />
+                                <AchievementCategory 
+                    icon={IconNames.LOCATE}
+                    category={'ACTIVE'}
+                    missions={15}
+                />
+                                <AchievementCategory 
+                    icon={IconNames.ENDORSED}
+                    category={'COMPLETED'}
+                    missions={7}
+                />
+                
+            </div> 
+        </div>
+    </div> 
+    );
   }
+}
+
+type AchievementCategoryProps = {
+    icon: any; 
+    category: string;
+    missions: number;
+};
+
+class AchievementCategory extends React.Component<AchievementCategoryProps> {
+    public constructor(props: AchievementCategoryProps) {
+        super(props);
+      }
+
+    public render() {
+        const { icon, category, missions } = this.props;
+        return (
+            <div>
+                <div> 
+                    <Icon color={'#ffffff'} iconSize={44} icon={icon} /> 
+                    <br />
+                    {category} ({missions})
+                </div> 
+            </div>
+        );
+    }
 }
 
 export default Achievement;
