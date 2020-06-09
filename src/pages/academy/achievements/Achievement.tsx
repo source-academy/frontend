e88cbaa254e415622afd5fdf8c7813e2f405c0a7
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-06-09 15:19:47 +0800

    Make Academy Components Functional

--- src/pages/academy/achievements/Achievement.tsx
+++ src/pages/academy/achievements/Achievement.tsx
@@ -7,59 +7,38 @@ export type DispatchProps = {};
 
 export type StateProps = {};
 
-class Achievement extends React.Component {
-  public render() {
-    return  (
-    <div className='Achievements'>
-        <div className='achievement-main'>
-           <div className='icons'> 
-            <div> </div> 
-                <AchievementCategory 
-                    icon={IconNames.GLOBE}
-                    category={'ALL'}
-                    missions={22}
-                />
-                                <AchievementCategory 
-                    icon={IconNames.LOCATE}
-                    category={'ACTIVE'}
-                    missions={15}
-                />
-                                <AchievementCategory 
-                    icon={IconNames.ENDORSED}
-                    category={'COMPLETED'}
-                    missions={7}
-                />
-                
-            </div> 
+function Achievement() {
+  return (
+    <div className="Achievements">
+      <div className="achievement-main">
+        <div className="icons">
+          <div></div>
+          <AchievementCategory icon={IconNames.GLOBE} category={'ALL'} missions={22} />
+          <AchievementCategory icon={IconNames.LOCATE} category={'ACTIVE'} missions={15} />
+          <AchievementCategory icon={IconNames.ENDORSED} category={'COMPLETED'} missions={7} />
         </div>
-    </div> 
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
 type AchievementCategoryProps = {
-    icon: any; 
-    category: string;
-    missions: number;
+  icon: any;
+  category: string;
+  missions: number;
 };
 
-class AchievementCategory extends React.Component<AchievementCategoryProps> {
-    public constructor(props: AchievementCategoryProps) {
-        super(props);
-      }
-
-    public render() {
-        const { icon, category, missions } = this.props;
-        return (
-            <div>
-                <div> 
-                    <Icon color={'#ffffff'} iconSize={44} icon={icon} /> 
-                    <br />
-                    {category} ({missions})
-                </div> 
-            </div>
-        );
-    }
+function AchievementCategory(props: AchievementCategoryProps) {
+  const { icon, category, missions } = props;
+  return (
+    <div>
+      <div>
+        <Icon color={'#ffffff'} iconSize={44} icon={icon} />
+        <br />
+        {category} ({missions})
+      </div>
+    </div>
+  );
 }
 
 export default Achievement;
