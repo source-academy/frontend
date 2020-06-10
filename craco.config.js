626c7bdb77dfdf8c85f8e10137dc828437ad7fd9
angelsl <angelsl@in04.sg>
2020-06-10 21:44:16 +0800

    Add SW_EXCLUDE_REGEXES to add entries to the service worker exclude list

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const cracoConfig = (module.exports = {
   webpack: {
     configure: webpackConfig => {
+      // modify Workbox configuration
+      if (cracoConfig.workbox) {
+        const workboxPlugin = webpackConfig.plugins.find(
+          plugin => plugin.constructor.name === 'GenerateSW'
+        );
+        workboxPlugin.config = cracoConfig.workbox(workboxPlugin.config);
+      }
+
       // workaround .mjs files by Acorn
       webpackConfig.module.rules.push({
         test: /\.mjs$/,
@@ -19,5 +27,13 @@ module.exports = {
       jestConfig.moduleNameMapper['ace-builds'] = '<rootDir>/node_modules/ace-builds';
       return jestConfig;
     }
+  },
+  workbox: workboxConfig => {
+    if (process.env.SW_EXCLUDE_REGEXES) {
+      workboxConfig.navigateFallbackBlacklist.push(
+        ...JSON.parse(process.env.SW_EXCLUDE_REGEXES).map(str => new RegExp(str))
+      );
+    }
+    return workboxConfig;
   }
-};
+});
