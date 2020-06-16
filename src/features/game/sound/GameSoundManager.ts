4b954b65527009303c7de199600f3b93794dc78c
Tiffany Chong <sciffany@gmail.com>
2020-06-17 03:06:48 +0800

    Comment out

--- src/features/game/sound/GameSoundManager.ts
+++ src/features/game/sound/GameSoundManager.ts
@@ -4,22 +4,38 @@ import commonSoundAssets from '../commons/CommonSoundAssets';
 class SoundManager {
   private soundMap: Map<AssetKey, Phaser.Sound.BaseSound>;
   private isMute: boolean;
+  private bgMusic: Phaser.Sound.BaseSound | undefined;
 
   constructor() {
     this.soundMap = new Map<AssetKey, Phaser.Sound.BaseSound>();
     this.isMute = false;
   }
 
-  initialise(scene: Phaser.Scene) {
+  initialise(scene: Phaser.Scene, bgMusicAsset?: string) {
     commonSoundAssets.forEach(asset => {
       this.soundMap.set(asset.key, scene.sound.add(asset.key));
     });
+
+    if (bgMusicAsset) {
+      this.bgMusic = scene.sound.add(bgMusicAsset);
+    }
   }
 
   playSound(assetKey: string) {
     if (this.soundMap.get(assetKey) && !this.isMute) {
       this.soundMap.get(assetKey)!.play();
     }
+  }
+
+  getBgMusic() {
+    if (!this.bgMusic) {
+      throw new Error('Error: cannot find bg music');
+    }
+    return this.bgMusic;
+  }
+
+  playBgMusic() {
+    this.getBgMusic().play({ loop: true });
   }
 }
 
