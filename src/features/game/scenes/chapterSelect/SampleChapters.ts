d9e8879b501b045eeeee058526c9a5ab5641bc91
Tiffany Chong <sciffany@gmail.com>
2020-06-18 21:06:27 +0800

    Return chapters

--- src/features/game/scenes/chapterSelect/SampleChapters.ts
+++ src/features/game/scenes/chapterSelect/SampleChapters.ts
@@ -3,17 +3,17 @@ import { ChapterDetail } from './ChapterSelectTypes';
 export const SampleChapters: ChapterDetail[] = [
   {
     title: 'Spaceship Emergency',
-    previewBgPath: 'http://source-academy-assets.s3.amazonaws.com/testFolder/test.png',
+    previewBgPath: 'https://source.unsplash.com/random/500x700',
     fileName: '../assets/chapter0.txt'
   },
   {
     title: 'Alien Attack',
-    previewBgPath: 'http://source-academy-assets.s3.amazonaws.com/testFolder/test.png',
+    previewBgPath: 'https://source.unsplash.com/random/500x700',
     fileName: '../assets/chapter1.txt'
   },
   {
     title: 'Jedi Master',
-    previewBgPath: 'http://source-academy-assets.s3.amazonaws.com/testFolder/test.png',
+    previewBgPath: 'https://source.unsplash.com/random/500x700',
     fileName: '../assets/chapter2.txt'
   }
 ];
