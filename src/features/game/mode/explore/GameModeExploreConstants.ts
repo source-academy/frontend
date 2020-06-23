7fed0fccc1749865dca94ba937d2814ff62c24f6
Tiffany Chong <sciffany@gmail.com>
2020-06-23 14:00:05 +0800

    Magnifying glass (#1295)

--- src/features/game/mode/explore/GameModeExploreConstants.ts
+++ src/features/game/mode/explore/GameModeExploreConstants.ts
@@ -1,3 +1,3 @@
-export const magnifyingGlass = 'url(../assets/magnifying_glass.ico), pointer';
-export const magnifyingGlassHighlight = 'url(../assets/magnifyingGlassTrigger.ico), pointer';
-export const magnifyingGlassChecked = 'url(../assets/magnifyingGlassTriggered.ico), pointer';
+export const magnifyingGlass = 'url(../assets/magnifying_glass.png), pointer';
+export const magnifyingGlassHighlight = 'url(../assets/magnifying_target.png), pointer';
+export const magnifyingGlassChecked = 'url(../assets/magnifying_check.png), pointer';
