e47c2b961ccd1baa2aad2e5a5cc6c13221d6e347
Anthony Halim <anthony.halim@live.com>
2020-06-23 14:23:51 +0800

    Fix broken link for default magnifying glass

--- src/features/game/mode/explore/GameModeExploreConstants.ts
+++ src/features/game/mode/explore/GameModeExploreConstants.ts
@@ -1,3 +1,3 @@
-export const magnifyingGlass = 'url(../assets/magnifying_glass.png), pointer';
+export const magnifyingGlass = 'url(../assets/magnifying.png), pointer';
 export const magnifyingGlassHighlight = 'url(../assets/magnifying_target.png), pointer';
 export const magnifyingGlassChecked = 'url(../assets/magnifying_check.png), pointer';
