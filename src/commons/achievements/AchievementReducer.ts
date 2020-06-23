f52bcb3f957177a67833d1836402cf26abd45bbf
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-06-23 22:43:33 +0800

    Rewrite AchievementReducer

--- src/commons/achievements/AchievementReducer.ts
+++ src/commons/achievements/AchievementReducer.ts
@@ -2,13 +2,17 @@ import { Reducer } from 'redux';
 
 import { SourceActionType } from '../../commons/utils/ActionsHelper';
 
-import { AchievementState, defaultAchievements } from './AchievementTypes';
+import { AchievementState, defaultAchievements, UPDATE_ACHIEVEMENTS } from './AchievementTypes';
 
-export const PlaygroundReducer: Reducer<AchievementState> = (
+export const AchievementReducer: Reducer<AchievementState> = (
   state = defaultAchievements,
   action: SourceActionType
 ) => {
   switch (action.type) {
+    case UPDATE_ACHIEVEMENTS: 
+      return {
+        achievements: action.payload
+      };
     default:
       return state;
   }
