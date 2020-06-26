6d398edd609c62648a2df954db6ab2dd8fdb830e
angelsl <angelsl@users.noreply.github.com>
2020-06-26 16:18:18 +0800

    Update contributors page (#1227)

--- src/pages/contributors/subcomponents/ContributorsDetails.tsx
+++ src/pages/contributors/subcomponents/ContributorsDetails.tsx
@@ -16,9 +16,82 @@ class ContributorsDetails extends React.Component {
             The <i>Source Academy</i> is designed by and for students of the National University of
             Singapore. Students who completed the CS1101S module come back to coach their juniors as
             "Avengers" or to further develop and improve the Academy. This page includes all
-            developers who contributed to the Source Academy <i>Cadet</i> (2018), a re-development
-            of its precursors, the original Source Academy (2016) and Source Academy 2 (2017).
+            developers who contributed to the Source Academy <i>Knight</i> (2020) and its its
+            precursor, <i>Cadet</i> (2018). Both of these succeeded Source Academy 2 (2017) and
+            ultimately the original Source Academy (2016).
           </p>
+          <div className="leadership">
+            <H5>
+              <strong>
+                <u>2020 Leadership</u>
+              </strong>
+            </H5>
+            <p>
+              Tiffany Chong
+              <br />
+              <strong>(Game)</strong>
+            </p>
+            {dot}
+            <p>
+              Anthony Halim
+              <br />
+              <strong>(Architecture)</strong>
+            </p>
+            {dot}
+            <p>
+              Kan Yip Keng, Jet
+              <br />
+              <strong>(Frontend)</strong>
+            </p>
+            {dot}
+            <p>
+              Low Jun Kai, Sean
+              <br />
+              <strong>(Frontend)</strong>
+            </p>
+            {dot}
+            <p>
+              Daryl Tan
+              <br />
+              <strong>(Source)</strong>
+            </p>
+            {dot}
+            <p className="wider">
+              Tee Hao Wei
+              <br />
+              <strong>(Backend &amp; DevOps)</strong>
+            </p>
+            {dot}
+            <p>
+              Martin Henz
+              <br />
+              <strong>(Coordination)</strong>
+            </p>
+          </div>
+          <div className="hallOfFame">
+            <H5>
+              <strong>
+                <u>Hall of Fame</u>
+              </strong>
+            </H5>
+            <p>
+              <strong>Cadet architect</strong>
+            </p>
+            <p>Evan Sebastian</p>
+            <p>
+              <strong>Cadet core team</strong>
+            </p>
+            <p>
+              Julius Putra Tanu Setiaji {dot} Lee Ning Yuan {dot} Vignesh Shankar {dot} Thomas Tan{' '}
+              {dot} Chen Shaowei
+            </p>
+            <p>
+              <strong>Graphic design</strong>
+            </p>
+            <p>
+              Ng Tse Pei {dot} Joey Yeo {dot} Tan Yu Wei
+            </p>
+          </div>
           <div className="leadership">
             <H5>
               <strong>
@@ -54,36 +127,6 @@ class ContributorsDetails extends React.Component {
               <br />
               <strong>(Tools)</strong>
             </p>
-            {dot}
-            <p>
-              Martin Henz
-              <br />
-              <strong>(Coordination)</strong>
-            </p>
-          </div>
-          <div className="hallOfFame">
-            <H5>
-              <strong>
-                <u>Hall of Fame</u>
-              </strong>
-            </H5>
-            <p>
-              <strong>Cadet architect</strong>
-            </p>
-            <p>Evan Sebastian</p>
-            <p>
-              <strong>Cadet core team</strong>
-            </p>
-            <p>
-              Julius Putra Tanu Setiaji {dot} Lee Ning Yuan {dot} Vignesh Shankar {dot} Thomas Tan{' '}
-              {dot} Chen Shaowei
-            </p>
-            <p>
-              <strong>Graphic design</strong>
-            </p>
-            <p>
-              Ng Tse Pei {dot} Joey Yeo {dot} Tan Yu Wei
-            </p>
           </div>
           <div className="contributors">
             <H5>
@@ -93,7 +136,7 @@ class ContributorsDetails extends React.Component {
             </H5>
             <p>
               Below are all contributors to the{' '}
-              <a href={Links.githubOrg}>Github repositories of the Source Academy</a>.
+              <a href={Links.githubOrg}>GitHub repositories of the Source Academy</a>.
             </p>
           </div>
         </Card>
