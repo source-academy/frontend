d6ba125873a6fa1fd37e0c84abf91ceeb3e3d674
Aulud <44989315+Aulud@users.noreply.github.com>
2020-06-30 01:19:32 +0800

    Add confirmation prompt for changing default Source chapter

--- src/pages/academy/subcomponents/AcademyDefaultChapter.tsx
+++ src/pages/academy/subcomponents/AcademyDefaultChapter.tsx
@@ -1,4 +1,4 @@
-import { Button, Classes, MenuItem } from '@blueprintjs/core';
+import { Button, Classes, Dialog, Intent, MenuItem } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import { ItemRenderer, Select } from '@blueprintjs/select';
 import * as React from 'react';
@@ -10,6 +10,7 @@ import {
   sourceLanguages,
   styliseChapter
 } from '../../../commons/application/ApplicationTypes';
+import controlButton from '../../../commons/ControlButton';
 import { Chapter } from '../../../commons/application/types/ChapterTypes';
 
 export type DefaultChapterProps = DispatchProps & StateProps;
@@ -36,19 +37,38 @@ const AcademyDefaultChapter: React.FunctionComponent<DefaultChapterProps> = prop
     handleFetchChapter();
   }, [handleFetchChapter]);
 
+  const { sourceChapter, sourceVariant } = props;
+
+  const [chosenChapter, setChapterChoice] = React.useState<Chapter>(sourceChapters[0]);
+  const [isDialogOpen, setDialogState] = React.useState<boolean>(false);
+
+  const handleOpenDialog = React.useCallback(
+    (choice: Chapter) => {
+      setDialogState(true);
+      setChapterChoice(choice);
+    },
+    [setDialogState, setChapterChoice]
+  );
+  const handleCloseDialog = React.useCallback(() => {
+    setDialogState(false);
+  }, [setDialogState]);
+  const handleConfirmDialog = React.useCallback(() => {
+    setDialogState(false);
+    handleUpdateChapter(chosenChapter);
+  }, [chosenChapter, setDialogState, handleUpdateChapter]);
+
   const chapterRenderer: ItemRenderer<Chapter> = (lang, { handleClick }) => (
     <MenuItem key={lang.displayName} onClick={handleClick} text={lang.displayName} />
   );
 
-  const ChapterSelectComponent = Select.ofType<Chapter>();
+  const ChapterSelect = Select.ofType<Chapter>();
 
   const chapSelect = (
     currentChap: number,
     currentVariant: Variant,
     handleSelect = (item: Chapter) => {}
   ) => (
-    <ChapterSelectComponent
-      className={Classes.MINIMAL}
+    <ChapterSelect
       items={sourceChapters}
       onItemSelect={handleSelect}
       itemRenderer={chapterRenderer}
@@ -59,10 +79,38 @@ const AcademyDefaultChapter: React.FunctionComponent<DefaultChapterProps> = prop
         text={styliseChapter(currentChap, currentVariant)}
         rightIcon={IconNames.DOUBLE_CARET_VERTICAL}
       />
-    </ChapterSelectComponent>
+    </ChapterSelect>
   );
 
-  return <div>{chapSelect(props.sourceChapter, props.sourceVariant, handleUpdateChapter)}</div>;
+  return (
+    <div>
+      {chapSelect(props.sourceChapter, props.sourceVariant, handleOpenDialog)}
+      <Dialog
+        canEscapeKeyClose={true}
+        canOutsideClickClose={true}
+        className="change-default-chapter-dialog"
+        icon={IconNames.ERROR}
+        isCloseButtonShown={true}
+        isOpen={isDialogOpen}
+        onClose={handleCloseDialog}
+        title="Updating default Source chapter"
+      >
+        <div className={Classes.DIALOG_BODY}>
+          Are you sure you want to update the <b>default Playground Source chapter</b> from{' '}
+          {styliseChapter(sourceChapter, sourceVariant)} to <b>{chosenChapter.displayName}</b>?
+        </div>
+        <div className={Classes.DIALOG_FOOTER}>
+          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+            {controlButton('Cancel', null, handleCloseDialog, { minimal: false })}
+            {controlButton('Confirm', null, handleConfirmDialog, {
+              minimal: false,
+              intent: Intent.DANGER
+            })}
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  );
 };
 
 export default AcademyDefaultChapter;
