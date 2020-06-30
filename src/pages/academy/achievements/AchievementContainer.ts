9d4af8570e752e34340165903bf5aafb6d19fa8d
Jet Kan <jetkan.yk@gmail.com>
2020-06-30 13:24:33 +0800

    Read achievement data from store

--- src/pages/academy/achievements/AchievementContainer.ts
+++ src/pages/academy/achievements/AchievementContainer.ts
@@ -4,12 +4,10 @@ import { bindActionCreators, Dispatch } from 'redux';
 import { OverallState } from '../../../commons/application/ApplicationTypes';
 import Achievement, { DispatchProps, StateProps } from './Achievement';
 import { getAchievements } from 'src/commons/achievements/AchievementActions';
-import { defaultMockAchievements } from 'src/commons/mocks/AchievementMocks';
 import Inferencer from './subcomponents/utils/Inferencer';
 
-// TODO: replace defaultAchievements with fetch database data
 const mapStateToProps: MapStateToProps<StateProps, {}, OverallState> = state => ({
-  inferencer: new Inferencer(defaultMockAchievements)
+  inferencer: new Inferencer(state.achievements.achievements)
 });
 
 const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = (dispatch: Dispatch) =>
