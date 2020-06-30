f126ee02b0e8ebe4e4991c32f18f186a45dd26c7
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-06-30 15:49:54 +0800

    Add Confirm Dialog for AchievementDeleter

--- src/pages/academy/achievementControl/subcomponents/editorTools/editableUtils/AchievementDeleter.tsx
+++ src/pages/academy/achievementControl/subcomponents/editorTools/editableUtils/AchievementDeleter.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import { Button, Dialog } from '@blueprintjs/core';
 
 type AchievementDeleterProps = {
   deleteAchievement: any;
@@ -8,13 +8,32 @@ type AchievementDeleterProps = {
 function AchievementDeleter(props: AchievementDeleterProps) {
   const { deleteAchievement } = props;
 
+  const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
+
   return (
-    <Button
-      icon={'trash'}
-      text={'Delete Achievement'}
-      intent={'danger'}
-      onClick={deleteAchievement}
-    />
+    <>
+      <Button
+        icon={'trash'}
+        text={'Delete Achievement'}
+        intent={'danger'}
+        onClick={() => setDialogOpen(!isDialogOpen)}
+      />
+      <Dialog
+        onClose={() => setDialogOpen(!isDialogOpen)}
+        isOpen={isDialogOpen}
+        className={'task-selector'}
+        title={'Confirm Delete'}
+      >
+        <p> Are you sure you want to delete this achievement?</p>
+
+        <Button
+          text={'Delete Achievement'}
+          className={'editor-button'}
+          intent={'danger'}
+          onClick={deleteAchievement}
+        />
+      </Dialog>
+    </>
   );
 }
 
