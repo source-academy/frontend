eeaa43f8873022a830c8f43983a1ca2fe02206b9
Anthony Halim <anthony.halim@live.com>
2020-07-05 23:31:10 +0800

    Add GameInputManager

---
+++ src/features/game/input/GameInputManager.ts
@@ -0,0 +1,43 @@
+class GameInputManager {
+  private scene: Phaser.Scene | undefined;
+  private keyboardListeners: Phaser.Input.Keyboard.Key[];
+  private eventListeners: Phaser.Input.InputPlugin[];
+
+  constructor() {
+    this.keyboardListeners = [];
+    this.eventListeners = [];
+  }
+
+  public initialise(scene: Phaser.Scene) {
+    this.scene = scene;
+  }
+
+  public getScene() {
+    if (!this.scene) {
+      throw console.error('Undefined scene');
+    }
+    return this.scene;
+  }
+
+  public registerKeyboardListener(
+    key: string | number | Phaser.Input.Keyboard.Key,
+    event: string | symbol,
+    callback: Function
+  ) {
+    const keyObj = this.getScene().input.keyboard.addKey(key);
+    const keyboardListener = keyObj.addListener(event, callback);
+    this.keyboardListeners.concat(keyboardListener);
+  }
+
+  public registerEventListener(event: string | symbol, callback: Function) {
+    const eventListener = this.getScene().input.addListener(event, callback);
+    this.eventListeners.concat(eventListener);
+  }
+
+  public clearListeners() {
+    this.keyboardListeners.forEach(keyboardListener => keyboardListener.removeAllListeners());
+    this.eventListeners.forEach(eventListener => eventListener.removeAllListeners());
+  }
+}
+
+export default GameInputManager;
