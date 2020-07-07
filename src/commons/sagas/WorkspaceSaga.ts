2a537724ad1a88133c6e259e09baf625f81f28bc
Tiffany Chong <sciffany@gmail.com>
2020-07-07 22:39:56 +0800

    Define phaser

--- src/commons/sagas/WorkspaceSaga.ts
+++ src/commons/sagas/WorkspaceSaga.ts
@@ -19,6 +19,7 @@ import { random } from 'lodash';
 import { SagaIterator } from 'redux-saga';
 import { call, delay, put, race, select, take, takeEvery } from 'redux-saga/effects';
 import * as Sourceror from 'sourceror';
+import Phaser from 'phaser';
 
 import { PlaygroundState } from '../../features/playground/PlaygroundTypes';
 import { OverallState, styliseChapter } from '../application/ApplicationTypes';
