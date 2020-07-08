47b950b3947877bfa4fd88560db580898e0fc1b3
Anthony Halim <anthony.halim@live.com>
2020-07-08 21:49:57 +0800

    Replace cursor assets

--- src/features/game/mode/explore/GameModeExploreConstants.ts
+++ src/features/game/mode/explore/GameModeExploreConstants.ts
@@ -1,5 +1,5 @@
 import { Constants } from '../../commons/CommonConstants';
 
 export const magnifyingGlass = `url(${Constants.assetsFolder}/ui/magnifying.png), pointer`;
-export const magnifyingGlassHighlight = `url(${Constants.assetsFolder}/ui/magnifying_target.png), pointer`;
+export const magnifyingGlassHighlight = `url(${Constants.assetsFolder}/ui/magnifying_trigg.png), pointer`;
 export const magnifyingGlassChecked = `url(${Constants.assetsFolder}/ui/magnifying_check.png), pointer`;
