9100230e4b5a1b6a008a8fa8b68af469d9936708
Anthony Halim <anthony.halim@live.com>
2020-07-10 21:25:36 +0800

    Move SFX from /ui/ to /sfx/

--- src/features/game/assets/SoundAssets.ts
+++ src/features/game/assets/SoundAssets.ts
@@ -4,16 +4,22 @@ import { GameSoundType } from '../sound/GameSoundTypes';
 const SoundAssets: AssetMap<SoundAsset> = {
   // SFX
   buttonHoverSound: {
-    key: 'card',
-    path: '/ui/card.mp3',
-    config: { volume: 0.1 },
+    key: 'btn-hover',
+    path: '/sfx/buttonHover.mp3',
+    config: { volume: 0.3 },
+    soundType: GameSoundType.SFX
+  },
+  buttonClickSound: {
+    key: 'btn-click',
+    path: '/sfx/buttonClick.mp3',
+    config: { volume: 0.3 },
     soundType: GameSoundType.SFX
   },
 
   // BGM
   galacticHarmony: {
     key: 'galactic-harmony',
-    path: '/ui/GalacticHarmony.mp3',
+    path: '/bgm/GalacticHarmony.mp3',
     config: { volume: 0.5, loop: true },
     soundType: GameSoundType.BGM
   }
