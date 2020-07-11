4f28cf34465a304e204f944a799162492cb9e1d3
Jet Kan <jetkan.yk@gmail.com>
2020-07-12 01:02:56 +0800

    Organize main card style

--- src/pages/achievements/subcomponents/cards/AchievementCard.tsx
+++ src/pages/achievements/subcomponents/cards/AchievementCard.tsx
@@ -58,9 +58,7 @@ function AchievementCard(props: AchievementCardProps) {
 
       <div className="content">
         <div className="heading">
-          <div className="title">
-            <h3>{title}</h3>
-          </div>
+          <h3>{title.toUpperCase()}</h3>
           <AchievementHints release={release} />
         </div>
 
