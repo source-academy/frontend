2c746df7c7c231bdbaa87ae58fedc78079d8cfe0
Anthony Halim <anthony.halim@live.com>
2020-07-14 19:33:54 +0800

    Add sound assets

--- src/features/game/assets/SoundAssets.ts
+++ src/features/game/assets/SoundAssets.ts
@@ -15,6 +15,48 @@ const SoundAssets: AssetMap<SoundAsset> = {
     config: { volume: 0.5 },
     soundType: GameSoundType.SFX
   },
+  radioButtonClick: {
+    key: 'radio-btn-click',
+    path: '/sfx/radioButtonClick.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  dialogueAdvance: {
+    key: 'dialogue-advance',
+    path: '/sfx/dialogueAdvance.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  popUpEnter: {
+    key: 'pop-up-enter',
+    path: '/sfx/popUpEnter.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  popUpExit: {
+    key: 'pop-up-exit',
+    path: '/sfx/popUpExit.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  menuEnter: {
+    key: 'menu-enter',
+    path: '/sfx/menuEnter.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  menuExit: {
+    key: 'menu-exit',
+    path: '/sfx/menuExit.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
+  modeEnter: {
+    key: 'mode-enter',
+    path: '/sfx/modeEnter.mp3',
+    config: { volume: 0.5 },
+    soundType: GameSoundType.SFX
+  },
 
   // BGM
   galacticHarmony: {
