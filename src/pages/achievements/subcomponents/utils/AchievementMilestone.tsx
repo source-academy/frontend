be3dd63eb3daba401aedc96ec6be8826b5ca65dc
Jet Kan <jetkan.yk@gmail.com>
2020-07-15 11:55:57 +0800

    Remove empty props in milestone

--- src/pages/achievements/subcomponents/utils/AchievementMilestone.tsx
+++ src/pages/achievements/subcomponents/utils/AchievementMilestone.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 type AchievementMilestoneProps = {};
 
 function AchievementMilestone(props: AchievementMilestoneProps) {
-  const {} = props;
-
   return (
     <div className="milestone">
       <h1>U SO GOOD</h1>
