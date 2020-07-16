d4e4126ae464db4c73b2551f6ba3555b0c1693c5
Tiffany Chong <sciffany@gmail.com>
2020-07-17 00:59:14 +0800

    Commit sound

--- src/features/game/sound/GameSoundManager.ts
+++ src/features/game/sound/GameSoundManager.ts
@@ -56,11 +56,10 @@ class GameSoundManager {
   }
 
   public playSound(soundKey: AssetKey) {
-    if (this.scene) {
-      const soundAsset = this.getParentGame().getSoundAsset(soundKey);
-      if (soundAsset) {
-        this.getBaseSoundManager().play(soundAsset.key, soundAsset.config);
-      }
+    const soundAsset = this.getParentGame().getSoundAsset(soundKey);
+    if (soundAsset) {
+      soundAsset.config.volume = this.getBaseSoundManager().volume;
+      this.getBaseSoundManager().play(soundAsset.key, { ...soundAsset.config });
     }
   }
 
