2b68f466c1ea1906fc180f24ab19c026427d9754
Anthony Halim <anthony.halim@live.com>
2020-07-17 19:15:53 +0800

    Finish hover popup for awards hall

--- src/features/game/scenes/awardsHall/AwardsHallConstants.ts
+++ src/features/game/scenes/awardsHall/AwardsHallConstants.ts
@@ -1,11 +1,40 @@
+import { screenCenter } from '../../commons/CommonConstants';
+import { Color } from '../../utils/StyleUtils';
+
 export const AwardsHallConstants = {
   defaultScrollSpeed: 20,
   itemPerCol: 4,
   tileDim: 2048,
   maxAwardsPerCol: 4,
-  awardsXSpacing: 200,
+  awardsXSpacing: 300,
   arrowXOffset: 875,
-  awardYStartPos: 100,
+  awardYStartPos: 300,
+  awardYSpace: screenCenter.y - 40,
+  awardDim: 200,
   hoverWidth: 300,
   hoverHeight: 300
+};
+
+export const awardHoverTitleStyle = {
+  fontFamily: 'Verdana',
+  fontSize: '20px',
+  fill: Color.lightBlue,
+  align: 'left',
+  wordWrap: { width: AwardsHallConstants.hoverWidth - 20 }
+};
+
+export const awardHoverKeyStyle = {
+  fontFamily: 'Verdana',
+  fontSize: '15px',
+  fill: Color.offWhite,
+  align: 'left',
+  wordWrap: { width: AwardsHallConstants.hoverWidth - 20 }
+};
+
+export const awardHoverDescStyle = {
+  fontFamily: 'Verdana',
+  fontSize: '15px',
+  fill: Color.lightBlue,
+  align: 'left',
+  wordWrap: { width: AwardsHallConstants.hoverWidth - 20 }
 };
--- src/features/game/scenes/awardsHall/AwardsHelper.ts
+++ src/features/game/scenes/awardsHall/AwardsHelper.ts
@@ -1,23 +1,41 @@
 import { AwardProperty } from '../../awards/GameAwardsTypes';
 import { HexColor } from '../../utils/StyleUtils';
-import { AwardsHallConstants } from './AwardsHallConstants';
+import {
+  awardHoverDescStyle,
+  awardHoverKeyStyle,
+  awardHoverTitleStyle,
+  AwardsHallConstants
+} from './AwardsHallConstants';
 
 export const createAwardsHoverContainer = (scene: Phaser.Scene, award: AwardProperty) => {
   const hoverContainer = new Phaser.GameObjects.Container(scene, 0, 0);
+
+  const awardTitle = new Phaser.GameObjects.Text(scene, 20, 20, award.title, awardHoverTitleStyle);
+  const awardAssetKey = new Phaser.GameObjects.Text(
+    scene,
+    20,
+    awardTitle.getBounds().bottom + 20,
+    award.assetKey,
+    awardHoverKeyStyle
+  );
+  const awardDesc = new Phaser.GameObjects.Text(
+    scene,
+    20,
+    awardAssetKey.getBounds().bottom + 20,
+    award.description,
+    awardHoverDescStyle
+  );
+
   const hoverTextBg = new Phaser.GameObjects.Rectangle(
     scene,
     0,
     0,
     AwardsHallConstants.hoverWidth,
-    AwardsHallConstants.hoverHeight,
+    awardDesc.getBounds().bottom + 20,
     HexColor.darkBlue
   )
-    .setOrigin(0.0, 0.5)
+    .setOrigin(0.0, 0.0)
     .setAlpha(0.8);
-
-  const awardTitle = new Phaser.GameObjects.Text(scene, 10, 10, award.title, {});
-  const awardAssetKey = new Phaser.GameObjects.Text(scene, 10, 10, award.assetKey, {});
-  const awardDesc = new Phaser.GameObjects.Text(scene, 0, 0, award.description, {});
 
   hoverContainer.add([hoverTextBg, awardTitle, awardAssetKey, awardDesc]);
   hoverContainer.setVisible(false);
