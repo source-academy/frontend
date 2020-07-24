ef439efa9c7b576f171bfb97ffa2848270e60cd7
Jet Kan <jetkan.yk@gmail.com>
2020-07-24 19:43:38 +0800

    Remove date on achievement dashboard

--- src/pages/achievement/dashboard/subcomponents/AchievementOverview.tsx
+++ src/pages/achievement/dashboard/subcomponents/AchievementOverview.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 import AchievementInferencer from './utils/AchievementInferencer';
 import AchievementLevel from './utils/AchievementLevel';
-import { prettifyDate } from './utils/DateHelper';
 
 type AchievementOverviewProps = {
-  name?: string;
-  studio?: string;
+  name: string;
+  studio: string;
   inferencer: AchievementInferencer;
 };
 
@@ -15,15 +14,11 @@ function AchievementOverview(props: AchievementOverviewProps) {
 
   const studentExp = inferencer.getStudentTotalExp();
 
-  const now = new Date();
-  const date = prettifyDate(now);
-
   return (
     <>
       <AchievementLevel studentExp={studentExp} />
       <h3>{name}</h3>
       <h3>{studio}</h3>
-      <h3>{date}</h3>
     </>
   );
 }
