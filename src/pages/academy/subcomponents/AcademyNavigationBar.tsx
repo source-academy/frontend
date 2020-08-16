8b7b135814aa769cc4c9500930cc055f5aec4db4
Aulud <44989315+Aulud@users.noreply.github.com>
2020-08-17 04:13:15 +0700

    Rename 'Other' assessments tab to 'Others' (#1468)

--- src/pages/academy/subcomponents/AcademyNavigationBar.tsx
+++ src/pages/academy/subcomponents/AcademyNavigationBar.tsx
@@ -75,7 +75,7 @@ const AcademyNavigationBar: React.FunctionComponent<OwnProps> = props => (
         className={classNames('NavigationBar__link', Classes.BUTTON, Classes.MINIMAL)}
       >
         <Icon icon={IconNames.MANUAL} />
-        <div className="navbar-button-text hidden-xs hidden-sm">Other</div>
+        <div className="navbar-button-text hidden-xs hidden-sm">Others</div>
       </NavLink>
     </NavbarGroup>
     {props.role === Role.Admin || props.role === Role.Staff ? (
