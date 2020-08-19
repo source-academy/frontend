0ab3824ee590d6ba9ff1c9f40c6035e2d48a0cec
Jet Kan <jetkan.yk@gmail.com>
2020-08-19 18:36:24 +0800

    Change forceRender to forceUpdate

--- src/pages/achievement/control/AchievementControl.tsx
+++ src/pages/achievement/control/AchievementControl.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Prompt } from 'react-router';
 
 import AchievementEditor from '../../../commons/achievement/control/AchievementEditor';
@@ -45,8 +45,7 @@ function AchievementControl(props: DispatchProps & StateProps) {
   /**
    * Monitors changes that are awaiting publish
    */
-  const publishState = useState<boolean>(false);
-  const [awaitPublish, setAwaitPublish] = publishState;
+  const [awaitPublish, setAwaitPublish] = useState<boolean>(false);
   const handlePublish = () => {
     // NOTE: Update goals first because goals must exist before their ID can be specified in achievements
     handleBulkUpdateGoals(goals);
@@ -55,15 +54,23 @@ function AchievementControl(props: DispatchProps & StateProps) {
   };
   const requestPublish = () => {
     setAwaitPublish(true);
-    forceRender();
+    forceUpdate();
   };
 
   /**
-   * Allows editor components to trigger a page re-render so that the AchievementPreview
-   * displays the latest local changes
+   * Allows editor components to trigger a page re-render whenever the inferencer is modified
+   * so that the AchievementPreview displays the latest local changes
+   *
+   * NOTE: Although the inferencer is passed to the value prop of AchievementContext.Provider,
+   * changes to the inferencer does not trigger a re-render in all AchievementContext.Consumer
+   * as expected because Context uses reference identity to determine when to re-render. When
+   * the editor components update the inferencer by calling inferencer.modifyAchievement(...)
+   * or inferencer.modifyGoalDefinition(...), the Context does not register the changes hence
+   * a forceUpdate() hook is needed.
+   *
+   * See: https://reactjs.org/docs/context.html#caveats
    */
-  const [render, setRender] = useState<boolean>();
-  const forceRender = () => setRender(!render);
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
 
   return (
     <AchievementContext.Provider value={inferencer}>
