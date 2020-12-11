e2ced67dfc4c25453faf7e4408743d7e47c6ceec
angelsl <angelsl@in04.sg>
2020-12-11 21:24:11 +0800

    Fix Coveralls

--- scripts/test-coveralls.sh
+++ scripts/test-coveralls.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-TEMPFILE="$(mktemp)"
-
 ./scripts/coverage-fix.sh do && \
   craco test --coverage --coverageReporters=text-lcov \
-    --collectCoverageFrom='!**/src/features/game/**' > "$TEMPFILE"
-coveralls < "$TEMPFILE" || echo "Warning: Coveralls upload failed"
+    --collectCoverageFrom='!**/src/features/game/**' > lcov.info
