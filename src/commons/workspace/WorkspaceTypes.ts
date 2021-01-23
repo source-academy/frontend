d2ce37009636554511262184d51d932d163b3acd
Kevin Foong <kevin9foong@gmail.com>
2021-01-23 14:30:54 +0800

    Add API call to fetch userId specific contest entries

--- src/commons/workspace/WorkspaceTypes.ts
+++ src/commons/workspace/WorkspaceTypes.ts
@@ -4,7 +4,7 @@ import { SourcecastWorkspaceState } from '../../features/sourceRecorder/sourceca
 import { SourcereelWorkspaceState } from '../../features/sourceRecorder/sourcereel/SourcereelTypes';
 import { InterpreterOutput } from '../application/ApplicationTypes';
 import { ExternalLibraryName } from '../application/types/ExternalTypes';
-import { AutogradingResult, Testcase } from '../assessment/AssessmentTypes';
+import { AutogradingResult, ContestEntry, Testcase } from '../assessment/AssessmentTypes';
 import { HighlightedLines, Position } from '../editor/EditorTypes';
 import { SideContentType } from '../sideContent/SideContentTypes';
 
@@ -104,6 +104,7 @@ export type WorkspaceState = {
   readonly stepLimit: number;
   readonly globals: Array<[string, any]>;
   readonly debuggerContext: DebuggerContext;
+  readonly contestEntries?: ContestEntry[];
 };
 
 type ReplHistory = {
