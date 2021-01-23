02049e7d64677774fd5f6cf6879f6d50835c2868
Lee Zheng Han <e0544595@u.nus.edu>
2021-01-23 11:46:14 +0800

    Add selector functions for colors of a point

--- public/externalLibs/graphics/webGLcurve.js
+++ public/externalLibs/graphics/webGLcurve.js
@@ -1,16 +1,3 @@
-// class Curve{
-//   curve = undefined
-//   toReplString = () => '<CURVE>'
-
-//   getCurve = function() {
-//     return this.curve
-//   }
-  
-//   setCurve = function(curve) {
-//     this.curve = curve
-//   }
-// }
-
 class Point{
   x = undefined
   y = undefined
@@ -46,15 +33,6 @@ class Point{
   }
 
 }
-
-// function makeColorfulCurve(curveColor, curveFunction) {
-//   var wrapper = new Curve();
-//   wrapper.setColor(curveColor);
-//   wrapper.setCurve(curveFunction);
-//   return wrapper;
-// }
-
-// var color_curve = makeColorfulCurve;
 
 function generateCurve(scaleMode, drawMode, numPoints, func, isFullView) {
   curveColor = t => func(t).getColor()
@@ -286,7 +264,7 @@ function make_color_point(x, y, r, g, b){
 }
 
 /**
- * retrieves the x-coordinate a given Point
+ * retrieves the x-coordinate of a given Point
  * @param {Point} p - given point
  * @returns {Number} x-coordinate of the Point
  */
@@ -295,7 +273,7 @@ function x_of(pt) {
 }
 
 /**
- * retrieves the y-coordinate a given Point
+ * retrieves the y-coordinate of a given Point
  * @param {Point} p - given point
  * @returns {Number} y-coordinate of the Point
  */
@@ -304,7 +282,7 @@ function y_of(pt) {
 }
 
 /**
- * retrieves the z-coordinate a given Point
+ * retrieves the z-coordinate of a given Point
  * @param {Point} p - given point
  * @returns {Number} z-coordinate of the Point
  */
@@ -312,181 +290,29 @@ function z_of(pt) {
   return pt.getZ();
 }
 
-// /*-----------------------Color functions----------------------*/
-// function hexToColor(hex) {
-//   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-//   return [
-//     parseInt(result[1], 16) / 255,
-//     parseInt(result[2], 16) / 255,
-//     parseInt(result[3], 16) / 255,
-//     1
-//   ]
-// }
+/**
+ * retrieves the red component of a given Point
+ * @param {Point} p - given point
+ * @returns {Number} Red component of the Point
+ */
+function r_of(pt) {
+  return pt.getColor()[0];
+}
 
-// /**
-//  * adds color to curve by specifying 
-//  * the red, green, blue (RGB) value, ranging from 0.0 to 1.0.
-//  * RGB is additive: if all values are 1, the color is white,
-//  * and if all values are 0, the color is black.
-//  * @param {Curve} Curve - the curve to add color to
-//  * @param {number} r - red value (0.0-1.0)
-//  * @param {number} g - green value (0.0-1.0)
-//  * @param {number} b - blue value (0.0-1.0)
-//  * @returns {Curve} the colored Curve
-//  */
-// function color(curve, r, g, b) {
-//   // throwIfNotRune('color', rune)
-//   var wrapper = new Curve()
-//   wrapper.setCurve(curve instanceof Curve ? curve.getCurve() : curve)
-//   var color = [r, g, b, 1]
-//   wrapper.setColor(t => color)
-//   return wrapper
-// }
+/**
+ * retrieves the red component of a given Point
+ * @param {Point} p - given point
+ * @returns {Number} Green component of the Point
+ */
+function g_of(pt) {
+  return pt.getColor()[1];
+}
 
-// function addColorFromHex(curve, hex) {
-//   // throwIfNotRune('addColorFromHex', rune)
-//   var wrapper = new Curve()
-//   wrapper.setCurve(curve instanceof Curve ? curve.getCurve() : curve)
-//   wrapper.setColor(t => hexToColor(hex))
-//   return wrapper
-// }
-
-// /**
-//  * Gives random color to the given curve.
-//  * The color is chosen randomly from the following nine 
-//  * colors: red, pink, purple, indigo, blue, green, yellow, orange, brown
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function random_color(curve) {
-//   // throwIfNotRune('random_color', rune)
-//   var wrapper = new Curve()
-//   wrapper.setCurve(curve instanceof Curve ? curve.getCurve() : curve)
-//   var randomColor = hexToColor(colorPalette[Math.floor(Math.random() * colorPalette.length)])
-//   wrapper.setColor(t => randomColor)
-//   return wrapper
-// }
-
-// // black and white not included because they are boring colors
-// // colorPalette is used in generateFlattenedRuneList to generate a random color
-// var colorPalette = [
-//   '#F44336',
-//   '#E91E63',
-//   '#AA00FF',
-//   '#3F51B5',
-//   '#2196F3',
-//   '#4CAF50',
-//   '#FFEB3B',
-//   '#FF9800',
-//   '#795548'
-// ]
-
-// /**
-//  * colors the given curve red.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function red(curve) {
-//   // throwIfNotRune('red', rune)
-//   return addColorFromHex(curve, '#F44336')
-// }
-
-// /**
-//  * colors the given curve pink.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function pink(curve) {
-//   // throwIfNotRune('pink', rune)
-//   return addColorFromHex(curve, '#E91E63')
-// }
-
-// /**
-//  * colors the given curve purple.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function purple(curve) {
-//   // throwIfNotRune('purple', rune)
-//   return addColorFromHex(curve, '#AA00FF')
-// }
-
-// /**
-//  * colors the given curve indigo.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function indigo(curve) {
-//   // throwIfNotRune('indigo', rune)
-//   return addColorFromHex(curve, '#3F51B5')
-// }
-
-// /**
-//  * colors the given curve blue.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function blue(curve) {
-//   // throwIfNotRune('blue', rune)
-//   return addColorFromHex(curve, '#2196F3')
-// }
-
-// /**
-//  * colors the given curve green.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Curve
-//  */
-// function green(curve) {
-//   // throwIfNotRune('green', rune)
-//   return addColorFromHex(curve, '#4CAF50')
-// }
-
-// /**
-//  * colors the given curve yellow.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function yellow(curve) {
-//   // throwIfNotRune('yellow', rune)
-//   return addColorFromHex(curve, '#FFEB3B')
-// }
-
-// /**
-//  * colors the given curve orange.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function orange(curve) {
-//   // throwIfNotRune('orange', rune)
-//   return addColorFromHex(curve, '#FF9800')
-// }
-
-// /**
-//  * colors the given curve brown.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function brown(curve) {
-//   // throwIfNotRune('brown', rune)
-//   return addColorFromHex(curve, '#795548')
-// }
-
-// /**
-//  * colors the given curve black.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function black(curve) {
-//   // throwIfNotRune('black', rune)
-//   return addColorFromHex(curve, '#000000')
-// }
-
-// /**
-//  * colors the given curve white.
-//  * @param {Curve} curve - the curve to color
-//  * @returns {Curve} the colored Rune
-//  */
-// function white(curve) {
-//   // throwIfNotRune('white', rune)
-//   return addColorFromHex(curve, '#FFFFFF')
-// }
+/**
+ * retrieves the red component of a given Point
+ * @param {Point} p - given point
+ * @returns {Number} Blue component of the Point
+ */
+function b_of(pt) {
+  return pt.getColor()[2];
+}
--- public/externalLibs/graphics/webGLhi_graph_ce.js
+++ public/externalLibs/graphics/webGLhi_graph_ce.js
@@ -57,26 +57,16 @@ function arc(t) {
 
 // SOME CURVE-TRANSFORMS
 
-// function applyTransformation(transformation, curve) {
-//   if (curve instanceof Curve) {
-//     var tempCurve = curve.getCurve()
-//     curve.setCurve(transformation(tempCurve))
-//     return curve
-//   } else {
-//     return transformation(curve)
-//   }
-// }
-
 function invert(curve) {
-  return transformation(c => (t => c(1 - t)), curve)
+  return t => curve(1 - t)
 }
 
 function rotate_pi_over_2(curve) {
   var transformation = c => (t => {
     var ct = c(t)
-    return make_point(-y_of(ct), x_of(ct))
+    return make_point(-y_of(ct), x_of(ct), r_)
   })
-  return transformation(transformation, curve)
+  return transformation(curve)
 }
 
 // CONSTRUCTORS OF CURVE-TRANSFORMS
@@ -89,7 +79,7 @@ function translate(x0, y0) {
       var ct = c(t)
       return make_point(x0 + x_of(ct), y0 + y_of(ct))
     })
-    return transformation(transformation, curve)
+    return transformation(curve)
   }
 }
 
@@ -105,7 +95,7 @@ function rotate_around_origin(theta) {
       var y = y_of(ct)
       return make_point(cth * x - sth * y, sth * x + cth * y)
     })
-    return transformation(transformation, curve)
+    return transformation(curve)
   }
 }
 
@@ -117,7 +107,7 @@ function deriv_t(n) {
       var ctdelta = c(t + delta_t)
       return make_point((x_of(ctdelta) - x_of(ct)) / delta_t, (y_of(ctdelta) - y_of(ct)) / delta_t)
     })
-    return transformation(transformation, curve)
+    return transformation(curve)
       }
 }
 
@@ -127,7 +117,7 @@ function scale_x_y(a, b) {
       var ct = c(t)
       return make_point(a * x_of(ct), b * y_of(ct))
     })
-    return transformation(transformation, curve)
+    return transformation(curve)
   }
 }
 
@@ -195,26 +185,13 @@ function full_view_proportional(xlo, xhi, ylo, yhi) {
 // Behavior is unspecified on closed curves (with start-point = end-point).
 
 function put_in_standard_position(curve) {
-  // if (curve instanceof Curve) {
-  //   var wrapper = new Curve()
-  //   var start_point = curve.getCurve()(0)
-  //   var curve_started_at_origin = translate(-x_of(start_point), -y_of(start_point))(curve.getCurve())
-  //   var new_end_point = curve_started_at_origin(1)
-  //   var theta = Math.atan2(y_of(new_end_point), x_of(new_end_point))
-  //   var curve_ended_at_x_axis = rotate_around_origin(-theta)(curve_started_at_origin)
-  //   var end_point_on_x_axis = x_of(curve_ended_at_x_axis(1))
-  //   wrapper.setCurve(scale(1 / end_point_on_x_axis)(curve_ended_at_x_axis))
-  //   wrapper.setColor(curve.getColor())
-  //   return wrapper
-  // } else {
-    var start_point = curve(0)
-    var curve_started_at_origin = translate(-x_of(start_point), -y_of(start_point))(curve)
-    var new_end_point = curve_started_at_origin(1)
-    var theta = Math.atan2(y_of(new_end_point), x_of(new_end_point))
-    var curve_ended_at_x_axis = rotate_around_origin(-theta)(curve_started_at_origin)
-    var end_point_on_x_axis = x_of(curve_ended_at_x_axis(1))
-    return scale(1 / end_point_on_x_axis)(curve_ended_at_x_axis)
-  // }
+  var start_point = curve(0)
+  var curve_started_at_origin = translate(-x_of(start_point), -y_of(start_point))(curve)
+  var new_end_point = curve_started_at_origin(1)
+  var theta = Math.atan2(y_of(new_end_point), x_of(new_end_point))
+  var curve_ended_at_x_axis = rotate_around_origin(-theta)(curve_started_at_origin)
+  var end_point_on_x_axis = x_of(curve_ended_at_x_axis(1))
+  return scale(1 / end_point_on_x_axis)(curve_ended_at_x_axis)
 }
 
 // Binary-transform = (Curve,Curve --> Curve)
@@ -222,34 +199,6 @@ function put_in_standard_position(curve) {
 // CONNECT-RIGIDLY makes a curve consisting of curve1 followed by curve2.
 
 function connect_rigidly(curve1, curve2) {
-  // if (!(curve1 instanceof Curve)) {
-  //   var temp = new Curve()
-  //   temp.setCurve(curve1)
-  //   temp.setColor(t => [0, 0, 0, 1])
-  //   curve1 = temp
-  // }
-  // if (!(curve2 instanceof Curve)) {
-  //   var temp = new Curve()
-  //   temp.setCurve(curve2)
-  //   temp.setColor(t => [0, 0, 0, 1])
-  //   curve2 = temp
-  // }
-  // var wrapper = new Curve()
-  // wrapper.setCurve(t => {
-  //   if (t < 1 / 2) {
-  //     return curve1.getCurve()(2 * t)
-  //   } else {
-  //     return curve2.getCurve()(2 * t - 1)
-  //   }
-  // })
-  // wrapper.setColor(t => {
-  //   if (t < 1 / 2) {
-  //     return curve1.getColor()(2 * t)
-  //   } else {
-  //     return curve2.getColor()(2 * t - 1)
-  //   }
-  // })
-  // return wrapper
   return t < 1 / 2 ? curve1.getCurve()(2 * t) : curve2.getCurve()(2 * t - 1)
 }
 
@@ -257,8 +206,8 @@ function connect_rigidly(curve1, curve2) {
 // a copy of curve2 starting at the end of curve1
 
 function connect_ends(curve1, curve2) {
-  var start_point_of_curve2 = /*curve2 instanceof Curve ? curve2.getCurve()(0) :*/ curve2(0)
-  var end_point_of_curve1 = /*curve1 instanceof Curve ? curve1.getCurve()(1) :*/ curve1(1)
+  var start_point_of_curve2 = curve2(0)
+  var end_point_of_curve1 = curve1(1)
   return connect_rigidly(
     curve1,
     translate(
