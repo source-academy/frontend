90ae9beb19f05bd24361e0d76949b805b9921f58
VimuthM <vimuthmendis@gmail.com>
2021-02-08 17:01:44 +0800

    Updated visualizer.js to work with Konva

--- public/externalLibs/index.js
+++ public/externalLibs/index.js
@@ -42,7 +42,7 @@ function loadAllLibs() {
     '/externalLibs/graphics/webGLcurve.js',
     '/externalLibs/graphics/webGLrune.js',
     // list visualizer
-    '/externalLibs/visualizer/KineticJS.js',
+    '/externalLibs/visualizer/konva.js',
     '/externalLibs/visualizer/visualizer.js',
     // binary tree library
     '/externalLibs/tree.js',
