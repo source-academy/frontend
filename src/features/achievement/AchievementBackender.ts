5bfdd97dc432f38f6fd947288b0e2fc750fce838
Jonas Chow <jonaschow99@gmail.com>
2021-02-21 17:30:39 +0800

    Changed double quotes to single quotes

--- src/features/achievement/AchievementBackender.ts
+++ src/features/achievement/AchievementBackender.ts
@@ -14,14 +14,14 @@ export const backendifyGoalProgress = (goal: GoalProgress) => ({
 });
 
 export const convertBackendMetaToFrontend = (meta: any) => {
-  if (meta.type === "Binary" || meta.type === "Manual") {
+  if (meta.type === 'Binary' || meta.type === 'Manual') {
     meta.maxXp = meta.max_xp;
     delete meta.max_xp;
-  } else if (meta.type === "Assessment") {
+  } else if (meta.type === 'Assessment') {
     meta.assessmentNumber = meta.assessment_number;
     delete meta.assessment_number;
     meta.requiredCompletionFrac = meta.required_completion_frac;
     delete meta.required_completion_frac;
   }
   return meta;
-}
+};
