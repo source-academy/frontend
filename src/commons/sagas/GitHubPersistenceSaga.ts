80a32db35e04996e025c61fa7e10f009ae2d9b2f
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-03-09 10:39:42 +0800

    feat: Added link to GitHub oauth page, updated environment example file to include GitHub client ID

--- src/commons/sagas/GitHubPersistenceSaga.ts
+++ src/commons/sagas/GitHubPersistenceSaga.ts
@@ -10,7 +10,18 @@ import {
 import { LOGIN_GITHUB, LOGOUT_GITHUB } from '../application/types/SessionTypes';
 
 export function* GithubPersistenceSaga(): SagaIterator {
-  yield takeLatest(LOGIN_GITHUB, function* () {});
+  yield takeLatest(LOGIN_GITHUB, function* () {
+		console.log("YEET SKEET LOGINEET");
+		
+		const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+		const githubOauthLoginLink = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+		const windowName = 'Connect With OAuth'
+		const windowSpecs = 'height=600,width=400';
+
+		window.open(githubOauthLoginLink, windowName, windowSpecs);
+
+		console.log("PEKO PEKO PAIN PEKO PAIN PAIN PAIN");
+  });
 
   yield takeLatest(LOGOUT_GITHUB, function* () {});
 
