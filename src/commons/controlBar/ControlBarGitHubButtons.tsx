e4a8ab0ff953f6d6dbaab37342b83e061e14ba33
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-03-10 15:15:04 +0800

    refactor: Removed an unnecessary print statement

--- src/commons/controlBar/ControlBarGitHubButtons.tsx
+++ src/commons/controlBar/ControlBarGitHubButtons.tsx
@@ -24,11 +24,9 @@ const stateToIntent: { [state in GitHubState]: Intent } = {
 };
 
 export const ControlBarGitHubButtons: React.FC<ControlBarGitHubButtonsProps> = props => {
-  const yeet = store.getState().session.githubUser;
-  console.log('github buttons: ' + yeet);
-
-  // >>>>> Replace this everywhere! Stop using the isLoggedIn property! <<<<<
-  //const isLoggedIn = props.loggedInAs !== undefined;
+  // The 'loggedInAs' is not used directly in this code block
+  // However, keeping it in will ensure that the component re-renders immediately
+  // Or else, the re-render has to be triggered by something else
 
   const isLoggedIn = store.getState().session.githubUser !== undefined;
   const shouldDisableButtons = !isLoggedIn;
