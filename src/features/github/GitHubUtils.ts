89a99092cd8480d29e54e4caeafbf6e8ede13a56
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-03-11 14:55:39 +0800

    style: ran yarn format to fix formatting issues

--- src/features/github/GitHubUtils.ts
+++ src/features/github/GitHubUtils.ts
@@ -4,9 +4,9 @@ export function encodeAsURL(messageBodyPrototype: URIField[]) {
   const uriComponents: string[] = [];
 
   messageBodyPrototype.forEach(element => {
-		const field = element.name || '';
-		
-		// We need to check for the edge-case where the value is literally false
+    const field = element.name || '';
+
+    // We need to check for the edge-case where the value is literally false
     const value = element.value === false ? false : element.value || '';
 
     uriComponents.push([encodeURIComponent(field), encodeURIComponent(value)].join('='));
