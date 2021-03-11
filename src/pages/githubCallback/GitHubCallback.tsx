112efc6946bd359d179c761d2f5abe96a533feee
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-03-11 17:56:48 +0800

    docs: Added comments for GitHubCallback page

--- src/pages/githubCallback/GitHubCallback.tsx
+++ src/pages/githubCallback/GitHubCallback.tsx
@@ -12,17 +12,21 @@ export type GitHubCallbackProps = {
   grabAccessCodeFromURL: (currentURLAddress: string) => string;
 };
 
+/**
+ * The page that the user is redirected to after they have approved the app through GitHub.
+ * This page will complete the OAuth workflow by sending the access code the back-end to retrieve the auth-token.
+ * The auth-token is then broadcasted back to the main browser page.
+ */
 export function GitHubCallback(props: GitHubCallbackProps) {
   const [message, setMessage] = useState('You have reached the GitHub callback page');
 
+  // Destructure the properties outside of 'useEffect'
   const clientId = props.clientID || '';
   const grabAccessCodeFromURL = props.grabAccessCodeFromURL;
   const exchangeAccessCodeForAuthTokenContainingObject =
     props.exchangeAccessCodeForAuthTokenContainingObject;
 
   useEffect(() => {
-    const broadcastChannel = new BroadcastChannel('GitHubOAuthAccessToken');
-
     const backendLink = 'https://api2.sourceacademy.nus.edu.sg/github_oauth';
 
     const currentAddress = window.location.search;
@@ -42,6 +46,7 @@ export function GitHubCallback(props: GitHubCallbackProps) {
       return;
     }
 
+    // Create message body to be sent to back-end microservice
     const messageBody = encodeAsURL([
       new URIField('code', accessCode),
       new URIField('clientId', clientId)
@@ -53,7 +58,10 @@ export function GitHubCallback(props: GitHubCallbackProps) {
         const requestSuccess = typeof res.access_token != 'undefined';
 
         if (requestSuccess) {
+          // Send auth token back to the main browser page
+          const broadcastChannel = new BroadcastChannel('GitHubOAuthAccessToken');
           broadcastChannel.postMessage(res.access_token);
+
           setMessage('Log-in successful! This window will close soon.');
           setTimeout(() => {
             window.close();
