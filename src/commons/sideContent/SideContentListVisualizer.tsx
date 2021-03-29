786eb20cc20e150aac045b0acfd936f60c921f04
Jeff Sieu <jeffsy00@gmail.com>
2021-03-29 14:08:51 +0800

    Add stepper functionality for multiple draw_data calls (#1665)
    * Add prev, next buttons, begin split pane for list visualizer
    
    * Improve UI for list visualizer
    
    * Fix things for PR
    
    * Fix tsx formatting
    
    * appendDrawing now takes in an array of structures
    
    * visualiseList updated
    
    Handles mulitple structures
    
    * Fix minor issues with list visualizer
    
    - Remove extraneous constructors
    - Remove unused variables
    - Change == to ===
    - Lowercase number in javadoc
    - Nested scrolling in data structure container
    - Spell visualize with a z
    
    * Fix tsx formatting
    
    * Add preventDefault to all list visualizer shapes
    
    Co-authored-by: martin-henz <henz@comp.nus.edu.sg>
    Co-authored-by: Vimuth <62249192+VimuthM@users.noreply.github.com>

--- src/commons/sideContent/SideContentListVisualizer.tsx
+++ src/commons/sideContent/SideContentListVisualizer.tsx
@@ -1,11 +1,18 @@
-import { Classes, NonIdealState, Spinner } from '@blueprintjs/core';
+import { Button, Classes, NonIdealState, Spinner } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import classNames from 'classnames';
 import * as React from 'react';
+import { HotKeys } from 'react-hotkeys';
 
 import { Links } from '../utils/Constants';
 
 type State = {
   loading: boolean;
+};
+
+const listVisualizerKeyMap = {
+  PREVIOUS_STEP: 'left',
+  NEXT_STEP: 'right'
 };
 
 class SideContentListVisualizer extends React.Component<{}, State> {
@@ -21,32 +28,91 @@ class SideContentListVisualizer extends React.Component<{}, State> {
   }
 
   public render() {
+    const listVisualizerHandlers = {
+      PREVIOUS_STEP: this.onPrevButtonClick,
+      NEXT_STEP: this.onNextButtonClick
+    };
+
+    const listVisualizer = (window as any).ListVisualizer;
     // Default text will be hidden by visualizer.js when 'draw_data' is called
     return (
-      <div ref={r => (this.$parent = r)} className={classNames('sa-list-visualizer', Classes.DARK)}>
-        <p id="data-visualizer-default-text" className={Classes.RUNNING_TEXT}>
-          The data visualizer visualises data structures.
-          <br />
-          <br />
-          It is activated by calling the function <code>draw_data(the_data)</code>, where{' '}
-          <code>the_data</code> would be the data structure that you want to visualise.
-          <br />
-          <br />
-          The data visualizer uses box-and-pointer diagrams, as introduced in{' '}
-          <a href={Links.textbookChapter2_2} rel="noopener noreferrer" target="_blank">
-            <i>
-              Structure and Interpretation of Computer Programs, JavaScript Adaptation, Chapter 2,
-              Section 2
-            </i>
-          </a>
-          .
-        </p>
-        {this.state.loading && (
-          <NonIdealState description="Loading Data Visualizer..." icon={<Spinner />} />
-        )}
-      </div>
+      <HotKeys keyMap={listVisualizerKeyMap} handlers={listVisualizerHandlers}>
+        <div
+          ref={r => (this.$parent = r)}
+          className={classNames('sa-list-visualizer', Classes.DARK)}
+        >
+          {(listVisualizer?.getStepCount() ?? 0) > 1 ? (
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                large={true}
+                outlined={true}
+                icon={IconNames.ARROW_LEFT}
+                onClick={this.onPrevButtonClick}
+                disabled={(listVisualizer?.getCurrentStep() ?? 1) === 1}
+              >
+                Prev
+              </Button>
+              <h3
+                className="bp3-text-large"
+                style={{ alignSelf: 'center', display: 'inline', margin: 0 }}
+              >
+                Call {listVisualizer?.getCurrentStep() ?? '0'}/{listVisualizer?.getStepCount()}
+              </h3>
+              <Button
+                large={true}
+                outlined={true}
+                icon={IconNames.ARROW_RIGHT}
+                onClick={this.onNextButtonClick}
+                disabled={
+                  listVisualizer
+                    ? listVisualizer.getCurrentStep() === listVisualizer.getStepCount()
+                    : true
+                }
+              >
+                Next
+              </Button>
+            </div>
+          ) : null}
+          <p
+            id="data-visualizer-default-text"
+            className={Classes.RUNNING_TEXT}
+            hidden={listVisualizer?.hasDrawing() ?? false}
+          >
+            The data visualizer visualizes data structures.
+            <br />
+            <br />
+            It is activated by calling the function <code>draw_data(the_data)</code>, where{' '}
+            <code>the_data</code> would be the data structure that you want to visualize.
+            <br />
+            <br />
+            The data visualizer uses box-and-pointer diagrams, as introduced in{' '}
+            <a href={Links.textbookChapter2_2} rel="noopener noreferrer" target="_blank">
+              <i>
+                Structure and Interpretation of Computer Programs, JavaScript Adaptation, Chapter 2,
+                Section 2
+              </i>
+            </a>
+            .
+          </p>
+          {this.state.loading && (
+            <NonIdealState description="Loading Data Visualizer..." icon={<Spinner />} />
+          )}
+        </div>
+      </HotKeys>
     );
   }
+
+  private onPrevButtonClick = () => {
+    const element = (window as any).ListVisualizer;
+    element.previous();
+    this.setState({});
+  };
+
+  private onNextButtonClick = () => {
+    const element = (window as any).ListVisualizer;
+    element.next();
+    this.setState({});
+  };
 
   private tryToLoad = () => {
     const element = (window as any).ListVisualizer;
