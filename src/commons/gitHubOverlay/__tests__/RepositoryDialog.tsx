bdfebc7823f2bc0b9890f68f5f69f50923aa8fab
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-04-09 10:33:02 +0800

    test: fixed RepositoryDialog test to use the correct act function

--- src/commons/gitHubOverlay/__tests__/RepositoryDialog.tsx
+++ src/commons/gitHubOverlay/__tests__/RepositoryDialog.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { act } from 'react-test-renderer';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 
 import RepositoryDialog from '../RepositoryDialog';
 
