a6f2099879630c4dd807cff4c2bf391b04ac5f0a
Lim Chek Jun <limchekjun@gmail.com>
2021-04-30 12:30:35 +0800

    fixed typo in NavigationBar test

--- src/commons/navigationBar/__tests__/NavigationBar.tsx
+++ src/commons/navigationBar/__tests__/NavigationBar.tsx
@@ -7,7 +7,7 @@ import NavigationBar from '../NavigationBar';
 test('NavigationBar renders "Not logged in" correctly', () => {
   const props = {
     handleLogOut: () => {},
-    handeGitHubLogIn: () => {},
+    handleGitHubLogIn: () => {},
     handleGitHubLogOut: () => {},
     title: 'Source Academy'
   };
@@ -18,7 +18,7 @@ test('NavigationBar renders "Not logged in" correctly', () => {
 test('NavigationBar renders correctly with student role', () => {
   const props = {
     handleLogOut: () => {},
-    handeGitHubLogIn: () => {},
+    handleGitHubLogIn: () => {},
     handleGitHubLogOut: () => {},
     title: 'Source Academy',
     role: Role.Student
