73eafb4c5f60f81eb0baedf9373a3b2cdfde7a03
Lim Chek Jun <limchekjun@gmail.com>
2021-04-30 12:12:39 +0800

    Updated props for Application and Navbar tests

--- src/commons/navigationBar/__tests__/NavigationBar.tsx
+++ src/commons/navigationBar/__tests__/NavigationBar.tsx
@@ -7,6 +7,8 @@ import NavigationBar from '../NavigationBar';
 test('NavigationBar renders "Not logged in" correctly', () => {
   const props = {
     handleLogOut: () => {},
+    handeGitHubLogIn: () => {},
+    handleGitHubLogOut: () => {},
     title: 'Source Academy'
   };
   const tree = shallow(<NavigationBar {...props} />);
@@ -16,6 +18,8 @@ test('NavigationBar renders "Not logged in" correctly', () => {
 test('NavigationBar renders correctly with student role', () => {
   const props = {
     handleLogOut: () => {},
+    handeGitHubLogIn: () => {},
+    handleGitHubLogOut: () => {},
     title: 'Source Academy',
     role: Role.Student
   };
