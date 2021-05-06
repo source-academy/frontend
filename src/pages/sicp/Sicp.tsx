f337f2eb5e4c1bac9c190b4f34936f3e52fb3766
Samuel Fang <samuelfangjw@gmail.com>
2021-05-06 16:22:59 +0800

    Add Sicp Placeholder Page

---
+++ src/pages/sicp/Sicp.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+type SicpProps = DispatchProps & StateProps & OwnProps;
+type DispatchProps = {};
+type StateProps = {};
+type OwnProps = {};
+
+const Sicp: React.FC<SicpProps> = props => {
+  return (
+    <div>
+      Sicp Placeholder Div
+    </div>
+  );
+};
+
+export default Sicp;
