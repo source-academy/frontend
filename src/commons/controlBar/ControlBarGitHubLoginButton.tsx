23aa7edca9ef7a05c3e7afc61f9e68d205bd59b6
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-05-07 15:08:49 +0800

    docs: removed unnecessary comments

--- src/commons/controlBar/ControlBarGitHubLoginButton.tsx
+++ src/commons/controlBar/ControlBarGitHubLoginButton.tsx
@@ -15,10 +15,6 @@ export type ControlBarGitHubLoginButtonProps = {
 };
 
 export const ControlBarGitHubLoginButton: React.FC<ControlBarGitHubLoginButtonProps> = props => {
-  // The 'loggedInAs' is not used directly in this code block
-  // However, keeping it in will ensure that the component re-renders immediately
-  // Or else, the re-render has to be triggered by something else
-
   const isMobileBreakpoint = useMediaQuery({ maxWidth: Constants.mobileBreakpoint });
   const isLoggedIn = store.getState().session.githubOctokitInstance !== undefined;
 
