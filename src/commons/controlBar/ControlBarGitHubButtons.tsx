05631a8c515656199a0fca34449a7b5400dc840c
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-05-07 15:07:22 +0800

    docs: removed unnecessary comments from ControlBarGitHubButtons

--- src/commons/controlBar/ControlBarGitHubButtons.tsx
+++ src/commons/controlBar/ControlBarGitHubButtons.tsx
@@ -26,10 +26,6 @@ const stateToIntent: { [state in GitHubState]: Intent } = {
 };
 
 export const ControlBarGitHubButtons: React.FC<ControlBarGitHubButtonsProps> = props => {
-  // The 'loggedInAs' is not used directly in this code block
-  // However, keeping it in will ensure that the component re-renders immediately
-  // Or else, the re-render has to be triggered by something else
-
   const isMobileBreakpoint = useMediaQuery({ maxWidth: Constants.mobileBreakpoint });
   const isLoggedIn = store.getState().session.githubOctokitInstance !== undefined;
 
