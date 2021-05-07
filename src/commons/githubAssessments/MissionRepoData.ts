e92e2b8f093f3ab0c40e9d7e7700b79534959fe2
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-05-07 15:32:48 +0800

    docs: Added documentation for GitHub-related components

--- src/commons/githubAssessments/MissionRepoData.ts
+++ src/commons/githubAssessments/MissionRepoData.ts
@@ -1,3 +1,10 @@
+/**
+ * Represents information about a GitHub repository containing a SourceAcademy mission.
+ * 
+ * Contains sufficient information for two purposes:
+ * 1. Retrieval of repository information
+ * 2. Establishing proper display order on the Mission Listing page
+ */
 type MissionRepoData = {
   repoOwner: string;
   repoName: string;
