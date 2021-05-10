158cfc06a628a062f6fcd7e153ef12b7335f4306
Samuel Fang <samuelfangjw@gmail.com>
2021-05-10 20:00:00 +0800

    Add dummy json file

--- src/features/sicp/parser/SicpParser.ts
+++ src/features/sicp/parser/SicpParser.ts
@@ -1,23 +1,15 @@
-// @ts-ignore
-import XMLParser from "react-xml-parser";
-import * as xml2js from 'xml2js';
-
-// @ts-ignore
-import data from '../assets/xml/chapter1/section1/subsection1.xml';
-
-export function parseSicpXml(chapter: integer, section:integer, subsection: integer) {
-    const parser = new xml2js.Parser();
-
-    // const __rootDirectory = "../assets/xml/";
-    // const __filepath = "chapter1/section1/subsection1.xml";
-    // const data = "<root>Hello xml2js!</root>"
-    const jsonDataFromXml = new XMLParser().parseFromString(data);
-
-    //TODO: Replace any type in parameter
-    parser.parseString(jsonDataFromXml, function (err: any, result: any) {
-        console.log(result);
-        console.log('Done');
+export const getData = () => {
+  fetch('sicp/test.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  })
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (myJson) {
+      console.log(myJson);
     });
-
-    return "a";
-}
+};
--- src/pages/sicp/Sicp.tsx
+++ src/pages/sicp/Sicp.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { SicpChapter } from 'src/features/sicp/SicpTypes';
 
 import SicpDisplay from './subcomponents/SicpDisplay';
@@ -15,7 +16,32 @@ export type OwnProps = {};
 const Sicp: React.FC<SicpProps> = props => {
   const { chapter } = props;
 
-  const content = "testcontent" + chapter;
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    console.log("effect called");
+    fetch('/sicp/test.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+          console.log(response);
+          return response.json();
+        })
+      .then(myJson => {
+          console.log(myJson);
+          setData(myJson);
+        });
+  };
+
+  useEffect(() => getData(), []);
+
+  //TODO check for null json files, get correct type
+  let text = "";
+  data.forEach(x => text = text + x["text"]);
+  const content = text + "test" + chapter;
 
   return (
     <SicpDisplay content={content} />
