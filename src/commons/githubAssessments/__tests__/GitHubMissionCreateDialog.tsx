48cab18a015b3709fa80fca661abf569bb9fb26f
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-05-14 13:02:12 +0800

    test: removed unnecessary import from GitHubMissionCreateDialog

--- src/commons/githubAssessments/__tests__/GitHubMissionCreateDialog.tsx
+++ src/commons/githubAssessments/__tests__/GitHubMissionCreateDialog.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import {
