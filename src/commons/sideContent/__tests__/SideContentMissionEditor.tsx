690d62e7d74a04b6d8915e3a3f8ba73c98f8d773
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-05-14 15:13:16 +0800

    feat: Added tests for SideContentMissionEditor

---
+++ src/commons/sideContent/__tests__/SideContentMissionEditor.tsx
@@ -0,0 +1,64 @@
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import SideContentMissionEditor from '../SideContentMissionEditor';
+import MissionMetadata from '../../githubAssessments/MissionMetadata';
+
+test('typing into SideContentMissionEditor text boxes triggers setter function', () => {
+  const missionMetadata = {
+    coverImage: 'dummyCoverImage',
+    kind: 'dummyKind',
+    number: 'dummyNumber',
+    title: 'dummyTitle',
+    sourceVersion: 1,
+    dueDate: new Date(),
+  
+    reading: 'dummyReading',
+    webSummary: 'dummySummary',
+  } as MissionMetadata;
+
+  let outsideValue = Object.assign({}, missionMetadata);
+
+  const setMissionMetadata = jest.fn((insideValue: MissionMetadata) => {
+    outsideValue = insideValue;
+  });
+
+  act(() => {
+    render(
+      <SideContentMissionEditor
+        missionMetadata={missionMetadata}
+        setMissionMetadata={setMissionMetadata}
+      />
+    );
+  });
+
+  const coverImageText = screen.getByDisplayValue('dummyCoverImage');
+  userEvent.clear(coverImageText);
+  userEvent.type(coverImageText, 'realCoverImage');
+  expect(outsideValue.coverImage).toBe('realCoverImage');
+
+  const kindText = screen.getByDisplayValue('dummyKind');
+  userEvent.clear(kindText);
+  userEvent.type(kindText, 'realKind');
+  expect(outsideValue.kind).toBe('realKind');
+
+  const numberText = screen.getByDisplayValue('dummyNumber');
+  userEvent.clear(numberText);
+  userEvent.type(numberText, 'realNumber');
+  expect(outsideValue.number).toBe('realNumber')
+
+  const titleText = screen.getByDisplayValue('dummyTitle');
+  userEvent.clear(titleText);
+  userEvent.type(titleText, 'realTitle');
+  expect(outsideValue.title).toBe('realTitle');
+
+  const readingText = screen.getByDisplayValue('dummyReading');
+  userEvent.clear(readingText);
+  userEvent.type(readingText, 'realReading');
+  expect(outsideValue.reading).toBe('realReading');
+
+  const summaryText = screen.getByDisplayValue('dummySummary');
+  userEvent.clear(summaryText);
+  userEvent.type(summaryText, 'realSummary');
+  expect(outsideValue.webSummary).toBe('realSummary');
+});
