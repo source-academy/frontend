4aee59e66eb555688464be3504fea52b13311b07
Samuel Fang <samuelfangjw@gmail.com>
2021-06-01 00:03:39 +0800

    Add previous and next buttons

--- src/commons/navigationBar/subcomponents/SicpNavigationBar.tsx
+++ src/commons/navigationBar/subcomponents/SicpNavigationBar.tsx
@@ -1,6 +1,10 @@
 import { Alignment, Drawer, Navbar, NavbarGroup, Position } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import * as React from 'react';
+import { useHistory, useLocation } from 'react-router';
+import controlButton from 'src/commons/ControlButton';
 
+import tocNavigation from '../../../features/sicp/data/toc-navigation.json';
 import SicpToc from '../../../pages/sicp/subcomponents/SicpToc';
 import { ControlBarTableOfContentsButton } from '../../controlBar/ControlBarTableOfContentsButton';
 
@@ -10,6 +14,9 @@ type OwnProps = {};
 
 const SicpNavigationBar: React.FC<SicpNavigationBarProps> = props => {
   const [isTocOpen, setIsTocOpen] = React.useState(false);
+  const location = useLocation().pathname;
+
+  const history = useHistory();
 
   const handleCloseToc = () => setIsTocOpen(false);
 
@@ -17,6 +24,49 @@ const SicpNavigationBar: React.FC<SicpNavigationBarProps> = props => {
     const handleOpenToc = () => setIsTocOpen(true);
     return <ControlBarTableOfContentsButton key="toc" handleOpenToc={handleOpenToc} />;
   }, []);
+
+  const prevButton = React.useMemo(() => {
+    const sect = tocNavigation[location];
+    if (!sect) {
+      return;
+    }
+
+    const prev = sect['prev'];
+    if (!prev) {
+      return;
+    }
+
+    const handlePrev = () => {
+      history.push(prev);
+    };
+
+    return (
+      prev && <div key="prev">{controlButton('Previous', IconNames.ARROW_LEFT, handlePrev)}</div>
+    );
+  }, [history, location]);
+
+  const nextButton = React.useMemo(() => {
+    const sect = tocNavigation[location];
+    if (!sect) {
+      return;
+    }
+
+    const next = sect['next'];
+    if (!next) {
+      return;
+    }
+
+    const handleNext = () => {
+      history.push(next);
+    };
+    return (
+      next && (
+        <div key="next">
+          {controlButton('Next', IconNames.ARROW_RIGHT, handleNext, { iconOnRight: true })}
+        </div>
+      )
+    );
+  }, [history, location]);
 
   const drawerProps = {
     onClose: handleCloseToc,
@@ -34,7 +84,8 @@ const SicpNavigationBar: React.FC<SicpNavigationBarProps> = props => {
   return (
     <>
       <Navbar className="NavigationBar secondary-navbar">
-        <NavbarGroup align={Alignment.LEFT}>{menuButton}</NavbarGroup>
+        <NavbarGroup align={Alignment.LEFT}>{[menuButton]}</NavbarGroup>
+        <NavbarGroup align={Alignment.RIGHT}>{[prevButton, nextButton]}</NavbarGroup>
       </Navbar>
       <Drawer {...drawerProps}>
         <SicpToc handleCloseToc={handleCloseToc} location="sidebar" />
