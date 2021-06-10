4ccbe5de4cb5ee46d88ad842494cc9dc9fe2cf9c
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2021-06-10 10:19:53 +0800

    chore: updated task description and assignment briefing to be more informative

--- src/pages/githubAssessments/GitHubAssessmentDefaultValues.ts
+++ src/pages/githubAssessments/GitHubAssessmentDefaultValues.ts
@@ -2,10 +2,72 @@ import { IMCQQuestion } from '../../commons/assessment/AssessmentTypes';
 import { MissionMetadata, TaskData } from '../../commons/githubAssessments/GitHubMissionTypes';
 
 export const defaultMissionBriefing =
-  'Welcome to Mission Mode! This is where the Mission Briefing for each assignment will appear.';
+`### Assignment Briefing (Click to Edit!)
+Welcome to Assignment Creator! This is where the Briefing for each assignment will appear.
+
+If you are on the SourceAcademy website, you may edit **this section** by clicking on it. You can also add or delete questions through the control bar.
+
+The text written here will show up as the \`README.md\` file on the GitHub repository.
+
+Please also refer to our instructor's guide [here](https://github.com/source-academy/general/tree/master/instructor/github) to learn more about how to use this website.
+
+### Brief Markdown Cheatsheet
+Everything here is written in Markdown.
+
+This means that you can **bolden** or *italicize* text by adding asterisks.
+
+Code-blocks can be formatted through the use of backticks:
+\`\`\`
+// This is a code example
+\`\`\`
+If you want to in-line code, rather than display a whole code-block, you can also do \`this\`. 
+
+Lists can be created by adding dashes to the start of a line:
+- First element
+- Second element
+- Third element
+
+
+It is also possible to embed images, like this:
+
+![an embedded image](https://avatars.githubusercontent.com/u/35620705?s=400&u=32f72fd1d65a0d6877ad1d5870ffa327dda754f1&v=4)
+
+If you need a more detailed cheatsheet, please click [here](https://www.markdownguide.org/cheat-sheet/)!
+`;
 
 export const defaultTaskDescription =
-  'Welcome to Mission Mode! This is where the Task Description for each assignment will appear.';
+`### Problem Description (Click to Edit!)
+Welcome to the Assignment Creator! This is where the problem description specific to each problem should be written.
+
+If you are on the SourceAcademy website, you may edit **this section** by clicking on it. You can also add or delete questions through the control bar.
+
+If you would like to add new questions outside of the website, simply duplicate this folder, and edit the \`Problem.md\` and \`StarterCode.js\` files accordingly. Make sure that the folders are named 'Q1', 'Q2', 'Q3' and so on.
+
+Please also refer to our instructor's guide [here](https://github.com/source-academy/general/tree/master/instructor/github).
+
+### Brief Markdown Cheatsheet
+Everything here is written in Markdown.
+
+This means that you can **bolden** or *italicize* text by adding asterisks.
+
+Code-blocks can be formatted through the use of backticks:
+\`\`\`
+// This is a code example
+\`\`\`
+If you want to in-line code, rather than display a whole code-block, you can also do \`this\`. 
+
+Lists can be created by adding dashes to the start of a line:
+- First element
+- Second element
+- Third element
+
+
+It is also possible to embed images, like this:
+
+![an embedded image](https://avatars.githubusercontent.com/u/35620705?s=400&u=32f72fd1d65a0d6877ad1d5870ffa327dda754f1&v=4)
+
+If you need a more detailed cheatsheet, please click [here](https://www.markdownguide.org/cheat-sheet/)!
+`;
 
 export const defaultStarterCode = '// Your code here!\n';
 
