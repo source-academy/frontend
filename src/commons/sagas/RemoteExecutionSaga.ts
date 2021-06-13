7cd7d664936c84abcf7a215ba1f8d22bb7aa7a47
Chow En Rong <53928333+chownces@users.noreply.github.com>
2021-06-13 21:53:27 +0800

    Make RemoteExecutionSaga compilable

--- src/commons/sagas/RemoteExecutionSaga.ts
+++ src/commons/sagas/RemoteExecutionSaga.ts
@@ -32,8 +32,8 @@ export function* remoteExecutionSaga(): SagaIterator {
     const [tokens, session]: [any, DeviceSession | undefined] = yield select(
       (state: OverallState) => [
         {
-          accessToken: state.session.accessToken,
-          refreshToken: state.session.refreshToken
+          accessToken: state.session.tokens.accessToken,
+          refreshToken: state.session.tokens.refreshToken
         },
         state.session.remoteExecutionSession
       ]
@@ -62,8 +62,8 @@ export function* remoteExecutionSaga(): SagaIterator {
       const [tokens, session]: [any, DeviceSession | undefined] = yield select(
         (state: OverallState) => [
           {
-            accessToken: state.session.accessToken,
-            refreshToken: state.session.refreshToken
+            accessToken: state.session.tokens.accessToken,
+            refreshToken: state.session.tokens.refreshToken
           },
           state.session.remoteExecutionSession
         ]
