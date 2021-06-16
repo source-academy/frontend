c2481dc4e79e5e243a401ba4c0891cca53f6e7c1
Samuel Fang <samuelfangjw@gmail.com>
2021-06-16 20:54:45 +0800

    Fix formatting for prettier

--- src/commons/utils/Constants.ts
+++ src/commons/utils/Constants.ts
@@ -32,8 +32,7 @@ const googleAppId = process.env.REACT_APP_GOOGLE_APP_ID;
 const githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID || '';
 const githubOAuthProxyUrl = process.env.REACT_APP_GITHUB_OAUTH_PROXY_URL || '';
 const interactiveSicpData =
-  process.env.REACT_APP_INTERACTIVE_SICP_DATA_URL ||
-  'https://source-academy.github.io/sicp/'; // data for interactive-sicp (images and json files)
+  process.env.REACT_APP_INTERACTIVE_SICP_DATA_URL || 'https://source-academy.github.io/sicp/'; // data for interactive-sicp (images and json files)
 
 const authProviders: Map<string, { name: string; endpoint: string; isDefault: boolean }> =
   new Map();
