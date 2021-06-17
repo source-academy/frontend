3adfe7eb1953cd05177b8fb3e96dba5ccf2355c7
Cheng Geng <47176493+ChengGeng97@users.noreply.github.com>
2021-06-17 14:28:23 +0800

    Playground Persistence Enhancement (fix issue #1787) (#1799)
    * fix: enhanced GitHub buttons in playground
    
    * style: yarn run format

--- src/features/github/GitHubTypes.ts
+++ src/features/github/GitHubTypes.ts
@@ -2,4 +2,8 @@ export const GITHUB_OPEN_FILE = 'GITHUB_OPEN_FILE';
 export const GITHUB_SAVE_FILE = 'GITHUB_SAVE_FILE';
 export const GITHUB_SAVE_FILE_AS = 'GITHUB_SAVE_FILE_AS';
 
-export type GitHubState = 'LOGGED_IN' | 'LOGGED_OUT';
+export type GitHubSaveInfo = {
+  repoName: string;
+  filePath: string;
+  lastSaved?: Date;
+};
