2c0e24afab188e5e1e0537aae568483705488648
Samuel Fang <samuelfangjw@gmail.com>
2021-06-24 15:38:40 +0800

    Remove unused import

--- src/features/sicp/parser/__tests__/ParseJson.tsx
+++ src/features/sicp/parser/__tests__/ParseJson.tsx
@@ -1,4 +1,4 @@
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import lzString from 'lz-string';
 import { CodeSnippetProps } from 'src/pages/sicp/subcomponents/CodeSnippet';
 
