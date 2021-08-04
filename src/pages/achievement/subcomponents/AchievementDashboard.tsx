daf663f29b0526694afdb29fc2229969adb78ae2
Chow En Rong <53928333+chownces@users.noreply.github.com>
2021-08-04 13:16:51 +0800

    Update display of group name in achievements dashboard

--- src/pages/achievement/subcomponents/AchievementDashboard.tsx
+++ src/pages/achievement/subcomponents/AchievementDashboard.tsx
@@ -113,7 +113,10 @@ function Dashboard(props: DispatchProps & StateProps) {
   return (
     <AchievementContext.Provider value={inferencer}>
       <div className="AchievementDashboard">
-        <AchievementOverview name={name || 'User'} studio={group || 'Staff'} />
+        <AchievementOverview
+          name={name || 'User'}
+          studio={role === Role.Student ? (group ? group : 'Student') : 'Staff'}
+        />
         {role && role !== Role.Student && (
           <AchievementManualEditor
             userState={userIdState}
