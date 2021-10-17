52e74a53f106002464bc8bc081cbfd43cbcc4978
Jonas Chow <jonaschow99@gmail.com>
2021-10-17 13:16:34 +0800

    Add a notification when copy button is clicked

--- src/commons/achievement/control/achievementEditor/AchievementUuidCopier.tsx
+++ src/commons/achievement/control/achievementEditor/AchievementUuidCopier.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import { Tooltip2 } from '@blueprintjs/popover2';
+import { showSuccessMessage } from 'src/commons/utils/NotificationsHelper';
 
 type AchievementUuidCopierProps = {
   uuid: string;
@@ -9,8 +10,11 @@ type AchievementUuidCopierProps = {
 function AchievmenetUuidCopier(props: AchievementUuidCopierProps) {
   const { uuid } = props;
 
-  const hoverText = 'Click to copy achievement uuid';
-  const copy = () => navigator.clipboard.writeText(uuid);
+  const hoverText = 'Click to copy achievement UUID';
+  const copy = () => {
+    navigator.clipboard.writeText(uuid);
+    showSuccessMessage('UUID copied to clipboard');
+  };
 
   return (
     <>
