5b9d47a9eed7e84f0f2402326c4c77683d4306c0
Gokul Rajiv <gokul.rajiv99@gmail.com>
2021-10-17 16:18:56 +0800

    Set game achievements

--- src/features/game/scenes/roomPreview/RoomPreview.ts
+++ src/features/game/scenes/roomPreview/RoomPreview.ts
@@ -243,12 +243,15 @@ export default class RoomPreview extends Phaser.Scene {
   private createAward(x: number, y: number, awardKey: ItemId) {
     const achievements = this.getUserStateManager().getAchievements();
     const collectibles = this.getUserStateManager().getCollectibles();
-    if (achievements.includes(awardKey) || collectibles.includes(awardKey)) {
-      const awardProp = getAwardProp(awardKey);
-      const award = new Phaser.GameObjects.Sprite(this, x, y, awardProp.assetKey);
-      return this.attachVerificationTag(award);
-    }
-    return new Phaser.GameObjects.Sprite(this, x, y, ImageAssets.cookies.key);
+    const defaultAward = new Phaser.GameObjects.Sprite(this, x, y, ImageAssets.cookies.key);
+
+    if (!achievements.includes(awardKey) && !collectibles.includes(awardKey)) return defaultAward;
+
+    const awardProp = getAwardProp(awardKey);
+    if (!awardProp.completed) return defaultAward;
+
+    const award = new Phaser.GameObjects.Sprite(this, x, y, awardProp.assetKey);
+    return this.attachVerificationTag(award);
   }
 
   /**
