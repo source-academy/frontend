e4e4f49f42ac7d16f9bfdd0a044afb23d8894165
En Rong <53928333+chownces@users.noreply.github.com>
2021-12-16 13:10:55 +0800

    Format prettier

--- src/commons/sideContent/githubAssessments/SideContentEditableTestcaseCard.tsx
+++ src/commons/sideContent/githubAssessments/SideContentEditableTestcaseCard.tsx
@@ -32,82 +32,83 @@ const renderResult = (value: any) => {
   }
 };
 
-const SideContentEditableTestcaseCard: React.FunctionComponent<SideContentEditableTestcaseCardProps> =
-  props => {
-    const {
-      index,
-      testcase,
-      setTestcaseProgram,
-      setTestcaseExpectedResult,
-      handleTestcaseEval,
-      deleteTestcase
-    } = props;
+const SideContentEditableTestcaseCard: React.FunctionComponent<
+  SideContentEditableTestcaseCardProps
+> = props => {
+  const {
+    index,
+    testcase,
+    setTestcaseProgram,
+    setTestcaseExpectedResult,
+    handleTestcaseEval,
+    deleteTestcase
+  } = props;
 
-    // TODO (Refactor): testcase type seems unused in GitHub Assessments
-    const extraClasses = React.useMemo(() => {
-      const isEvaluated = testcase.result !== undefined || testcase.errors;
-      const isEqual = stringify(testcase.result) === testcase.answer;
+  // TODO (Refactor): testcase type seems unused in GitHub Assessments
+  const extraClasses = React.useMemo(() => {
+    const isEvaluated = testcase.result !== undefined || testcase.errors;
+    const isEqual = stringify(testcase.result) === testcase.answer;
 
-      return {
-        correct: isEvaluated && isEqual,
-        wrong: isEvaluated && !isEqual,
-        secret: testcase.type === TestcaseTypes.secret || testcase.type === TestcaseTypes.opaque
-      };
-    }, [testcase]);
+    return {
+      correct: isEvaluated && isEqual,
+      wrong: isEvaluated && !isEqual,
+      secret: testcase.type === TestcaseTypes.secret || testcase.type === TestcaseTypes.opaque
+    };
+  }, [testcase]);
 
-    const handleRunTestcase = React.useCallback(() => {
-      handleTestcaseEval(index);
-    }, [index, handleTestcaseEval]);
+  const handleRunTestcase = React.useCallback(() => {
+    handleTestcaseEval(index);
+  }, [index, handleTestcaseEval]);
 
-    const testProgram = testcase.program;
-    const expectedAnswer = testcase.answer;
+  const testProgram = testcase.program;
+  const expectedAnswer = testcase.answer;
 
-    const playButton = (
-      <Button className="testcase-button" icon={IconNames.PLAY} onClick={handleRunTestcase} />
-    );
-    const deleteButton = (
-      <Button
-        className="testcase-button"
-        icon={IconNames.DELETE}
-        onClick={() => deleteTestcase(index)}
-      />
-    );
+  const playButton = (
+    <Button className="testcase-button" icon={IconNames.PLAY} onClick={handleRunTestcase} />
+  );
+  const deleteButton = (
+    <Button
+      className="testcase-button"
+      icon={IconNames.DELETE}
+      onClick={() => deleteTestcase(index)}
+    />
+  );
 
-    const answer = React.useMemo(() => {
-      let answer = 'No Answer';
-      if (testcase.errors) {
-        answer = parseError(testcase.errors);
-      } else {
-        if (testcase.result !== undefined) {
-          answer = renderResult(testcase.result) as string;
-        }
+  const answer = React.useMemo(() => {
+    let answer = 'No Answer';
+    if (testcase.errors) {
+      answer = parseError(testcase.errors);
+    } else {
+      if (testcase.result !== undefined) {
+        answer = renderResult(testcase.result) as string;
       }
-      return answer;
-    }, [testcase]);
+    }
+    return answer;
+  }, [testcase]);
 
-    return (
-      <div className={classNames('EditableAutograderCard', extraClasses)}>
-        <Card className={Classes.INTERACTIVE} elevation={Elevation.ONE}>
-          {
-            <>
-              <InputGroup
-                className="testcase-program"
-                value={testProgram}
-                onChange={(event: any) => setTestcaseProgram(event.target.value)}
-              />
-              <InputGroup
-                className="testcase-expected"
-                value={expectedAnswer}
-                onChange={(event: any) => setTestcaseExpectedResult(event.target.value)}
-              />
-              <InputGroup className="testcase-actual" value={answer} readOnly={true} />
-            </>
-          }
-          {playButton}
-          {deleteButton}
-        </Card>
-      </div>
-    );
-  };
+  return (
+    <div className={classNames('EditableAutograderCard', extraClasses)}>
+      <Card className={Classes.INTERACTIVE} elevation={Elevation.ONE}>
+        {
+          <>
+            <InputGroup
+              className="testcase-program"
+              value={testProgram}
+              onChange={(event: any) => setTestcaseProgram(event.target.value)}
+            />
+            <InputGroup
+              className="testcase-expected"
+              value={expectedAnswer}
+              onChange={(event: any) => setTestcaseExpectedResult(event.target.value)}
+            />
+            <InputGroup className="testcase-actual" value={answer} readOnly={true} />
+          </>
+        }
+        {playButton}
+        {deleteButton}
+      </Card>
+    </div>
+  );
+};
 
 export default SideContentEditableTestcaseCard;
