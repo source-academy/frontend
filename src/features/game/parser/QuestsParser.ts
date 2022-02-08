b35c419be3aa9f1aafe7124d973ea698d1ad66fb
lhw-1 <leehyungwoonsamuel@gmail.com>
2022-02-08 22:42:53 +0800

    Fix Formatting errors

--- src/features/game/parser/QuestsParser.ts
+++ src/features/game/parser/QuestsParser.ts
@@ -9,16 +9,16 @@ export default class QuestsParser {
   /**
    * This function parses the strings and creates each quest
    * based on two parameters:
-   * 
+   *
    * (1) The specified objective tied to the quest
    * (2) The quest description
-   * 
+   *
    * E.g.
-   * 
+   *
    * quests
    *     talkedToScottie, Talk to Scottie - your best friend.
    *     checkedScreen, Check the monitor in your room for further instructions.
-   * 
+   *
    * @param questDetails the CSV lines containing descriptions about the quests
    */
   public static parse(questDetails: string[]) {
