b3f489527253e1eb2fd5e283700ff3f18f4b150d
ShenyiCui <shenyicui@outlook.com>
2022-08-23 04:10:47 +0800

    chore: update CA level to reflect new year
    - update CA level to reflect new year
    - decrease level from 36 to 24

--- src/commons/achievement/overview/AchievementMilestone.tsx
+++ src/commons/achievement/overview/AchievementMilestone.tsx
@@ -5,20 +5,21 @@ type AchievementMilestoneProps = {
 // 36k XP = Level 37
 function AchievementMilestone(props: AchievementMilestoneProps) {
   const { studentXp } = props;
+  const CA_FULlFILLMENT_LEVEL = process.env.REACT_APP_CA_FULFILLMENT_LEVEL;
   return (
     <div className="milestone">
       <h2>ACHIEVEMENT LEVEL</h2>
       <div className="details">
         <div className="level-badge">
           <span className="level-icon" />
-          <p>37</p>
+          <p>{CA_FULlFILLMENT_LEVEL}</p>
         </div>
         <p className="description">Complete CS1101S CA Component</p>
       </div>
       <div className="details">
         <div className="level-badge">
           <span className="level-icon" />
-          <p>37+</p>
+          <p>{CA_FULlFILLMENT_LEVEL}+</p>
         </div>
         <p className="description">Counts towards CS1010R</p>
       </div>
