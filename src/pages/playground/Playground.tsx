1edfb242ce02b53d7af6862e4e285f8061b44a7e
Ian Yong <ianyongyc@gmail.com>
2022-09-05 15:50:43 +0800

    Move draggable REPL logic out of MobileSideContent into MobileWorkspace (#2206)
    * Move REPL run logic out of MobileSideContent into MobileWorkspace
    
    * Standardise signature of handleShowRepl with that of handleHideRepl
    
    * Separate out checks in handleTabChangeForRepl for readability
    
    * Fix comment ending with a comma instead of period

--- src/pages/playground/Playground.tsx
+++ src/pages/playground/Playground.tsx
@@ -775,8 +775,7 @@ const Playground: React.FC<PlaygroundProps> = props => {
       selectedTabId: selectedTab,
       onChange: onChangeTabs,
       tabs: mobileTabs,
-      workspaceLocation: isSicpEditor ? 'sicp' : 'playground',
-      handleEditorEval: props.handleEditorEval
+      workspaceLocation: isSicpEditor ? 'sicp' : 'playground'
     }
   };
 
