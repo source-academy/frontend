7a60474443eb95f9e7874214438cb84568998b98
Ian Yong <ianyongyc@gmail.com>
2022-09-19 00:32:11 +0800

    Add SideBar panel

--- src/commons/sideBar/SideBar.tsx
+++ src/commons/sideBar/SideBar.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 
 export type SideBarTab = {
   label: string;
+  body: JSX.Element;
 };
 
 const SideBar: React.FC = () => {
   const [selectedTabIndex, setSelectedTabIndex] = React.useState<number | null>(null);
-  const tabs: SideBarTab[] = [{ label: 'Project' }, { label: 'Test' }];
+  const tabs: SideBarTab[] = [
+    { label: 'Project', body: <div>Hello World!</div> },
+    { label: 'Test', body: <div></div> }
+  ];
 
   const handleTabSelection = (tabIndex: number) => {
     if (selectedTabIndex === tabIndex) {
@@ -19,16 +23,19 @@ const SideBar: React.FC = () => {
   };
 
   return (
-    <div className="tab-container">
-      {tabs.map((tab, index) => (
-        <Card
-          key={index}
-          className={classNames('tab', { selected: selectedTabIndex === index })}
-          onClick={() => handleTabSelection(index)}
-        >
-          {tab.label}
-        </Card>
-      ))}
+    <div className="sidebar-container">
+      <div className="tab-container">
+        {tabs.map((tab, index) => (
+          <Card
+            key={index}
+            className={classNames('tab', { selected: selectedTabIndex === index })}
+            onClick={() => handleTabSelection(index)}
+          >
+            {tab.label}
+          </Card>
+        ))}
+      </div>
+      {selectedTabIndex !== null && <Card className="panel">{tabs[selectedTabIndex].body}</Card>}
     </div>
   );
 };
