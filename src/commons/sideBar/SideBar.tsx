7174e2e29ccc118243252fc7cef784de8310b3fb
Ian Yong <ianyongyc@gmail.com>
2022-09-18 23:24:43 +0800

    Add notion of SideBarTab

--- src/commons/sideBar/SideBar.tsx
+++ src/commons/sideBar/SideBar.tsx
@@ -1,11 +1,20 @@
 import { Card } from '@blueprintjs/core';
 import React from 'react';
 
+export type SideBarTab = {
+  label: string;
+};
+
 const SideBar: React.FC = () => {
+  const tabs: SideBarTab[] = [{ label: 'Project' }, { label: 'Test' }];
+
   return (
     <div className="tab-container">
-      <Card className="tab">Project</Card>
-      <Card className="tab">Test</Card>
+      {tabs.map((tab, index) => (
+        <Card key={index} className="tab">
+          {tab.label}
+        </Card>
+      ))}
     </div>
   );
 };
