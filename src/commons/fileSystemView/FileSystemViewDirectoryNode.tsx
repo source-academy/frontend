a7ac248560f8df845117f433a73cd65241c2feaf
Ian Yong <ianyongyc@gmail.com>
2022-09-26 11:01:40 +0800

    Add directory & file icons

--- src/commons/fileSystemView/FileSystemViewDirectoryNode.tsx
+++ src/commons/fileSystemView/FileSystemViewDirectoryNode.tsx
@@ -1,3 +1,5 @@
+import { Icon } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import { FSModule } from 'browserfs/dist/node/core/FS';
 import path from 'path';
 import React from 'react';
@@ -22,7 +24,9 @@ const FileSystemViewDirectoryNode: React.FC<FileSystemViewDirectoryNodeProps> = 
 
   return (
     <>
-      <div className="file-system-view-dir-node-container" onClick={toggleIsExpanded}>
+      <div className="file-system-view-node-container" onClick={toggleIsExpanded}>
+        {isExpanded && <Icon icon={IconNames.CHEVRON_DOWN} />}
+        {!isExpanded && <Icon icon={IconNames.CHEVRON_RIGHT} />}
         {dirName}
       </div>
       {isExpanded && <FileSystemViewList fileSystem={fileSystem} basePath={fullPath} />}
--- src/commons/fileSystemView/FileSystemViewFileNode.tsx
+++ src/commons/fileSystemView/FileSystemViewFileNode.tsx
@@ -1,3 +1,5 @@
+import { Icon } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
 import { FSModule } from 'browserfs/dist/node/core/FS';
 import React from 'react';
 
@@ -12,7 +14,12 @@ const FileSystemViewFileNode: React.FC<FileSystemViewFileNodeProps> = (
 ) => {
   const { fileName } = props;
 
-  return <div className="file-system-view-file-node-container">{fileName}</div>;
+  return (
+    <div className="file-system-view-node-container">
+      <Icon icon={IconNames.DOCUMENT} />
+      {fileName}
+    </div>
+  );
 };
 
 export default FileSystemViewFileNode;
