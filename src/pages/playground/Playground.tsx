source-academy/frontend

commit 96f647b73f0a71ac8f2dd9b687024e7d1c927b75

Add HTML Chapter to SA (#2227)
* Add HTML chapter

* Add warning popup to shared HTML code

* Route console errors to output

* Modify desktop styling

* Add iframe error formatting check

* Add SideContentHtmlDisplay unit tests

* Fix app crash on message handling

* Fix naming

* Fix code quality

* Update code comments

* Update HTML Chapter description

* Fix mobile HTML display size

src/pages/playground/Playground.tsx
@@ -13,10 +13,14 @@ import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import { RouteComponentProps, useHistory, useLocation } from 'react-router';
 import { showFullJSWarningOnUrlLoad } from 'src/commons/fullJS/FullJSUtils';
+import { showHTMLDisclaimer } from 'src/commons/html/HTMLUtils';
+import SideContentHtmlDisplay from 'src/commons/sideContent/SideContentHtmlDisplay';
 
 import {
   InterpreterOutput,
+  isSourceLanguage,
   OverallState,
+  ResultOutput,
   sourceLanguages
 } from '../../commons/application/ApplicationTypes';
 import { ExternalLibraryName } from '../../commons/application/types/ExternalTypes';
@@ -64,6 +68,7 @@ export type OwnProps = {
 };
 
 export type DispatchProps = {
+  handleAddHtmlConsoleError: (errorMsg: string) => void;
   handleBrowseHistoryDown: () => void;
   handleBrowseHistoryUp: () => void;
   handleChangeExecTime: (execTime: number) => void;
@@ -138,13 +143,19 @@ export type StateProps = {
 
 const keyMap = { goGreen: 'h u l k' };
 
-export function handleHash(hash: string, props: PlaygroundProps) {
+export async function handleHash(hash: string, props: PlaygroundProps) {
   const qs = parseQuery(hash);
 
   const chapter = stringParamToInt(qs.chap) || undefined;
   if (chapter === Chapter.FULL_JS) {
     showFullJSWarningOnUrlLoad();
   } else {
+    if (chapter === Chapter.HTML) {
+      const continueToHtml = await showHTMLDisclaimer();
+      if (!continueToHtml) {
+        return;
+      }
+    }
     const programLz = qs.lz ?? qs.prgrm;
     const program = programLz && decompressFromEncodedURIComponent(programLz);
     if (program) {
@@ -341,8 +352,8 @@ const Playground: React.FC<PlaygroundProps> = props => {
         sourceChapter={props.playgroundSourceChapter}
         key="autorun"
         autorunDisabled={usingRemoteExecution}
-        // Disable pause for FullJS, because: one cannot stop `eval()`
-        pauseDisabled={usingRemoteExecution || props.playgroundSourceChapter === Chapter.FULL_JS}
+        // Disable pause for non-Source languages since they cannot be paused
+        pauseDisabled={usingRemoteExecution || !isSourceLanguage(props.playgroundSourceChapter)}
       />
     ),
     [
@@ -568,6 +579,25 @@ const Playground: React.FC<PlaygroundProps> = props => {
   const tabs = React.useMemo(() => {
     const tabs: SideContentTab[] = [playgroundIntroductionTab];
 
+    // For HTML Chapter, HTML Display tab is added only after code is run
+    if (props.playgroundSourceChapter === Chapter.HTML) {
+      if (props.output.length > 0 && props.output[0].type === 'result') {
+        tabs.push({
+          label: 'HTML Display',
+          iconName: IconNames.MODAL,
+          body: (
+            <SideContentHtmlDisplay
+              content={(props.output[0] as ResultOutput).value}
+              handleAddHtmlConsoleError={props.handleAddHtmlConsoleError}
+            />
+          ),
+          id: SideContentType.htmlDisplay,
+          toSpawn: () => true
+        });
+      }
+      return tabs;
+    }
+
     // (TEMP) Remove tabs for fullJS until support is integrated
     if (props.playgroundSourceChapter === Chapter.FULL_JS) {
       return [...tabs, dataVisualizerTab];
@@ -614,7 +644,8 @@ const Playground: React.FC<PlaygroundProps> = props => {
     props.playgroundSourceChapter,
     props.playgroundSourceVariant,
     usingRemoteExecution,
-    remoteExecutionTab
+    remoteExecutionTab,
+    props.handleAddHtmlConsoleError
   ]);
 
   // Remove Intro and Remote Execution tabs for mobile
@@ -705,7 +736,10 @@ const Playground: React.FC<PlaygroundProps> = props => {
     [selectedTab]
   );
 
-  const replDisabled = props.playgroundSourceVariant === Variant.CONCURRENT || usingRemoteExecution;
+  const replDisabled =
+    props.playgroundSourceChapter === Chapter.HTML ||
+    props.playgroundSourceVariant === Variant.CONCURRENT ||
+    usingRemoteExecution;
 
   const editorProps = {
     onChange: onChangeMethod,
@@ -756,7 +790,7 @@ const Playground: React.FC<PlaygroundProps> = props => {
         isSicpEditor ? null : sessionButtons,
         persistenceButtons,
         githubButtons,
-        usingRemoteExecution || props.playgroundSourceChapter === Chapter.FULL_JS
+        usingRemoteExecution || !isSourceLanguage(props.playgroundSourceChapter)
           ? null
           : props.usingSubst
           ? stepperStepLimit

src/pages/playground/__tests__/Playground.tsx
@@ -30,6 +30,7 @@ const baseProps = {
   persistenceFile: undefined,
   githubOctokitObject: { octokit: undefined },
   githubSaveInfo: { repoName: '', filePath: '' },
+  handleAddHtmlConsoleError: (errorMsg: string) => {},
   handleBrowseHistoryDown: () => {},
   handleBrowseHistoryUp: () => {},
   handleChangeExecTime: (execTime: number) => {},

src/commons/application/ApplicationTypes.ts
@@ -124,12 +124,23 @@ export const fullJSLanguage: SALanguage = {
   displayName: 'full JavaScript'
 };
 
+export const htmlLanguage: SALanguage = {
+  chapter: Chapter.HTML,
+  variant: Variant.DEFAULT,
+  displayName: 'HTML'
+};
+
 export const styliseSublanguage = (chapter: Chapter, variant: Variant = Variant.DEFAULT) => {
-  return chapter === Chapter.FULL_JS
-    ? fullJSLanguage.displayName
-    : `Source \xa7${chapter}${
+  switch (chapter) {
+    case Chapter.FULL_JS:
+      return fullJSLanguage.displayName;
+    case Chapter.HTML:
+      return htmlLanguage.displayName;
+    default:
+      return `Source \xa7${chapter}${
         variantDisplay.has(variant) ? ` ${variantDisplay.get(variant)}` : ''
       }`;
+  }
 };
 
 export const sublanguages: Language[] = [
@@ -159,9 +170,13 @@ export const sourceLanguages: SALanguage[] = sublanguages.map(sublang => {
 export const defaultLanguages = sourceLanguages.filter(
   sublang => sublang.variant === Variant.DEFAULT
 );
+
 export const variantLanguages = sourceLanguages.filter(
   sublang => sublang.variant !== Variant.DEFAULT
 );
+
+export const isSourceLanguage = (chapter: Chapter) =>
+  [Chapter.SOURCE_1, Chapter.SOURCE_2, Chapter.SOURCE_3, Chapter.SOURCE_4].includes(chapter);
 
 const currentEnvironment = (): ApplicationEnvironment => {
   switch (process.env.NODE_ENV) {

src/commons/controlBar/ControlBarChapterSelect.tsx
@@ -7,6 +7,7 @@ import * as React from 'react';
 import {
   defaultLanguages,
   fullJSLanguage,
+  htmlLanguage,
   SALanguage,
   sourceLanguages,
   styliseSublanguage,
@@ -36,10 +37,12 @@ export function ControlBarChapterSelect(props: ControlBarChapterSelectProps) {
     const defaultChoices = defaultLanguages.map(renderItem);
     const variantChoices = variantLanguages.map(renderItem);
     const fullJSChoice = renderItem(fullJSLanguage, 0);
+    const htmlChoice = renderItem(htmlLanguage, 0);
     return (
       <Menu ulRef={itemsParentRef}>
         {defaultChoices}
         {Constants.playgroundOnly && fullJSChoice}
+        {Constants.playgroundOnly && htmlChoice}
         <MenuItem key="variant-menu" text="Variants" icon="cog">
           {variantChoices}
         </MenuItem>

src/commons/html/HTMLUtils.tsx
@@ -1,0 +1,25 @@
+import { showSimpleConfirmDialog, SimpleConfirmDialogProps } from '../utils/DialogHelper';
+
+const DISCLAIMER_DIALOG_PROPS: SimpleConfirmDialogProps = {
+  icon: 'warning-sign',
+  title: 'Beware when running shared HTML code!',
+  contents: (
+    <p>
+      You are about to access HTML code written by others, which may contain malicious scripts.
+      <br />
+      <br />
+      This might pose a security concern if you are not careful and fully aware of what the shared
+      HTML code contains!
+      <br />
+      <br />
+      <strong>Do you still want to proceed?</strong>
+    </p>
+  ),
+  positiveIntent: 'danger',
+  positiveLabel: 'Proceed',
+  negativeLabel: 'Cancel'
+};
+
+export function showHTMLDisclaimer(): Promise<boolean> {
+  return showSimpleConfirmDialog(DISCLAIMER_DIALOG_PROPS);
+}

src/commons/repl/Repl.tsx
@@ -40,7 +40,12 @@ type OwnProps = {
 
 const Repl = React.forwardRef<AceEditor, ReplProps>((props, ref) => {
   const cards = props.output.map((slice, index) => (
-    <Output output={slice} key={index} usingSubst={props.usingSubst || false} />
+    <Output
+      output={slice}
+      key={index}
+      usingSubst={props.usingSubst ?? false}
+      isHtml={props.sourceChapter === Chapter.HTML}
+    />
   ));
   return (
     <div className="Repl" style={{ display: props.hidden ? 'none' : undefined }}>
@@ -64,13 +69,13 @@ export const Output: React.FC<OutputProps> = (props: OutputProps) => {
     case 'code':
       return (
         <Card>
-          <Pre className="codeOutput">{props.output.value}</Pre>
+          <Pre className="code-output">{props.output.value}</Pre>
         </Card>
       );
     case 'running':
       return (
         <Card>
-          <Pre className="logOutput">{props.output.consoleLogs.join('\n')}</Pre>
+          <Pre className="log-output">{props.output.consoleLogs.join('\n')}</Pre>
         </Card>
       );
     case 'result':
@@ -78,20 +83,26 @@ export const Output: React.FC<OutputProps> = (props: OutputProps) => {
       if (props.usingSubst && props.output.value instanceof Array) {
         return (
           <Card>
-            <Pre className="logOutput">Check out the Stepper tab!</Pre>
+            <Pre className="log-output">Check out the Stepper tab!</Pre>
+          </Card>
+        );
+      } else if (props.isHtml) {
+        return (
+          <Card>
+            <Pre className="log-output">Check out the HTML Display tab!</Pre>
           </Card>
         );
       } else if (props.output.consoleLogs.length === 0) {
         return (
           <Card>
-            <Pre className="resultOutput">{renderResult(props.output.value)}</Pre>
+            <Pre className="result-output">{renderResult(props.output.value)}</Pre>
           </Card>
         );
       } else {
         return (
           <Card>
-            <Pre className="logOutput">{props.output.consoleLogs.join('\n')}</Pre>
-            <Pre className="resultOutput">{renderResult(props.output.value)}</Pre>
+            <Pre className="log-output">{props.output.consoleLogs.join('\n')}</Pre>
+            <Pre className="result-output">{renderResult(props.output.value)}</Pre>
           </Card>
         );
       }
@@ -99,15 +110,15 @@ export const Output: React.FC<OutputProps> = (props: OutputProps) => {
       if (props.output.consoleLogs.length === 0) {
         return (
           <Card>
-            <Pre className="errorOutput">{parseError(props.output.errors)}</Pre>
+            <Pre className="error-output">{parseError(props.output.errors)}</Pre>
           </Card>
         );
       } else {
         return (
           <Card>
-            <Pre className="logOutput">{props.output.consoleLogs.join('\n')}</Pre>
+            <Pre className="log-output">{props.output.consoleLogs.join('\n')}</Pre>
             <br />
-            <Pre className="errorOutput">{parseError(props.output.errors)}</Pre>
+            <Pre className="error-output">{parseError(props.output.errors)}</Pre>
           </Card>
         );
       }

src/commons/repl/ReplTypes.ts
@@ -3,4 +3,5 @@ import { InterpreterOutput } from '../application/ApplicationTypes';
 export type OutputProps = {
   output: InterpreterOutput;
   usingSubst?: boolean;
+  isHtml?: boolean;
 };

src/commons/repl/__tests__/__snapshots__/Repl.tsx.snap
@@ -2,7 +2,7 @@
 
 exports[`Code output renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"codeOutput\\">
+  <Component className=\\"code-output\\">
     display(&#39;&#39;);
   </Component>
 </Blueprint4.Card>"
@@ -16,7 +16,7 @@ exports[`Empty output renders an empty card 1`] = `
 
 exports[`Error output (no consoleLogs) renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"errorOutput\\">
+  <Component className=\\"error-output\\">
     Line &lt;unknown&gt;: Expected , got .
   </Component>
 </Blueprint4.Card>"
@@ -24,14 +24,14 @@ exports[`Error output (no consoleLogs) renders correctly 1`] = `
 
 exports[`Error output (with consoleLogs) renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"logOutput\\">
+  <Component className=\\"log-output\\">
     a
     bb
     cccccccccccccccccccccccccccccccc
     d
   </Component>
   <br />
-  <Component className=\\"errorOutput\\">
+  <Component className=\\"error-output\\">
     Line &lt;unknown&gt;: Expected , got .
   </Component>
 </Blueprint4.Card>"
@@ -40,10 +40,10 @@ exports[`Error output (with consoleLogs) renders correctly 1`] = `
 exports[`Repl renders correctly 1`] = `
 "<div className=\\"Repl\\" style={{...}}>
   <div className=\\"repl-output-parent\\">
-    <Output output={{...}} usingSubst={false} />
-    <Output output={{...}} usingSubst={false} />
-    <Output output={{...}} usingSubst={false} />
-    <Output output={{...}} usingSubst={false} />
+    <Output output={{...}} usingSubst={false} isHtml={false} />
+    <Output output={{...}} usingSubst={false} isHtml={false} />
+    <Output output={{...}} usingSubst={false} isHtml={false} />
+    <Output output={{...}} usingSubst={false} isHtml={false} />
     <HotKeysEnabled className=\\"repl-input-parent row bp4-card bp4-elevation-0\\" handlers={{...}}>
       <ForwardRef handleBrowseHistoryDown={[Function: handleBrowseHistoryDown]} handleBrowseHistoryUp={[Function: handleBrowseHistoryUp]} handleReplValueChange={[Function: handleReplValueChange]} handleReplEval={[Function: handleReplEval]} handleReplOutputClear={[Function: handleReplOutputClear]} output={{...}} replValue=\\"\\" sourceChapter={1} sourceVariant=\\"default\\" externalLibrary=\\"NONE\\" replButtons={{...}} />
     </HotKeysEnabled>
@@ -53,7 +53,7 @@ exports[`Repl renders correctly 1`] = `
 
 exports[`Result output (no consoleLogs) renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"resultOutput\\">
+  <Component className=\\"result-output\\">
     42
   </Component>
 </Blueprint4.Card>"
@@ -61,13 +61,13 @@ exports[`Result output (no consoleLogs) renders correctly 1`] = `
 
 exports[`Result output (with consoleLogs) renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"logOutput\\">
+  <Component className=\\"log-output\\">
     a
     bb
     cccccccccccccccccccccccccccccccc
     d
   </Component>
-  <Component className=\\"resultOutput\\">
+  <Component className=\\"result-output\\">
     42
   </Component>
 </Blueprint4.Card>"
@@ -75,7 +75,7 @@ exports[`Result output (with consoleLogs) renders correctly 1`] = `
 
 exports[`Running output renders correctly 1`] = `
 "<Blueprint4.Card elevation={0} interactive={false}>
-  <Component className=\\"logOutput\\">
+  <Component className=\\"log-output\\">
     a
     bb
     cccccccccccccccccccccccccccccccc

commit 8d92d3e9335945cc29a298c14427b5c05083100c

Fix rebase errors introduced in #2227

src/pages/playground/Playground.tsx
@@ -219,8 +219,7 @@ const Playground: React.FC<PlaygroundProps> = props => {
           callbackFunction={setDeviceSecret}
         />
       ),
-      id: SideContentType.remoteExecution,
-      toSpawn: () => true
+      id: SideContentType.remoteExecution
     }),
     [deviceSecret]
   );
@@ -590,8 +589,7 @@ const Playground: React.FC<PlaygroundProps> = props => {
               handleAddHtmlConsoleError={props.handleAddHtmlConsoleError}
             />
           ),
-          id: SideContentType.htmlDisplay,
-          toSpawn: () => true
+          id: SideContentType.htmlDisplay
         });
       }
       return tabs;