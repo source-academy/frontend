d437cb266e3f603f91fcd1cfb83d7657a8e2ee26
shenyihong <shen_yi_hong@outlook.com>
2022-12-01 22:53:37 +0800

    fix(repl): shift `stringify` call to reducers

--- src/commons/workspace/WorkspaceReducer.ts
+++ src/commons/workspace/WorkspaceReducer.ts
@@ -1,3 +1,4 @@
+import { stringify } from 'js-slang/dist/utils/stringify';
 import { Reducer } from 'redux';
 
 import { SourcecastReducer } from '../../features/sourceRecorder/sourcecast/SourcecastReducer';
@@ -332,20 +333,24 @@ export const WorkspaceReducer: Reducer<WorkspaceManagerState> = (
         }
       };
     case EVAL_INTERPRETER_SUCCESS:
+      const newOutputEntry: Partial<ResultOutput> = {
+        type: 'result',
+        value: stringify(action.payload.value)
+      };
+
       lastOutput = state[workspaceLocation].output.slice(-1)[0];
       if (lastOutput !== undefined && lastOutput.type === 'running') {
         newOutput = state[workspaceLocation].output.slice(0, -1).concat({
-          type: action.payload.type,
-          value: action.payload.value,
-          consoleLogs: lastOutput.consoleLogs
+          consoleLogs: lastOutput.consoleLogs,
+          ...newOutputEntry
         } as ResultOutput);
       } else {
         newOutput = state[workspaceLocation].output.concat({
-          type: action.payload.type,
-          value: action.payload.value,
-          consoleLogs: []
+          consoleLogs: [],
+          ...newOutputEntry
         } as ResultOutput);
       }
+
       return {
         ...state,
         [workspaceLocation]: {
