56a282676b48dcc5e8c7a1bef320c2f5f8bd3075
Ian Yong <ianyongyc@gmail.com>
2022-12-10 19:10:33 +0800

    Do not reset breakpoints on program evaluation

--- src/commons/workspace/WorkspaceReducer.ts
+++ src/commons/workspace/WorkspaceReducer.ts
@@ -354,9 +354,7 @@ export const WorkspaceReducer: Reducer<WorkspaceManagerState> = (
           output: newOutput,
           isRunning: false,
           // TODO: Hardcoded to make use of the first editor tab. Rewrite after editor tabs are added.
-          editorTabs: [
-            { ...state[workspaceLocation].editorTabs[0], highlightedLines: [], breakpoints: [] }
-          ]
+          editorTabs: [{ ...state[workspaceLocation].editorTabs[0], highlightedLines: [] }]
         }
       };
     case EVAL_TESTCASE_SUCCESS:
