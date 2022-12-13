32b8b42a0433e52d5d977c3a3f3ebefc03acb18f
Ian Yong <ianyongyc@gmail.com>
2022-12-13 23:26:06 +0800

    Remove ref forwarding in SourceRecorderEditor

--- src/commons/sourceRecorder/SourceRecorderEditor.tsx
+++ src/commons/sourceRecorder/SourceRecorderEditor.tsx
@@ -5,7 +5,6 @@ import 'js-slang/dist/editors/ace/theme/source';
 import { isEqual } from 'lodash';
 import * as React from 'react';
 import AceEditor, { IAceEditorProps } from 'react-ace';
-import ReactAce from 'react-ace/lib/ace';
 import { HotKeys } from 'react-hotkeys';
 
 import {
@@ -51,7 +50,6 @@ type StateProps = {
 };
 
 type OwnProps = {
-  forwardedRef?: React.RefObject<ReactAce>; // for the mobile Sourcecast Workspace
   setDraggableReplPosition?: () => void; // for the mobile Sourcecast Workspace
 };
 
@@ -220,7 +218,7 @@ class SourcecastEditor extends React.PureComponent<SourceRecorderEditorProps, {}
             editorProps={{
               $blockScrolling: Infinity
             }}
-            ref={mergeRefs(this.AceEditor, this.props.forwardedRef)}
+            ref={this.AceEditor}
             markers={this.getMarkers()}
             fontSize={17}
             height="100%"
@@ -312,31 +310,6 @@ class SourcecastEditor extends React.PureComponent<SourceRecorderEditorProps, {}
 /* Override handler, so does not trigger when focus is in editor */
 const handlers = {
   goGreen: () => {}
-};
-
-/**
- * Custom mergeRef function for class components.
- * For functional components, please use useMergedRef defined in commons/utils/Hooks.ts
- *
- * This function is defined here as it is used only in SourceRecorderEditor.tsx
- * It will unlikely be used elsewhere since we are migrating to React Hooks.
- */
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const mergeRefs = (...refs) => {
-  const filteredRefs = refs.filter(Boolean);
-  if (!filteredRefs.length) return null;
-  if (filteredRefs.length === 1) return filteredRefs[0];
-  return (inst: any) => {
-    for (const ref of filteredRefs) {
-      if (typeof ref === 'function') {
-        ref(inst);
-      } else if (ref) {
-        ref.current = inst;
-      }
-    }
-  };
 };
 
 export default SourcecastEditor;
