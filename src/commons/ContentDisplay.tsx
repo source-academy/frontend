bef6ee8c827979e0276784f82039a64394a72a2c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-12-15 00:11:24 +0700

    Move to functional components (Part I) (#2259)
    * Use FC for content display
    
    * Use FC for autograder tab
    
    * Update component function expression
    
    * Use FC for Source recorder delete cell
    
    * Improve code quality
    
    * Use FC for dashboard
    
    * Use FC for mission control
    
    Also remove unused `editOverview` state variable.
    
    * Remove redundant function
    
    * Use FC for manage question tab
    
    * Remove redundant function
    
    * Use FC for grading actions cell
    
    Also removed unused `isAlertOpen` state variable.
    
    * Use FC for grading tab
    
    * Remove redundant function
    
    * Use FC for MCQ question tab
    
    * Improve code quality
    
    * Use FC for profile
    
    Co-authored-by: En Rong <53928333+chownces@users.noreply.github.com>

--- src/commons/ContentDisplay.tsx
+++ src/commons/ContentDisplay.tsx
@@ -1,5 +1,5 @@
 import { Card, Elevation } from '@blueprintjs/core';
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 export type ContentDisplayProps = {
   fullWidth?: boolean;
@@ -7,26 +7,23 @@ export type ContentDisplayProps = {
   loadContentDispatch: () => void;
 };
 
-class ContentDisplay extends React.Component<ContentDisplayProps, {}> {
-  public componentDidMount() {
-    this.props.loadContentDispatch();
-  }
+const ContentDisplay: React.FC<ContentDisplayProps> = props => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => props.loadContentDispatch(), []);
 
-  public render() {
-    return (
-      <div className="ContentDisplay row center-xs">
-        <div
-          className={`${
-            this.props.fullWidth ? 'col-md-12' : 'col-md-10 col-xs-11'
-          } contentdisplay-content-parent`}
-        >
-          <Card className="contentdisplay-content" elevation={Elevation.THREE}>
-            {this.props.display}
-          </Card>
-        </div>
+  return (
+    <div className="ContentDisplay row center-xs">
+      <div
+        className={`${
+          props.fullWidth ? 'col-md-12' : 'col-md-10 col-xs-11'
+        } contentdisplay-content-parent`}
+      >
+        <Card className="contentdisplay-content" elevation={Elevation.THREE}>
+          {props.display}
+        </Card>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ContentDisplay;
