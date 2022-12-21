de3365c8fedaa302cd18d591041d7a04dd19c8d7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-12-21 22:16:15 +0700

    Use FC for grading editor

--- src/pages/academy/grading/subcomponents/GradingEditor.tsx
+++ src/pages/academy/grading/subcomponents/GradingEditor.tsx
@@ -10,7 +10,7 @@ import {
   Pre
 } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMde, { ReactMdeProps } from 'react-mde';
 import { Prompt } from 'react-router';
 
@@ -52,196 +52,50 @@ type OwnProps = {
   gradedAt?: string;
 };
 
-/**
- * Keeps track of the current editor state
- *
- * @prop xpAdjustmentInput a potentially null string which defines the
- *   result for the number XP input. This property being null
- *   will show the hint text in the NumericInput. This property is a string
- *   so as to allow input such as the '-' character.
- *
- * @prop editorValue the text in the react-mde editor, that will be saved
- *   to a comment displayed below the numerical XP
- *
- * @prop selectedTab the selected tab for the react-mde editor
- *   (either 'write' or 'preview')
- *
- * @prop currentlySaving determines whether the 'You have unsaved changes'
- *   prompt should appear on page navigation, to prevent the
- *   'Save and Continue' button from activating the prompt
- *   in cases where navigation occurs before Redux has
- *   updated the props of the Editor component
- *
- *   This may pose a problem if the user clicks 'Save and Continue'
- *   and the saving process fails. The prompt would no longer
- *   appear although there exist unsaved changes
- */
-type State = {
-  xpAdjustmentInput: string | null;
-  editorValue: string;
-  selectedTab: ReactMdeProps['selectedTab'];
-  currentlySaving: boolean;
-};
-
 const gradingEditorButtonClass = 'grading-editor-button';
 
-class GradingEditor extends React.Component<GradingEditorProps, State> {
-  constructor(props: GradingEditorProps) {
-    super(props);
-    this.state = this.makeInitialState();
-  }
+const GradingEditor: React.FC<GradingEditorProps> = props => {
+  /**
+   * A potentially null string which defines the
+   * result for the number XP input. This property being null
+   * will show the hint text in the NumericInput. This property is a string
+   * so as to allow input such as the '-' character.
+   */
+  const [xpAdjustmentInput, setXpAdjustmentInput] = useState<string | null>(
+    props.xpAdjustment.toString()
+  );
+  /**
+   * The text in the react-mde editor, that will be saved
+   * to a comment displayed below the numerical XP */
+  const [editorValue, setEditorValue] = useState(props.comments);
+  /**
+   * The selected tab for the react-mde editor (either 'write' or 'preview')
+   */
+  const [selectedTab, setSelectedTab] = useState<ReactMdeProps['selectedTab']>('write');
+  /**
+   * Determines whether the 'You have unsaved changes'
+   * prompt should appear on page navigation, to prevent the
+   * 'Save and Continue' button from activating the prompt
+   * in cases where navigation occurs before Redux has
+   * updated the props of the Editor component
+   *
+   * This may pose a problem if the user clicks 'Save and Continue'
+   * and the saving process fails. The prompt would no longer
+   * appear although there exist unsaved changes
+   */
+  const [currentlySaving, setCurrentlySaving] = useState(false);
 
-  public render() {
-    const hasUnsavedChanges = this.hasUnsavedChanges();
-    const saveButtonOpts = {
-      intent: hasUnsavedChanges ? Intent.WARNING : Intent.NONE,
-      minimal: !hasUnsavedChanges,
-      className: gradingEditorButtonClass
-    };
-    const discardButtonOpts = {
-      intent: hasUnsavedChanges ? Intent.DANGER : Intent.NONE,
-      minimal: !hasUnsavedChanges,
-      className: gradingEditorButtonClass
-    };
-    const saveAndContinueButtonOpts = {
-      intent: hasUnsavedChanges ? Intent.SUCCESS : Intent.NONE,
-      minimal: !hasUnsavedChanges,
-      className: gradingEditorButtonClass
-    };
-    const onTabChange = (tab: ReactMdeProps['selectedTab']) =>
-      this.setState({
-        ...this.state,
-        selectedTab: tab
-      });
+  useEffect(() => {
+    makeInitialState();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.submissionId, props.questionId]);
 
-    // Derived values
-    const totalXp =
-      this.props.initialXp + (stringParamToInt(this.state.xpAdjustmentInput || undefined) || 0);
-    const xpPlaceholder = `${this.props.initialXp > 0 ? '-' : ''}${this.props.initialXp} to ${
-      this.props.maxXp - this.props.initialXp
-    }`;
-
-    return (
-      <div className="GradingEditor">
-        {!this.state.currentlySaving && hasUnsavedChanges ? (
-          <Prompt message={'You have unsaved changes. Are you sure you want to leave?'} />
-        ) : null}
-
-        <div className="grading-editor-header">
-          <H3>Currently Grading: {this.props.studentName}</H3>
-        </div>
-        {this.props.solution !== null ? (
-          <div className="grading-editor-marking-scheme">
-            <Pre>{this.props.solution.toString()} </Pre>
-          </div>
-        ) : null}
-
-        <div className="grading-editor-container">
-          <div className="grading-editor-xp">
-            <div className="autograder-xp">
-              <div>Autograder XP:</div>
-              <div>
-                {`${this.props.initialXp} / ${this.props.maxXp}`}{' '}
-                <Button
-                  icon="refresh"
-                  small
-                  minimal
-                  onClick={this.onClickReautogradeAnswer}
-                ></Button>
-              </div>
-            </div>
-            <div className="xp-adjustment">
-              <div>XP adjustment:</div>
-              <div>
-                <NumericInput
-                  className="adjustment-input"
-                  onValueChange={this.onXpAdjustmentInputChange}
-                  value={this.state.xpAdjustmentInput || ''}
-                  buttonPosition={Position.RIGHT}
-                  fill={true}
-                  placeholder={xpPlaceholder}
-                  intent={totalXp < 0 || totalXp > this.props.maxXp ? Intent.DANGER : Intent.NONE}
-                  min={0 - this.props.initialXp}
-                  max={
-                    this.props.maxXp > this.props.initialXp
-                      ? this.props.maxXp - this.props.initialXp
-                      : undefined
-                  }
-                  stepSize={50}
-                  minorStepSize={25}
-                  majorStepSize={100}
-                />
-              </div>
-            </div>
-            <div className="final-xp">
-              <div>Final XP:</div>
-              <div>{`${totalXp} / ${this.props.maxXp}`}</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="react-mde-parent">
-          <ReactMde
-            value={this.state.editorValue}
-            onChange={this.handleEditorValueChange}
-            selectedTab={this.state.selectedTab}
-            onTabChange={onTabChange}
-            generateMarkdownPreview={this.generateMarkdownPreview}
-            minEditorHeight={200}
-            maxEditorHeight={1000}
-            minPreviewHeight={240}
-            getIcon={this.blueprintIconProvider}
-          />
-        </div>
-
-        {this.state.selectedTab === 'write' && (
-          <div className="grading-editor-draft-buttons">
-            <div className="grading-editor-save-button">
-              {controlButton(
-                'Save Changes',
-                IconNames.FLOPPY_DISK,
-                this.validateXpBeforeSave(this.props.handleGradingSave),
-                saveButtonOpts
-              )}
-            </div>
-            <div className="grading-editor-discard-button">
-              {controlButton(
-                'Discard Changes',
-                IconNames.TRASH,
-                this.discardChanges,
-                discardButtonOpts
-              )}
-            </div>
-          </div>
-        )}
-        <div className="grading-editor-save-continue-button">
-          {controlButton(
-            'Save and Continue',
-            IconNames.UPDATED,
-            this.validateXpBeforeSave(this.onClickSaveAndContinue),
-            saveAndContinueButtonOpts
-          )}
-        </div>
-        {this.props.graderName && this.props.gradedAt && (
-          <>
-            <Divider />
-            <div className="grading-editor-last-graded-details">
-              Last edited by <b>{this.props.graderName}</b> on {getPrettyDate(this.props.gradedAt)}
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
-
-  public componentDidUpdate(prevProps: GradingEditorProps, prevState: State) {
-    if (
-      prevProps.submissionId !== this.props.submissionId ||
-      prevProps.questionId !== this.props.questionId
-    ) {
-      this.setState(this.makeInitialState());
-    }
-  }
+  const makeInitialState = () => {
+    setXpAdjustmentInput(props.xpAdjustment.toString());
+    setEditorValue(props.comments);
+    setSelectedTab('write');
+    setCurrentlySaving(false);
+  };
 
   /**
    * A custom icons provider. It uses a bulky mapping function
@@ -249,7 +103,7 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
    *
    * See {@link https://github.com/andrerpena/react-mde}
    */
-  private blueprintIconProvider(name: string) {
+  function blueprintIconProvider(name: string) {
     const blueprintIcon = mdeToBlueprintIconMapping(name);
     return <Icon icon={blueprintIcon.iconName} htmlTitle={blueprintIcon.title} />;
   }
@@ -259,24 +113,18 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
    * returning the relevant saving function (for the 'Save Draft'
    * and 'Submit and Continue' buttons)
    */
-  private validateXpBeforeSave =
+  const validateXpBeforeSave =
     (handleSaving: GradingSaveFunction): (() => void) =>
     () => {
-      const xpAdjustmentInput =
-        stringParamToInt(this.state.xpAdjustmentInput || undefined) || undefined;
-      const xp = this.props.initialXp + (xpAdjustmentInput || 0);
-      if (xp < 0 || xp > this.props.maxXp) {
+      const newXpAdjustmentInput = stringParamToInt(xpAdjustmentInput || undefined) || undefined;
+      const xp = props.initialXp + (newXpAdjustmentInput || 0);
+      if (xp < 0 || xp > props.maxXp) {
         showWarningMessage(
-          `XP ${xp.toString()} is out of bounds. Maximum xp is ${this.props.maxXp.toString()}.`
+          `XP ${xp.toString()} is out of bounds. Maximum xp is ${props.maxXp.toString()}.`
         );
         return;
       } else {
-        handleSaving(
-          this.props.submissionId,
-          this.props.questionId,
-          xpAdjustmentInput,
-          this.state.editorValue
-        );
+        handleSaving(props.submissionId, props.questionId, newXpAdjustmentInput, editorValue);
       }
     };
 
@@ -284,19 +132,21 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
    * Sets the state currentlySaving to true to disable
    * the 'You have unsaved changes' prompt
    */
-  private onClickSaveAndContinue: GradingSaveFunction = (
+  const onClickSaveAndContinue: GradingSaveFunction = (
     submissionId: number,
     questionId: number,
     xpAdjustment: number | undefined,
     comments?: string
   ) => {
     const callback = (): void => {
-      this.props.handleGradingSaveAndContinue(submissionId, questionId, xpAdjustment, comments!);
+      props.handleGradingSaveAndContinue(submissionId, questionId, xpAdjustment, comments!);
     };
-    this.setState({ ...this.state, currentlySaving: true }, callback);
+    setCurrentlySaving(true);
+    // TODO: Check (not sure how) if this results in a regression.
+    callback();
   };
 
-  private onClickReautogradeAnswer = async () => {
+  const onClickReautogradeAnswer = async () => {
     const confirm = await showSimpleConfirmDialog({
       contents: (
         <>
@@ -308,7 +158,7 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
       positiveIntent: 'danger'
     });
     if (confirm) {
-      this.props.handleReautogradeAnswer(this.props.submissionId, this.props.questionId);
+      props.handleReautogradeAnswer(props.submissionId, props.questionId);
     }
   };
 
@@ -316,18 +166,12 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
    * Send a warning prompt that loading from a local draft
    * will overwrite any unsaved changes
    */
-  private discardChanges = (): void => {
-    if (!this.hasUnsavedChanges() || window.confirm('This will reset the editor. Are you sure?')) {
-      this.setState(
-        {
-          ...this.state,
-          xpAdjustmentInput: this.props.xpAdjustment!.toString(),
-          editorValue: this.props.comments
-        },
-        () => {
-          showSuccessMessage('Discarded!', 1000);
-        }
-      );
+  const discardChanges = (): void => {
+    if (!checkHasUnsavedChanges() || window.confirm('This will reset the editor. Are you sure?')) {
+      setXpAdjustmentInput(props.xpAdjustment!.toString());
+      setEditorValue(props.comments);
+      // TODO: Check (not sure how) if this results in a regression.
+      showSuccessMessage('Discarded!', 1000);
     }
   };
 
@@ -337,29 +181,21 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
    * @param valueAsNumber an unused parameter, as we use strings for the input. @see State
    * @param valueAsString a string that contains the input. To be parsed by another function.
    */
-  private onXpAdjustmentInputChange = (valueAsNumber: number, valueAsString: string | null) => {
-    this.setState({
-      ...this.state,
-      xpAdjustmentInput: valueAsString
-    });
+  const onXpAdjustmentInputChange = (valueAsNumber: number, valueAsString: string | null) => {
+    // FIXME: Null safety violation after converting to FC.
+    setXpAdjustmentInput(valueAsString!);
   };
 
-  private handleEditorValueChange = (editorValue: string) => {
-    this.setState({
-      ...this.state,
-      editorValue
-    });
+  const handleEditorValueChange = (editorValue: string) => {
+    setEditorValue(editorValue);
   };
 
-  private hasUnsavedChanges = () => {
-    const xpAdjustmentInput = stringParamToInt(this.state.xpAdjustmentInput || undefined);
-    return (
-      this.props.xpAdjustment !== xpAdjustmentInput ||
-      this.props.comments !== this.state.editorValue
-    );
+  const checkHasUnsavedChanges = () => {
+    const newXpAdjustmentInput = stringParamToInt(xpAdjustmentInput || undefined);
+    return props.xpAdjustment !== newXpAdjustmentInput || props.comments !== editorValue;
   };
 
-  private generateMarkdownPreview = (markdown: string) =>
+  const generateMarkdownPreview = (markdown: string) =>
     Promise.resolve(
       <Markdown
         content={markdown}
@@ -370,15 +206,129 @@ class GradingEditor extends React.Component<GradingEditorProps, State> {
       />
     );
 
-  private makeInitialState(): State {
-    return {
-      xpAdjustmentInput: this.props.xpAdjustment.toString(),
-      editorValue: this.props.comments,
-      selectedTab: 'write',
-      currentlySaving: false
-    };
-  }
-}
+  // Render
+  const hasUnsavedChanges = checkHasUnsavedChanges();
+  const saveButtonOpts = {
+    intent: hasUnsavedChanges ? Intent.WARNING : Intent.NONE,
+    minimal: !hasUnsavedChanges,
+    className: gradingEditorButtonClass
+  };
+  const discardButtonOpts = {
+    intent: hasUnsavedChanges ? Intent.DANGER : Intent.NONE,
+    minimal: !hasUnsavedChanges,
+    className: gradingEditorButtonClass
+  };
+  const saveAndContinueButtonOpts = {
+    intent: hasUnsavedChanges ? Intent.SUCCESS : Intent.NONE,
+    minimal: !hasUnsavedChanges,
+    className: gradingEditorButtonClass
+  };
+  const onTabChange = (tab: ReactMdeProps['selectedTab']) => setSelectedTab(tab);
+
+  // Derived values
+  const totalXp = props.initialXp + (stringParamToInt(xpAdjustmentInput || undefined) || 0);
+  const xpPlaceholder = `${props.initialXp > 0 ? '-' : ''}${props.initialXp} to ${
+    props.maxXp - props.initialXp
+  }`;
+
+  return (
+    <div className="GradingEditor">
+      {!currentlySaving && hasUnsavedChanges ? (
+        <Prompt message={'You have unsaved changes. Are you sure you want to leave?'} />
+      ) : null}
+
+      <div className="grading-editor-header">
+        <H3>Currently Grading: {props.studentName}</H3>
+      </div>
+      {props.solution !== null ? (
+        <div className="grading-editor-marking-scheme">
+          <Pre>{props.solution.toString()} </Pre>
+        </div>
+      ) : null}
+
+      <div className="grading-editor-container">
+        <div className="grading-editor-xp">
+          <div className="autograder-xp">
+            <div>Autograder XP:</div>
+            <div>
+              {`${props.initialXp} / ${props.maxXp}`}{' '}
+              <Button icon="refresh" small minimal onClick={onClickReautogradeAnswer}></Button>
+            </div>
+          </div>
+          <div className="xp-adjustment">
+            <div>XP adjustment:</div>
+            <div>
+              <NumericInput
+                className="adjustment-input"
+                onValueChange={onXpAdjustmentInputChange}
+                value={xpAdjustmentInput || ''}
+                buttonPosition={Position.RIGHT}
+                fill={true}
+                placeholder={xpPlaceholder}
+                intent={totalXp < 0 || totalXp > props.maxXp ? Intent.DANGER : Intent.NONE}
+                min={0 - props.initialXp}
+                max={props.maxXp > props.initialXp ? props.maxXp - props.initialXp : undefined}
+                stepSize={50}
+                minorStepSize={25}
+                majorStepSize={100}
+              />
+            </div>
+          </div>
+          <div className="final-xp">
+            <div>Final XP:</div>
+            <div>{`${totalXp} / ${props.maxXp}`}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="react-mde-parent">
+        <ReactMde
+          value={editorValue}
+          onChange={handleEditorValueChange}
+          selectedTab={selectedTab}
+          onTabChange={onTabChange}
+          generateMarkdownPreview={generateMarkdownPreview}
+          minEditorHeight={200}
+          maxEditorHeight={1000}
+          minPreviewHeight={240}
+          getIcon={blueprintIconProvider}
+        />
+      </div>
+
+      {selectedTab === 'write' && (
+        <div className="grading-editor-draft-buttons">
+          <div className="grading-editor-save-button">
+            {controlButton(
+              'Save Changes',
+              IconNames.FLOPPY_DISK,
+              validateXpBeforeSave(props.handleGradingSave),
+              saveButtonOpts
+            )}
+          </div>
+          <div className="grading-editor-discard-button">
+            {controlButton('Discard Changes', IconNames.TRASH, discardChanges, discardButtonOpts)}
+          </div>
+        </div>
+      )}
+      <div className="grading-editor-save-continue-button">
+        {controlButton(
+          'Save and Continue',
+          IconNames.UPDATED,
+          validateXpBeforeSave(onClickSaveAndContinue),
+          saveAndContinueButtonOpts
+        )}
+      </div>
+      {props.graderName && props.gradedAt && (
+        <>
+          <Divider />
+          <div className="grading-editor-last-graded-details">
+            Last edited by <b>{props.graderName}</b> on {getPrettyDate(props.gradedAt)}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 /**
  * Maps react-mde icon names to blueprintjs counterparts
