805b516a034a83e82bd935721bd747bcecf75421
Ian Yong <ianyongyc@gmail.com>
2023-02-24 21:48:32 +0800

    Add tooltip to multiple files mode toggle button

--- src/commons/controlBar/ControlBarToggleMultipleFilesModeButton.tsx
+++ src/commons/controlBar/ControlBarToggleMultipleFilesModeButton.tsx
@@ -1,5 +1,6 @@
 import { Colors } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
+import { Tooltip2 } from '@blueprintjs/popover2';
 import React from 'react';
 
 import ControlButton from '../ControlButton';
@@ -12,14 +13,17 @@ type ControlBarToggleMultipleFilesModeButtonProps = {
 export const ControlBarToggleMultipleFilesModeButton: React.FC<
   ControlBarToggleMultipleFilesModeButtonProps
 > = ({ isMultipleFilesEnabled, toggleMultipleFilesMode }) => {
+  const tooltipContent = `${isMultipleFilesEnabled ? 'Disable' : 'Enable'} multiple files mode`;
   return (
-    <ControlButton
-      label="Multiple Files"
-      icon={IconNames.FOLDER_CLOSE}
-      options={{
-        iconColor: isMultipleFilesEnabled ? Colors.BLUE4 : undefined
-      }}
-      onClick={toggleMultipleFilesMode}
-    />
+    <Tooltip2 content={tooltipContent}>
+      <ControlButton
+        label="Multiple Files"
+        icon={IconNames.FOLDER_CLOSE}
+        options={{
+          iconColor: isMultipleFilesEnabled ? Colors.BLUE4 : undefined
+        }}
+        onClick={toggleMultipleFilesMode}
+      />
+    </Tooltip2>
   );
 };
