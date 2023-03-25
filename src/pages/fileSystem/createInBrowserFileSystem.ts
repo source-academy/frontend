c4e2efe211baaffa720b3a6d7250ad07228badde
Ian Yong <ianyongyc@gmail.com>
2023-03-25 19:38:23 +0800

    Create files that do not exist for editor tabs

--- src/pages/fileSystem/createInBrowserFileSystem.ts
+++ src/pages/fileSystem/createInBrowserFileSystem.ts
@@ -1,11 +1,14 @@
 import { BFSRequire, configure } from 'browserfs';
 import { ApiError } from 'browserfs/dist/node/core/api_error';
+import { FSModule } from 'browserfs/dist/node/core/FS';
 import { Store } from 'redux';
 
+import { OverallState } from '../../commons/application/ApplicationTypes';
 import { setInBrowserFileSystem } from '../../commons/fileSystem/FileSystemActions';
+import { EditorTabState } from '../../commons/workspace/WorkspaceTypes';
 import { BASE_PLAYGROUND_FILE_PATH } from '../playground/Playground';
 
-export const createInBrowserFileSystem = (store: Store) => {
+export const createInBrowserFileSystem = (store: Store<OverallState>) => {
   configure(
     {
       fs: 'MountableFileSystem',
@@ -22,7 +25,42 @@ export const createInBrowserFileSystem = (store: Store) => {
       if (err) {
         console.error(err);
       }
-      store.dispatch(setInBrowserFileSystem(BFSRequire('fs')));
+      // Set the browser file system reference in the Redux store for retrieval
+      // in other parts of the codebase.
+      const fileSystem = BFSRequire('fs');
+      store.dispatch(setInBrowserFileSystem(fileSystem));
+      // Create files for editor tabs if they do not exist. This can happen when
+      // editor tabs are initialised from the Redux store defaults, as opposed to
+      // being created by the user.
+      const workspaceStates = store.getState().workspaces;
+      Object.entries(workspaceStates).forEach(([_workspaceLocation, workspaceState]) => {
+        const editorTabs = workspaceState.editorTabs;
+        createFilesForEditorTabs(fileSystem, editorTabs);
+      });
     }
   );
+};
+
+// An EditorTabWithFile is an editor tab with an associated file path.
+type EditorTabWithFile = EditorTabState & {
+  filePath: string;
+};
+
+const createFilesForEditorTabs = (fileSystem: FSModule, editorTabs: EditorTabState[]) => {
+  const editorTabsWithFile = editorTabs.filter(
+    (editorTab): editorTab is EditorTabWithFile => editorTab.filePath !== undefined
+  );
+  editorTabsWithFile.forEach(editorTab => {
+    fileSystem.exists(editorTab.filePath, filePathExists => {
+      if (filePathExists) {
+        return;
+      }
+
+      fileSystem.writeFile(editorTab.filePath, editorTab.value, err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  });
 };
