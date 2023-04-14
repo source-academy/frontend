6fb009d029bb77e98fcb2c0a09f1982e8bc5772c
Lim RuiZhi <70838461+Ruizhi2001@users.noreply.github.com>
2023-04-14 15:00:16 +0800

    Ruizhi2001 (#2444)
    * Buggy implementation of language switcher
    
    * pushed master into ruizhi2001
    
    * Made menu more robust
    
    * minor fix in the lang change button
    
    * minor clean up
    
    * Added different chapters for Scheme and Python, added proper names
    
    * remove comments
    
    * minor changes
    
    * some formatting and minor change
    
    * Updated frontend for compatibility
    
    * fixing the tests
    
    * minor changes
    
    * Added Test case for NavigationBarLangSelect and now menu closes when the lang select button is clicked
    
    * minor update
    
    * another minor change to the test
    
    * Formatted frontend files
    
    ---------
    
    Co-authored-by: s-kybound <abadkyriel@gmail.com>
    Co-authored-by: Martin Henz <henz@comp.nus.edu.sg>

---
+++ src/commons/navigationBar/subcomponents/NavigationBarLangSelectButton.tsx
@@ -0,0 +1,42 @@
+import { Button, Menu, MenuItem, Position } from '@blueprintjs/core';
+import { Popover2 } from '@blueprintjs/popover2';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { playgroundChangeLang } from 'src/features/playground/PlaygroundActions';
+import { store } from 'src/pages/createStore';
+
+const NavigationBarLangSelectButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const lang = store.getState().playground.lang;
+  const dispatch = useDispatch();
+  const selectLang = (language: string) => {
+    dispatch(playgroundChangeLang(language));
+    setIsOpen(false);
+    console.log('LANG:', lang);
+  };
+
+  return (
+    <Popover2
+      hasBackdrop
+      interactionKind="click"
+      position={Position.BOTTOM_RIGHT}
+      isOpen={isOpen}
+      content={
+        <Menu>
+          <MenuItem onClick={() => selectLang('Source')} text="Source" />
+          <MenuItem onClick={() => selectLang('Scheme')} text="Scheme" />
+          <MenuItem onClick={() => selectLang('Python')} text="Python" />
+        </Menu>
+      }
+      onClose={() => setIsOpen(false)}
+    >
+      <>
+        <Button rightIcon="caret-down" onClick={() => setIsOpen(true)}>
+          {lang}
+        </Button>
+      </>
+    </Popover2>
+  );
+};
+
+export default NavigationBarLangSelectButton;
