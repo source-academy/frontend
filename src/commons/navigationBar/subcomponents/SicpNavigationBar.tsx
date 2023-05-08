22548fc673f94159d5bda5f6834aa22af6e902a6
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-08 20:16:07 +0800

    Simplify array creation for readability

--- src/commons/navigationBar/subcomponents/SicpNavigationBar.tsx
+++ src/commons/navigationBar/subcomponents/SicpNavigationBar.tsx
@@ -153,35 +153,7 @@ const SicpNavigationBar: React.FC = () => {
       }
       return null;
     }
-    const letters = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z',
-      ' '
-    ];
+    const letters = [...'abcdefghijklmnopqrstuvwxyz'.split(''), ' '];
 
     function toEnd(query: any) {
       let node = trie;
