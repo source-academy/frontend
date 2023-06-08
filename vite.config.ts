ab41839f730c093289383bfaa8c0d7e83d21ef22
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-08 16:52:15 +0800

    Fix environment variable issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import envCompatible from 'vite-plugin-env-compatible';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  envPrefix: 'REACT_APP_',
+  plugins: [react(), envCompatible()],
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src')
