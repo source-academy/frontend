d5183076ec7bc15d132fa82dc5bed48ae54959d8
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-08 16:30:44 +0800

    Fix module resolution error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
 });
