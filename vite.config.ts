e6545bc7a289d56d429a0294bb3856fcb6a8cc71
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-16 15:11:07 +0800

    Fix environment variables not loading, dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,53 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
-import envCompatible from 'vite-plugin-env-compatible';
+import { defineConfig, loadEnv } from 'vite';
+import fs from 'vite-plugin-fs';
+import wasm from 'vite-plugin-wasm';
+
+const envPrefix = 'REACT_APP_';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  envPrefix: 'REACT_APP_',
-  plugins: [react(), envCompatible()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-      constants: 'constants-browserify',
-      http: 'stream-http',
-      https: 'https-browserify',
-      os: 'os-browserify/browser',
-      stream: 'stream-browserify',
-      timers: 'timers-browserify',
-      url: 'url'
+export default defineConfig(({ mode }) => {
+  // const env = { ...process.env, ...loadEnv(mode, process.cwd(), envPrefix) };
+  const env = loadEnv(mode, process.cwd(), envPrefix);
+  return {
+    envPrefix: envPrefix,
+    plugins: [react(), wasm(), fs()],
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, './src'),
+        constants: 'constants-browserify',
+        http: 'stream-http',
+        https: 'https-browserify',
+        os: 'os-browserify/browser',
+        stream: 'stream-browserify',
+        timers: 'timers-browserify',
+        url: 'url'
+      }
+    },
+    define: {
+      'process.env': env
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            buffer: true,
+            process: false
+          })
+        ]
+      }
+    },
+    server: {
+      port: 8000
+    },
+    build: {
+      outDir: 'build'
     }
-  },
-  define: {
-    child_process: false,
-    fs: false
-  }
+  };
 });
