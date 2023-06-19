2412c205273707d2f7c4029653abf6c96cd3e256
En Rong <53928333+chownces@users.noreply.github.com>
2023-06-19 22:13:16 +0800

    Deprecate enzyme usage

--- src/commons/__tests__/ContentDisplay.tsx
+++ src/commons/__tests__/ContentDisplay.tsx
@@ -1,6 +1,5 @@
-import { mount } from 'enzyme';
-
 import ContentDisplay, { ContentDisplayProps } from '../ContentDisplay';
+import { renderTree } from '../utils/TestUtils';
 
 const mockProps: ContentDisplayProps = {
   display: <div> Test Content </div>,
@@ -9,6 +8,6 @@ const mockProps: ContentDisplayProps = {
 
 test('ContentDisplay page renders correctly', () => {
   const app = <ContentDisplay {...mockProps} />;
-  const tree = mount(app);
-  expect(tree.debug()).toMatchSnapshot();
+  const tree = renderTree(app);
+  expect(tree).toMatchSnapshot();
 });
