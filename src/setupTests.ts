050b3007441431331b262cca75d241f222244073
En Rong <53928333+chownces@users.noreply.github.com>
2023-06-21 17:18:43 +0800

    Remove enzyme related packages

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { configure } from 'enzyme';
-configure({ adapter: new Adapter() });
-
 // Mock ResizeObserver in tests
 global.ResizeObserver = require('resize-observer-polyfill');
