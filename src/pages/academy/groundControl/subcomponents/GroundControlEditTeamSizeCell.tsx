7b82b2b104c77516069499e3dc676525c25d9eca
Lu Yiting <e0774189@u.nus.edu>
2023-07-03 15:59:10 +0800

    Update team size edit cell

--- src/pages/academy/groundControl/subcomponents/GroundControlEditTeamSizeCell.tsx
+++ src/pages/academy/groundControl/subcomponents/GroundControlEditTeamSizeCell.tsx
@@ -4,18 +4,8 @@ import '@tremor/react/dist/esm/tremor.css';
 import { Icon as BpIcon } from '@blueprintjs/core';
 
 import {
-
   Button,
   Flex,
-  Footer,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeaderCell,
-  TableRow,
-  Text,
-  TextInput
 } from '@tremor/react';
 import * as React from 'react';
 
@@ -42,39 +32,20 @@ const EditTeamSizeCell: React.FunctionComponent<EditTeamSizeCellProps> = props =
 
   const [value, setValue] = React.useState(currentTeamSize);
 
-  const handleIncrement = () => {
-    setValue(value + 1);
-  };
-
-  const handleDecrement = () => {
-    if (value > minTeamSize) {
-      setValue(value - 1);
-    }
-  };
 
   return (
     <div className="number-input">
-      <Flex justifyContent="justify-center" spaceX="space-x-3">
-        <Button
-          size="xs"
-          icon={() => <BpIcon icon={IconNames.MINUS} />}
-          variant="light"
-          onClick={handleDecrement}
-          disabled={value === minTeamSize}
-        />
+      <Flex>
         <input
           type="number"
           className="input-value"
           value={value}
-          readOnly
+          onChange={e => {    
+            const inputValue = parseInt(e.target.value);
+            if (!isNaN(inputValue) && inputValue >= minTeamSize && inputValue <= maxTeamSize) {
+              setValue(inputValue);
+            }}}
           style={{ width: '4rem', padding: '0.2rem', textAlign: 'center' }}
-        />
-        <Button
-          size="xs"
-          icon={() => <BpIcon icon={IconNames.PLUS} />}
-          variant="light"
-          onClick={handleIncrement}
-          disabled={value === maxTeamSize}
         />
       </Flex>
     </div>
