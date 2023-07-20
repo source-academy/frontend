7211013a445b9eaef2f2e2f0f405c1ec8746da70
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-20 16:07:23 +0800

    Remove test files

--- src/pages/stories/__tests__/Stories.tsx
+++
@@ -1,9 +0,0 @@
-// import { shallowRender } from 'src/commons/utils/TestUtils';
-
-// import Stories from '../Stories';
-
-// test('Stories renders correctly', () => {
-//   const Element: React.FC = () => <Stories />;
-//   const tree = shallowRender(<Element />);
-//   expect(tree).toMatchSnapshot();
-// });
