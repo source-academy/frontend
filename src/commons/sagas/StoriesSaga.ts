9319646d5fdf2bc64206defcc57e8d4fea0512e2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 21:03:33 +0800

    Fix incorrect API return type

--- src/commons/sagas/StoriesSaga.ts
+++ src/commons/sagas/StoriesSaga.ts
@@ -1,7 +1,12 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeLatest } from 'redux-saga/effects';
 import { getStories, getStory } from 'src/features/stories/storiesComponents/BackendAccess';
-import { FETCH_STORY, GET_STORIES_LIST, StoryListView } from 'src/features/stories/StoriesTypes';
+import {
+  FETCH_STORY,
+  GET_STORIES_LIST,
+  StoryListView,
+  StoryView
+} from 'src/features/stories/StoriesTypes';
 
 import { actions } from '../utils/ActionsHelper';
 
@@ -20,7 +25,7 @@ export function* storiesSaga(): SagaIterator {
 
   yield takeLatest(FETCH_STORY, function* (action: ReturnType<typeof actions.fetchStory>) {
     const storyId = action.payload;
-    const story: StoryListView = yield call(async () => {
+    const story: StoryView = yield call(async () => {
       const resp = await getStory(storyId);
       if (!resp) {
         return null;
