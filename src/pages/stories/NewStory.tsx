5826f2749d7703526558286154dba0f22e6c7532
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 12:12:32 +0800

    Fix styles

--- src/pages/stories/NewStory.tsx
+++ src/pages/stories/NewStory.tsx
@@ -65,7 +65,7 @@ const NewStory: React.FC = () => {
       >
         <AceEditor
           className="repl-react-ace react-ace"
-          width="50%"
+          width="100%"
           height="100%"
           theme="source"
           value={content}
