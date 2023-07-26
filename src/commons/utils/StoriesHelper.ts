4dd16ba2d52b67006ae57e623251df46c5d36644
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 17:16:55 +0800

    Connect stories Read/Update to API (#2564)
    * Move getStory function to BackendAccess.ts
    
    * Fix incorrect JSON payload
    
    * Create StoryView type
    
    * Connect read, update story operations to API
    
    * Update default story
    
    * Fix typo
    * Format according to Markdown rules for readability

--- src/commons/utils/StoriesHelper.ts
+++ src/commons/utils/StoriesHelper.ts
@@ -11,33 +11,42 @@ env:
     chapter: 4
     variant: default
   rune:
-    chatper: 4
+    chapter: 4
     variant: default
 ---
+
 # Stories for Source Academy Quick Start
 
 ## Source Blocks
-You can create source blocks as follows:
-\`\`\`\`
-\`\`\`source
-const incr_two = (x) => x + 2;
-\`\`\`
-\`\`\`\`
-Will render as:
-\`\`\`source
-const incr_two = (x) => x + 2;
 
+You can create source blocks as follows:
+
+\`\`\`\`
+\`\`\`source
+const incr_two = (x) => x + 2;
 \`\`\`
+\`\`\`\`
+
+Will render as:
+
+\`\`\`source
+const incr_two = (x) => x + 2;
+\`\`\`
+
 Try running the code above!
 
 Subsequent source blocks will continue the evaluation flow. Try running the following source block:
+
 \`\`\`source
 incr_two(10);
 \`\`\`
+
 You can reset the environment by clicking on the \`Reset Env\` button on the top right of the source block.
 
 ## Changing Configs
+
 You can change the chapter and variant of source blocks by adding a header to the top of this markdown file.
+
 \`\`\`
 ---
 config:
@@ -45,9 +54,11 @@ config:
   variant: DEFAULT
 ---
 \`\`\`
+
 The current valid CHAPTER/VARIANT combinations can be found [here](https://github.com/source-academy/js-slang#usage).
 
 ## Adding New Environments
+
 Sometimes, you may want a different evaluation flow for source blocks. Consider these two implementations of the fibonacci function:
 ### Iterative Fib
 
@@ -63,21 +74,27 @@ function fib(n) {
   return a;
 }
 \`\`\`
+
 \`\`\`source-env:iterFib
 fib(15);
 \`\`\`
+
 ### Recursive Fib
+
 \`\`\`source-env:recuFib
 function fib(n) {
   return n < 2 ? n : fib(n-1) + fib(n-2);
 }
 \`\`\`
+
 \`\`\`source-env:recuFib
 fib(15);
 \`\`\`
+
 It would make sense for these two execution flows to be separated so the functions do not overwrite each other.
 
 This can be done by configuring the code blocks to be from different environments. In the header, we can add
+
 \`\`\`
 env:
   iterFib:
@@ -87,14 +104,18 @@ env:
     chapter: 4
     variant: default
 \`\`\`
+
 to create two different environments. Then, we add \`source-env:iterFib\` and \`source-env:recuFib\` to the parameters of the source blocks to set them to their respective environments!
 
 ## Module Support
+
 Stories also supports modules!
+
 \`\`\`source-env:rune
 import {show, heart} from 'rune';
 
 show(heart);
-\`\`\``;
+\`\`\`
+`;
 
 export default DEFAULT_STORY;
--- src/features/stories/storiesComponents/BackendAccess.ts
+++ src/features/stories/storiesComponents/BackendAccess.ts
@@ -22,6 +22,27 @@ export const getStories = async (): Promise<Response | null> => {
   }
 };
 
+export const getStory = async (storyId: number): Promise<Response | null> => {
+  try {
+    const resp = await fetch(`${Constants.storiesBackendUrl}/stories/${storyId}`);
+    if (!resp.ok) {
+      showWarningMessage(
+        `Error while communicating with stories backend: ${resp.status} ${resp.statusText}${
+          resp.status === 401 || resp.status === 403
+            ? '; try logging in again, after manually saving any work.'
+            : ''
+        }`
+      );
+      return null;
+    }
+    return resp;
+  } catch (e) {
+    console.log(e);
+    showWarningMessage('Error while communicating with stories backend; check your network?');
+    return null;
+  }
+};
+
 export const insertStory = async (
   authorId: number,
   title: string,
@@ -35,12 +56,10 @@ export const insertStory = async (
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        story: {
-          authorId: authorId,
-          title: title,
-          content: content,
-          pinOrder: pinOrder
-        }
+        authorId: authorId,
+        title: title,
+        content: content,
+        pinOrder: pinOrder
       })
     });
     if (!resp.ok) {
@@ -74,11 +93,9 @@ export const updateStory = async (
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        story: {
-          title: title,
-          content: content,
-          pinOrder: pinOrder
-        }
+        title: title,
+        content: content,
+        pinOrder: pinOrder
       })
     });
     if (!resp.ok) {
