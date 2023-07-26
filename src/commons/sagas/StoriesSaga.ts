ddfb6f35de3ea488b522a939217f40c9ce0afd03
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 20:52:40 +0800

    Support FETCH_STORY in saga

--- src/commons/sagas/StoriesSaga.ts
+++ src/commons/sagas/StoriesSaga.ts
@@ -1,7 +1,7 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put, takeLatest } from 'redux-saga/effects';
-import { getStories } from 'src/features/stories/storiesComponents/BackendAccess';
-import { GET_STORIES_LIST, StoryListView } from 'src/features/stories/StoriesTypes';
+import { getStories, getStory } from 'src/features/stories/storiesComponents/BackendAccess';
+import { FETCH_STORY, GET_STORIES_LIST, StoryListView } from 'src/features/stories/StoriesTypes';
 
 import { actions } from '../utils/ActionsHelper';
 
@@ -16,6 +16,19 @@ export function* storiesSaga(): SagaIterator {
     });
 
     yield put(actions.updateStoriesList(allStories));
+  });
+
+  yield takeLatest(FETCH_STORY, function* (action: ReturnType<typeof actions.fetchStory>) {
+    const storyId = action.payload;
+    const story: StoryListView = yield call(async () => {
+      const resp = await getStory(storyId);
+      if (!resp) {
+        return null;
+      }
+      return resp.json();
+    });
+
+    yield put(actions.setCurrentStory(story));
   });
 }
 
