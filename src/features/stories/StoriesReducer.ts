c8ee9b1b011bd4c7ccfc0c630bdad68df8757537
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 19:49:55 +0800

    Update StoriesReducer

--- src/features/stories/StoriesReducer.ts
+++ src/features/stories/StoriesReducer.ts
@@ -21,6 +21,7 @@ import {
   STORIES_UPDATE_GITHUB_SAVE_INFO,
   StoriesState,
   TOGGLE_STORIES_USING_SUBST,
+  UPDATE_ALL_STORIES,
   UPDATE_STORIES_CONTENT
 } from './StoriesTypes';
 
@@ -200,6 +201,12 @@ export const StoriesReducer: Reducer<StoriesState> = (
       return {
         ...state,
         content: action.payload
+      };
+    // New cases post-refactor
+    case UPDATE_ALL_STORIES:
+      return {
+        ...state,
+        storyList: action.payload
       };
     default:
       return state;
