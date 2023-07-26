31520aa69ca4c11bb6d9916bd4038a5cc6566c65
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 10:38:10 +0800

    Improve and clean up NewStory UI (#2558)
    * Rename component to match file name
    
    * Remove GitHub control bar from NewStory
    
    Part of refactoring to use stories backend for CRUD operations.
    
    * Improve code quality and UI
    
    * Update styles
    * Remove extraneous div
    * Simplify onChange handling
    
    * Use Source editor theme in stories editor
    
    Improves style consistency.
    
    * Remove unnecessary div
    
    * Delete unused components
    
    * Remove unused styles
    
    * Comment out unused utils pending refactor

--- src/pages/stories/NewStory.tsx
+++ src/pages/stories/NewStory.tsx
@@ -1,22 +1,16 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import 'js-slang/dist/editors/ace/theme/source';
+
+import { useCallback, useEffect, useState } from 'react';
 import AceEditor, { IEditorProps } from 'react-ace';
 import { useDispatch } from 'react-redux';
-import { loginGitHub, logoutGitHub } from 'src/commons/application/actions/SessionActions';
-import { ControlBarGitHubButtons } from 'src/commons/controlBar/github/ControlBarGitHubButtons';
 import { useTypedSelector } from 'src/commons/utils/Hooks';
-import {
-  githubOpenFile,
-  githubSaveFile,
-  githubSaveFileAs
-} from 'src/features/github/GitHubActions';
 import { updateStoriesContent } from 'src/features/stories/StoriesActions';
 
-import ControlBar, { ControlBarProps } from '../../commons/controlBar/ControlBar';
 import UserBlogContent from '../../features/stories/storiesComponents/UserBlogContent';
 
 const NewStory: React.FC = () => {
   const dispatch = useDispatch();
-  const [lastEdit, setLastEdit] = useState(new Date());
+  // const [lastEdit, setLastEdit] = useState(new Date());
   const [editorScrollTop, setEditorScrollTop] = useState(0);
   const [editorScrollHeight, setEditorScrollHeight] = useState(1);
 
@@ -39,70 +33,38 @@ const NewStory: React.FC = () => {
   const content = useTypedSelector(store => store.stories.content);
 
   const onEditorValueChange = useCallback((val: string) => {
-    setLastEdit(new Date());
+    // setLastEdit(new Date());
     dispatch(updateStoriesContent(val));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const githubOctokitObject = useTypedSelector(store => store.session.githubOctokitObject);
-  const githubSaveInfo = useTypedSelector(store => store.stories.githubSaveInfo);
-  const githubPersistenceIsDirty =
-    githubSaveInfo && (!githubSaveInfo.lastSaved || githubSaveInfo.lastSaved < lastEdit);
-  const githubButtons = useMemo(() => {
-    return (
-      <ControlBarGitHubButtons
-        key="github"
-        loggedInAs={githubOctokitObject.octokit}
-        githubSaveInfo={githubSaveInfo}
-        isDirty={githubPersistenceIsDirty}
-        onClickOpen={() => dispatch(githubOpenFile(true))}
-        onClickSaveAs={() => dispatch(githubSaveFileAs(true))}
-        onClickSave={() => dispatch(githubSaveFile(true))}
-        onClickLogIn={() => dispatch(loginGitHub())}
-        onClickLogOut={() => dispatch(logoutGitHub())}
-        isFolderModeEnabled={false}
-      />
-    );
-  }, [dispatch, githubOctokitObject, githubPersistenceIsDirty, githubSaveInfo]);
-
-  const controlBarProps: ControlBarProps = {
-    editorButtons: [githubButtons]
-  };
-
   return (
-    <div>
-      <ControlBar {...controlBarProps} />
-      <div
-        style={{
-          height: 'calc(100vh - 90px)',
-          width: '100vw',
-          display: 'flex',
-          flexDirection: 'row',
-          paddingBottom: 10,
-          justifyContent: 'center'
+    <div
+      style={{
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+      }}
+    >
+      <AceEditor
+        className="repl-react-ace react-ace"
+        width="50%"
+        height="100%"
+        theme="source"
+        value={content}
+        onChange={onEditorValueChange}
+        onScroll={onScroll}
+        fontSize={17}
+        highlightActiveLine={false}
+        showPrintMargin={false}
+        wrapEnabled={true}
+        setOptions={{
+          fontFamily: "'Inconsolata', 'Consolas', monospace"
         }}
-      >
-        <AceEditor
-          className="repl-react-ace react-ace"
-          width="40%"
-          height="100%"
-          theme="source"
-          value={content}
-          onChange={c => {
-            onEditorValueChange(c);
-          }}
-          onScroll={onScroll}
-          fontSize={17}
-          highlightActiveLine={false}
-          showPrintMargin={false}
-          wrapEnabled={true}
-          setOptions={{
-            fontFamily: "'Inconsolata', 'Consolas', monospace"
-          }}
-        />
-        <div className="newUserblog" id="userblogContainer">
-          <UserBlogContent fileContent={content} />
-        </div>
+      />
+      <div className="newUserblog" id="userblogContainer">
+        <UserBlogContent fileContent={content} />
       </div>
     </div>
   );
