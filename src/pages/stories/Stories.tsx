aa440e18c8efaedb0c246fef16f1da53c9ac343b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 10:00:01 +0800

    Update stories main page UI (#2557)
    * Use blueprint-style button for consistency
    
    * Remove unused SCSS
    
    We utilise the UI library instead of writing our own SCSS for
    consistency with the other parts of the codebase.
    
    * Use tremor styles for card display
    
    * Update top bar layout
    
    * Remove extraneous div
    * Fix spacing
    * Add title
    * Fix search bar width
    
    * Update SCSS
    
    * Remove unused styles
    * Increase width
    
    * Remove extraneous div and padding
    
    * Remove commented code
    
    * Remove invalid property access
    
    * Create and use list view type
    
    * Fix "Actions" column layout
    
    * Simplify some code

--- src/pages/stories/Stories.tsx
+++ src/pages/stories/Stories.tsx
@@ -1,8 +1,9 @@
 import '@tremor/react/dist/esm/tremor.css';
 
-import { Icon as BpIcon } from '@blueprintjs/core';
+import { Button as BpButton, Icon as BpIcon } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import {
+  Card,
   Flex,
   Icon,
   Table,
@@ -12,7 +13,8 @@ import {
   TableHeaderCell,
   TableRow,
   Text,
-  TextInput
+  TextInput,
+  Title
 } from '@tremor/react';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
@@ -20,15 +22,19 @@ import { Link, useNavigate } from 'react-router-dom';
 // import Constants from 'src/commons/utils/Constants';
 import { getStories } from '../../features/stories/storiesComponents/BackendAccess';
 
+type StoryListView = {
+  authorId: number;
+  authorName: string;
+  title: string;
+  content: string;
+};
+
 const Stories: React.FC = () => {
   // const [user, setUser] = useState<string>('');
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<StoryListView[]>([]);
   // const [query, setQuery] = useState('');
 
   const navigate = useNavigate();
-  const handleSubmit = (): void => {
-    navigate(`/stories/new`);
-  };
 
   // const handleSubmitUser = (): void => {
   //   if (user !== '') {
@@ -45,101 +51,75 @@ const Stories: React.FC = () => {
 
   useEffect(() => {
     getStories().then(res => {
-      res?.json().then(r2 => {
-        console.log(r2);
-        setData(r2);
-      });
+      res?.json().then(setData);
     });
   }, []);
 
   return (
     <div className="storiesHome">
-      <div className="storiesInteract">
-        <Flex marginTop="mt-2" justifyContent="justify-between" alignItems="items-center">
-          <button className="storiesButton" onClick={handleSubmit}>
-            <BpIcon icon={IconNames.PLUS} />
-            Add Story
-          </button>
-          <div>
-            <TextInput
-              maxWidth="max-w-xl"
-              icon={() => <BpIcon icon={IconNames.SEARCH} style={{ marginLeft: '0.75rem' }} />}
-              placeholder="Search for author..."
-              // onChange={e => setQuery(e.target.value)}
-            />
-          </div>
+      <Card>
+        <Flex justifyContent="justify-between">
+          <Flex justifyContent="justify-start" spaceX="space-x-6">
+            <Title>All Stories</Title>
+            <BpButton onClick={() => navigate(`/stories/new`)} icon={IconNames.PLUS}>
+              Add Story
+            </BpButton>
+          </Flex>
+          <TextInput
+            maxWidth="max-w-xl"
+            icon={() => <BpIcon icon={IconNames.SEARCH} style={{ marginLeft: '0.75rem' }} />}
+            placeholder="Search for author..."
+            // onChange={e => setQuery(e.target.value)}
+          />
         </Flex>
-        {/* {Constants.storiesBackendUrl && (
-          <div>
-            <p className="titleText">Recent Posts</p>
-            <div className="recent">
-              {data.map(user => (
-                <div>
-                  <p className="postTitle">
-                    <a href={`/stories/view/${user.githubname}/${user.filename}`}>
-                      {user.filename}
-                    </a>
-                  </p>
-                  <p className="postUser">
-                    by <a href={`/stories/view/${user.githubname}`}>{user.githubname}</a>
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        )} */}
-      </div>
 
-      <div>
-        <div style={{ padding: '0px 50px 0px 50px' }}>
-          <Table marginTop="mt-10">
-            <TableHead>
-              <TableRow>
-                {columns.map(column => (
-                  <TableHeaderCell key={column.id}>{column.header}</TableHeaderCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data
-                // .filter(story => story.authorId.toLowerCase().includes(query)
-                .map(story => (
-                  <TableRow key={story.id}>
-                    <TableCell>{story.authorId}</TableCell>
-                    <TableCell>
-                      <Text>{story.title}</Text>
-                    </TableCell>
-                    <TableCell>
-                      <Text>
-                        {story.content.length > 35
-                          ? `${story.content.substring(0, 35)} ...`
-                          : story.content}
-                      </Text>
-                    </TableCell>
-                    <TableCell>
-                      <Flex spaceX="space-x-2">
-                        <Link to={`/stories/view`}>
-                          <Icon
-                            tooltip="View"
-                            icon={() => <BpIcon icon={IconNames.EyeOpen} />}
-                            variant="light"
-                          />
-                        </Link>
-                        <Link to={`/stories/edit`}>
-                          <Icon
-                            tooltip="Edit"
-                            icon={() => <BpIcon icon={IconNames.EDIT} />}
-                            variant="light"
-                          />
-                        </Link>
-                      </Flex>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </div>
-      </div>
+        <Table marginTop="mt-10">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableHeaderCell key={column.id}>{column.header}</TableHeaderCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data
+              // .filter(story => story.authorId.toLowerCase().includes(query)
+              .map(story => (
+                <TableRow>
+                  <TableCell>{story.authorId}</TableCell>
+                  <TableCell>
+                    <Text>{story.title}</Text>
+                  </TableCell>
+                  <TableCell>
+                    <Text>
+                      {story.content.length > 35
+                        ? `${story.content.substring(0, 35)} ...`
+                        : story.content}
+                    </Text>
+                  </TableCell>
+                  <TableCell>
+                    <Flex justifyContent="justify-start" spaceX="space-x-2">
+                      <Link to={`/stories/view`}>
+                        <Icon
+                          tooltip="View"
+                          icon={() => <BpIcon icon={IconNames.EyeOpen} />}
+                          variant="light"
+                        />
+                      </Link>
+                      <Link to={`/stories/edit`}>
+                        <Icon
+                          tooltip="Edit"
+                          icon={() => <BpIcon icon={IconNames.EDIT} />}
+                          variant="light"
+                        />
+                      </Link>
+                    </Flex>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Card>
     </div>
   );
 };
