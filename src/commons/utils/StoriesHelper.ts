198e20e3b76e7e813da800bbbde14f0b898ee8a8
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-05 23:39:51 +0800

    Update default story to match new behavior

--- src/commons/utils/StoriesHelper.ts
+++ src/commons/utils/StoriesHelper.ts
@@ -34,14 +34,14 @@ env:
 You can create source blocks as follows:
 
 \`\`\`\`
-\`\`\`source
+\`\`\`{source}
 const incr_two = (x) => x + 2;
 \`\`\`
 \`\`\`\`
 
 Will render as:
 
-\`\`\`source
+\`\`\`{source}
 const incr_two = (x) => x + 2;
 \`\`\`
 
@@ -49,7 +49,7 @@ Try running the code above!
 
 Subsequent source blocks will continue the evaluation flow. Try running the following source block:
 
-\`\`\`source
+\`\`\`{source}
 incr_two(10);
 \`\`\`
 
@@ -74,7 +74,7 @@ The current valid CHAPTER/VARIANT combinations can be found [here](https://githu
 Sometimes, you may want a different evaluation flow for source blocks. Consider these two implementations of the fibonacci function:
 ### Iterative Fib
 
-\`\`\`source-env:iterFib
+\`\`\`{source} env:iterFib
 function fib(n) {
   let a = 0;
   let b = 1;
@@ -87,19 +87,19 @@ function fib(n) {
 }
 \`\`\`
 
-\`\`\`source-env:iterFib
+\`\`\`{source} env:iterFib
 fib(15);
 \`\`\`
 
 ### Recursive Fib
 
-\`\`\`source-env:recuFib
+\`\`\`{source} env:recuFib
 function fib(n) {
   return n < 2 ? n : fib(n-1) + fib(n-2);
 }
 \`\`\`
 
-\`\`\`source-env:recuFib
+\`\`\`{source} env:recuFib
 fib(15);
 \`\`\`
 
@@ -117,13 +117,13 @@ env:
     variant: default
 \`\`\`
 
-to create two different environments. Then, we add \`source-env:iterFib\` and \`source-env:recuFib\` to the parameters of the source blocks to set them to their respective environments!
+to create two different environments. Then, we add \`source env:iterFib\` and \`source env:recuFib\` to the parameters of the source blocks to set them to their respective environments!
 
 ## Module Support
 
 Stories also supports modules!
 
-\`\`\`source-env:rune
+\`\`\`{source} env:rune
 import {show, heart} from 'rune';
 
 show(heart);
