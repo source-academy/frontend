ef8807f1761127afa578cb3ea198ec1b71881a6e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-05 22:57:22 +0800

    Create more tuple manipulation types
    Also added comments for documentation.

--- src/commons/utils/TypeHelper.ts
+++ src/commons/utils/TypeHelper.ts
@@ -8,7 +8,35 @@ export type KeysOfType<O, T> = {
   [K in keyof O]: O[K] extends T ? K : never;
 }[keyof O];
 
+/* =========================================
+ * Utility types for tuple type manipulation
+ * ========================================= */
+
+/** Creates a new tuple type with the first element of T removed. */
+export type RemoveFirst<T extends any[]> = T extends [any, ...infer U] ? U : T;
+/** Creates a new tuple type with the first N elements of T removed. */
+export type RemoveFirstN<
+  T extends any[],
+  N extends number,
+  Carry extends any[] = []
+> = Carry['length'] extends N ? T : RemoveFirstN<RemoveFirst<T>, N, [any, ...Carry]>;
+/** Creates a new tuple type with the first N elements of T. */
+export type FirstN<
+  T extends any[],
+  N extends number,
+  Counter extends any[] = []
+> = Counter['length'] extends N ? Counter : FirstN<RemoveFirst<T>, N, [...Counter, T[0]]>;
+
+/** Creates a new tuple type with the last element of T removed. */
 export type RemoveLast<T extends any[]> = T extends [...infer U, any] ? U : T;
+/** Creates a new tuple type with the last N elements of T. */
+export type LastN<
+  T extends any[],
+  N extends number,
+  Counter extends any[] = []
+> = Counter['length'] extends N
+  ? Counter
+  : LastN<RemoveLast<T>, N, [[never, ...T][T['length']], ...Counter]>;
 
 /**
  * Prevents invalid keys from being passed in to an object of the specified type argument.
