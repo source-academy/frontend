a4e093e3695a76d72ebe0429390f91faa7202242
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-08 15:45:33 +0800

    Restore HTML display tab

--- src/features/stories/storiesComponents/SourceBlock.tsx
+++ src/features/stories/storiesComponents/SourceBlock.tsx
@@ -4,18 +4,23 @@ import { Chapter, Variant } from 'js-slang/dist/types';
 import React, { useEffect, useRef, useState } from 'react';
 import AceEditor from 'react-ace';
 import { useDispatch } from 'react-redux';
-import { styliseSublanguage } from 'src/commons/application/ApplicationTypes';
+import { ResultOutput, styliseSublanguage } from 'src/commons/application/ApplicationTypes';
 import { ControlBarRunButton } from 'src/commons/controlBar/ControlBarRunButton';
 import ControlButton from 'src/commons/ControlButton';
 import { SideContentTab, SideContentType } from 'src/commons/sideContent/SideContentTypes';
 import Constants from 'src/commons/utils/Constants';
 import { useTypedSelector } from 'src/commons/utils/Hooks';
+import { addHtmlConsoleError } from 'src/commons/workspace/WorkspaceActions';
 import {
   clearStoryEnv,
   evalStory,
   toggleStoriesUsingSubst
 } from 'src/features/stories/StoriesActions';
-import { dataVisualizerTab, makeSubstVisualizerTabFrom } from 'src/pages/playground/PlaygroundTabs';
+import {
+  dataVisualizerTab,
+  makeHtmlDisplayTabFrom,
+  makeSubstVisualizerTabFrom
+} from 'src/pages/playground/PlaygroundTabs';
 
 import { ExternalLibraryName } from '../../../commons/application/types/ExternalTypes';
 import { Output } from '../../../commons/repl/Repl';
@@ -122,25 +127,17 @@ const SourceBlock: React.FC<SourceBlockProps> = props => {
 
     // TODO: Restore logic post refactor
 
-    // // For HTML Chapter, HTML Display tab is added only after code is run
-    // if (chapter === Chapter.HTML) {
-    //   if (output.length > outputIndex && output[outputIndex].type === 'result') {
-    //     tabs.push({
-    //       label: 'HTML Display',
-    //       iconName: IconNames.MODAL,
-    //       body: (
-    //         <SideContentHtmlDisplay
-    //           content={(output[outputIndex] as ResultOutput).value}
-    //           handleAddHtmlConsoleError={errorMsg =>
-    //             dispatch(addHtmlConsoleError(errorMsg, 'stories', true))
-    //           }
-    //         />
-    //       ),
-    //       id: SideContentType.htmlDisplay
-    //     });
-    //   }
-    //   return tabs;
-    // }
+    // For HTML Chapter, HTML Display tab is added only after code is run
+    if (chapter === Chapter.HTML) {
+      if (output.length > outputIndex && output[outputIndex].type === 'result') {
+        tabs.push(
+          makeHtmlDisplayTabFrom(output[outputIndex] as ResultOutput, errorMsg =>
+            dispatch(addHtmlConsoleError(errorMsg, 'stories', env))
+          )
+        );
+      }
+      return tabs;
+    }
 
     // // (TEMP) Remove tabs for fullJS until support is integrated
     // if (chapter === Chapter.FULL_JS) {
