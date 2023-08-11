e8a5431f9f50c5dffda3a1125370eb9a9109f34e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-11 11:28:22 +0800

    Remove early return
    This caused issues with tab handling due to interactions with show/hide
    behavior.

--- src/features/stories/storiesComponents/SourceBlock.tsx
+++ src/features/stories/storiesComponents/SourceBlock.tsx
@@ -90,10 +90,6 @@ const SourceBlock: React.FC<SourceBlockProps> = props => {
       event: React.MouseEvent<HTMLElement>
     ) => {
       // TODO: Migrate relevant updated logic from Playground component
-      if (newTabId === prevTabId) {
-        return;
-      }
-
       if (chapter <= Chapter.SOURCE_2 && newTabId === SideContentType.substVisualizer) {
         dispatch(toggleStoriesUsingSubst(true, env));
       }
