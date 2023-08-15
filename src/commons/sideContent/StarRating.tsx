a4b5e709c0304bc0807a1e42d3c5018739b36cd5
clementlemon02 <clementling02@gmail.com>
2023-08-15 23:31:11 +0200

    add star rating feature

---
+++ src/commons/sideContent/StarRating.tsx
@@ -0,0 +1,33 @@
+export {};
+
+import React from 'react';
+import { Icon } from '@blueprintjs/core';
+
+interface StarRatingProps {
+  value: number;
+  onChange: (rating: number) => void;
+}
+
+const StarRating: React.FC<StarRatingProps> = ({ value, onChange }) => {
+  const maxStars = 5;
+
+  const handleStarClick = (selectedValue: number) => {
+    if (onChange) {
+      onChange(selectedValue);
+    }
+  };
+
+  return (
+    <div className="star-rating">
+      {[...Array(maxStars)].map((_, index) => (
+        <Icon
+          key={index}
+          icon={value >= index + 1 ? 'star' : 'star-empty'}
+          onClick={() => handleStarClick(index + 1)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default StarRating;
