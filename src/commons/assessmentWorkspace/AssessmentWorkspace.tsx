43f310dec99bf047950874b512261e9117a9151a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-16 19:08:28 +0800

    Add type guard to prevent reoccurrence

--- src/commons/assessmentWorkspace/AssessmentWorkspace.tsx
+++ src/commons/assessmentWorkspace/AssessmentWorkspace.tsx
@@ -67,6 +67,7 @@ import { SideContentTab, SideContentType } from '../sideContent/SideContentTypes
 import Constants from '../utils/Constants';
 import { useResponsive, useTypedSelector } from '../utils/Hooks';
 import { assessmentTypeLink } from '../utils/ParamParseHelper';
+import { assertType } from '../utils/TypeHelper';
 import Workspace, { WorkspaceProps } from '../workspace/Workspace';
 import {
   beginClearContext,
@@ -362,7 +363,7 @@ const AssessmentWorkspace: React.FC<AssessmentWorkspaceProps> = props => {
     // TODO: Hardcoded to make use of the first editor tab. Refactoring is needed for this workspace to enable Folder mode.
     handleEditorUpdateBreakpoints(0, []);
     handleUpdateCurrentAssessmentId(assessmentId, questionId);
-    handleResetWorkspace({
+    const resetWorkspaceOptions = assertType<WorkspaceState>()({
       autogradingResults: options.autogradingResults ?? [],
       // TODO: Hardcoded to make use of the first editor tab. Rewrite after editor tabs are added.
       editorTabs: [{ value: options.editorValue ?? '', highlightedLines: [], breakpoints: [] }],
@@ -370,6 +371,7 @@ const AssessmentWorkspace: React.FC<AssessmentWorkspaceProps> = props => {
       programPostpendValue: options.programPostpendValue ?? '',
       editorTestcases: options.editorTestcases ?? []
     });
+    handleResetWorkspace(resetWorkspaceOptions);
     handleChangeExecTime(
       question.library.execTimeMs ?? defaultWorkspaceManager.assessment.execTime
     );
