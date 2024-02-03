c3c3837dd43a3b390b6ca3a082c51c33fb8e3114
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-03 23:06:05 +0800

    Migrate source recorder actions to RTK

--- src/features/sourceRecorder/SourceRecorderActions.ts
+++ src/features/sourceRecorder/SourceRecorderActions.ts
@@ -1,4 +1,4 @@
-import { action } from 'typesafe-actions';
+import { createAction } from '@reduxjs/toolkit';
 
 import { WorkspaceLocation } from '../../commons/workspace/WorkspaceTypes';
 import {
@@ -15,70 +15,65 @@ import {
   SET_SOURCECAST_PLAYBACK_STATUS
 } from './SourceRecorderTypes';
 
-export const saveSourcecastData = (
-  title: string,
-  description: string,
-  uid: string,
-  audio: Blob,
-  playbackData: PlaybackData,
-  workspaceLocation: WorkspaceLocation
-) =>
-  action(SAVE_SOURCECAST_DATA, {
-    title,
-    description,
-    uid,
-    audio,
-    audioUrl: window.URL.createObjectURL(audio),
-    playbackData,
-    workspaceLocation
-  });
-
-export const setCurrentPlayerTime = (playerTime: number, workspaceLocation: WorkspaceLocation) =>
-  action(SET_CURRENT_PLAYER_TIME, {
-    playerTime,
-    workspaceLocation
-  });
-
-export const setCodeDeltasToApply = (deltas: CodeDelta[], workspaceLocation: WorkspaceLocation) =>
-  action(SET_CODE_DELTAS_TO_APPLY, {
-    deltas,
-    workspaceLocation
-  });
-
-export const setInputToApply = (inputToApply: Input, workspaceLocation: WorkspaceLocation) =>
-  action(SET_INPUT_TO_APPLY, {
-    inputToApply,
-    workspaceLocation
-  });
-
-export const setSourcecastData = (
-  title: string,
-  description: string,
-  uid: string,
-  audioUrl: string,
-  playbackData: PlaybackData,
-  workspaceLocation: WorkspaceLocation
-) =>
-  action(SET_SOURCECAST_DATA, {
-    title,
-    description,
-    uid,
-    audioUrl,
-    playbackData,
-    workspaceLocation
-  });
-
-export const setSourcecastDuration = (duration: number, workspaceLocation: WorkspaceLocation) =>
-  action(SET_SOURCECAST_PLAYBACK_DURATION, {
-    duration,
-    workspaceLocation
-  });
-
-export const setSourcecastStatus = (
-  playbackStatus: PlaybackStatus,
-  workspaceLocation: WorkspaceLocation
-) =>
-  action(SET_SOURCECAST_PLAYBACK_STATUS, {
-    playbackStatus,
-    workspaceLocation
-  });
+export const saveSourcecastData = createAction(
+  SAVE_SOURCECAST_DATA,
+  (
+    title: string,
+    description: string,
+    uid: string,
+    audio: Blob,
+    playbackData: PlaybackData,
+    workspaceLocation: WorkspaceLocation
+  ) => ({
+    payload: {
+      title,
+      description,
+      uid,
+      audio,
+      audioUrl: window.URL.createObjectURL(audio),
+      playbackData,
+      workspaceLocation
+    }
+  })
+);
+export const setCurrentPlayerTime = createAction(
+  SET_CURRENT_PLAYER_TIME,
+  (playerTime: number, workspaceLocation: WorkspaceLocation) => ({
+    payload: { playerTime, workspaceLocation }
+  })
+);
+export const setCodeDeltasToApply = createAction(
+  SET_CODE_DELTAS_TO_APPLY,
+  (deltas: CodeDelta[], workspaceLocation: WorkspaceLocation) => ({
+    payload: { deltas, workspaceLocation }
+  })
+);
+export const setInputToApply = createAction(
+  SET_INPUT_TO_APPLY,
+  (inputToApply: Input, workspaceLocation: WorkspaceLocation) => ({
+    payload: { inputToApply, workspaceLocation }
+  })
+);
+export const setSourcecastData = createAction(
+  SET_SOURCECAST_DATA,
+  (
+    title: string,
+    description: string,
+    uid: string,
+    audioUrl: string,
+    playbackData: PlaybackData,
+    workspaceLocation: WorkspaceLocation
+  ) => ({ payload: { title, description, uid, audioUrl, playbackData, workspaceLocation } })
+);
+export const setSourcecastDuration = createAction(
+  SET_SOURCECAST_PLAYBACK_DURATION,
+  (duration: number, workspaceLocation: WorkspaceLocation) => ({
+    payload: { duration, workspaceLocation }
+  })
+);
+export const setSourcecastStatus = createAction(
+  SET_SOURCECAST_PLAYBACK_STATUS,
+  (playbackStatus: PlaybackStatus, workspaceLocation: WorkspaceLocation) => ({
+    payload: { playbackStatus, workspaceLocation }
+  })
+);
