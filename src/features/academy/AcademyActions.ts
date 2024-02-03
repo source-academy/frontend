9bda4e2bda476b1f126e6ee20dc7316de4fc0b47
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-03 23:03:24 +0800

    Migrate academy actions to RTK

--- src/features/academy/AcademyActions.ts
+++ src/features/academy/AcademyActions.ts
@@ -1,7 +1,7 @@
+import { createAction } from '@reduxjs/toolkit';
 import { UpdateCourseConfiguration } from 'src/commons/application/types/SessionTypes';
 import { NameUsernameRole } from 'src/pages/academy/adminPanel/subcomponents/AddStoriesUserPanel';
 import { UsernameRoleGroup } from 'src/pages/academy/adminPanel/subcomponents/AddUserPanel';
-import { action } from 'typesafe-actions';
 
 import {
   ADD_NEW_STORIES_USERS_TO_COURSE,
@@ -10,13 +10,18 @@ import {
   SAVE_CANVAS
 } from './AcademyTypes';
 
-export const saveCanvas = (canvas: HTMLCanvasElement) => action(SAVE_CANVAS, canvas);
-
-export const createCourse = (courseConfig: UpdateCourseConfiguration) =>
-  action(CREATE_COURSE, courseConfig);
-
-export const addNewUsersToCourse = (users: UsernameRoleGroup[], provider: string) =>
-  action(ADD_NEW_USERS_TO_COURSE, { users, provider });
-
-export const addNewStoriesUsersToCourse = (users: NameUsernameRole[], provider: string) =>
-  action(ADD_NEW_STORIES_USERS_TO_COURSE, { users, provider });
+export const saveCanvas = createAction(SAVE_CANVAS, (canvas: HTMLCanvasElement) => ({
+  payload: canvas
+}));
+export const createCourse = createAction(
+  CREATE_COURSE,
+  (courseConfig: UpdateCourseConfiguration) => ({ payload: courseConfig })
+);
+export const addNewUsersToCourse = createAction(
+  ADD_NEW_USERS_TO_COURSE,
+  (users: UsernameRoleGroup[], provider: string) => ({ payload: { users, provider } })
+);
+export const addNewStoriesUsersToCourse = createAction(
+  ADD_NEW_STORIES_USERS_TO_COURSE,
+  (users: NameUsernameRole[], provider: string) => ({ payload: { users, provider } })
+);
