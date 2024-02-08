5d30a9deb570896d9bd8da7724a3d091645197b0
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-08 19:27:37 +0800

    Refactor MCQ Chooser logic

--- src/commons/mcqChooser/McqChooser.tsx
+++ src/commons/mcqChooser/McqChooser.tsx
@@ -10,73 +10,48 @@ export type McqChooserProps = {
   handleMCQSubmit: (choiceId: number) => void;
 };
 
-export const McqChooser: React.FC<McqChooserProps> = ({ mcq, handleMCQSubmit }) => {
-  /**
-   * A function to generate an onClick function that causes
-   * and mcq submission with a given answer id.
-   *
-   * Post-condition: the local state will be updated to store the
-   * mcq option selected, and a notification will be displayed with
-   * a hint, if the question is ungraded.
-   *
-   * @param i the id of the answer
-   */
-  const onButtonClickFactory = (i: number) => (e: any) => {
-    if (i !== mcq.answer) {
-      handleMCQSubmit(i);
-    }
-    const shouldDisplayMessage = mcq.solution !== null && mcq.choices[i].hint;
-    if (shouldDisplayMessage) {
-      const hintElement = (
-        <Markdown className="markdown-notification" content={mcq.choices[i].hint!} />
-      );
-      if (i === mcq.solution) {
+const McqChooser: React.FC<McqChooserProps> = ({ mcq, handleMCQSubmit }) => {
+  const options = mcq.choices.map((choice, i) => {
+    const isActive = i === mcq.answer;
+    const hasSolution = mcq.solution !== undefined;
+    const isSolution = i === mcq.solution;
+
+    /**
+     * Post-condition: the local state will be updated to store the
+     * MCQ option selected, and a notification will be displayed with
+     * a hint, if the question is ungraded.
+     */
+    const handleClick = () => {
+      if (!isActive) {
+        handleMCQSubmit(i);
+      }
+      if (!choice.hint || !hasSolution) {
+        return;
+      }
+
+      const hintElement = <Markdown className="markdown-notification" content={choice.hint} />;
+      if (isSolution) {
         showSuccessMessage(hintElement, 4000);
       } else {
         showWarningMessage(hintElement, 4000);
       }
-    }
-  };
+    };
 
-  /**
-   * Handles the logic for what intent an MCQ option should show up as.
-   * This is dependent on the presence of an actual solution (for ungraded assessments),
-   * the current selection, and whether the selected option is active.
-   *
-   * @param currentOption the current button key, corresponding to a choice ID
-   * @param chosenOption the mcq option that is chosen in the state, i.e what should show up as "selected"
-   * @param solution the solution to the mcq, if any
-   */
-  const getButtonIntent = (
-    currentOption: number,
-    chosenOption: number | null,
-    solution?: number
-  ): Intent => {
-    const active = currentOption === chosenOption;
-    const correctOptionSelected = active && solution !== undefined && currentOption === solution;
-    if (solution === undefined) {
-      return Intent.NONE;
-    } else if (active && correctOptionSelected) {
-      return Intent.SUCCESS;
-    } else if (active && !correctOptionSelected) {
-      return Intent.DANGER;
-    } else {
-      return Intent.NONE;
-    }
-  };
-
-  const options = mcq.choices.map((choice, i) => (
-    <Button
-      key={i}
-      className="mcq-option col-xs-12"
-      active={i === mcq.answer}
-      intent={getButtonIntent(i, mcq.answer, mcq.solution)}
-      onClick={onButtonClickFactory(i)}
-      minimal
-    >
-      <Markdown content={choice.content} />
-    </Button>
-  ));
+    return (
+      <Button
+        key={i}
+        className="mcq-option col-xs-12"
+        active={isActive}
+        intent={
+          isActive && hasSolution ? (isSolution ? Intent.SUCCESS : Intent.DANGER) : Intent.NONE
+        }
+        onClick={handleClick}
+        minimal
+      >
+        <Markdown content={choice.content} />
+      </Button>
+    );
+  });
   return (
     <div className="MCQChooser row" data-testid="MCQChooser">
       <Card className="mcq-content-parent col-xs-12 middle-xs">
