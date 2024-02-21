f5916bf41b80d733993a107749cb1a9a623a28af
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-02-21 10:34:35 +0800

    Remove AnimationUtils.tsx file
    replaced with AnimationUtils.ts

--- src/features/envVisualizer/animationComponents/AnimationUtils.ts
+++ src/features/envVisualizer/animationComponents/AnimationUtils.ts
@@ -1,11 +1,32 @@
-import { ControlItemComponent } from '../compactComponents/ControlItemComponent';
-import { StashItemComponent } from '../compactComponents/StashItemComponent';
+import { Binding } from '../compactComponents/Binding';
+import { Frame } from '../compactComponents/Frame';
+import { Visible } from '../components/Visible';
 
-export function getNodePositionFromItem(item: ControlItemComponent | StashItemComponent) {
+export function getNodePositionFromItem(item: Visible) {
   return {
     x: item.x(),
     y: item.y(),
     height: item.height(),
     width: item.width()
   };
+}
+
+// Given a current frame, find a binding by traversing the enclosing environments (frames).
+export function lookupBinding(currFrame: Frame, bindingName: string): [Frame, Binding] {
+  let frame: Frame | undefined = currFrame;
+  while (frame !== undefined) {
+    const binding = frame.bindings.find(b => b.keyString.split(':')[0] === bindingName);
+    // return the top most global frame if we have reached the top of the tree
+    if (frame?.environment?.id === "-1") {
+      return [frame, frame.bindings[0]];
+    }
+    if (binding) {
+      return [frame, binding];
+    }
+    frame = frame.parentFrame;
+  }
+  // this line should never be reached as long as the interpreter works correctly
+  throw new Error(
+    `Error: Binding with name "${bindingName}" cannot be found within the environment!`
+  );
 }
