9589f50e9e51f326e3924e3c752d5a38d3fbb66b
Lee Yi <leeyi45@gmail.com>
2024-02-23 13:14:27 -0500

    Manage Side Content using Redux (#2688)
    * Update execution saga to use both async and non-async versions of js-slang
    
    * Use posix paths instead of regular path library
    
    * Update webpack config to resolve posix path module
    
    * Revert posix path change
    
    * Relocate side content
    
    * Move alerts and side content spawning into redux
    
    * Use redux to manage tab alerts
    
    * Ran format
    
    * Add tests for side content saga
    
    * Update side content state to include selected tab
    
    * Update stories types to include selected tab for side content
    
    * Update mobile side content to use new side content format
    
    * Switch to side content hook for workspace components
    
    * Update naming and add alerts to subst visualizer
    
    * Reset side content when changing chapters
    
    * Move tab change effects to playground saga
    
    * Update tests
    
    * Update sidecontentsaga
    
    * SideContentLocation implementation
    
    * Finish merge
    
    * Move SideContentHeight to SideContentReducer
    
    * Add dev settings button
    
    * Remove unnecessary code
    
    * Ran format
    
    * Add action to remove alerts without changing selected tab
    
    * Fix incorrect naming issue
    
    * Standardize import format
    
    * Remove old commented code
    
    * Remove unnecessary explicit typings
    
    * Remove old commented code
    
    * Reorganize imports
    
    Improves reviewing experience when using diff editor.
    
    * Remove old commented code
    
    * Scope `case` block
    
    * Remove old commented code
    
    * Update snapshots
    
    * Fix broken import
    
    * Reran `yarn install` post merge conflict
    
    * Merge with master
    
    * Relocate changeSideContentHeight to SideContentActions
    
    * Fix broken import
    
    * Disable unnecessary eslint rule
    
    * Remove commons saga and actions
    
    * Remove formik
    
    * Fix stories runtime error
    
    Also added a guard against possible custom environments including a "."
    character.
    
    * Reorder lines
    
    To minimize merge conflicts with other PRs.
    
    ---------
    
    Co-authored-by: Richard Dominick <34370238+RichDom2185@users.noreply.github.com>

--- src/commons/sideContent/SideContent.tsx
+++ src/commons/sideContent/SideContent.tsx
@@ -1,56 +1,41 @@
 import { Card, Icon, Tab, TabProps, Tabs } from '@blueprintjs/core';
 import { Tooltip2 } from '@blueprintjs/popover2';
-import React from 'react';
 
-import { propsAreEqual } from '../utils/MemoizeHelper';
 import { assertType } from '../utils/TypeHelper';
-import { WorkspaceLocation } from '../workspace/WorkspaceTypes';
-import GenericSideContent, { generateIconId, GenericSideContentProps } from './GenericSideContent';
-import { SideContentTab, SideContentType } from './SideContentTypes';
+import { generateTabAlert, getTabId } from './SideContentHelper';
+import SideContentProvider from './SideContentProvider';
+import type {
+  ChangeTabsCallback,
+  SideContentLocation,
+  SideContentTab,
+  SideContentType
+} from './SideContentTypes';
 
-/**
- * @property onChange A function that is called whenever the
- * active tab is changed by the user.
- *
- * @property tabs An array of SideContentTabs.
- *  The tabs will be rendered in order of the array.
- *  If this array is empty, no tabs will be rendered.
- *
- * @property renderActiveTabPanelOnly Set this property to
- * true to enable unmounting of tab panels whenever tabs are
- * switched. If it is left undefined, the value will default
- * to false, and the tab panels will all be loaded with the
- * mounting of the SideContent component. Switching tabs
- * will merely hide them from view.
- */
-export type SideContentProps = Omit<GenericSideContentProps, 'renderFunction'> & StateProps;
-
-type StateProps = {
-  selectedTabId?: SideContentType; // Optional due to uncontrolled tab component in EditingWorkspace
+export type SideContentProps = {
   renderActiveTabPanelOnly?: boolean;
   editorWidth?: string;
-  sideContentHeight?: number;
+  tabs?: {
+    beforeDynamicTabs: SideContentTab[];
+    afterDynamicTabs: SideContentTab[];
+  };
+  onChange?: ChangeTabsCallback;
+  selectedTabId?: SideContentType;
+  defaultTab?: SideContentType;
+  workspaceLocation: SideContentLocation;
 };
-
-/**
- * Adds 'side-content-tab-alert' style to newly spawned module tabs or HTML Display tab
- */
-const generateClassName = (id: string | undefined) =>
-  id === SideContentType.module || id === SideContentType.htmlDisplay
-    ? 'side-content-tooltip side-content-tab-alert'
-    : 'side-content-tooltip';
 
 const renderTab = (
   tab: SideContentTab,
-  workspaceLocation?: WorkspaceLocation,
+  shouldAlert: boolean,
+  workspaceLocation?: SideContentLocation,
   editorWidth?: string,
   sideContentHeight?: number
 ) => {
   const iconSize = 20;
-  const tabId = tab.id === undefined || tab.id === SideContentType.module ? tab.label : tab.id;
+  const tabId = getTabId(tab);
   const tabTitle = (
     <Tooltip2 content={tab.label}>
-      <div className={generateClassName(tab.id)} id={generateIconId(tabId)}>
+      <div className={generateTabAlert(shouldAlert)}>
         <Icon icon={tab.iconName} size={iconSize} />
       </div>
     </Tooltip2>
@@ -82,36 +67,34 @@ const renderTab = (
   return <Tab key={tabId} {...tabProps} panel={tabPanel} />;
 };
 
-const SideContent: React.FC<SideContentProps> = ({
-  selectedTabId,
-  renderActiveTabPanelOnly,
-  editorWidth,
-  sideContentHeight,
-  ...otherProps
-}) => {
-  return (
-    <GenericSideContent
-      {...otherProps}
-      renderFunction={(dynamicTabs, changeTabsCallback) => (
-        <div className="side-content">
-          <Card>
-            <div className="side-content-tabs">
-              <Tabs
-                id="side-content-tabs"
-                onChange={changeTabsCallback}
-                renderActiveTabPanelOnly={renderActiveTabPanelOnly}
-                selectedTabId={selectedTabId}
-              >
-                {dynamicTabs.map(tab =>
-                  renderTab(tab, otherProps.workspaceLocation, editorWidth, sideContentHeight)
-                )}
-              </Tabs>
-            </div>
-          </Card>
-        </div>
-      )}
-    />
-  );
-};
+const SideContent = ({ renderActiveTabPanelOnly, editorWidth, ...props }: SideContentProps) => (
+  <SideContentProvider {...props}>
+    {({ tabs: allTabs, alerts: tabAlerts, changeTabsCallback, selectedTab, height }) => (
+      <div className="side-content">
+        <Card>
+          <div className="side-content-tabs">
+            <Tabs
+              id="side-content-tabs"
+              onChange={changeTabsCallback}
+              renderActiveTabPanelOnly={renderActiveTabPanelOnly}
+              selectedTabId={selectedTab}
+            >
+              {allTabs.map(tab => {
+                const tabId = getTabId(tab);
+                return renderTab(
+                  tab,
+                  tabAlerts.includes(tabId),
+                  props.workspaceLocation,
+                  editorWidth,
+                  height
+                );
+              })}
+            </Tabs>
+          </div>
+        </Card>
+      </div>
+    )}
+  </SideContentProvider>
+);
 
-export default React.memo(SideContent, propsAreEqual);
+export default SideContent;
