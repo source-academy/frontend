99bf85d38f9810796aa166657f94e40d4ca4a313
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-24 03:32:57 +0800

    Migrate GitHub actions to RTK

--- src/features/github/GitHubActions.ts
+++ src/features/github/GitHubActions.ts
@@ -1,9 +1,9 @@
-import { action } from 'typesafe-actions';
+import { createAction } from '@reduxjs/toolkit';
 
 import { GITHUB_OPEN_FILE, GITHUB_SAVE_FILE, GITHUB_SAVE_FILE_AS } from './GitHubTypes';
 
-export const githubOpenFile = () => action(GITHUB_OPEN_FILE);
+export const githubOpenFile = createAction(GITHUB_OPEN_FILE, () => ({ payload: {} }));
 
-export const githubSaveFile = () => action(GITHUB_SAVE_FILE);
+export const githubSaveFile = createAction(GITHUB_SAVE_FILE, () => ({ payload: {} }));
 
-export const githubSaveFileAs = () => action(GITHUB_SAVE_FILE_AS);
+export const githubSaveFileAs = createAction(GITHUB_SAVE_FILE_AS, () => ({ payload: {} }));
