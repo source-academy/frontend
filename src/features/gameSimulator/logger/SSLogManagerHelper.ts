519cebab2ff05e86ac69bc8ccab4d25ba6e7839e
Lee Hyung Woon / 이형운 <leehyungwoonsamuel@gmail.com>
2024-02-24 17:25:22 +0800

    Remove Object Placement feature from Game Simulator (#2810)
    * Remove Object Placement feature from Game Simulator

--- src/features/gameSimulator/logger/SSLogManagerHelper.ts
+++
@@ -1,7 +0,0 @@
-import { AssetPath } from 'src/features/game/commons/CommonTypes';
-
-export function getIdFromShortPath(shortPath: AssetPath) {
-  return shortPath.split('/').pop()!.split('.')[0];
-}
-
-export const padWithTab = (str: string) => '    ' + str;
