6496254851bf2379b57f7dc3a67d4ee7a1dddbc2
Wang Yiwen <e0968880@u.nus.edu>
2024-03-01 18:10:51 +0800

    Migrate request to Saga

--- src/commons/sagas/RequestsSaga.ts
+++ src/commons/sagas/RequestsSaga.ts
@@ -1329,6 +1329,27 @@ export async function deleteDevice(device: Pick<Device, 'id'>, tokens?: Tokens):
   return true;
 }
 
+/**
+ * POST /chat
+ */
+
+export async function chat(payload: { role: string; content: string }[]): Promise<string> {
+  const { accessToken, refreshToken } = store.getState().session;
+  const tokens = { accessToken, refreshToken };
+  const response = await request(`chat`, 'POST', {
+    ...tokens,
+    body: { json: payload }
+  });
+  if (!response) {
+    throw new Error('Unknown error occurred.');
+  }
+  if (!response.ok) {
+    const message = await response.text();
+    throw new Error(`Failed to chat to louis: ${message}`);
+  }
+  return response.text();
+}
+
 function fillTokens(tokens?: Tokens): Tokens {
   tokens = tokens || getTokensFromStore();
   if (!tokens) {
