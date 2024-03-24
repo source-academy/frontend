fa0a2a303c59a70e504786a1fe466d8af0051c03
Dang Hoang Duy <e1129930@u.nus.edu>
2024-03-24 20:09:37 +0800

    Update changpointSteps in context to match js-slang 3

--- src/commons/sagas/WorkspaceSaga.ts
+++ src/commons/sagas/WorkspaceSaga.ts
@@ -1239,6 +1239,9 @@ export function* evalCode(
         yield put(
           actions.updateBreakpointSteps(context.runtime.breakpointSteps, workspaceLocation)
         );
+        yield put(
+          actions.updateChangePointSteps(context.runtime.changepointSteps, workspaceLocation)
+        );
       }
     } else {
       // Safe to use ! as storyEnv will be defined from above when we call from EVAL_STORY
