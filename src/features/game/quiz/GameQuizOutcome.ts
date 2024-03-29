112b8a372186c40746b4e4d8cab26d9631ec0c75
reginateh <e1312290@u.nus.edu>
2024-03-29 11:24:00 +0800

    remove redundant file and console logs

--- src/features/game/quiz/GameQuizOutcome.ts
+++
@@ -1,7 +0,0 @@
-import GameQuizReactionManager from './GameQuizReactionManager';
-import { DialogueObject } from '../dialogue/GameDialogueTypes';
-export default class GameQuizOutcomeManager extends GameQuizReactionManager {
-    constructor(dialogueObj : DialogueObject) {
-        super(dialogueObj);
-    }
-}
