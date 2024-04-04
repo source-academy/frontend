2c479d299e2824044cf5dee880eb1a80ee7a2db2
Chang-CH <chang.ch4348@gmail.com>
2024-04-04 16:12:21 +0800

    clean up code structure

--- src/commons/utils/JavaHelper.ts
+++ src/commons/utils/JavaHelper.ts
@@ -6,31 +6,103 @@ import { initModuleContext, loadModuleBundle } from 'js-slang/dist/modules/loade
 import DisplayBufferService from './DisplayBufferService';
 
 const CDN = 'https://source-academy.github.io/modules/java/java-packages/src/';
-const supportedModules: string[] = ['rune'];
 
 export async function javaRun(javaCode: string, context: Context) {
   let compiled = {};
+
+  let files = {};
+  let buffer: string[] = [];
+
+  const readClassFiles = (path: string) => {
+    let item = files[path as keyof typeof files];
+
+    // not found: attempt to fetch from CDN
+    if (!item && path) {
+      const splits = path.split('/');
+      splits.pop(); // classname.class
+      const pkg = splits.join('_');
+
+      const request = new XMLHttpRequest();
+      request.open('GET', `${CDN}${pkg}.json`, false);
+      request.send(null);
+      if (request.status !== 200) {
+        throw new Error('File not found: ' + path);
+      }
+      const json = JSON.parse(request.responseText);
+      // we might want to cache the files in IndexedDB here
+      files = { ...files, ...json };
+
+      if (!files[path as keyof typeof files]) {
+        throw new Error('File not found: ' + path);
+      }
+
+      item = files[path as keyof typeof files];
+    }
+
+    // convert base64 to classfile object
+    const binaryString = atob(item);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return parseBin(new DataView(bytes.buffer));
+  };
+  const loadNatives = (path: string) => {
+    // dynamic load modules
+    if (path.startsWith('modules')) {
+      const module = path.split('/')[1] as string;
+      initModuleContext(module, context, true);
+      const moduleFuncs = loadModuleBundle(module, context);
+      const { proxy } = createModuleProxy(module, moduleFuncs);
+      return Promise.resolve({ default: proxy });
+    }
+
+    return import(`java-slang/dist/jvm/stdlib/${path}.js`).then(m => {
+      return m;
+    });
+  };
+  const stdout = (str: string) => {
+    // java flush pushes a single newline to buffer
+    if (str === '\n') {
+      if (buffer.length > 0) {
+        DisplayBufferService.push(buffer.join(''), context.externalContext);
+      }
+      buffer = [];
+      return;
+    }
+
+    if (str.endsWith('\n')) {
+      buffer.push(str);
+      DisplayBufferService.push(buffer.join(''), context.externalContext);
+      buffer = [];
+    } else {
+      buffer.push(str);
+    }
+  };
+  const stderr = (msg: string) => {
+    context.errors.push({
+      type: 'Runtime' as any,
+      severity: 'Error' as any,
+      location: {
+        start: {
+          line: -1,
+          column: -1
+        },
+        end: {
+          line: -1,
+          column: -1
+        }
+      },
+      explain: () => msg,
+      elaborate: () => msg
+    });
+  };
 
   // FIXME: Remove when the compiler is working
   if (javaCode.startsWith('// From JSON')) {
     const json = JSON.parse(javaCode.slice(13));
     compiled = json;
   }
-
-  // load modules
-  const lib: {
-    [key: string]: {
-      methods: { [key: string]: (...args: any[]) => any };
-    };
-  } = {};
-  await Promise.all(
-    supportedModules.map(async module => {
-      await initModuleContext(module, context, true);
-      const moduleFuncs = await loadModuleBundle(module, context);
-      const { javaClassname, proxy } = await createModuleProxy(module, moduleFuncs);
-      lib[javaClassname] = { methods: proxy };
-    })
-  );
 
   // load cached classfiles from IndexedDB
   return loadCachedFiles(() =>
@@ -42,12 +114,10 @@ export async function javaRun(javaCode: string, context: Context) {
       })
   )
     .then(stdlib => {
-      let files = {
+      files = {
         ...stdlib,
         ...compiled
       };
-
-      let buffer: string[] = [];
 
       // run the JVM
       return new Promise((resolve, reject) => {
@@ -55,82 +125,10 @@ export async function javaRun(javaCode: string, context: Context) {
           javaClassPath: '',
           nativesPath: '',
           callbacks: {
-            readFileSync: (path: string) => {
-              let item = files[path as keyof typeof files];
-
-              // not found: attempt to fetch from CDN
-              if (!item && path) {
-                const splits = path.split('/');
-                splits.pop(); // classname.class
-                const pkg = splits.join('_');
-
-                const request = new XMLHttpRequest();
-                request.open('GET', `${CDN}${pkg}.json`, false);
-                console.log('get ', `${CDN}${pkg}.json`);
-                request.send(null);
-                if (request.status !== 200) {
-                  throw new Error('File not found: ' + path);
-                }
-                const json = JSON.parse(request.responseText);
-                // we might want to cache the files in IndexedDB here
-                files = { ...files, ...json };
-
-                if (!files[path as keyof typeof files]) {
-                  throw new Error('File not found: ' + path);
-                }
-
-                item = files[path as keyof typeof files];
-              }
-
-              // convert base64 to classfile object
-              const binaryString = atob(item);
-              const bytes = new Uint8Array(binaryString.length);
-              for (let i = 0; i < binaryString.length; i++) {
-                bytes[i] = binaryString.charCodeAt(i);
-              }
-              return parseBin(new DataView(bytes.buffer));
-            },
-            readFile: (path: string) => {
-              return import(`java-slang/dist/jvm/stdlib/${path}.js`).then(m => {
-                return m;
-              });
-            },
-            stdout: (str: string) => {
-              // java flush pushes a single newline to buffer
-              if (str === '\n') {
-                if (buffer.length > 0) {
-                  DisplayBufferService.push(buffer.join(''), context.externalContext);
-                }
-                buffer = [];
-                return;
-              }
-
-              if (str.endsWith('\n')) {
-                buffer.push(str);
-                DisplayBufferService.push(buffer.join(''), context.externalContext);
-                buffer = [];
-              } else {
-                buffer.push(str);
-              }
-            },
-            stderr: (msg: string) => {
-              context.errors.push({
-                type: 'Runtime' as any,
-                severity: 'Error' as any,
-                location: {
-                  start: {
-                    line: -1,
-                    column: -1
-                  },
-                  end: {
-                    line: -1,
-                    column: -1
-                  }
-                },
-                explain: () => msg,
-                elaborate: () => msg
-              });
-            },
+            readFileSync: readClassFiles,
+            readFile: loadNatives,
+            stdout,
+            stderr,
             onFinish: () => {
               resolve(
                 context.errors.length
@@ -139,7 +137,7 @@ export async function javaRun(javaCode: string, context: Context) {
               );
             }
           },
-          natives: lib
+          natives: {}
         })();
       });
     })
