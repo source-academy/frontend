ff4206f047787db52ceb76646a7af6320ae9ff5a
Jo An <142368471+jayjay19630@users.noreply.github.com>
2024-04-06 18:49:27 +0800

    Assign entries for voting (#2889)
    * Implement assign entries for voting with request
    
    * Create style for assign entries button
    
    * Code quality improvements
    
    * Add handleAssignEntries function to configure cell
    
    * Create new AssignEntriesButton component and configure controls css module

--- src/pages/academy/groundControl/subcomponents/GroundControlConfigureCell.tsx
+++ src/pages/academy/groundControl/subcomponents/GroundControlConfigureCell.tsx
@@ -13,6 +13,7 @@ import React, { useCallback, useState } from 'react';
 
 import { AssessmentOverview } from '../../../../commons/assessment/AssessmentTypes';
 import ControlButton from '../../../../commons/ControlButton';
+import AssignEntriesButton from './configureControls/AssignEntriesButton';
 
 type Props = {
   handleConfigureAssessment: (
@@ -20,24 +21,32 @@ type Props = {
     hasVotingFeatures: boolean,
     hasTokenCounter: boolean
   ) => void;
+  handleAssignEntriesForVoting: (id: number) => void;
   data: AssessmentOverview;
 };
 
-const ConfigureCell: React.FC<Props> = ({ handleConfigureAssessment, data }) => {
+const ConfigureCell: React.FC<Props> = ({
+  handleConfigureAssessment,
+  handleAssignEntriesForVoting,
+  data
+}) => {
   const [isDialogOpen, setDialogState] = useState(false);
   const [hasVotingFeatures, setHasVotingFeatures] = useState(!!data.hasVotingFeatures);
   const [hasTokenCounter, setHasTokenCounter] = useState(!!data.hasTokenCounter);
   const [isTeamAssessment, setIsTeamAssessment] = useState(false);
+  const [isVotingPublished] = useState(!!data.isVotingPublished);
 
   const handleOpenDialog = useCallback(() => setDialogState(true), []);
   const handleCloseDialog = useCallback(() => setDialogState(false), []);
 
+  // Updates assessment overview with changes to hasVotingFeatures and hasTokenCounter
   const handleConfigure = useCallback(() => {
     const { id } = data;
     handleConfigureAssessment(id, hasVotingFeatures, hasTokenCounter);
     handleCloseDialog();
   }, [data, handleCloseDialog, handleConfigureAssessment, hasTokenCounter, hasVotingFeatures]);
 
+  // Toggles in configuration pannel
   const toggleHasTokenCounter = useCallback(() => setHasTokenCounter(prev => !prev), []);
   const toggleVotingFeatures = useCallback(() => setHasVotingFeatures(prev => !prev), []);
   const toggleIsTeamAssessment = useCallback(() => setIsTeamAssessment(prev => !prev), []);
@@ -117,11 +126,10 @@ const ConfigureCell: React.FC<Props> = ({ handleConfigureAssessment, data }) => 
                     label="Export Score Leaderboard (Coming soon!)"
                   />
                 </div>
-                <Switch
-                  className="publish-voting"
-                  disabled={true}
-                  inline
-                  label="Publish Voting (Coming soon!)"
+                <AssignEntriesButton
+                  handleAssignEntriesForVoting={handleAssignEntriesForVoting}
+                  assessmentId={data.id}
+                  isVotingPublished={isVotingPublished}
                 />
               </div>
             </Collapse>
