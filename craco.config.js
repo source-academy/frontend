8a7386ebcee1eab6525da719fca02f71fd7a967f
Bryan Loh <bryanlohxz@gmail.com>
2024-04-11 22:46:33 +0800

    Add process/browser to craco polyfill

--- craco.config.js
+++ craco.config.js
@@ -47,9 +47,10 @@ const cracoConfig = {
         'https': require.resolve('https-browserify'),
         'os': require.resolve('os-browserify/browser'),
         'path/posix': require.resolve('path-browserify'),
+        'process/browser': require.resolve('process/browser'),
         'stream': require.resolve('stream-browserify'),
         'timers': require.resolve('timers-browserify'),
-        'url': require.resolve('url/')
+        'url': require.resolve('url/'),
       };
 
       // workaround .mjs files by Acorn
