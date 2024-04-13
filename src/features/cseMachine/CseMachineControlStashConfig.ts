65ff844fc5c2729905be10bc9729b1bb05c7881e
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-04-13 20:55:14 +0800

    Re-add color dependencies for Java CSE

--- src/features/cseMachine/CseMachineControlStashConfig.ts
+++ src/features/cseMachine/CseMachineControlStashConfig.ts
@@ -30,5 +30,11 @@ export const ControlStashConfig = Object.freeze({
   FontStyle: 'normal',
   FontVariant: 'normal',
   FontStyleHeader: 'bold',
-  FontSizeHeader: 18
+  FontSizeHeader: 18,
+
+  // Colors still used by the Java CSE Machine but not the JS CSE
+  SA_WHITE: '#999999',
+  SA_BLUE: '#2c3e50',
+  PRINT_BACKGROUND: 'white',
+  STASH_DANGER_ITEM: '#ff0000'
 });
