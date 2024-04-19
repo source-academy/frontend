0a509e9311904f4086f12f4e9901bd7a93296637
Wong Chee Hong <c.h.wong2606@gmail.com>
2024-04-19 18:25:03 +0800

    Consistent formatting for i18n test instance file

--- src/i18n/i18nForTests.ts
+++ src/i18n/i18nForTests.ts
@@ -3,19 +3,18 @@ import { initReactI18next } from 'react-i18next';
 
 import { defaultResources, i18nLanguageCode } from './locales';
 
-i18n.use(initReactI18next).init({
-  lng: i18nLanguageCode.ENGLISH,
-  fallbackLng: i18nLanguageCode.ENGLISH,
-  ns: ['translationsNS'],
-  defaultNS: 'commons',
-
-  debug: true,
-
-  interpolation: {
-    escapeValue: false // not needed for react!!
-  },
-
-  resources: defaultResources
-});
+i18n
+  .use(initReactI18next)
+  .init({
+    lng: i18nLanguageCode.ENGLISH,
+    fallbackLng: i18nLanguageCode.ENGLISH,
+    debug: true,
+    ns: ['translationsNS'],
+    defaultNS: 'commons',
+    resources: defaultResources,
+    interpolation: {
+      escapeValue: false // not needed for react!!
+    },
+  });
 
 export default i18n;
