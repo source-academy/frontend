66c4160b79e41e9d235be2d0fc2351e7d96640e3
En Rong <53928333+chownces@users.noreply.github.com>
2024-05-03 12:05:14 +0800

    Update waitFor usage

--- src/pages/playground/__tests__/Playground.tsx
+++ src/pages/playground/__tests__/Playground.tsx
@@ -1,4 +1,5 @@
-import { render, waitFor } from '@testing-library/react';
+import { Router } from '@remix-run/router';
+import { act, render } from '@testing-library/react';
 import { require as acequire } from 'ace-builds';
 import { FSModule } from 'browserfs/dist/node/core/FS';
 import { Chapter } from 'js-slang/dist/types';
@@ -26,6 +27,14 @@ jest.mock('ace-builds', () => ({
 }));
 
 const acequireMock = acequire as jest.Mock;
+
+// Using @testing-library/react to render snapshot instead of react-test-renderer
+// as the useRefs require the notion of React DOM
+const renderTree = async (router: Router) => {
+  const app = render(<RouterProvider router={router} />);
+  await act(() => app);
+  return app.container;
+};
 
 describe('Playground tests', () => {
   let routes: RouteObject[];
@@ -64,9 +73,7 @@ describe('Playground tests', () => {
       initialIndex: 0
     });
 
-    // Using @testing-library/react to render snapshot instead of react-test-renderer
-    // as the useRefs require the notion of React DOM
-    const tree = await waitFor(() => render(<RouterProvider router={router} />).container);
+    const tree = await renderTree(router);
     expect(tree).toMatchSnapshot();
 
     expect(getSourceChapterFromStore(mockStore)).toBe(defaultPlayground.languageConfig.chapter);
@@ -79,9 +86,7 @@ describe('Playground tests', () => {
       initialIndex: 0
     });
 
-    // Using @testing-library/react to render snapshot instead of react-test-renderer
-    // as the useRefs require the notion of React DOM
-    const tree = await waitFor(() => render(<RouterProvider router={router} />).container);
+    const tree = await renderTree(router);
     expect(tree).toMatchSnapshot();
 
     expect(getSourceChapterFromStore(mockStore)).toBe(Chapter.SOURCE_2);
