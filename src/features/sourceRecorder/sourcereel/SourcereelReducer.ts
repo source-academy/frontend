63920e30c668c7665b5a7296c7ddaeed1e36637c
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-05 18:30:39 +0800

    Migrate SourcereelReducer to RTK

--- src/features/sourceRecorder/sourcereel/SourcereelReducer.ts
+++ src/features/sourceRecorder/sourcereel/SourcereelReducer.ts
@@ -1,80 +1,53 @@
-import { SourceActionType } from '../../../commons/utils/ActionsHelper';
+import { createReducer } from '@reduxjs/toolkit';
+import { defaultWorkspaceManager } from 'src/commons/application/ApplicationTypes';
+
 import { RecordingStatus } from '../SourceRecorderTypes';
 import {
-  RECORD_INIT,
-  RECORD_INPUT,
-  RESET_INPUTS,
-  SourcereelWorkspaceState,
-  TIMER_PAUSE,
-  TIMER_RESET,
-  TIMER_RESUME,
-  TIMER_START,
-  TIMER_STOP
-} from './SourcereelTypes';
+  recordInit,
+  recordInput,
+  resetInputs,
+  timerPause,
+  timerReset,
+  timerResume,
+  timerStart,
+  timerStop
+} from './SourcereelActions';
 
-export const SourcereelReducer = (state: SourcereelWorkspaceState, action: SourceActionType) => {
-  switch (action.type) {
-    case RECORD_INIT:
-      return {
-        ...state,
-        playbackData: {
-          init: action.payload.initData,
-          inputs: []
-        }
-      };
-    case RECORD_INPUT:
-      return {
-        ...state,
-        playbackData: {
-          ...state.playbackData,
-          inputs: [...state.playbackData.inputs, action.payload.input]
-        }
-      };
-    case RESET_INPUTS:
-      return {
-        ...state,
-        playbackData: {
-          ...state.playbackData,
-          inputs: action.payload.inputs
-        }
-      };
-    case TIMER_PAUSE:
-      return {
-        ...state,
-        recordingStatus: RecordingStatus.paused,
-        timeElapsedBeforePause:
-          state.timeElapsedBeforePause + action.payload.timeNow - state.timeResumed
-      };
-    case TIMER_RESET:
-      return {
-        ...state,
-        recordingStatus: RecordingStatus.notStarted,
-        timeElapsedBeforePause: 0,
-        timeResumed: 0
-      };
-    case TIMER_RESUME:
-      return {
-        ...state,
-        recordingStatus: RecordingStatus.recording,
-        timeElapsedBeforePause:
-          action.payload.timeBefore >= 0 ? action.payload.timeBefore : state.timeElapsedBeforePause,
-        timeResumed: action.payload.timeNow
-      };
-    case TIMER_START:
-      return {
-        ...state,
-        recordingStatus: RecordingStatus.recording,
-        timeElapsedBeforePause: 0,
-        timeResumed: action.payload.timeNow
-      };
-    case TIMER_STOP:
-      return {
-        ...state,
-        recordingStatus: RecordingStatus.finished,
-        timeElapsedBeforePause: 0,
-        timeResumed: 0
-      };
-    default:
-      return state;
-  }
-};
+export const SourcereelReducer = createReducer(defaultWorkspaceManager.sourcereel, builder => {
+  builder
+    .addCase(recordInit, (state, action) => {
+      state.playbackData.init = action.payload.initData;
+      state.playbackData.inputs = [];
+    })
+    .addCase(recordInput, (state, action) => {
+      state.playbackData.inputs.push(action.payload.input);
+    })
+    .addCase(resetInputs, (state, action) => {
+      state.playbackData.inputs = action.payload.inputs;
+    })
+    .addCase(timerPause, (state, action) => {
+      state.recordingStatus = RecordingStatus.paused;
+      state.timeElapsedBeforePause += action.payload.timeNow - state.timeResumed;
+    })
+    .addCase(timerReset, (state, action) => {
+      state.recordingStatus = RecordingStatus.notStarted;
+      state.timeElapsedBeforePause = 0;
+      state.timeResumed = 0;
+    })
+    .addCase(timerResume, (state, action) => {
+      state.recordingStatus = RecordingStatus.recording;
+      state.timeElapsedBeforePause =
+        action.payload.timeBefore >= 0 ? action.payload.timeBefore : state.timeElapsedBeforePause;
+      state.timeResumed = action.payload.timeNow;
+    })
+    .addCase(timerStart, (state, action) => {
+      state.recordingStatus = RecordingStatus.recording;
+      state.timeElapsedBeforePause = 0;
+      state.timeResumed = action.payload.timeNow;
+    })
+    .addCase(timerStop, (state, action) => {
+      state.recordingStatus = RecordingStatus.finished;
+      state.timeElapsedBeforePause = 0;
+      state.timeResumed = 0;
+    });
+});
