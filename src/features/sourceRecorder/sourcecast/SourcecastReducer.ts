ef623c104f5a3013c1fc405a53853bead53568e8
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-05 18:30:50 +0800

    Migrate SourcecastReducer to RTK

--- src/features/sourceRecorder/sourcecast/SourcecastReducer.ts
+++ src/features/sourceRecorder/sourcecast/SourcecastReducer.ts
@@ -1,69 +1,49 @@
-import { SourceActionType } from '../../../commons/utils/ActionsHelper';
-import {
-  SAVE_SOURCECAST_DATA,
-  SET_CODE_DELTAS_TO_APPLY,
-  SET_CURRENT_PLAYER_TIME,
-  SET_INPUT_TO_APPLY,
-  SET_SOURCECAST_DATA,
-  SET_SOURCECAST_PLAYBACK_DURATION,
-  SET_SOURCECAST_PLAYBACK_STATUS
-} from '../SourceRecorderTypes';
-import { SourcecastWorkspaceState, UPDATE_SOURCECAST_INDEX } from './SourcecastTypes';
+import { createReducer } from '@reduxjs/toolkit';
+import { defaultWorkspaceManager } from 'src/commons/application/ApplicationTypes';
 
-export const SourcecastReducer = (
-  state: SourcecastWorkspaceState,
-  action: SourceActionType
-): SourcecastWorkspaceState => {
-  switch (action.type) {
-    case SAVE_SOURCECAST_DATA:
-      return {
-        ...state,
-        title: action.payload.title,
-        description: action.payload.description,
-        uid: action.payload.uid,
-        audioUrl: action.payload.audioUrl,
-        playbackData: action.payload.playbackData
-      };
-    case SET_CURRENT_PLAYER_TIME:
-      return {
-        ...state,
-        currentPlayerTime: action.payload.playerTime
-      };
-    case SET_CODE_DELTAS_TO_APPLY:
-      return {
-        ...state,
-        codeDeltasToApply: action.payload.deltas
-      };
-    case SET_INPUT_TO_APPLY:
-      return {
-        ...state,
-        inputToApply: action.payload.inputToApply
-      };
-    case SET_SOURCECAST_DATA:
-      return {
-        ...state,
-        title: action.payload.title,
-        description: action.payload.description,
-        uid: action.payload.uid,
-        audioUrl: action.payload.audioUrl,
-        playbackData: action.payload.playbackData
-      };
-    case SET_SOURCECAST_PLAYBACK_DURATION:
-      return {
-        ...state,
-        playbackDuration: action.payload.duration
-      };
-    case SET_SOURCECAST_PLAYBACK_STATUS:
-      return {
-        ...state,
-        playbackStatus: action.payload.playbackStatus
-      };
-    case UPDATE_SOURCECAST_INDEX:
-      return {
-        ...state,
-        sourcecastIndex: action.payload.index
-      };
-    default:
-      return state;
-  }
-};
+import {
+  saveSourcecastData,
+  setCodeDeltasToApply,
+  setCurrentPlayerTime,
+  setInputToApply,
+  setSourcecastData,
+  setSourcecastDuration,
+  setSourcecastStatus
+} from '../SourceRecorderActions';
+import { updateSourcecastIndex } from './SourcecastActions';
+
+export const SourcecastReducer = createReducer(defaultWorkspaceManager.sourcecast, builder => {
+  builder
+    .addCase(saveSourcecastData, (state, action) => {
+      state.title = action.payload.title;
+      state.description = action.payload.description;
+      state.uid = action.payload.uid;
+      state.audioUrl = action.payload.audioUrl;
+      state.playbackData = action.payload.playbackData;
+    })
+    .addCase(setCurrentPlayerTime, (state, action) => {
+      state.currentPlayerTime = action.payload.playerTime;
+    })
+    .addCase(setCodeDeltasToApply, (state, action) => {
+      state.codeDeltasToApply = action.payload.deltas;
+    })
+    .addCase(setInputToApply, (state, action) => {
+      state.inputToApply = action.payload.inputToApply;
+    })
+    .addCase(setSourcecastData, (state, action) => {
+      state.title = action.payload.title;
+      state.description = action.payload.description;
+      state.uid = action.payload.uid;
+      state.audioUrl = action.payload.audioUrl;
+      state.playbackData = action.payload.playbackData;
+    })
+    .addCase(setSourcecastDuration, (state, action) => {
+      state.playbackDuration = action.payload.duration;
+    })
+    .addCase(setSourcecastStatus, (state, action) => {
+      state.playbackStatus = action.payload.playbackStatus;
+    })
+    .addCase(updateSourcecastIndex, (state, action) => {
+      state.sourcecastIndex = action.payload.index;
+    });
+});
