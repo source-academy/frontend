bd1c96dc94b7f73ef08715cdf75b620e7f2df8e2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-06 18:08:43 +0800

    Remove outdated comment about disabled SA

--- src/commons/application/ApplicationWrapper.tsx
+++ src/commons/application/ApplicationWrapper.tsx
@@ -16,10 +16,9 @@ import { updateReactRouter } from './actions/CommonsActions';
 /**
  * Application wrapper component which figures out which deployment and set of routes to render.
  *
- * There are 3 main types of deployments as follows:
+ * There are 2 main types of deployments as follows:
  * 1. Playground-only (stripped-down backendless version of SA - e.g. https://sourceacademy.org)
  * 2. Full Academy (full SA to be deployed and configured with the backend - e.g. https://sourceacademy.nus.edu.sg)
- * 3. Disabled (disabled SA which only allows `staff` and `admin` accounts to log in - e.g. during examinations)
  */
 const ApplicationWrapper: React.FC = () => {
   const dispatch = useDispatch();
