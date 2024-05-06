fe7c3d9959fa6cad3c780021bb050126bb02ea8d
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-06 22:58:37 +0800

    Use namespace import in new reducers
    This is to ease the migration in future PRs where the named exports for
    action creators are removed.

--- src/features/sourceRecorder/sourcecast/SourcecastReducer.ts
+++ src/features/sourceRecorder/sourcecast/SourcecastReducer.ts
@@ -1,46 +1,38 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { defaultWorkspaceManager } from 'src/commons/application/ApplicationTypes';
 
-import {
-  saveSourcecastData,
-  setCodeDeltasToApply,
-  setCurrentPlayerTime,
-  setInputToApply,
-  setSourcecastData,
-  setSourcecastDuration,
-  setSourcecastStatus
-} from '../SourceRecorderActions';
+import * as SourceRecorderActions from '../SourceRecorderActions';
 import { updateSourcecastIndex } from './SourcecastActions';
 
 export const SourcecastReducer = createReducer(defaultWorkspaceManager.sourcecast, builder => {
   builder
-    .addCase(saveSourcecastData, (state, action) => {
+    .addCase(SourceRecorderActions.saveSourcecastData, (state, action) => {
       state.title = action.payload.title;
       state.description = action.payload.description;
       state.uid = action.payload.uid;
       state.audioUrl = action.payload.audioUrl;
       state.playbackData = action.payload.playbackData;
     })
-    .addCase(setCurrentPlayerTime, (state, action) => {
+    .addCase(SourceRecorderActions.setCurrentPlayerTime, (state, action) => {
       state.currentPlayerTime = action.payload.playerTime;
     })
-    .addCase(setCodeDeltasToApply, (state, action) => {
+    .addCase(SourceRecorderActions.setCodeDeltasToApply, (state, action) => {
       state.codeDeltasToApply = action.payload.deltas;
     })
-    .addCase(setInputToApply, (state, action) => {
+    .addCase(SourceRecorderActions.setInputToApply, (state, action) => {
       state.inputToApply = action.payload.inputToApply;
     })
-    .addCase(setSourcecastData, (state, action) => {
+    .addCase(SourceRecorderActions.setSourcecastData, (state, action) => {
       state.title = action.payload.title;
       state.description = action.payload.description;
       state.uid = action.payload.uid;
       state.audioUrl = action.payload.audioUrl;
       state.playbackData = action.payload.playbackData;
     })
-    .addCase(setSourcecastDuration, (state, action) => {
+    .addCase(SourceRecorderActions.setSourcecastDuration, (state, action) => {
       state.playbackDuration = action.payload.duration;
     })
-    .addCase(setSourcecastStatus, (state, action) => {
+    .addCase(SourceRecorderActions.setSourcecastStatus, (state, action) => {
       state.playbackStatus = action.payload.playbackStatus;
     })
     .addCase(updateSourcecastIndex, (state, action) => {
--- src/features/sourceRecorder/sourcereel/SourcereelReducer.ts
+++ src/features/sourceRecorder/sourcereel/SourcereelReducer.ts
@@ -2,50 +2,41 @@ import { createReducer } from '@reduxjs/toolkit';
 import { defaultWorkspaceManager } from 'src/commons/application/ApplicationTypes';
 
 import { RecordingStatus } from '../SourceRecorderTypes';
-import {
-  recordInit,
-  recordInput,
-  resetInputs,
-  timerPause,
-  timerReset,
-  timerResume,
-  timerStart,
-  timerStop
-} from './SourcereelActions';
+import * as SourcereelActions from './SourcereelActions';
 
 export const SourcereelReducer = createReducer(defaultWorkspaceManager.sourcereel, builder => {
   builder
-    .addCase(recordInit, (state, action) => {
+    .addCase(SourcereelActions.recordInit, (state, action) => {
       state.playbackData.init = action.payload.initData;
       state.playbackData.inputs = [];
     })
-    .addCase(recordInput, (state, action) => {
+    .addCase(SourcereelActions.recordInput, (state, action) => {
       state.playbackData.inputs.push(action.payload.input);
     })
-    .addCase(resetInputs, (state, action) => {
+    .addCase(SourcereelActions.resetInputs, (state, action) => {
       state.playbackData.inputs = action.payload.inputs;
     })
-    .addCase(timerPause, (state, action) => {
+    .addCase(SourcereelActions.timerPause, (state, action) => {
       state.recordingStatus = RecordingStatus.paused;
       state.timeElapsedBeforePause += action.payload.timeNow - state.timeResumed;
     })
-    .addCase(timerReset, (state, action) => {
+    .addCase(SourcereelActions.timerReset, (state, action) => {
       state.recordingStatus = RecordingStatus.notStarted;
       state.timeElapsedBeforePause = 0;
       state.timeResumed = 0;
     })
-    .addCase(timerResume, (state, action) => {
+    .addCase(SourcereelActions.timerResume, (state, action) => {
       state.recordingStatus = RecordingStatus.recording;
       state.timeElapsedBeforePause =
         action.payload.timeBefore >= 0 ? action.payload.timeBefore : state.timeElapsedBeforePause;
       state.timeResumed = action.payload.timeNow;
     })
-    .addCase(timerStart, (state, action) => {
+    .addCase(SourcereelActions.timerStart, (state, action) => {
       state.recordingStatus = RecordingStatus.recording;
       state.timeElapsedBeforePause = 0;
       state.timeResumed = action.payload.timeNow;
     })
-    .addCase(timerStop, (state, action) => {
+    .addCase(SourcereelActions.timerStop, (state, action) => {
       state.recordingStatus = RecordingStatus.finished;
       state.timeElapsedBeforePause = 0;
       state.timeResumed = 0;
