5428c577e9856e9d9c20928edfab96edcf5737d5
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-12 15:54:56 +0800

    Refactor GradingFlex
    * Add React import
    * Remove unnecessary type annotations
    * Extract default styles outside component
    * Remove unused props

--- src/commons/grading/GradingFlex.tsx
+++ src/commons/grading/GradingFlex.tsx
@@ -1,31 +1,30 @@
 import { Property } from 'csstype';
+import React from 'react';
 
-type GradingFlexProps = {
+const defaultStyles: React.CSSProperties = {
+  display: 'flex'
+};
+
+type Props = {
   justifyContent?: Property.JustifyContent;
   alignItems?: Property.AlignItems;
   flexDirection?: Property.FlexDirection;
   children?: React.ReactNode;
   style?: React.CSSProperties;
   className?: string;
-} & React.RefAttributes<HTMLDivElement>;
+};
 
-const GradingFlex: React.FC<GradingFlexProps> = ({
+const GradingFlex: React.FC<Props> = ({
   justifyContent,
   alignItems,
   flexDirection,
   children,
   style,
   className
-}: GradingFlexProps) => {
-  const defaultStyle: React.CSSProperties = {
-    display: 'flex',
-    justifyContent: justifyContent,
-    alignItems: alignItems,
-    flexDirection: flexDirection
-  };
-
+}) => {
+  const styles: React.CSSProperties = { ...style, justifyContent, alignItems, flexDirection };
   return (
-    <div className={className} style={{ ...defaultStyle, ...style }}>
+    <div className={className} style={{ ...defaultStyles, ...styles }}>
       {children}
     </div>
   );
