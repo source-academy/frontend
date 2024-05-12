36238ea41969539f7d2ada23f4ef95e6b5ba29de
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-12 20:13:45 +0800

    Add SVGR plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import * as sass from 'sass';
 import { defineConfig, loadEnv } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const ignoreSvgIcon = () => new sass.SassString('');
 
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), envPrefix);
   return {
     envPrefix,
-    plugins: [react()],
+    plugins: [react(), svgr()],
     resolve: {
       alias: {
         fs: 'browserfs',
