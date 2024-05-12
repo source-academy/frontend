a1be899fded18b9a6e10d77ddf22afd78f49dab9
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-12 20:08:14 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import react from '@vitejs/plugin-react';
+import * as sass from 'sass';
+import { defineConfig, loadEnv } from 'vite';
+
+const ignoreSvgIcon = () => new sass.SassString('');
+
+const envPrefix = 'REACT_APP_';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Compatibility with Create React App environment variables
+  const env = loadEnv(mode, process.cwd(), envPrefix);
+  return {
+    envPrefix,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        fs: 'browserfs',
+        path: 'path-browserify',
+        src: '/src'
+      }
+    },
+    define: {
+      'process.env': env
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          functions: {
+            // FIXME: Support Blueprint's custom function
+            // https://github.com/palantir/blueprint/issues/5334
+            // https://github.com/palantir/blueprint/issues/6051
+            'svg-icon($path, $selectors: null)': ignoreSvgIcon
+          }
+        }
+      }
+    }
+  };
+});
