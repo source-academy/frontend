30d06e63d7110ccd570668fe1a2a8dc2aeaa5190
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-05-12 20:16:26 +0800

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import * as sass from 'sass';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const ignoreSvgIcon = () => new sass.SassString('');
 
@@ -13,7 +14,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), envPrefix);
   return {
     envPrefix,
-    plugins: [react(), svgr()],
+    // https://cathalmacdonnacha.com/migrating-from-create-react-app-cra-to-vite
+    plugins: [react(), tsconfigPaths(), svgr({ include: '**/*.svg?react' })],
     resolve: {
       alias: {
         fs: 'browserfs',
