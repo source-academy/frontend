862f83efc3903292aae0366ed9eaefe7654e58af
JustATin555 <usingenglish.2014@gmail.com>
2025-02-17 14:07:00 +0800

    Added REACT_APP_USE_BACKEND_ASSET_PREFIX to .env to allow for base url usage during development

--- src/features/game/commons/CommonConstants.ts
+++ src/features/game/commons/CommonConstants.ts
@@ -4,6 +4,7 @@ import FontAssets from '../assets/FontAssets';
 
 export const Constants = {
   assetsFolder: Links.sourceAcademyAssets,
+  useBackendAssetPrefix: process.env.REACT_APP_USE_BACKEND_ASSET_PREFIX?.toUpperCase() === "TRUE",
   fadeDuration: 600,
   nullFunction: () => {},
   nullInteractionId: '',
