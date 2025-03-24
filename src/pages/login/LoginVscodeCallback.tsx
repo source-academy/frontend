7b8092d7c38e62a789afae0d4cce67e737051fe5
heyzec <61238538+heyzec@users.noreply.github.com>
2025-03-25 00:36:24 +0800

    copy spinner

--- src/pages/login/LoginVscodeCallback.tsx
+++ src/pages/login/LoginVscodeCallback.tsx
@@ -1,13 +1,18 @@
+import { Card, Classes, Elevation, NonIdealState, Spinner, SpinnerSize } from '@blueprintjs/core';
+import classNames from 'classnames';
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 import SessionActions from 'src/commons/application/actions/SessionActions';
 import { parseQuery } from 'src/commons/utils/QueryHelper';
+import classes from 'src/styles/Login.module.scss';
 
 const LoginVscodeCallback: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
+  const { t } = useTranslation('login');
 
   const { access_token: accessToken, refresh_token: refreshToken } = parseQuery(location.search);
 
@@ -24,7 +29,18 @@ const LoginVscodeCallback: React.FC = () => {
     }
   }, []);
 
-  return <> </>;
+  return (
+    <div className={classNames(classes['Login'], Classes.DARK)}>
+      <Card elevation={Elevation.FOUR}>
+        <div>
+          <NonIdealState
+            description={t('Logging In')}
+            icon={<Spinner size={SpinnerSize.LARGE} />}
+          />
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 // react-router lazy loading
