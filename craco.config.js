c186debf9399ae5d45cc8ef5156aa9d8985e9070
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-04-25 23:37:37 +0800

    Migrate jest config

--- craco.config.js
+++ craco.config.js
@@ -1,86 +1,16 @@
 const cracoConfig = {
   jest: {
-    configure: jestConfig => {
-      jestConfig.transformIgnorePatterns = [
-        // Will give something like
-        // '[/\\\\]node_modules[/\\\\]
-        //  (?!
-        //  (   @ion-phaser[/\\\\]react[/\\\\.*]    )|
-        //  (   js-slang[/\\\\.*]                   )|
-        //  (   array-move[/\\\\.*]                 )|
-        //      ...
-        //  (   comma-separated-tokens[/\\\\.*]   )
-        //  ).*.(js|jsx|ts|tsx)$'
-        ignoreModulePaths(
-          '@ion-phaser/react',
-          'js-slang',
-          'array-move',
-          'konva',
-          'react-konva',
-          'react-debounce-render',
-          'devlop',
-          'hastscript',
-          'hast-to-hyperscript',
-          'hast-util-.+',
-          'mdast-util-.+',
-          'micromark',
-          'micromark-.+',
-          'vfile',
-          'vfile-message',
-          'unist-util-.+',
-          'web-namespaces',
-          'rehype-react',
-          'unified',
-          'bail',
-          'is-plain-obj',
-          'trough',
-          'decode-named-character-reference',
-          'character-entities',
-          'trim-lines',
-          'property-information',
-          'space-separated-tokens',
-          'comma-separated-tokens',
-          'query-string',
-          'decode-uri-component',
-          'split-on-first',
-          'filter-obj',
-          '@sourceacademy/c-slang',
-          'java-parser',
-          'conductor'
-        ),
-        '^.+\\.module\\.(css|sass|scss)$'
-      ];
-      jestConfig.moduleNameMapper['unist-util-visit-parents/do-not-use-color'] =
-        '<rootDir>/node_modules/unist-util-visit-parents/lib';
-      jestConfig.moduleNameMapper['vfile/do-not-use-conditional-minpath'] =
-        '<rootDir>/node_modules/vfile/lib';
-      jestConfig.moduleNameMapper['vfile/do-not-use-conditional-minproc'] =
-        '<rootDir>/node_modules/vfile/lib';
-      jestConfig.moduleNameMapper['vfile/do-not-use-conditional-minurl'] =
-        '<rootDir>/node_modules/vfile/lib';
-
-      jestConfig.setupFiles = [
-        ...jestConfig.setupFiles,
-        './src/i18n/i18n.ts' // Setup i18next configuration
-      ];
-      return jestConfig;
-    }
+    // configure: jestConfig => {
+    //   jestConfig.setupFiles = [
+    //     ...jestConfig.setupFiles,
+    //     './src/i18n/i18n.ts' // Setup i18next configuration
+    //   ];
+    //   return jestConfig;
+    // }
   },
   babel: {
     presets: [['@babel/preset-typescript']]
   }
-};
-
-const ignoreModulePaths = (...paths) => {
-  const moduleRoot = replaceSlashes('/node_modules/');
-  const modulePaths = paths
-    .map(replaceSlashes)
-    .map(path => `(${path}[/\\\\.*])`)
-    .join('|');
-  return moduleRoot + '(?!' + modulePaths + ').*.(js|jsx|ts|tsx)$';
-};
-const replaceSlashes = target => {
-  return target.replaceAll('/', '[/\\\\]');
 };
 
 module.exports = cracoConfig;
