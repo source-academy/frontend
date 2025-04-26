c3d009855e3a2b7964c3581b7f8c282ffa7490ed
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-04-26 22:26:14 +0800

    Fix and check tsc

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,11 @@ declare module '*.svg?react' {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+declare module '*.module.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
