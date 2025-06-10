a9c30417c31fe167897b672b2c106429743c8ad4
Lee Yi <leeyi45@gmail.com>
2025-06-10 01:42:41 -0400

    Remove Concurrent and Interpreter (#3130)
    * Remove concurrent variant
    
    * Fix broken tests
    
    * Begin fixing
    
    * Fix all broken workspace reducer tests
    
    * FIx more broken workspace saga tests
    
    * Refactor eval code to utilize more type safety
    
    * Minor refactoring
    
    * Fix broken tsc
    
    * Upgrade saga handler to allow using takeLatest or takeLeading
    
    * Modify sagas to use new combineSagaHandlers function
    
    * Add redux tests
    
    * Add more tests
    
    * Modify actions and sagas to use new redux utils
    
    * Fix Linting
    
    ---------
    
    Co-authored-by: Martin Henz <henz@comp.nus.edu.sg>

--- src/commons/utils/TypeHelper.ts
+++ src/commons/utils/TypeHelper.ts
@@ -1,3 +1,5 @@
+import type { actions, SourceActionType } from './ActionsHelper';
+
 export type MaybePromise<T, U = T> = T extends Promise<infer V> ? V : U;
 
 export type PromiseResolveType<T> = MaybePromise<T, never>;
@@ -158,10 +160,28 @@ export function objectValues<T>(obj: Record<any, T>) {
 }
 
 /**
+ * Utility type for getting the key-value tuple types
+ * of a record
+ */
+type DeconstructRecord<T extends Record<any, any>> = Exclude<
+  {
+    [K in keyof T]: [K, T[K]];
+  }[keyof T],
+  undefined
+>[];
+
+/**
  * Type safe `Object.entries`
  */
-export function objectEntries<K extends string | number | symbol, V>(
-  obj: Partial<Record<K, V>>
-): [K, V][] {
-  return Object.entries(obj) as [K, V][];
+export function objectEntries<T extends Record<any, any>>(obj: T) {
+  return Object.entries(obj) as DeconstructRecord<T>;
 }
+
+/**
+ * Utility for extracting the ActionCreator type from all the action
+ * creators using the specific type string
+ */
+export type ActionTypeToCreator<T extends SourceActionType['type']> = Extract<
+  (typeof actions)[keyof typeof actions],
+  (...args: any[]) => { type: T }
+>;
