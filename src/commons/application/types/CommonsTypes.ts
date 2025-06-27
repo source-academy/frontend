644893860e373a6aee9a1b9909e1fbeeda4e8cd2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-06-27 23:58:03 +0800

    Fix format

--- src/commons/application/types/CommonsTypes.ts
+++ src/commons/application/types/CommonsTypes.ts
@@ -1,4 +1,4 @@
-import type { createBrowserRouter } from "react-router";
+import type { createBrowserRouter } from 'react-router';
 
 export type Router = ReturnType<typeof createBrowserRouter>;
 export type RouterState = Router | null;
