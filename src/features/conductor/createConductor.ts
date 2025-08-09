6f9a4f832f2822f366aa6edbec002c25955ceb08
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-09 20:56:04 +0800

    Revert more conductor changes

--- src/features/conductor/createConductor.ts
+++ src/features/conductor/createConductor.ts
@@ -1,22 +1,6 @@
-// NOTE: Conductor dependency removed, providing stub implementation
-// import { Conduit, IConduit } from 'conductor/dist/conduit';
+import { Conduit, IConduit } from 'conductor/dist/conduit';
 
 import { BrowserHostPlugin } from './BrowserHostPlugin';
-
-// Stub types
-interface IConduit {
-  registerPlugin: (plugin: any, ...args: any[]) => BrowserHostPlugin;
-  terminate: () => void;
-}
-class Conduit implements IConduit {
-  constructor(worker: Worker, flag: boolean) {}
-  registerPlugin(plugin: any, ...args: any[]): BrowserHostPlugin {
-    return new plugin(this, [], ...args);
-  }
-  terminate() {
-    // Stub implementation
-  }
-}
 
 export function createConductor(
   evaluatorPath: string,
