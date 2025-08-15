d781cc81c5710c2f3513c39d7c078c704a197469
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-08-15 13:55:26 +0000

    Remove unused export

--- src/features/languageDirectory/LanguageDirectoryTypes.ts
+++ src/features/languageDirectory/LanguageDirectoryTypes.ts
@@ -2,8 +2,3 @@ export type LanguageDirectoryState = {
   readonly selectedLanguageId: string | null;
   readonly selectedEvaluatorId: string | null;
 };
-
-export const defaultLanguageDirectoryState: LanguageDirectoryState = {
-  selectedLanguageId: null,
-  selectedEvaluatorId: null
-};
