86c2004017b8ca94c2f98cc666a31d8a5092d299
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-20 00:25:10 +0800

    Update tests and snapshots

--- src/pages/sicp/subcomponents/__tests__/SicpIndexPage.test.tsx
+++ src/pages/sicp/subcomponents/__tests__/SicpIndexPage.test.tsx
@@ -2,11 +2,14 @@ import { MemoryRouter } from 'react-router';
 import { renderTreeJson } from 'src/commons/utils/TestUtils';
 
 import SicpIndexPage from '../../subcomponents/SicpIndexPage';
+import { SicpLanguageContextProvider } from '../SicpLanguageProvider';
 
 test('Sicp index page', async () => {
   const tree = await renderTreeJson(
     <MemoryRouter>
-      <SicpIndexPage />
+      <SicpLanguageContextProvider>
+        <SicpIndexPage />
+      </SicpLanguageContextProvider>
     </MemoryRouter>
   );
   expect(tree).toMatchSnapshot();
