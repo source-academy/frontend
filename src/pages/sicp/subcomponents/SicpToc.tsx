52b4d5dd996938f613cb3864de32fca3f01f4e00
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-20 00:21:30 +0800

    Revert SICP ToC changes and rewrite logic

--- src/pages/sicp/subcomponents/SicpToc.tsx
+++ src/pages/sicp/subcomponents/SicpToc.tsx
@@ -1,15 +1,11 @@
 import { Tree, TreeNodeInfo } from '@blueprintjs/core';
-import { NonIdealState, Spinner } from '@blueprintjs/core';
 import { cloneDeep } from 'lodash';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import Constants from 'src/commons/utils/Constants';
-import { readSicpLangLocalStorage } from 'src/features/sicp/utils/SicpUtils';
 
 import fallbackToc from '../../../features/sicp/data/toc.json';
-
-const baseUrl = Constants.sicpBackendUrl + 'json/';
-const loadingComponent = <NonIdealState title="Loading Content" icon={<Spinner />} />;
+import { useSicpLanguageContext } from './SicpLanguageProvider';
 
 type TocProps = OwnProps;
 
@@ -20,10 +16,18 @@ type OwnProps = {
 /**
  * Table of contents of SICP.
  */
-
-const Toc: React.FC<{ toc: TreeNodeInfo[]; props: TocProps }> = ({ toc, props }) => {
-  const [sidebarContent, setSidebarContent] = useState(toc);
+const SicpToc: React.FC<TocProps> = props => {
+  const [sidebarContent, setSidebarContent] = useState(fallbackToc as TreeNodeInfo[]);
   const navigate = useNavigate();
+  const { sicpLanguage } = useSicpLanguageContext();
+
+  useEffect(() => {
+    const loadLocalizedToc = async () => {
+      const resp = await fetch(`${Constants.sicpBackendUrl}json/${sicpLanguage}/toc.json`);
+      return (await resp.json()) as TreeNodeInfo[];
+    };
+    loadLocalizedToc().then(setSidebarContent).catch(console.error);
+  }, [sicpLanguage]);
 
   const handleNodeExpand = (_node: TreeNodeInfo, path: integer[]) => {
     const newState = cloneDeep(sidebarContent);
@@ -48,60 +52,14 @@ const Toc: React.FC<{ toc: TreeNodeInfo[]; props: TocProps }> = ({ toc, props })
   );
 
   return (
-    <Tree
-      className="sicp-toc-tree"
-      contents={sidebarContent}
-      onNodeClick={handleNodeClicked}
-      onNodeCollapse={handleNodeCollapse}
-      onNodeExpand={handleNodeExpand}
-    />
-  );
-};
-
-const SicpToc: React.FC<TocProps> = props => {
-  const [lang, setLang] = useState(readSicpLangLocalStorage());
-  const [toc, setToc] = useState([] as TreeNodeInfo[]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  React.useEffect(() => {
-    const handleLangChange = () => {
-      setLang(readSicpLangLocalStorage());
-    };
-    window.addEventListener('sicp-tb-lang-change', handleLangChange);
-    return () => window.removeEventListener('sicp-tb-lang-change', handleLangChange);
-  }, []);
-
-  React.useEffect(() => {
-    setLoading(true);
-    fetch(baseUrl + lang + '/toc.json')
-      .then(response => {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then(json => {
-        setToc(json as TreeNodeInfo[]);
-      })
-      .catch(error => {
-        console.log(error);
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [lang]);
-
-  return (
     <div className="sicp-toc">
-      {loading ? (
-        <div className="sicp-content">{loadingComponent}</div>
-      ) : error ? (
-        <Toc toc={fallbackToc as TreeNodeInfo[]} props={props} />
-      ) : (
-        <Toc toc={toc} props={props} />
-      )}
+      <Tree
+        className="sicp-toc-tree"
+        contents={sidebarContent}
+        onNodeClick={handleNodeClicked}
+        onNodeCollapse={handleNodeCollapse}
+        onNodeExpand={handleNodeExpand}
+      />
     </div>
   );
 };
