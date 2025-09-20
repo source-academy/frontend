8bbe9af84e7c593df950193902938b32ceba07ce
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-09-20 23:33:12 +0800

    fix formatting

--- src/commons/featureFlags/featureSelector.ts
+++ src/commons/featureFlags/featureSelector.ts
@@ -2,5 +2,6 @@ import { OverallState } from '../application/ApplicationTypes';
 import { FeatureFlag } from './FeatureFlag';
 
 export function featureSelector<T>(featureFlag: FeatureFlag<T>) {
-  return (state: OverallState) => (state.featureFlags.modifiedFlags[featureFlag.flagName] || featureFlag.defaultValue) as T;
+  return (state: OverallState) =>
+    (state.featureFlags.modifiedFlags[featureFlag.flagName] || featureFlag.defaultValue) as T;
 }
