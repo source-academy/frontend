e3b39821dcc0c6ebaca0df94dd9df0f5ed64ba8a
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-09-20 23:03:27 +0800

    add missing plugin directory fields

--- src/commons/application/reducers/RootReducer.ts
+++ src/commons/application/reducers/RootReducer.ts
@@ -4,6 +4,7 @@ import { FeatureFlagsReducer as featureFlags } from '../../..//commons/featureFl
 import { AchievementReducer as achievement } from '../../../features/achievement/AchievementReducer';
 import { DashboardReducer as dashboard } from '../../../features/dashboard/DashboardReducer';
 import { LanguageDirectoryReducer as languageDirectory } from '../../../features/directory/LanguageDirectoryReducer';
+import { PluginDirectoryReducer as pluginDirectory } from '../../../features/directory/PluginDirectoryReducer';
 import { LeaderboardReducer as leaderboard } from '../../../features/leaderboard/LeaderboardReducer';
 import { PlaygroundReducer as playground } from '../../../features/playground/PlaygroundReducer';
 import { StoriesReducer as stories } from '../../../features/stories/StoriesReducer';
@@ -29,7 +30,8 @@ const rootReducer: Reducer<OverallState, SourceActionType> = combineReducers({
   fileSystem,
   sideContent,
   vscode,
-  languageDirectory
+  languageDirectory,
+  pluginDirectory
 });
 
 export default rootReducer;
