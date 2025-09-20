678c1dad66cc2670a4658e7af88e5ccb11cbd161
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-09-20 22:51:28 +0800

    add directory.plugin.url to public flags

--- src/commons/featureFlags/publicFlags.ts
+++ src/commons/featureFlags/publicFlags.ts
@@ -2,11 +2,13 @@ import { flagConductorEnable } from '../../features/conductor/flagConductorEnabl
 import { flagConductorEvaluatorUrl } from '../../features/conductor/flagConductorEvaluatorUrl';
 import { flagDirectoryLanguageEnable } from '../../features/directory/flagDirectoryLanguageEnable';
 import { flagDirectoryLanguageUrl } from '../../features/directory/flagDirectoryLanguageUrl';
+import { flagDirectoryPluginUrl } from '../../features/directory/flagDirectoryPluginUrl';
 import { FeatureFlag } from './FeatureFlag';
 
 export const publicFlags: FeatureFlag<any>[] = [
   flagConductorEnable,
   flagConductorEvaluatorUrl,
   flagDirectoryLanguageEnable,
-  flagDirectoryLanguageUrl
+  flagDirectoryLanguageUrl,
+  flagDirectoryPluginUrl
 ];
