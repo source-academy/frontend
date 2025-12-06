b720e4f5f47b77c288d84d27a0850e90156c9c95
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-12-06 18:37:34 +0800

    Migrate changes post-merge

---
+++ src/styles/GradingCommentSelector.module.scss.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// WARNING: THIS FILE IS AUTO GENERATED, PLEASE DO NOT EDIT IT MANUALLY.
+// prettier-ignore
+export type Styles = {
+  "grading-comment-selector": string;
+  "grading-comment-selector-item": string;
+  "grading-comment-selector-title": string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
