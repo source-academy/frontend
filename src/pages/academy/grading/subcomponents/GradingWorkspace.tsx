d04b4ecefc9a716111bea80f106ccc84013c3c17
tzj04 <190485478+tzj04@users.noreply.github.com>
2026-03-30 21:41:15 +0800

    fix(grading): gate is_llm using grading.enable_llm_grading instead of session-level flag

--- src/pages/academy/grading/subcomponents/GradingWorkspace.tsx
+++ src/pages/academy/grading/subcomponents/GradingWorkspace.tsx
@@ -76,7 +76,6 @@ const GradingWorkspace: React.FC<Props> = props => {
 
   const grading = useTypedSelector(state => state.session.gradings[props.submissionId]);
   const courseId = useTypedSelector(state => state.session.courseId);
-  const llm_grading = useTypedSelector(state => state.session.enableLlmGrading);
   const {
     autogradingResults,
     isFolderModeEnabled,
@@ -327,7 +326,10 @@ const GradingWorkspace: React.FC<Props> = props => {
                 ? [grading!.answers[questionId].student.username]
                 : grading!.answers[questionId].team!.map(member => member.username)
             }
-            is_llm={!!llm_grading && grading!.answers[questionId].question.type == 'programming'}
+            is_llm={
+              !!grading!.enable_llm_grading &&
+              grading!.answers[questionId].question.type == 'programming'
+            }
             comments={grading!.answers[questionId].grade.comments ?? ''}
             graderName={
               grading!.answers[questionId].grade.grader
