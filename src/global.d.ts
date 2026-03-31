382fb50129aff5166f61191aebb9a8a83ea924c7
tzj04 <190485478+tzj04@users.noreply.github.com>
2026-03-31 14:13:28 +0800

    fix(groundControl): resolve React 19 runtime compatibility issues

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="@rsbuild/core/types" />
 
 declare module '@blueprintjs/core' {
-	export * from '@blueprintjs/core/lib/esnext/index';
+  export * from '@blueprintjs/core/lib/esnext/index';
 }
