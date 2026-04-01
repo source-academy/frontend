46323f508be77a6b079d7cedb276914b5c6740c4
Wang Jiahua <jhwang0324@gmail.com>
2026-04-01 17:46:33 +0800

    ran linter

--- src/features/cseMachine/__tests__/CseMachineAnimation.test.tsx
+++ src/features/cseMachine/__tests__/CseMachineAnimation.test.tsx
@@ -221,8 +221,8 @@ test('AnimationComponent animates correctly with conflicting animateTo calls', a
           elapsed < d * 0.5
             ? [easing(elapsed, 0, 200, d), 4]
             : // Larger tolerance value at the start because of overshoot from 2nd animation,
-            // will gradually go back to value of 100 towards the end.
-            [100, easing(elapsed - d * 0.5, 15, 1, d)]
+              // will gradually go back to value of 100 towards the end.
+              [100, easing(elapsed - d * 0.5, 15, 1, d)]
       };
     }
   });
