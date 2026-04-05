54384a714b89df17c2b0c00c445b03939fef49f9
Yuz <chenyuzhen2007@outlook.com>
2026-04-05 13:02:19 +0800

    Update S3 url link in ExploreModeConstants

--- src/features/game/mode/explore/GameModeExploreConstants.ts
+++ src/features/game/mode/explore/GameModeExploreConstants.ts
@@ -4,8 +4,8 @@ const ExploreModeConstants = {
   normal: `url(${toS3Path('/ui/magnifying.png', false)}), pointer`,
   hover: `url(${toS3Path('/ui/magnifying_trigg.png', false)}), pointer`,
   checked: `url(${toS3Path('/ui/magnifying_check.png', false)}), pointer`,
-  move: `url(/assets/footprint.png) 32 32, pointer`,
-  chat: `url(/assets/chat.png) 32 32, pointer`
+  move: `url(${toS3Path('/ui/footprint.png', false)}) 32 32, pointer`,
+  chat: `url(${toS3Path('/ui/chat.png', false)}) 32 32, pointer`
 };
 
 export default ExploreModeConstants;
