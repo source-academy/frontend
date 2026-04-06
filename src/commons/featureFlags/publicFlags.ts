e5f14312ee01eba6235f6e0c05f7825d70ff73db
yytelliot <yytelliot@gmail.com>
2026-04-06 19:47:11 +0800

    Revert publicFlags.ts

--- src/commons/featureFlags/publicFlags.ts
+++ src/commons/featureFlags/publicFlags.ts
@@ -1,12 +1,10 @@
 import { flagConductorEnable } from '../../features/conductor/flagConductorEnable';
 import { flagDirectoryLanguageUrl } from '../../features/directory/flagDirectoryLanguageUrl';
 import { flagDirectoryPluginUrl } from '../../features/directory/flagDirectoryPluginUrl';
-import { flagConductorEvaluatorUrl } from 'src/features/conductor/flagConductorEvaluatorUrl';
 import { FeatureFlag } from './FeatureFlag';
 
 export const publicFlags: FeatureFlag<any>[] = [
   flagConductorEnable,
   flagDirectoryLanguageUrl,
   flagDirectoryPluginUrl,
-  flagConductorEvaluatorUrl
 ];
