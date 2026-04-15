fb1dc1c506c08257e6917292f1685d8dc53c649e
elsxyss <evanliangsx@gmail.com>
2026-04-15 14:48:50 +0800

    Minor Bug: Retaining previous Call step when toggling between view modes + Tightening tree verification

--- src/features/dataVisualizer/dataVisualizer.tsx
+++ src/features/dataVisualizer/dataVisualizer.tsx
@@ -37,7 +37,7 @@ export default class DataVisualizer {
 
   private constructor() {}
 
-  public static isBinaryTree(structures: Data[]): boolean {
+  public static isBinaryTree(structures: Data[], data: any): boolean {
     if (structures == null) {
       return true;
     }
@@ -50,7 +50,11 @@ export default class DataVisualizer {
     if (structures.length != 2 || !(structures[1] instanceof Array)) {
       return false;
     }
+    if (data != null && !(structures[0] instanceof Array) && (typeof structures[0] != typeof data)) {
+      return false;
+    }
     let next = structures[1];
+    data=structures[0];
 
     let ans = false;
     let count = 0;
@@ -68,23 +72,37 @@ export default class DataVisualizer {
     const left = structures[1];
     const right = structures[1][1];
     if (left instanceof Array && right instanceof Array) {
-      return ans && this.isBinaryTree(left[0]) && this.isBinaryTree(right[0]);
+      return ans && this.isBinaryTree(left[0], data) && this.isBinaryTree(right[0], data);
     } else {
       return false;
     }
   }
 
-  public static isGeneralTree(structures: Data[]): boolean {
-    if (structures == null || (structures.length == 2 && structures[1] == null)) {
+  public static isGeneralTree(structures: Data[], data: any): boolean {
+    console.log(structures);
+    console.log(data);
+    if (structures == null) {
+      return true;
+    }
+    if (data != null && !(structures[0] instanceof Array) && typeof structures[0] != typeof data &&  structures[0] != null) {
+      return false;
+    }
+    if (structures.length == 2 && structures[1] == null){
+      if (structures[0] instanceof Array){
+        return this.isGeneralTree(structures[0], data);
+      }
       return true;
     }
     if (!(structures[0] instanceof Array) && structures[1] != null) {
-      return this.isGeneralTree(structures[1]);
+      if (!(structures[0] instanceof Array)){
+        data = structures[0];
+      }
+      return this.isGeneralTree(structures[1], data);
     }
     if (structures.length != 2 || (!(structures[1] instanceof Array) && structures[1] != null)) {
       return false;
     }
-    return this.isGeneralTree(structures[1]) && this.isGeneralTree(structures[0]);
+    return this.isGeneralTree(structures[1], data) && this.isGeneralTree(structures[0], data);
   }
 
   public static initializeTreeMetaData(
@@ -193,8 +211,8 @@ export default class DataVisualizer {
       DataVisualizer.isBinTree = false;
       DataVisualizer.isGenTree = false;
     } else {
-      DataVisualizer.isBinTree = this.isBinaryTree(root);
-      DataVisualizer.isGenTree = this.isGeneralTree(root);
+      DataVisualizer.isBinTree = this.isBinaryTree(root, null);
+      DataVisualizer.isGenTree = this.isGeneralTree(root, null);
       if (DataVisualizer.isBinTree || DataVisualizer.isGenTree) {
         this.initializeTreeMetaData(root, 0, 0, false);
       }
