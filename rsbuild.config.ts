5880dde9cb9e16dcbe01251eb996922026cfc122
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-05-13 21:10:13 +0800

    Remove manual polyfills and other package improvements (#3850)
    * Remove manual polyfills
    
    * Dedupe deps
    
    * Increase PWA cache size
    
    * Clean up rsbuild config
    
    * Rename ESLint config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -54,16 +54,6 @@ export default defineConfig({
       // };
       // config.output.webassemblyModuleFilename = 'static/[hash].module.wasm';
 
-      // // workaround .mjs files by Acorn
-      // config.module.rules.push({
-      //   test: /\.mjs$/,
-      //   include: /node_modules/,
-      //   type: 'javascript/auto',
-      //   resolve: {
-      //     fullySpecified: false
-      //   }
-      // });
-
       config.ignoreWarnings = [
         (warning: any) => {
           // Ignore the warnings that occur because js-slang uses dynamic imports
@@ -76,41 +66,21 @@ export default defineConfig({
             warning.message
           );
         }
-
-        // {
-        //   // Ignore warnings for dependencies that do not ship with a source map.
-        //   // This is because we cannot do anything about our dependencies.
-        //   module: /node_modules/,
-        //   message: /Failed to parse source map/
-        // },
-        // [
       ];
-
-      // config.plugins = [
-      //   ...config.plugins,
-      //   // Make environment variables available in the browser by polyfilling the 'process' Node.js module.
-      //   new webpack.ProvidePlugin({
-      //     process: 'process/browser'
-      //   }),
-      //   // Make the 'buffer' Node.js module available in the browser.
-      //   new webpack.ProvidePlugin({
-      //     Buffer: ['buffer', 'Buffer']
-      //   })
-      // ];
 
       config.plugins = [
         ...config.plugins,
         new InjectManifest({
           swSrc: './src/service-worker.ts',
           swDest: 'service-worker.js',
-          maximumFileSizeToCacheInBytes: 20 * 1024 * 1024
+          maximumFileSizeToCacheInBytes: 30 * 1024 * 1024
         })
       ];
 
       // Workaround to suppress warnings caused by ts-morph in js-slang
-      // if (config.module) {
-      //   config.module.noParse = /node_modules\/@ts-morph\/common\/dist\/typescript\.js$/;
-      // }
+      if (config.module) {
+        config.module.noParse = /node_modules\/@ts-morph\/common\/dist\/typescript\.js$/;
+      }
 
       return config;
     }
