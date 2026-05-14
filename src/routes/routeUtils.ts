0ef69c32e8921cb05f9f8edc9781d6aacaae2948
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-05-14 22:59:03 +0800

    Support varargs

--- src/routes/routeUtils.ts
+++ src/routes/routeUtils.ts
@@ -1,12 +1,12 @@
 import type { MiddlewareFunction, RouteObject } from 'react-router';
 
 export function createRoutes(
-  routeMap: Record<string, RouteObject['lazy'] | [RouteObject['lazy'], MiddlewareFunction]>,
+  routeMap: Record<string, RouteObject['lazy'] | [RouteObject['lazy'], ...MiddlewareFunction[]]>,
 ): RouteObject[] {
   return Object.entries(routeMap).map(([path, lazyAndMiddleware]) => {
     if (Array.isArray(lazyAndMiddleware)) {
-      const [lazyFn, middleware] = lazyAndMiddleware;
-      return { path, lazy: lazyFn, middleware: [middleware] };
+      const [lazyFn, ...middleware] = lazyAndMiddleware;
+      return { path, lazy: lazyFn, middleware };
     }
     const lazyFn = lazyAndMiddleware;
     return { path, lazy: lazyFn };
