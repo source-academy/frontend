fa60349d456e02f71a84f846a2b45ca3ec9a3dab
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-06-01 20:19:35 +0800

    Remove unneeded FormSelect

--- src/components/ui/form/index.ts
+++ src/components/ui/form/index.ts
@@ -1,8 +1,7 @@
 export { default as FormContainer } from './FormContainer';
 export { default as FormField } from './FormField';
 export { default as FormFieldRow } from './FormFieldRow';
-export { default as FormSelect } from './FormSelect';
 export { default as FormFooter } from './FormFooter';
+export { default as InputContainer } from './InputContainer';
 export { default as RemoveButton } from './RemoveButton';
 export { default as StudentFormField } from './StudentFormField';
-export { default as InputContainer } from './InputContainer';
