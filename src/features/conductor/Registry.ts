cb5aabaa3883fc0a3a2bdeb1a386d47f49debc53
Aarav Malani <aarav.malani@gmail.com>
2026-06-19 14:58:02 +0530

    feat: add react konva and konva to require provider

--- src/features/conductor/Registry.ts
+++ src/features/conductor/Registry.ts
@@ -8,4 +8,4 @@ export type PluginRegistry = Map<string, PluginClass<[ITabService]>>;
 
 export const registry: PluginRegistry = new Map();
 registry.set('__autocomplete_plugin_web', AutoCompletePlugin);
-registry.set('__web_test', TestPlugin);
+registry.set('test', TestPlugin);
