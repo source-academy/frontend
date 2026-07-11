b4b00f02b36b9dab2be1bea6e4d63249175e9607
Isha <ishasovasaria@gmail.com>
2026-07-11 11:14:00 +0800

    Add Louis bot rendering for sicpy

--- src/new_routes/sicpy/_layout.tsx
+++ src/new_routes/sicpy/_layout.tsx
@@ -5,12 +5,15 @@ import classNames from 'classnames';
 import { useEffect, useRef, useState } from 'react';
 import { Outlet, useLocation, useNavigate, useParams } from 'react-router';
 import Constants from 'src/commons/utils/Constants';
+import { useSession } from 'src/commons/utils/Hooks';
 import { readLocalStorage, setLocalStorage } from 'src/commons/utils/LocalStorageHelper';
+import type { SicpSection } from 'src/features/sicp/chatCompletion/chatCompletion';
 import { CodeSnippetProvider } from 'src/features/sicp/CodeSnippetProvider';
 import { parseArr, ParseJsonError } from 'src/features/sicp/parser/ParseJson';
 
 import SicpErrorBoundary from '../../features/sicp/errors/SicpErrorBoundary';
 import getSicpError, { SicpErrorType } from '../../features/sicp/errors/SicpErrors';
+import Chatbot from '../../pages/sicp/subcomponents/chatbot/Chatbot';
 
 const baseUrl = Constants.sicpBackendUrl + 'json_py/';
 const extension = '.json';
@@ -32,6 +35,27 @@ const scrollRefIntoView = (
   parent.scrollTo({ behavior: 'smooth', top: ref.offsetTop - relativeTop });
 };
 
+const getText = () => {
+  const divs = document.querySelectorAll('p.sicp-text');
+  let visibleParagraphs = '';
+
+  divs.forEach(div => {
+    const rect = div.getBoundingClientRect();
+
+    if (
+      rect.top <= window.innerHeight &&
+      rect.bottom >= 0 &&
+      rect.left <= window.innerWidth &&
+      rect.right >= 0
+    ) {
+      const text = div.textContent;
+      visibleParagraphs += text + '\n';
+    }
+  });
+
+  return visibleParagraphs;
+};
+
 function SicpPyLayout() {
   const [data, setData] = useState(<></>);
   const [loading, setLoading] = useState(false);
@@ -40,6 +64,12 @@ function SicpPyLayout() {
   const refs = useRef<Record<string, HTMLElement | null>>({});
   const navigate = useNavigate();
   const location = useLocation();
+  const { isLoggedIn } = useSession();
+
+  function getSection() {
+    // To discard the '/sicpy/'
+    return location.pathname.replace('/sicpy/', '') as SicpSection;
+  }
 
   useEffect(() => {
     if (!section) {
@@ -114,6 +144,9 @@ function SicpPyLayout() {
           )}
         </CodeSnippetProvider>
       </SicpErrorBoundary>
+      {isLoggedIn && Constants.featureFlags.enableSicpChatbot && (
+        <Chatbot getSection={getSection} getText={getText} />
+      )}
     </div>
   );
 }
