c52f64138194f3fbb3b5a433a58a700f5d44d95a
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-07-14 14:47:46 +0800

    Update tests

--- src/features/sicp/TableOfContentsHelper.test.ts
+++ src/features/sicp/TableOfContentsHelper.test.ts
@@ -1,33 +1,31 @@
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
 import { getNext, getPrev } from './TableOfContentsHelper';
 
-vi.mock('./data/toc-navigation.json', () => ({
-  default: {
-    '1': { next: '2' },
-    '2': { next: '3', prev: '1' },
-    '3': { prev: '2' },
-  },
-}));
+const tocNavigation = {
+  '1': { next: '2' },
+  '2': { next: '3', prev: '1' },
+  '3': { prev: '2' },
+} as const;
 
 describe('Table of contents helper', () => {
   test('generate next correctly', () => {
-    expect(getNext('1')).toBe('2');
-    expect(getNext('2')).toBe('3');
-    expect(getNext('3')).toBeUndefined();
+    expect(getNext(tocNavigation, '1')).toBe('2');
+    expect(getNext(tocNavigation, '2')).toBe('3');
+    expect(getNext(tocNavigation, '3')).toBeUndefined();
   });
 
   test('generate prev correctly', () => {
-    expect(getPrev('1')).toBeUndefined();
-    expect(getPrev('2')).toBe('1');
-    expect(getPrev('3')).toBe('2');
+    expect(getPrev(tocNavigation, '1')).toBeUndefined();
+    expect(getPrev(tocNavigation, '2')).toBe('1');
+    expect(getPrev(tocNavigation, '3')).toBe('2');
   });
 
   test('handle invalid values correctly', () => {
-    expect(getNext('invalid')).toBeUndefined();
-    expect(getNext('')).toBeUndefined();
+    expect(getNext(tocNavigation, 'invalid')).toBeUndefined();
+    expect(getNext(tocNavigation, '')).toBeUndefined();
 
-    expect(getPrev('invalid')).toBeUndefined();
-    expect(getPrev('')).toBeUndefined();
+    expect(getPrev(tocNavigation, 'invalid')).toBeUndefined();
+    expect(getPrev(tocNavigation, '')).toBeUndefined();
   });
 });
