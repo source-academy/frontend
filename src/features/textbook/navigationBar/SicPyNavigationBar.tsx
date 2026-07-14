63ce7a731b7cb8f8cc7ada98a8917d461b5187ad
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-07-14 14:48:08 +0800

    Deduplicate textbook navbars

--- src/features/textbook/navigationBar/SicPyNavigationBar.tsx
+++ src/features/textbook/navigationBar/SicPyNavigationBar.tsx
@@ -1,93 +1,20 @@
-import type { TreeNodeInfo } from '@blueprintjs/core';
-import { Alignment, Drawer, Navbar, NavbarGroup, Position } from '@blueprintjs/core';
-import { IconNames } from '@blueprintjs/icons';
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
-import ControlButton from 'src/commons/ControlButton';
-import {
-  getNextPy as getNext,
-  getPrevPy as getPrev,
-} from 'src/features/sicp/TableOfContentsHelperPy';
-import SicpToc from 'src/pages/sicp/subcomponents/SicpToc';
+import tocNavigation from 'src/features/sicp/data/toc-navigation-py.json';
+import toc from 'src/features/sicp/data/toc-py.json';
+import { getNext, getPrev } from 'src/features/sicp/TableOfContentsHelper';
 
-import toc from '../../../features/sicp/data/toc-py.json';
-import { TableOfContentsButton } from '../../sicp/TableOfContentsButton';
 import { fetchSicpySearchData } from './autocomplete/query';
-import SearchAutocomplete from './autocomplete/SearchAutocomplete';
+import SicpTextbookNavigationBar from './SicpTextbookNavigationBar';
 
 function SicPyNavigationBar() {
-  const [isTocOpen, setIsTocOpen] = useState(false);
-  const { section } = useParams<{ section: string }>();
-  const navigate = useNavigate();
-  const prev = getPrev(section ?? '');
-  const next = getNext(section ?? '');
-
-  const handleOpenToc = () => setIsTocOpen(true);
-  const handleCloseToc = () => setIsTocOpen(false);
-  const handleClickToc = (node: TreeNodeInfo) => {
-    setIsTocOpen(false);
-    handleNavigation(String(node.nodeData));
-  };
-
-  const handleNavigation = (sect: string) => {
-    navigate('/sicpy/' + sect);
-  };
-
-  // Button to open table of contents
-  const tocButton = <TableOfContentsButton key="toc" handleOpenToc={handleOpenToc} />;
-
-  // Previous button only displayed when next page is valid.
-  const prevButton = prev && (
-    <div key="prev">
-      <ControlButton
-        label="Previous"
-        icon={IconNames.ARROW_LEFT}
-        onClick={() => handleNavigation(prev)}
-      />
-    </div>
-  );
-
-  // Next button only displayed when next page is valid.
-  const nextButton = next && (
-    <div key="next">
-      <ControlButton
-        label="Next"
-        icon={IconNames.ARROW_RIGHT}
-        onClick={() => handleNavigation(next)}
-        options={{ iconOnRight: true }}
-      />
-    </div>
-  );
-
-  const drawerProps = {
-    onClose: handleCloseToc,
-    autoFocus: true,
-    canEscapeKeyClose: true,
-    canOutsideClickClose: true,
-    enforceFocus: true,
-    hasBackdrop: true,
-    isOpen: isTocOpen,
-    position: Position.LEFT,
-    usePortal: false,
-  };
-
   return (
-    <>
-      <Navbar className="SicpNavigationBar secondary-navbar">
-        <NavbarGroup align={Alignment.START}>{tocButton}</NavbarGroup>
-        <NavbarGroup align={Alignment.END}>{[prevButton, nextButton]}</NavbarGroup>
-        <NavbarGroup align={Alignment.CENTER}>
-          <SearchAutocomplete
-            queryKey="sicpPySearchData"
-            fetchSearchData={fetchSicpySearchData}
-            onNavigate={handleNavigation}
-          />
-        </NavbarGroup>
-      </Navbar>
-      <Drawer {...drawerProps} className="sicp-toc-drawer">
-        <SicpToc handleClick={handleClickToc} toc={toc} />
-      </Drawer>
-    </>
+    <SicpTextbookNavigationBar
+      routePrefix="/sicpy"
+      getPrev={section => getPrev(tocNavigation, section)}
+      getNext={section => getNext(tocNavigation, section)}
+      queryKey="sicpPySearchData"
+      fetchSearchData={fetchSicpySearchData}
+      toc={toc}
+    />
   );
 }
 
--- src/features/textbook/navigationBar/SicpNavigationBar.tsx
+++ src/features/textbook/navigationBar/SicpNavigationBar.tsx
@@ -1,90 +1,20 @@
-import type { TreeNodeInfo } from '@blueprintjs/core';
-import { Alignment, Drawer, Navbar, NavbarGroup, Position } from '@blueprintjs/core';
-import { IconNames } from '@blueprintjs/icons';
-import { useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
-import ControlButton from 'src/commons/ControlButton';
+import toc from 'src/features/sicp/data/toc.json';
+import tocNavigation from 'src/features/sicp/data/toc-navigation.json';
 import { getNext, getPrev } from 'src/features/sicp/TableOfContentsHelper';
 
-import toc from '../../../features/sicp/data/toc.json';
-import SicpToc from '../../../pages/sicp/subcomponents/SicpToc';
-import { TableOfContentsButton } from '../../sicp/TableOfContentsButton';
 import { fetchSicpSearchData } from './autocomplete/query';
-import SearchAutocomplete from './autocomplete/SearchAutocomplete';
+import SicpTextbookNavigationBar from './SicpTextbookNavigationBar';
 
 function SicpNavigationBar() {
-  const [isTocOpen, setIsTocOpen] = useState(false);
-  const { section } = useParams<{ section: string }>();
-  const navigate = useNavigate();
-  const prev = getPrev(section ?? '');
-  const next = getNext(section ?? '');
-
-  const handleOpenToc = () => setIsTocOpen(true);
-  const handleCloseToc = () => setIsTocOpen(false);
-  const handleClickToc = (node: TreeNodeInfo) => {
-    setIsTocOpen(false);
-    handleNavigation(String(node.nodeData));
-  };
-
-  const handleNavigation = (sect: string) => {
-    navigate('/sicpjs/' + sect);
-  };
-
-  // Button to open table of contents
-  const tocButton = <TableOfContentsButton key="toc" handleOpenToc={handleOpenToc} />;
-
-  // Previous button only displayed when next page is valid.
-  const prevButton = prev && (
-    <div key="prev">
-      <ControlButton
-        label="Previous"
-        icon={IconNames.ARROW_LEFT}
-        onClick={() => handleNavigation(prev)}
-      />
-    </div>
-  );
-
-  // Next button only displayed when next page is valid.
-  const nextButton = next && (
-    <div key="next">
-      <ControlButton
-        label="Next"
-        icon={IconNames.ARROW_RIGHT}
-        onClick={() => handleNavigation(next)}
-        options={{ iconOnRight: true }}
-      />
-    </div>
-  );
-
-  const drawerProps = {
-    onClose: handleCloseToc,
-    autoFocus: true,
-    canEscapeKeyClose: true,
-    canOutsideClickClose: true,
-    enforceFocus: true,
-    hasBackdrop: true,
-    isOpen: isTocOpen,
-    position: Position.LEFT,
-    usePortal: false,
-  };
-
   return (
-    <>
-      <Navbar className="SicpNavigationBar secondary-navbar">
-        <NavbarGroup align={Alignment.START}>{tocButton}</NavbarGroup>
-        <NavbarGroup align={Alignment.END}>{[prevButton, nextButton]}</NavbarGroup>
-        <NavbarGroup align={Alignment.CENTER}>
-          <SearchAutocomplete
-            queryKey="sicpSearchData"
-            fetchSearchData={fetchSicpSearchData}
-            onNavigate={handleNavigation}
-          />
-        </NavbarGroup>
-      </Navbar>
-      <Drawer {...drawerProps} className="sicp-toc-drawer">
-        <SicpToc handleClick={handleClickToc} toc={toc} />
-      </Drawer>
-    </>
+    <SicpTextbookNavigationBar
+      routePrefix="/sicpjs"
+      getPrev={section => getPrev(tocNavigation, section)}
+      getNext={section => getNext(tocNavigation, section)}
+      queryKey="sicpSearchData"
+      fetchSearchData={fetchSicpSearchData}
+      toc={toc}
+    />
   );
 }
 
