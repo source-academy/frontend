6fed6e61fdb9ae1376533e433fe238e1516a3d11
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-07-14 15:42:05 +0800

    Add comment explaining sync

--- src/features/textbook/toc/SicpToc.tsx
+++ src/features/textbook/toc/SicpToc.tsx
@@ -13,6 +13,10 @@ type Props = {
 function SicpToc({ handleClick, toc }: Props) {
   const [sidebarContent, setSidebarContent] = useState(toc);
 
+  // Note: Technically this should have a useEffect to sync the state whenever
+  // toc props changes, but since our usage uses a static toc, we can skip that for now.
+  // https://github.com/source-academy/frontend/pull/4096#discussion_r3576815218
+
   const handleNodeExpand = (_node: TreeNodeInfo, path: number[]) => {
     const newState = cloneDeep(sidebarContent);
     Tree.nodeFromPath(path, newState).isExpanded = true;
